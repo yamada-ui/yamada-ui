a2ce93198a367e6f5b36fedce57b9a14617fbaae
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-08 19:34:56 +0900

    change type

--- utils/function.ts
+++ utils/function.ts
@@ -2,5 +2,7 @@ import { isFunction } from './'
 
 export const noop = () => {}
 
-export const runIfFunc = <T, U>(valOrFunc: T | ((...funcArgs: U[]) => T), ...args: U[]): T =>
-  isFunction(valOrFunc) ? valOrFunc(...args) : valOrFunc
+export const runIfFunc = <T, U extends Array<any>>(
+  valOrFunc: T | ((...funcArgs: U) => T),
+  ...args: U
+): T => (isFunction(valOrFunc) ? valOrFunc(...args) : valOrFunc)

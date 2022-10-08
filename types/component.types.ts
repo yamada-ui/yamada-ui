926275295fc68844b809892a4bdda3bbb8fc5e98
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-08 15:43:40 +0900

    update type

--- types/component.types.ts
+++ types/component.types.ts
@@ -1,11 +1,6 @@
 import { CSSObject } from '@emotion/styled'
-import {
-  ComponentProps as ReactComponentProps,
-  ElementType,
-  ValidationMap,
-  WeakValidationMap,
-} from 'react'
-import { DOMElements, CSSUIProps } from './'
+import * as React from 'react'
+import { DOMElements, CSSUIProps, StylesProps } from './'
 
 export type UIProps = CSSUIProps & {
   isTruncated?: boolean
@@ -20,34 +15,48 @@ export type OmitProps<Y, M extends keyof any = never> = Omit<
   'transition' | 'as' | 'color' | 'animation' | M
 >
 
-export type Props<Y extends object = {}, M extends object = {}> = OmitProps<Y, keyof M> & M
+export type IntersectionProps<Y extends object = {}, M extends object = {}> = OmitProps<
+  Y,
+  keyof M
+> &
+  M
+
+export type PropsOf<T extends As> = React.ComponentPropsWithoutRef<T> & {
+  as?: As
+}
 
 export type ComponentProps<
   Y extends object,
   M extends object,
   D extends object = {},
   H extends As = As,
-> = Props<Y, D> &
-  Props<M, D> & {
+> = IntersectionProps<Y, D> &
+  IntersectionProps<M, D> & {
     as?: H
   }
 
 export type Component<Y extends As, M extends object = {}> = {
   <D extends As = Y>(
-    props: ComponentProps<ReactComponentProps<Y>, ReactComponentProps<D>, M, D>,
+    props: ComponentProps<React.ComponentProps<Y>, React.ComponentProps<D>, M, D>,
   ): JSX.Element
 
   displayName?: string
-  propTypes?: WeakValidationMap<any>
-  contextTypes?: ValidationMap<any>
+  propTypes?: React.WeakValidationMap<any>
+  contextTypes?: React.ValidationMap<any>
   defaultProps?: Partial<any>
   id?: string
 }
 
-export type As<Y = any> = ElementType<Y>
+export type As<Y = any> = React.ElementType<Y>
 
 export type HTMLUIComponents = {
   [Y in DOMElements]: UIComponent<Y, {}>
 }
 
 export type UIComponent<T extends As, P = {}> = Component<T, UIProps & P>
+
+export type HTMLUIProps<T extends As> = Omit<
+  PropsOf<T>,
+  T extends 'svg' ? 'ref' | 'children' | keyof StylesProps : 'ref' | keyof StylesProps
+> &
+  UIProps & { as?: As }

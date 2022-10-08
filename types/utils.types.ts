10723912d36f95ed4997f77dbe2e4e4f493b85cf
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-08 19:32:15 +0900

    remove unneeded logic

--- types/utils.types.ts
+++ types/utils.types.ts
@@ -1,26 +1,14 @@
 type Primitive = null | undefined | string | number | boolean | symbol | bigint
 
-type ArrayKey = number
-
-type IsTuple<T extends ReadonlyArray<any>> = number extends T['length'] ? false : true
-
-type TupleKeys<T extends ReadonlyArray<any>> = Exclude<keyof T, keyof any[]>
-
 type PathImpl<K extends string | number | symbol, V> = K extends string | number
   ? V extends Primitive
     ? `${K}`
     : `${K}.${Path<V>}`
   : ``
 
-export type Path<T> = T extends ReadonlyArray<infer V>
-  ? IsTuple<T> extends true
-    ? {
-        [K in TupleKeys<T>]-?: PathImpl<K & string, T[K]>
-      }[TupleKeys<T>]
-    : PathImpl<ArrayKey, V>
-  : {
-      [K in keyof T]-?: PathImpl<K, T[K]>
-    }[keyof T]
+export type Path<T> = {
+  [K in keyof T]-?: PathImpl<K, T[K]>
+}[keyof T]
 
 export type Dict<T = any> = Record<string, T>
 

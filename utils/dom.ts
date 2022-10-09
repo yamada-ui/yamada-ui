02df78ecdb43d99d0405207306bb32a26874cc5b
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-09 18:10:39 +0900

    add getValidChildren

--- utils/dom.ts
+++ utils/dom.ts
@@ -1,4 +1,9 @@
+import { Children, isValidElement, ReactElement, ReactNode } from 'react'
+
 export const createdDom = (): boolean =>
   !!(typeof window !== 'undefined' && window.document && window.document.createElement)
+
+export const getValidChildren = (children: ReactNode): ReactElement[] =>
+  Children.toArray(children).filter((child) => isValidElement(child)) as ReactElement[]
 
 export const cx = (...classNames: (string | undefined)[]) => classNames.filter(Boolean).join(' ')

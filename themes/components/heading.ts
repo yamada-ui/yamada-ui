2572000697d02c3f027e1162e56f3bbf53733877
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-09 14:59:43 +0900

    add sizes

--- themes/components/heading.ts
+++ themes/components/heading.ts
@@ -6,10 +6,24 @@ export const Heading: ComponentStyle = {
     fontWeight: 'bold',
   },
   sizes: {
-    '4xl': {},
-    xl: {
-      fontSize: '4xl',
+    '4xl': {
+      fontSize: { base: '7xl', sm: '6xl' },
     },
+    '3xl': {
+      fontSize: { base: '6xl', sm: '5xl' },
+    },
+    '2xl': {
+      fontSize: { base: '5xl', sm: '4xl' },
+    },
+    xl: {
+      fontSize: { base: '4xl', sm: '3xl' },
+    },
+    lg: {
+      fontSize: { base: '3xl', sm: '2xl' },
+    },
+    md: { fontSize: 'xl' },
+    sm: { fontSize: 'md' },
+    xs: { fontSize: 'sm' },
   },
   defaultProps: { size: 'xl' },
 }

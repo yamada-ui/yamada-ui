5aaafb961bf1d8e7719c00be48893e66a970734d
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-09 18:10:20 +0900

    add style

---
+++ themes/components/divider.ts
@@ -0,0 +1,20 @@
+import { ComponentStyle } from '../../types'
+
+export const Divider: ComponentStyle = {
+  baseStyle: {
+    opacity: 0.6,
+    borderColor: 'inherit',
+  },
+  variants: {
+    solid: {
+      borderStyle: 'solid',
+    },
+    dashed: {
+      borderStyle: 'dashed',
+    },
+    dotted: {
+      borderStyle: 'dotted',
+    },
+  },
+  defaultProps: { variant: 'solid' },
+}

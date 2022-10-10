e195018e3c55f9595348f465c8f0579ed16bdb45
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-10 22:55:15 +0900

    change context to react

--- utils/dom.ts
+++ utils/dom.ts
@@ -1,9 +1,2 @@
-import { Children, isValidElement, ReactElement, ReactNode } from 'react'
-
 export const createdDom = (): boolean =>
   !!(typeof window !== 'undefined' && window.document && window.document.createElement)
-
-export const getValidChildren = (children: ReactNode): ReactElement[] =>
-  Children.toArray(children).filter((child) => isValidElement(child)) as ReactElement[]
-
-export const cx = (...classNames: (string | undefined)[]) => classNames.filter(Boolean).join(' ')

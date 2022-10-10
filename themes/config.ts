4f691666f694d9dbf6a11b303e59dd2d74fc8d39
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-11 03:49:58 +0900

    remove comment

--- themes/config.ts
+++ themes/config.ts
@@ -4,28 +4,6 @@ export type Config = {
   var?: {
     prefix?: string
   }
-  // screenLoading?: {
-  //   disabled?: boolean
-  //   bg?: BoxProps['bg']
-  //   loading?: {
-  //     variant?: LoadingProps['variant']
-  //     size?: LoadingProps['size']
-  //     color?: LoadingProps['color']
-  //     secondaryColor?: LoadingProps['secondaryColor']
-  //   }
-  // }
-  // pageLoading?: {
-  //   overlay?: BoxProps['bg']
-  //   color?: CenterProps['color']
-  //   fontSize?: CenterProps['fontSize']
-  //   loading?: {
-  //     variant?: LoadingProps['variant']
-  //     bg?: CenterProps['bg']
-  //     size?: LoadingProps['size']
-  //     color?: LoadingProps['color']
-  //     secondaryColor?: LoadingProps['secondaryColor']
-  //   }
-  // }
 }
 
 export const config: Config = {

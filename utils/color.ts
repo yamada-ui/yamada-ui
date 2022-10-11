687b5d21b84f5484824d7e926021fbb569b3eab0
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-12 05:51:51 +0900

    add toneColor

--- utils/color.ts
+++ utils/color.ts
@@ -15,3 +15,19 @@ export const transparentizeColor =
 
     return new TinyColor(raw).setAlpha(opacity).toRgbString()
   }
+
+export const toneColor = (color: string, l: number) => (theme: StyledTheme<Dict>) => {
+  const raw = getColor(theme, color)
+
+  if (l < 0 || 900 < l) return color
+
+  let n = (l - 500) / 10
+
+  const isLighten = n <= 0
+
+  if (isLighten) n *= -1
+
+  if (n !== 0) n = n - 5 * (isLighten ? 1 : -1)
+
+  return new TinyColor(raw)[isLighten ? 'lighten' : 'shade'](n).toString()
+}

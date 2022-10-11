113f859546861c236d89ee84af502b7a6f5e54ad
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-11 22:11:32 +0900

    add button

--- themes/components/index.ts
+++ themes/components/index.ts
@@ -1,5 +1,6 @@
+import { Button } from './button'
 import { Container } from './container'
 import { Divider } from './divider'
 import { Heading } from './heading'
 
-export default { Container, Divider, Heading }
+export default { Button, Container, Divider, Heading }

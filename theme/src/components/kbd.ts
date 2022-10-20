62592e3f3b6a7c5da4853d073843081fb633569d
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-21 01:55:25 +0900

    add kbd

---
+++ theme/src/components/kbd.ts
@@ -0,0 +1,16 @@
+import { ComponentStyle } from '@yamada-ui/styled'
+
+export const kbd: ComponentStyle = {
+  baseStyle: {
+    fontFamily: 'mono',
+    bg: ['gray.100', 'whiteAlpha'],
+    borderRadius: 'md',
+    borderWidth: '1px',
+    borderBottomWidth: '3px',
+    fontSize: '0.8em',
+    fontWeight: 'bold',
+    lineHeight: 'normal',
+    px: '0.4em',
+    whiteSpace: 'nowrap',
+  },
+}

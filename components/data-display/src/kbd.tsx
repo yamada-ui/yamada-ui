9db2ac35db9fab60bc2232a6bf6d3ab96659096f
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-21 01:55:25 +0900

    add kbd

---
+++ components/data-display/src/kbd.tsx
@@ -0,0 +1,18 @@
+import {
+  ui,
+  forwardRef,
+  HTMLUIProps,
+  ThemeProps,
+  useComponentStyle,
+  omitThemeProps,
+} from '@yamada-ui/system'
+import { cx } from '@yamada-ui/utils'
+
+export type KbdProps = HTMLUIProps<'kbd'> & ThemeProps<'kbd'>
+
+export const Kbd = forwardRef<KbdProps, 'kbd'>((props, ref) => {
+  const css = useComponentStyle('kbd', props)
+  const { className, ...rest } = omitThemeProps(props)
+
+  return <ui.kbd ref={ref} className={cx('ui-kbd', className)} __css={css} {...rest} />
+})
--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -2,5 +2,6 @@ import { Button } from './button'
 import { Container } from './container'
 import { Divider } from './divider'
 import { Heading } from './heading'
+import { kbd } from './kbd'
 
-export default { Button, Container, Divider, Heading }
+export default { Button, Container, Divider, Heading, kbd }

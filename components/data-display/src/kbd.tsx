7c16c37214fd57fd2e140340f097031e7b7d0d73
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-21 11:58:13 +0900

    kbd to Kbd

--- components/data-display/src/kbd.tsx
+++ components/data-display/src/kbd.tsx
@@ -11,7 +11,7 @@ import { cx } from '@yamada-ui/utils'
 export type KbdProps = HTMLUIProps<'kbd'> & ThemeProps<'kbd'>
 
 export const Kbd = forwardRef<KbdProps, 'kbd'>((props, ref) => {
-  const css = useComponentStyle('kbd', props)
+  const css = useComponentStyle('Kbd', props)
   const { className, ...rest } = omitThemeProps(props)
 
   return <ui.kbd ref={ref} className={cx('ui-kbd', className)} __css={css} {...rest} />

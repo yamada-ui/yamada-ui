29ecb3871f747051795c35ec6e08f206092db129
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-21 11:58:13 +0900

    kbd to Kbd

--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -2,6 +2,6 @@ import { Button } from './button'
 import { Container } from './container'
 import { Divider } from './divider'
 import { Heading } from './heading'
-import { kbd } from './kbd'
+import { Kbd } from './kbd'
 
-export default { Button, Container, Divider, Heading, kbd }
+export default { Button, Container, Divider, Heading, Kbd }

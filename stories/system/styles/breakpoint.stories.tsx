3d3033a78e01b3d1efcc563fce4f229fd49d6196
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-22 01:43:46 +0900

    add styles story

---
+++ stories/system/styles/breakpoint.stories.tsx
@@ -0,0 +1,43 @@
+import { Box, useBreakpoint, useBreakpointValue } from '@yamada-ui/react'
+
+export default {
+  title: 'Components / Styles / Breakpoint',
+}
+
+export const basic = () => {
+  const breakpoint = useBreakpoint()
+
+  return (
+    <Box
+      bg={{
+        base: 'red.500',
+        xl: 'blue.500',
+        lg: 'green.500',
+        md: 'yellow.500',
+        sm: 'purple.500',
+      }}
+      p='md'
+      rounded='md'
+      color='white'
+    >
+      The current breakpoint is "{breakpoint}"
+    </Box>
+  )
+}
+
+export const useHook = () => {
+  const breakpoint = useBreakpoint()
+  const bg = useBreakpointValue({
+    base: 'red.500',
+    xl: 'blue.500',
+    lg: 'green.500',
+    md: 'yellow.500',
+    sm: 'purple.500',
+  })
+
+  return (
+    <Box bg={bg} p='md' rounded='md' color='white'>
+      The current breakpoint is "{breakpoint}"
+    </Box>
+  )
+}
---
+++ stories/system/styles/color-scheme.stories.tsx
@@ -0,0 +1,32 @@
+import { Box, useScheme, useSchemetValue } from '@yamada-ui/react'
+
+export default {
+  title: 'Components / Styles / Scheme',
+}
+
+export const basic = () => {
+  const { scheme } = useScheme()
+
+  return (
+    <Box
+      bg={['blackAlpha.800', 'whiteAlpha.800']}
+      p='md'
+      rounded='md'
+      color={['whiteAlpha.800', 'blackAlpha.800']}
+    >
+      The current scheme is "{scheme}"
+    </Box>
+  )
+}
+
+export const useHook = () => {
+  const { scheme } = useScheme()
+  const bg = useSchemetValue('blackAlpha.800', 'whiteAlpha.800')
+  const color = useSchemetValue('whiteAlpha.800', 'blackAlpha.800')
+
+  return (
+    <Box bg={bg} p='md' rounded='md' color={color}>
+      The current scheme is "{scheme}"
+    </Box>
+  )
+}

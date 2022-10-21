949f93b49e3d4d7526d873cfeb56af20f5c78419
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-21 17:31:19 +0900

    add Tag

--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -3,5 +3,7 @@ import { Container } from './container'
 import { Divider } from './divider'
 import { Heading } from './heading'
 import { Kbd } from './kbd'
+import { Badge } from './badge'
+import { Tag } from './tag'
 
-export default { Button, Container, Divider, Heading, Kbd }
+export default { Button, Container, Divider, Heading, Kbd, Badge, Tag }

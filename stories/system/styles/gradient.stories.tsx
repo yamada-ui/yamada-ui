594a7de1f1a14a9483ca263370c672831ef52a7e
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 01:43:46 +0900

    add styles story

---
+++ stories/system/styles/gradient.stories.tsx
@@ -0,0 +1,37 @@
+import { Box, Heading } from '@yamada-ui/react'
+
+export default {
+  title: 'Components / Styles / Gradient',
+}
+
+export const withToken = () => {
+  return <Box w='full' height='xs' bgGradient='linear(to-r, green.200, pink.500)'></Box>
+}
+
+export const withColorCode = () => {
+  return (
+    <Heading
+      w='full'
+      size='2xl'
+      bgGradient='linear(to-l, #7928CA, #FF0080)'
+      bgClip='text'
+      isTruncated
+    >
+      クリリンのことか……クリリンのことかーーーっ！！！！！
+    </Heading>
+  )
+}
+
+export const withBreakpoint = () => {
+  return (
+    <Box
+      w='full'
+      height='xs'
+      bgGradient={{
+        base: 'linear(to-tr, teal.300, yellow.400)',
+        lg: 'linear(to-t, blue.200, teal.500)',
+        md: 'linear(to-b, orange.100, purple.300)',
+      }}
+    ></Box>
+  )
+}

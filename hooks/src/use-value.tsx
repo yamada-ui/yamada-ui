833e2b866501e349e535bf9c9f069f2e706bc026
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 15:33:37 +0900

    change types

--- hooks/src/use-value.tsx
+++ hooks/src/use-value.tsx
@@ -1,12 +1,13 @@
 import { useSchemetValue } from '@yamada-ui/providers'
-import { isObject, isArray } from '@yamada-ui/utils'
+import { ResponsiveObject, SchemeArray } from '@yamada-ui/styled'
+import { isObject, isArray, Dict } from '@yamada-ui/utils'
 import { useBreakpointValue } from '.'
 
-export const useValue = <T extends any>(value: T): T => {
-  if (isObject(value)) {
+export const useValue = <T extends any>(value: T | ResponsiveObject<T> | SchemeArray<T>) => {
+  if (isObject<ResponsiveObject<T>>(value)) {
     return useBreakpointValue(value)
-  } else if (isArray(value)) {
-    return useSchemetValue(...(value as unknown as [any, any]))
+  } else if (isArray<SchemeArray<T>>(value)) {
+    return useSchemetValue(...value)
   } else {
     return value
   }

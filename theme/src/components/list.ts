e60417a497920e4e5e803f2b03e3efb310c19d2e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-23 00:07:35 +0900

    add list

---
+++ theme/src/components/list.ts
@@ -0,0 +1,12 @@
+import { ComponentStyle } from '@yamada-ui/styled'
+
+export const List: ComponentStyle = {
+  baseStyle: {
+    container: {},
+    icon: {
+      me: '2',
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+  },
+}

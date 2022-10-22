17b8df662d5165c8199f36715cffa239d9d4778f
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 21:43:21 +0900

    replace style

--- theme/src/components/tag.ts
+++ theme/src/components/tag.ts
@@ -4,34 +4,53 @@ import { transparentizeColor, toneColor, getColor } from '@yamada-ui/utils'
 
 export const Tag: ComponentStyle = {
   baseStyle: {
-    maxW: '100%',
-    display: 'inline-flex',
-    alignItems: 'center',
-    gap: '1',
-    fontWeight: 'medium',
-    lineHeight: 1.2,
-    outline: 0,
-    borderRadius: 'md',
+    container: {
+      outline: 0,
+      fontWeight: 'medium',
+      lineHeight: 1.2,
+      borderRadius: 'md',
+      _focusVisible: {
+        boxShadow: 'outline',
+      },
+    },
+    label: {
+      lineHeight: 1.8,
+      overflow: 'visible',
+    },
+    closeButton: {
+      transitionProperty: 'common',
+      transitionDuration: 'normal',
+      borderRadius: 'full',
+      opacity: 0.5,
+      _disabled: {
+        opacity: 0.4,
+      },
+      _focusVisible: {
+        boxShadow: 'outline',
+        bg: 'rgba(0, 0, 0, 0.14)',
+      },
+      _hover: {
+        opacity: 0.8,
+        _disabled: {
+          opacity: 0.4,
+          cursor: 'not-allowed',
+        },
+      },
+      _active: {
+        opacity: 1,
+      },
+    },
   },
 
   sizes: {
     sm: {
-      minH: '5',
-      minW: '5',
-      fontSize: 'xs',
-      px: '2',
+      container: { minH: '5', minW: '5', fontSize: 'xs', px: '2' },
     },
     md: {
-      minH: '6',
-      minW: '6',
-      fontSize: 'sm',
-      px: '2',
+      container: { minH: '6', minW: '6', fontSize: 'sm', px: '2' },
     },
     lg: {
-      minH: '8',
-      minW: '8',
-      fontSize: 'md',
-      px: '3',
+      container: { minH: '8', minW: '8', fontSize: 'md', px: '3' },
     },
   },
 
@@ -39,29 +58,40 @@ export const Tag: ComponentStyle = {
     solid: ({ theme, colorScheme: c }) => {
       if (c && defaultColors.includes(c)) {
         return {
-          bg: [toneColor(c, 500)(theme), transparentizeColor(toneColor(c, 500)(theme), 0.6)(theme)],
-          color: [`white`, `whiteAlpha.800`],
+          container: {
+            bg: [
+              toneColor(c, 500)(theme),
+              transparentizeColor(toneColor(c, 500)(theme), 0.6)(theme),
+            ],
+            color: [`white`, `whiteAlpha.800`],
+          },
         }
       } else {
         return {
-          bg: [`${c}.500`, transparentizeColor(`${c}.500`, 0.6)(theme)],
-          color: [`white`, `whiteAlpha.800`],
+          container: {
+            bg: [`${c}.500`, transparentizeColor(`${c}.500`, 0.6)(theme)],
+            color: [`white`, `whiteAlpha.800`],
+          },
         }
       }
     },
     subtle: ({ theme, colorScheme: c }) => {
       if (c && defaultColors.includes(c)) {
         return {
-          bg: [
-            toneColor(c, 100)(theme),
-            transparentizeColor(toneColor(c, 200)(theme), 0.16)(theme),
-          ],
-          color: [toneColor(c, 800)(theme), toneColor(c, 200)(theme)],
+          container: {
+            bg: [
+              toneColor(c, 100)(theme),
+              transparentizeColor(toneColor(c, 200)(theme), 0.16)(theme),
+            ],
+            color: [toneColor(c, 800)(theme), toneColor(c, 200)(theme)],
+          },
         }
       } else {
         return {
-          bg: [`${c}.100`, transparentizeColor(`${c}.200`, 0.16)(theme)],
-          color: [`${c}.800`, `${c}.200`],
+          container: {
+            bg: [`${c}.100`, transparentizeColor(`${c}.200`, 0.16)(theme)],
+            color: [`${c}.800`, `${c}.200`],
+          },
         }
       }
     },
@@ -73,8 +103,7 @@ export const Tag: ComponentStyle = {
             : transparentizeColor(toneColor(c, 200)(theme), 0.8)(theme)
 
         return {
-          color,
-          boxShadow: `inset 0 0 0px 1px ${color}`,
+          container: { color, boxShadow: `inset 0 0 0px 1px ${color}` },
         }
       } else {
         const color =
@@ -83,8 +112,7 @@ export const Tag: ComponentStyle = {
             : transparentizeColor(`${c}.200`, 0.8)(theme)
 
         return {
-          color,
-          boxShadow: `inset 0 0 0px 1px ${color}`,
+          container: { color, boxShadow: `inset 0 0 0px 1px ${color}` },
         }
       }
     },

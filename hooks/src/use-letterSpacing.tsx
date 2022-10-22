c50551fb39cadebee0782a90e5b5fbdd315dcd4c
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 15:33:22 +0900

    Integration into useToken

--- hooks/src/use-letterSpacing.tsx
+++
@@ -1,9 +0,0 @@
-import { Theme } from '@yamada-ui/styled'
-import { getMemoizedObject as get } from '@yamada-ui/utils'
-import { useTheme } from '@yamada-ui/providers'
-
-export const useLetterSpacing = (path: Theme['letterSpacings']) => {
-  const theme = useTheme()
-
-  return get(theme, `letterSpacings.${path}`)
-}
--- hooks/src/use-zIndex.tsx
+++
@@ -1,9 +0,0 @@
-import { Theme } from '@yamada-ui/styled'
-import { getMemoizedObject as get } from '@yamada-ui/utils'
-import { useTheme } from '@yamada-ui/providers'
-
-export const useZIndex = (path: Theme['zIndices']) => {
-  const theme = useTheme()
-
-  return get(theme, `zIndices.${path}`)
-}

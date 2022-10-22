a15bcf1474e27159a0b1e97f512b842fb90b2844
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 17:20:02 +0900

    replace style

--- components/data-display/src/badge.tsx
+++ components/data-display/src/badge.tsx
@@ -5,14 +5,22 @@ import {
   ThemeProps,
   useComponentStyle,
   omitThemeProps,
+  CSSUIObject,
 } from '@yamada-ui/system'
 import { cx } from '@yamada-ui/utils'
 
 export type BadgeProps = HTMLUIProps<'span'> & ThemeProps<'Badge'>
 
 export const Badge = forwardRef<BadgeProps, 'span'>((props, ref) => {
-  const css = useComponentStyle('Badge', props)
+  const style = useComponentStyle('Badge', props)
   const { className, ...rest } = omitThemeProps(props)
+
+  const css: CSSUIObject = {
+    display: 'inline-block',
+    whiteSpace: 'nowrap',
+    verticalAlign: 'middle',
+    ...style,
+  }
 
   return <ui.span ref={ref} className={cx('ui-badge', className)} __css={css} {...rest} />
 })

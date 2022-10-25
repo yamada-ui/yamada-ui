282187fd8fac4a08844f940a9f74b0c58419e334
hirotomo.yamada <combined_basilisk@pfg.app>
2022-10-26 02:28:04 +0900

    add useAnimation

--- hooks/src/use-animation.tsx
+++ hooks/src/use-animation.tsx
@@ -1,17 +1,30 @@
 import * as CSS from 'csstype'
-import { css, CSSUIProps, Token, keyframes as emotionKeyframes, CSSObject } from '@yamada-ui/styled'
+import {
+  css,
+  StylesProps,
+  Token,
+  keyframes as emotionKeyframes,
+  CSSObject,
+  StyledTheme,
+} from '@yamada-ui/styled'
 import { useTheme } from '@yamada-ui/providers'
-import { Dict } from '@yamada-ui/utils'
+import { Dict, isArray, isUndefined, runIfFunc } from '@yamada-ui/utils'
+import { useRef, useState } from 'react'
 
 export type AnimationStyle = {
-  keyframes: Record<string, CSSUIProps<'unresponsive', 'unscheme'>>
-  delay?: Token<CSS.Property.AnimationDelay>
-  direction?: Token<CSS.Property.AnimationDirection>
-  duration?: Token<CSS.Property.AnimationDuration>
-  fillMode?: Token<CSS.Property.AnimationFillMode>
-  iterationCount?: Token<CSS.Property.AnimationIterationCount>
-  playState?: Token<CSS.Property.AnimationPlayState>
-  timingFunction?: Token<CSS.Property.AnimationTimingFunction>
+  keyframes: Record<string, StylesProps<'unresponsive', 'unscheme'>>
+  delay?: Token<CSS.Property.AnimationDelay, unknown, 'unresponsive', 'unscheme'>
+  direction?: Token<CSS.Property.AnimationDirection, unknown, 'unresponsive', 'unscheme'>
+  duration?: Token<CSS.Property.AnimationDuration, 'transitionDuration', 'unresponsive', 'unscheme'>
+  fillMode?: Token<CSS.Property.AnimationFillMode, unknown, 'unresponsive', 'unscheme'>
+  iterationCount?: Token<CSS.Property.AnimationIterationCount, unknown, 'unresponsive', 'unscheme'>
+  playState?: Token<CSS.Property.AnimationPlayState, unknown, 'unresponsive', 'unscheme'>
+  timingFunction?: Token<
+    CSS.Property.AnimationTimingFunction,
+    'transitionEasing',
+    'unresponsive',
+    'unscheme'
+  >
 }
 
 const transformConfig = (obj: Omit<AnimationStyle, 'keyframes'>): Dict =>
@@ -23,16 +36,55 @@ const transformConfig = (obj: Omit<AnimationStyle, 'keyframes'>): Dict =>
     return obj
   }, {} as Dict)
 
-export const useAnimation = ({ keyframes: _keyframes, ...rest }: AnimationStyle): CSSObject => {
+const createCSS =
+  (keyframes: AnimationStyle['keyframes'], config: Omit<AnimationStyle, 'keyframes'>) =>
+  (theme: StyledTheme<Dict>): CSSObject => {
+    const generatedKeyframes = css(keyframes)(theme)
+
+    const generatedConfig = css(transformConfig(config))(theme)
+
+    const animationName = emotionKeyframes(generatedKeyframes)
+
+    return { animationName, ...generatedConfig }
+  }
+
+export const useAnimation = ({ keyframes, ...config }: AnimationStyle): CSSObject => {
   const theme = useTheme()
 
-  const keyframes = css(_keyframes)(theme)
+  const css = createCSS(keyframes, config)(theme)
 
-  const _config = transformConfig(rest)
+  return css
+}
 
-  const config = css(_config)(theme)
+export const useDynamicAnimation = <
+  T extends Array<AnimationStyle> | Record<string, AnimationStyle>,
+>(
+  styles: T,
+  init?: keyof T,
+): [CSSObject | undefined, (key: keyof T | ((key: keyof T | undefined) => keyof T)) => void] => {
+  const theme = useTheme()
+  const key = useRef<string | undefined>(!isUndefined(init) ? String(init) : undefined)
+  const cache = useRef<Map<string, CSSObject>>(new Map<string, CSSObject>())
+  const [animations, setAnimations] = useState<CSSObject | undefined>(() => {
+    for (const [key, { keyframes, ...config }] of Object.entries(styles)) {
+      if (!cache.current.has(key)) cache.current.set(key, createCSS(keyframes, config)(theme))
+    }
 
-  const animationName = emotionKeyframes(keyframes)
+    return cache.current.get(key.current ?? '')
+  })
 
-  return { animationName, ...config }
+  const setAnimation = (keysIfFunc: keyof T | ((key: keyof T | undefined) => keyof T)) => {
+    const _key = runIfFunc(
+      keysIfFunc,
+      (!isUndefined(key.current) && isArray(styles) ? Number(key.current) : key.current) as
+        | keyof T
+        | undefined,
+    )
+
+    key.current = String(_key)
+
+    setAnimations(cache.current.get(key.current))
+  }
+
+  return [animations, setAnimation]
 }

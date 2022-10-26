92625b95ae88a27d519a5e56491ad3269997440f
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-27 02:39:28 +0900

    LINKのやつのラッパー作るー！

---
+++ components/navigation/src/link.tsx
@@ -0,0 +1,23 @@
+import {
+    ui,
+    forwardRef,
+    HTMLUIProps,
+    omitThemeProps,
+    ThemeProps,
+    useComponentStyle,
+  } from '@yamada-ui/system'
+  import { cx } from '@yamada-ui/utils'
+
+  type LinkOptions<T extends string, K extends boolean | number> = {
+    [key in T]-?: K
+  }
+  
+  export type LinkProps = HTMLUIProps<'a'> & ThemeProps<'Link'> & LinkOptions<"isExternal", boolean>
+  
+  export const Link = forwardRef<LinkProps, 'a'>((props, ref) => {
+    const css = useComponentStyle('Link', props)
+    const { className, ...rest } = omitThemeProps(props)
+  
+    return <ui.a ref={ref} className={cx('ui-Link', className)} __css={css} {...rest} />
+  })
+// <Link href="" >LINK</Link>

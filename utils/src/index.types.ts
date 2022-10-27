873f4a398e8ac15dda9c2554e5f41cfa28bc66cb
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-27 23:36:59 +0900

    LINK isExternal追加

--- utils/src/index.types.ts
+++ utils/src/index.types.ts
@@ -10,9 +10,6 @@ export type Path<T> = {
   [K in keyof T]-?: PathImpl<K, T[K]>
 }[keyof T]
 
-const hoge = {hoge:"hoge.foo.bar.piyo.fuga",foo2:"hoge.foo2"}
-const hoge2 = hoge["hoge"]
-
 export type Dict<T = any> = Record<string, T>
 
 export type StringLiteral = string & {}
@@ -24,13 +21,3 @@ export type Length = string | 0 | number
 export type DynamicRecord<T> = {
   [K in keyof T]-?: T[K] extends Primitive ? string | number : DynamicRecord<T[K]>
 }
-
-const path: Path<{hoge:{foo:{bar:{piyo:{fuga: "end"}}}, foo2:"end2"}}> = "hoge.foo.bar.piyo.fuga"
-const path2:"hoge.foo.bar.piyo.fuga"|"hoge.foo2"  = "hoge.foo.bar.piyo.fuga"
-
-type Hogehoges = {hoge : string}[]
-
-const hogehoges:Hogehoges = []
-const hogehoge = {foo: "hoge"} as unknown as Hogehoges[number] // アサーション：型の矯正的な上書き
-
-hogehoges.push(hogehoge)

a626fbe2a77f38d6b66325a9260328b37490f2bd
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-29 02:35:48 +0900

    fix navigation

--- react/src/index.ts
+++ react/src/index.ts
@@ -8,6 +8,6 @@ export * from '@yamada-ui/media-and-icons'
 export * from '@yamada-ui/data-display'
 export * from '@yamada-ui/feedback'
 export * from '@yamada-ui/forms'
-export * from '@yamada-ui/navigation/src' // なぜか/srcいる
+export * from '@yamada-ui/navigation'
 
 export * from './extend-theme'

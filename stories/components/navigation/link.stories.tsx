7f6e58764181c94e0c6b8b49007af4d4553796eb
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-28 20:56:58 +0900

    List replaceed Link

--- stories/components/navigation/link.stories.tsx
+++ stories/components/navigation/link.stories.tsx
@@ -1,20 +1,19 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { List, } from '@yamada-ui/react'
-import { Link } from '../../../components/navigation/src/link'
+import { Link } from '@yamada-ui/react'
 
 export default {
   title: 'Components / Navigation / Link',
-  component: List,
-} as ComponentMeta<typeof List>
+  component: Link,
+} as ComponentMeta<typeof Link>
 
-export const link: ComponentStory<typeof List> = () => {
-  return (
-    <Link href='/'>LINK</Link>
-  )
+export const link: ComponentStory<typeof Link> = () => {
+  return <Link href='/'>LINK</Link>
 }
 
-export const linkIsExternal: ComponentStory<typeof List> = () => {
+export const linkIsExternal: ComponentStory<typeof Link> = () => {
   return (
-    <Link href='/' isExternal>LINK</Link>
+    <Link href='/' isExternal>
+      LINK
+    </Link>
   )
 }

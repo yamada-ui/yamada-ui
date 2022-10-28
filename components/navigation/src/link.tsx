c876f7a1349f8fe7cfac7e69acd89d3cafac28bf
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-10-28 20:55:56 +0900

    rel & target fixed.

--- components/navigation/src/link.tsx
+++ components/navigation/src/link.tsx
@@ -1,23 +1,31 @@
 import {
-    ui,
-    forwardRef,
-    HTMLUIProps,
-    omitThemeProps,
-    ThemeProps,
-    useComponentStyle,
-  } from '@yamada-ui/system'
-  import { cx } from '@yamada-ui/utils'
+  ui,
+  forwardRef,
+  HTMLUIProps,
+  omitThemeProps,
+  ThemeProps,
+  useComponentStyle,
+} from '@yamada-ui/system'
+import { cx } from '@yamada-ui/utils'
 
-  type LinkOptions = {
-    isExternal?: boolean
-  }
-  
-  export type LinkProps = HTMLUIProps<'a'> & ThemeProps<'Link'> & LinkOptions
-  
-  export const Link = forwardRef<LinkProps, 'a'>((props, ref) => {
-    const css = useComponentStyle('Link', props)
-    const { className, isExternal, ...rest } = omitThemeProps(props)
-  
-    return <ui.a ref={ref} rel='nofollow' target={isExternal ? '_blank' : '_parent'} className={cx('ui-link', className)} __css={css} {...rest} />
-    
-  })
+type LinkOptions = {
+  isExternal?: boolean
+}
+
+export type LinkProps = HTMLUIProps<'a'> & ThemeProps<'Link'> & LinkOptions
+
+export const Link = forwardRef<LinkProps, 'a'>((props, ref) => {
+  const css = useComponentStyle('Link', props)
+  const { className, isExternal, ...rest } = omitThemeProps(props)
+
+  return (
+    <ui.a
+      ref={ref}
+      target={isExternal ? '_blank' : undefined}
+      rel={isExternal ? 'noopener' : undefined}
+      className={cx('ui-link', className)}
+      __css={css}
+      {...rest}
+    />
+  )
+})

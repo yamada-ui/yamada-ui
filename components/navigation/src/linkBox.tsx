9adeee38b39bf214231eecc62056d59e722f3861
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-30 21:18:22 +0900

    add LinkOverlay

--- components/navigation/src/linkBox.tsx
+++ components/navigation/src/linkBox.tsx
@@ -7,40 +7,17 @@ import {
   useComponentStyle,
 } from '@yamada-ui/system'
 import { cx } from '@yamada-ui/utils'
-import { Link } from './link'
 
-type LinkBoxOption = {
-  isExternal?: boolean
-  href?: string
-}
-
-export type LinkBoxProps = HTMLUIProps<'article'> & ThemeProps<'article'> & LinkBoxOption
+export type LinkBoxProps = HTMLUIProps<'div'> & ThemeProps<'div'> //& LinkBoxOption
 
 export const LinkBox = forwardRef<LinkBoxProps, 'article'>((props, ref) => {
-  const css = useComponentStyle('article', props)
-  const { className, isExternal, ...rest } = omitThemeProps(props)
-  const { children, href } = props
+  const css = useComponentStyle('LinkBox', props)
+  const { className, ...rest } = omitThemeProps(props)
+  const { children } = props
 
   return (
     <ui.article className={cx('ui-article', className)} __css={css} position={'relative'} {...rest}>
-      <Link
-        isExternal={isExternal}
-        href={href}
-        position={'static'}
-        _before={{
-          content: '""',
-          cursor: 'inherit',
-          display: 'block',
-          position: 'absolute',
-          top: '0px',
-          left: '0px',
-          zIndex: '0',
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        {children}
-      </Link>
+      {children}
     </ui.article>
   )
 })

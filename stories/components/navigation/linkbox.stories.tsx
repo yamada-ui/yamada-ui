da0064dde4c3d202160bab49c634d38c01df6fc7
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-30 21:18:22 +0900

    add LinkOverlay

--- stories/components/navigation/linkbox.stories.tsx
+++ stories/components/navigation/linkbox.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { Center, LinkBox } from '@yamada-ui/react'
+import { Box, Heading, LinkBox, LinkOverlay, Text } from '@yamada-ui/react'
 
 export default {
   title: 'Components / Navigation / LinkBox',
@@ -8,20 +8,58 @@ export default {
 
 export const linkBox: ComponentStory<typeof LinkBox> = () => {
   return (
-    <>
-      <LinkBox href='/' minW='20' minH='20' rounded='md' bg='gray.500' color='white'>
-        <Center>LINK BOX</Center>
+    <Box width={'full'} bgColor={'white'}>
+      <LinkBox
+        as={'article'}
+        maxW={'sm'}
+        p={'5'}
+        border={'1px black solid'}
+        rounded={'md'}
+        bgColor={'white'}
+        color={'#000'}
+        margin={'1.5'}
+      >
+        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+          13 days ago
+        </Box>
+        <Heading size={'md'} my={'2'}>
+          <LinkOverlay href='/'>New Year, New Beginnings: Smashing Workshops & Audits</LinkOverlay>
+        </Heading>
+        <Text>
+          Catch up on what’s been cookin’ at Smashing and explore some of the most popular community
+          resources.
+        </Text>
       </LinkBox>
-    </>
+    </Box>
   )
 }
 
 export const linkBoxWithIsExternal: ComponentStory<typeof LinkBox> = () => {
   return (
-    <>
-      <LinkBox isExternal href='/' minW='20' minH='20' rounded='md' bg='gray.500' color='white'>
-        <Center>LINK BOX</Center>
+    <Box width={'full'} bgColor={'white'}>
+      <LinkBox
+        as={'article'}
+        maxW={'sm'}
+        p={'5'}
+        border={'1px black solid'}
+        rounded={'md'}
+        bgColor={'white'}
+        color={'#000'}
+        margin={'1.5'}
+      >
+        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
+          13 days ago
+        </Box>
+        <Heading size={'md'} my={'2'}>
+          <LinkOverlay href='/' isExternal>
+            New Year, New Beginnings: Smashing Workshops & Audits
+          </LinkOverlay>
+        </Heading>
+        <Text>
+          Catch up on what’s been cookin’ at Smashing and explore some of the most popular community
+          resources.
+        </Text>
       </LinkBox>
-    </>
+    </Box>
   )
 }

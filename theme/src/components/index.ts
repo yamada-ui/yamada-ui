ca8537f902f4d3a398893a2be290ce820e06d91e
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-01 01:42:01 +0900

    add link

--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -1,10 +1,11 @@
+import { Badge } from './badge'
 import { Button } from './button'
 import { Container } from './container'
 import { Divider } from './divider'
 import { Heading } from './heading'
 import { Kbd } from './kbd'
-import { Badge } from './badge'
-import { Tag } from './tag'
+import { Link } from './link'
 import { List } from './list'
+import { Tag } from './tag'
 
-export default { Button, Container, Divider, Heading, Kbd, Badge, Tag, List }
+export default { Button, Container, Divider, Heading, Kbd, Badge, Tag, List, Link }

400c2dced43ebc08fffab0e099e560e1a6d28189
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-31 13:02:52 +0900

    move LinkOverlay & other fixed

--- components/navigation/src/linkBox.tsx
+++ components/navigation/src/linkBox.tsx
@@ -8,16 +8,52 @@ import {
 } from '@yamada-ui/system'
 import { cx } from '@yamada-ui/utils'
 
-export type LinkBoxProps = HTMLUIProps<'div'> & ThemeProps<'div'> //& LinkBoxOption
+type LinkOverlayOption = {
+  isExternal?: boolean
+  href?: string
+}
 
-export const LinkBox = forwardRef<LinkBoxProps, 'article'>((props, ref) => {
-  const css = useComponentStyle('LinkBox', props)
-  const { className, ...rest } = omitThemeProps(props)
-  const { children } = props
+export type LinkOverlayProps = HTMLUIProps<'a'> & ThemeProps<'Link'> & LinkOverlayOption
+
+export const LinkOverlay = forwardRef<LinkOverlayProps, 'a'>((props, ref) => {
+  const { className, isExternal, href, children, ...rest } = omitThemeProps(props)
 
   return (
-    <ui.article className={cx('ui-article', className)} __css={css} position={'relative'} {...rest}>
+    <ui.a
+      ref={ref}
+      target={isExternal ? '_blank' : undefined}
+      rel={isExternal ? 'noopener' : undefined}
+      className={cx('ui-link', className)}
+      isExternal={isExternal}
+      href={href}
+      position={'static'}
+      _before={{
+        content: '""',
+        cursor: 'inherit',
+        display: 'block',
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        zIndex: '0',
+        width: '100%',
+        height: '100%',
+      }}
+      {...rest}
+    >
       {children}
-    </ui.article>
+    </ui.a>
+  )
+})
+
+export type LinkBoxProps = HTMLUIProps<'div'> & ThemeProps<'div'> //& LinkBoxOption
+
+export const LinkBox = forwardRef<LinkBoxProps, 'div'>((props, ref) => {
+  const css = useComponentStyle('LinkBox', props)
+  const { className, children, ...rest } = omitThemeProps(props)
+
+  return (
+    <ui.div className={cx('ui-link-box', className)} __css={css} position={'relative'} {...rest}>
+      {children}
+    </ui.div>
   )
 })

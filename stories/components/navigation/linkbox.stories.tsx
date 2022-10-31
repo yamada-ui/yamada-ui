4b89fddad89c3f9744cce4fa520a2c856a356d0a
hirotomo.yamada <combined_basilisk@pfg.app>
2022-11-01 01:41:44 +0900

    change layout

--- stories/components/navigation/linkbox.stories.tsx
+++ stories/components/navigation/linkbox.stories.tsx
@@ -1,65 +1,71 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { Box, Heading, LinkBox, LinkOverlay, Text } from '@yamada-ui/react'
+import { Link, Heading, LinkBox, LinkOverlay, Text } from '@yamada-ui/react'
 
 export default {
   title: 'Components / Navigation / LinkBox',
   component: LinkBox,
 } as ComponentMeta<typeof LinkBox>
 
-export const linkBox: ComponentStory<typeof LinkBox> = () => {
+export const basic: ComponentStory<typeof LinkBox> = () => {
   return (
-    <Box width='full' bgColor='white'>
-      <LinkBox
-        as='article'
-        maxW='sm'
-        p='5'
-        border='1px black solid'
-        rounded='md'
-        bgColor='white'
-        color='#000'
-        margin='1.5'
-      >
-        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-          13 days ago
-        </Box>
-        <Heading size='md' my='2'>
-          <LinkOverlay href='/'>New Year, New Beginnings: Smashing Workshops & Audits</LinkOverlay>
-        </Heading>
-        <Text>
-          Catch up on what’s been cookin’ at Smashing and explore some of the most popular community
-          resources.
-        </Text>
-      </LinkBox>
-    </Box>
+    <LinkBox
+      as='article'
+      maxW='sm'
+      p='md'
+      rounded='md'
+      border='1px solid'
+      borderColor={['border', 'blackAlpha.200']}
+      boxShadow='md'
+    >
+      <Text as='span'>鳥山明</Text>
+
+      <Heading size='md' my='sm'>
+        <LinkOverlay
+          href='https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB'
+          isExternal
+        >
+          ドラゴンボール
+        </LinkOverlay>
+      </Heading>
+
+      <Text>
+        『ドラゴンボール』（DRAGON
+        BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。
+      </Text>
+    </LinkBox>
   )
 }
 
-export const linkBoxWithIsExternal: ComponentStory<typeof LinkBox> = () => {
+export const withNestedLink: ComponentStory<typeof LinkBox> = () => {
   return (
-    <Box width='full' bgColor='white'>
-      <LinkBox
-        as='article'
-        maxW='sm'
-        p='5'
-        border='1px black solid'
-        rounded='md'
-        bgColor='white'
-        color='#000'
-        margin='1.5'
-      >
-        <Box as='time' dateTime='2021-01-15 15:30:00 +0000 UTC'>
-          13 days ago
-        </Box>
-        <Heading size='md' my='2'>
-          <LinkOverlay href='/' isExternal>
-            New Year, New Beginnings: Smashing Workshops & Audits
-          </LinkOverlay>
-        </Heading>
-        <Text>
-          Catch up on what’s been cookin’ at Smashing and explore some of the most popular community
-          resources.
-        </Text>
-      </LinkBox>
-    </Box>
+    <LinkBox
+      as='article'
+      maxW='sm'
+      p='md'
+      rounded='md'
+      border='1px solid'
+      borderColor={['border', 'blackAlpha.200']}
+      boxShadow='md'
+    >
+      <Text as='span'>鳥山明</Text>
+
+      <Heading size='md' my='sm'>
+        <LinkOverlay
+          href='https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB'
+          isExternal
+        >
+          ドラゴンボール
+        </LinkOverlay>
+      </Heading>
+
+      <Text mb='sm'>
+        『ドラゴンボール』（DRAGON
+        BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。
+      </Text>
+
+      <Link href='https://dragon-ball-official.com/' isExternal fontWeight='bold' color='link'>
+        オフィシャルサイト
+      </Link>
+    </LinkBox>
   )
 }

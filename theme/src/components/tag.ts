4c9724c4648578bce6d445b47ea08e3d0b57440f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-03 00:01:23 +0900

    formatted

--- theme/src/components/tag.ts
+++ theme/src/components/tag.ts
@@ -55,66 +55,66 @@ export const Tag: ComponentStyle = {
   },
 
   variants: {
-    solid: ({ theme, colorScheme: c }) => {
-      if (c && defaultColors.includes(c)) {
-        return {
-          container: {
+    solid: {
+      container: ({ theme, colorScheme: c }) => {
+        if (c && defaultColors.includes(c)) {
+          return {
             bg: [
               toneColor(c, 500)(theme),
               transparentizeColor(toneColor(c, 500)(theme), 0.6)(theme),
             ],
             color: [`white`, `whiteAlpha.800`],
-          },
-        }
-      } else {
-        return {
-          container: {
+          }
+        } else {
+          return {
             bg: [`${c}.500`, transparentizeColor(`${c}.500`, 0.6)(theme)],
             color: [`white`, `whiteAlpha.800`],
-          },
+          }
         }
-      }
+      },
     },
-    subtle: ({ theme, colorScheme: c }) => {
-      if (c && defaultColors.includes(c)) {
-        return {
-          container: {
+    subtle: {
+      container: ({ theme, colorScheme: c }) => {
+        if (c && defaultColors.includes(c)) {
+          return {
             bg: [
               toneColor(c, 100)(theme),
               transparentizeColor(toneColor(c, 200)(theme), 0.16)(theme),
             ],
             color: [toneColor(c, 800)(theme), toneColor(c, 200)(theme)],
-          },
-        }
-      } else {
-        return {
-          container: {
+          }
+        } else {
+          return {
             bg: [`${c}.100`, transparentizeColor(`${c}.200`, 0.16)(theme)],
             color: [`${c}.800`, `${c}.200`],
-          },
+          }
         }
-      }
+      },
     },
-    outline: ({ theme, scheme, colorScheme: c }) => {
-      if (c && defaultColors.includes(c)) {
-        const color =
-          scheme === 'light'
-            ? toneColor(c, 500)(theme)
-            : transparentizeColor(toneColor(c, 200)(theme), 0.8)(theme)
+    outline: {
+      container: ({ theme, scheme, colorScheme: c }) => {
+        if (c && defaultColors.includes(c)) {
+          const color =
+            scheme === 'light'
+              ? toneColor(c, 500)(theme)
+              : transparentizeColor(toneColor(c, 200)(theme), 0.8)(theme)
 
-        return {
-          container: { color, boxShadow: `inset 0 0 0px 1px ${color}` },
-        }
-      } else {
-        const color =
-          scheme === 'light'
-            ? getColor(theme, `${c}.500`)
-            : transparentizeColor(`${c}.200`, 0.8)(theme)
+          return {
+            color,
+            boxShadow: `inset 0 0 0px 1px ${color}`,
+          }
+        } else {
+          const color =
+            scheme === 'light'
+              ? getColor(theme, `${c}.500`)
+              : transparentizeColor(`${c}.200`, 0.8)(theme)
 
-        return {
-          container: { color, boxShadow: `inset 0 0 0px 1px ${color}` },
+          return {
+            color,
+            boxShadow: `inset 0 0 0px 1px ${color}`,
+          }
         }
-      }
+      },
     },
   },
 

40b1d0508520f40932095b62ceda05174f30577d
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-03 15:55:20 +0900

    add mode

--- styled/src/style.ts
+++ styled/src/style.ts
@@ -1,5 +1,5 @@
 import { keyframes as emotionKeyframes, Keyframes, CSSObject } from '@emotion/react'
-import { styles, pseudos } from './'
+import { styles, pseudos, Scheme } from './'
 
 const styleProps = { ...styles, ...pseudos }
 
@@ -7,5 +7,10 @@ export const isPropStyle = (prop: string, value?: any): boolean =>
   value === undefined
     ? prop in styleProps
     : prop in styleProps && value !== null && value !== undefined
+
+export const mode =
+  <L extends any, D extends any>(light: L, dark: D) =>
+  (scheme: Scheme | undefined = 'light'): L | D =>
+    scheme === 'light' ? light : dark
 
 export const keyframes = (...arg: CSSObject[]): Keyframes => emotionKeyframes(...arg)

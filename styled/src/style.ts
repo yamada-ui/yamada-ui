6af062614e284d64472c35638f0809f289eb05e2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-03 17:17:02 +0900

    add isDefaultColor

--- styled/src/style.ts
+++ styled/src/style.ts
@@ -3,6 +3,8 @@ import { styles, pseudos, Scheme } from './'
 
 const styleProps = { ...styles, ...pseudos }
 
+export const defaultColors = ['primary', 'secondary', 'warning', 'danger', 'link']
+
 export const isPropStyle = (prop: string, value?: any): boolean =>
   value === undefined
     ? prop in styleProps
@@ -12,5 +14,10 @@ export const mode =
   <L extends any, D extends any>(light: L, dark: D) =>
   (scheme: Scheme | undefined = 'light'): L | D =>
     scheme === 'light' ? light : dark
+
+export const isDefaultColor =
+  (defaultColor: any, otherColor: any) =>
+  (colorScheme: string | undefined): any =>
+    colorScheme && defaultColors.includes(colorScheme) ? defaultColor : otherColor
 
 export const keyframes = (...arg: CSSObject[]): Keyframes => emotionKeyframes(...arg)

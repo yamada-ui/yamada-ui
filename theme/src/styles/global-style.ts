757c7c6e29e3781d38a6f0e7d62f37b096cd59fd
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-05 21:44:55 +0900

    add '*' style

--- theme/src/styles/global-style.ts
+++ theme/src/styles/global-style.ts
@@ -3,10 +3,14 @@ import { UIStyle } from '@yamada-ui/styled'
 export const globalStyle: UIStyle = () => ({
   body: {
     fontFamily: 'body',
-    background: ['white', 'black'],
+    bg: ['white', 'black'],
     color: ['black', 'white'],
     lineHeight: 'base',
     overflowX: 'hidden',
+  },
+  '*': {
+    transitionProperty: 'all',
+    transitionDuration: 'normal',
   },
   '*::placeholder': {
     color: ['gray.400', 'whiteAlpha.400'],

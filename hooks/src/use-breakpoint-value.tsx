23718b12848fd06d1dda6cf3a58c24c6395dea74
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-05 21:43:52 +0900

    omit useState, useEffect

--- hooks/src/use-breakpoint-value.tsx
+++ hooks/src/use-breakpoint-value.tsx
@@ -1,20 +1,8 @@
 import { ResponsiveObject } from '@yamada-ui/styled'
-import { useState, useEffect } from 'react'
 import { useBreakpoint } from '.'
 
 export const useBreakpointValue = <T extends any>(values: ResponsiveObject<T>) => {
   const breakpoint = useBreakpoint()
-  const [value, setValue] = useState<ResponsiveObject<T>[keyof ResponsiveObject<T>]>(() =>
-    values[breakpoint] ? values[breakpoint] : values.base,
-  )
 
-  useEffect(() => {
-    if (values[breakpoint]) {
-      setValue(values[breakpoint])
-    } else {
-      setValue(values.base)
-    }
-  }, [breakpoint, values])
-
-  return value
+  return values.hasOwnProperty(breakpoint) ? values[breakpoint] : values.base
 }

a5cdfe857dc8d52390e388e8a4cd5d511b92c8bc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-06 07:30:18 +0900

    remove '*' styles

--- theme/src/styles/global-style.ts
+++ theme/src/styles/global-style.ts
@@ -5,12 +5,10 @@ export const globalStyle: UIStyle = () => ({
     fontFamily: 'body',
     bg: ['white', 'black'],
     color: ['black', 'white'],
+    transitionProperty: 'background-color',
+    transitionDuration: 'normal',
     lineHeight: 'base',
     overflowX: 'hidden',
-  },
-  '*': {
-    transitionProperty: 'all',
-    transitionDuration: 'normal',
   },
   '*::placeholder': {
     color: ['gray.400', 'whiteAlpha.400'],

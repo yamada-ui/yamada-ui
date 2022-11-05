f59db02e06ee2c4f32572eb3c2dceb13de077030
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-05 19:50:51 +0900

    add  initial value

--- hooks/src/use-breakpoint-value.tsx
+++ hooks/src/use-breakpoint-value.tsx
@@ -4,15 +4,15 @@ import { useBreakpoint } from '.'
 
 export const useBreakpointValue = <T extends any>(values: ResponsiveObject<T>) => {
   const breakpoint = useBreakpoint()
-  const [value, setValue] = useState<T | undefined>(undefined)
+  const [value, setValue] = useState<ResponsiveObject<T>[keyof ResponsiveObject<T>]>(() =>
+    values[breakpoint] ? values[breakpoint] : values.base,
+  )
 
   useEffect(() => {
     if (values[breakpoint]) {
       setValue(values[breakpoint])
-    } else if ('base' in values) {
-      setValue(values.base)
     } else {
-      setValue(undefined)
+      setValue(values.base)
     }
   }, [breakpoint, values])
 

da67d7d86a662b48448eb78aae4fccb0d4fcf24e
hirotomo.yamada <combined_basilisk@pfg.app>
2022-11-06 10:57:38 +0900

    add text style

---
+++ stories/system/theme/text-styles.stories.tsx
@@ -0,0 +1,28 @@
+import { UIProvider, extendTheme, TextStyles, Heading } from '@yamada-ui/react'
+
+export default {
+  title: 'System / Theme / Text Layout',
+}
+
+export const useTextStyle = () => {
+  const textStyles: TextStyles = {
+    gradient: {
+      w: 'full',
+      fontSize: '5xl',
+      bgGradient: 'linear(to-l, #7928CA, #FF0080)',
+      bgClip: 'text',
+    },
+  }
+
+  const theme = extendTheme({
+    styles: { textStyles },
+  })
+
+  return (
+    <UIProvider theme={theme}>
+      <Heading textStyle='gradient' isTruncated>
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </Heading>
+    </UIProvider>
+  )
+}

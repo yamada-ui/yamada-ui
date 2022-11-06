709a06baf1d82d4a80ea98c709d750195197dfa5
hirotomo.yamada <combined_basilisk@pfg.app>
2022-11-06 10:06:01 +0900

    add configure

---
+++ stories/system/theme/configure.stories.tsx
@@ -0,0 +1,45 @@
+import { UIProvider, extendConfig, Center, Image } from '@yamada-ui/react'
+import { FC } from 'react'
+
+export default {
+  title: 'System / Theme / Configure',
+}
+
+export const extendConfigure = () => {
+  const config = extendConfig({
+    /**
+     * Define the config you want to customize, merging them with the default config
+     */
+  })
+
+  return (
+    <UIProvider config={config}>
+      <App />
+    </UIProvider>
+  )
+}
+
+export const overrideConfigure = () => {
+  const config = {
+    /**
+     * Define the config you want to customize
+     */
+  }
+
+  return (
+    <UIProvider config={config}>
+      <App />
+    </UIProvider>
+  )
+}
+
+const App: FC = () => {
+  return (
+    <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Image
+        maxW='xl'
+        src='https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/yamada-ui.png'
+      />
+    </Center>
+  )
+}

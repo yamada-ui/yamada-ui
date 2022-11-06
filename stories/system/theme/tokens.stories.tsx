6caf6a67a99ce657757e016aac52af7f40593e1f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-07 00:30:42 +0900

    add Tokens

--- stories/system/theme/tokens.stories.tsx
+++ stories/system/theme/tokens.stories.tsx
@@ -1,7 +1,39 @@
-import { UIProvider, extendTheme, Box, Semantics } from '@yamada-ui/react'
+import {
+  UIProvider,
+  extendTheme,
+  Box,
+  Semantics,
+  useColorScheme,
+  ThemeTokens,
+} from '@yamada-ui/react'
 
 export default {
-  title: 'System / Theme / Semantic Token',
+  title: 'System / Theme / Tokens',
+}
+
+export const useColorSchemeToken = () => {
+  const { colorScheme } = useColorScheme()
+
+  const colors: ThemeTokens = {
+    primary: ['#2196F3', '#F44336'],
+  }
+
+  const theme = extendTheme({ colors })
+
+  return (
+    <UIProvider theme={theme}>
+      <Box
+        bg='primary'
+        p='md'
+        rounded='md'
+        color='white'
+        transitionProperty='all'
+        transitionDuration='normal'
+      >
+        The current colorScheme is "{colorScheme}"
+      </Box>
+    </UIProvider>
+  )
 }
 
 export const useSemanticToken = () => {

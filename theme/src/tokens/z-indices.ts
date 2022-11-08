cd3489811000f5be0a5987fbc5860dc67a3acaad
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-08 23:40:14 +0900

    add tokens

--- theme/src/tokens/z-indices.ts
+++ theme/src/tokens/z-indices.ts
@@ -4,7 +4,16 @@ export const zIndices = {
   yamcha: 1,
   kurillin: 9,
   nappa: 99,
-  freeza: 999,
+
+  guldo: 100,
+  jeice: 110,
+  burter: 120,
+  recoome: 130,
+  ginyu: 140,
+  zarbon: 150,
+  dodoria: 160,
+
+  freeza: 9996,
   vegeta: 9997,
   sonGoku: 9998,
   beerus: 9999,

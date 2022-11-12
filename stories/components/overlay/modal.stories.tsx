e38fc72d3bf19d39d8cf7408a5ed146ad6c7b3a2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-13 07:38:02 +0900

    add customCloseButton, customOverlay

--- stories/components/overlay/modal.stories.tsx
+++ stories/components/overlay/modal.stories.tsx
@@ -3,6 +3,8 @@ import {
   Button,
   useDisclosure,
   Modal,
+  ModalOverlay,
+  ModalCloseButton,
   ModalHeader,
   ModalBody,
   ModalFooter,
@@ -324,6 +326,34 @@ export const disabledCloseButton: ComponentStory<typeof Modal> = () => {
   )
 }
 
+export const customCloseButton: ComponentStory<typeof Modal> = () => {
+  const [isOpen, onOpen, onClose] = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalCloseButton color='gray.400' />
+
+        <ModalHeader>ドラゴンボール</ModalHeader>
+
+        <ModalBody>
+          『ドラゴンボール』（DRAGON
+          BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。世界中に散らばった七つの球をすべて集めると、どんな願いも一つだけ叶えられるという秘宝・ドラゴンボールと、主人公・孫悟空（そん・ごくう）を中心に展開する、「冒険」「夢」「バトル」「友情」などを描いた長編漫画。
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variant='ghost' onClick={onClose}>
+            とじる
+          </Button>
+          <Button colorStyle='primary'>Wikipadia</Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  )
+}
+
 export const disabledOverlay: ComponentStory<typeof Modal> = () => {
   const [isOpen, onOpen, onClose] = useDisclosure()
 
@@ -332,6 +362,36 @@ export const disabledOverlay: ComponentStory<typeof Modal> = () => {
       <Button onClick={onOpen}>Open Modal</Button>
 
       <Modal isOpen={isOpen} onClose={onClose} overlay={false}>
+        <ModalHeader>ドラゴンボール</ModalHeader>
+
+        <ModalBody>
+          『ドラゴンボール』（DRAGON
+          BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。世界中に散らばった七つの球をすべて集めると、どんな願いも一つだけ叶えられるという秘宝・ドラゴンボールと、主人公・孫悟空（そん・ごくう）を中心に展開する、「冒険」「夢」「バトル」「友情」などを描いた長編漫画。
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variant='ghost' onClick={onClose}>
+            とじる
+          </Button>
+          <Button colorStyle='primary'>Wikipadia</Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  )
+}
+
+export const customOverlay: ComponentStory<typeof Modal> = () => {
+  const [isOpen, onOpen, onClose] = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalCloseButton color='red.500' />
+
+        <ModalOverlay bg='blackAlpha.300' backdropFilter='blur(10px)' />
+
         <ModalHeader>ドラゴンボール</ModalHeader>
 
         <ModalBody>

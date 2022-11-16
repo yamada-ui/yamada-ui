f913507e3fd119ee8a3f07a53828c491b759797d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-16 19:43:19 +0900

    add argument type

--- hooks/src/use-token.tsx
+++ hooks/src/use-token.tsx
@@ -2,27 +2,27 @@ import { useColorScheme, useTheme } from '@yamada-ui/providers'
 import { Theme } from '@yamada-ui/styled'
 import { getMemoizedObject as get, isArray } from '@yamada-ui/utils'
 
-export const useToken = <T extends keyof Omit<Theme, 'components'>>(
-  name: T,
-  path: Theme[T] | number | undefined,
+export const useToken = <
+  Y extends string | number = string,
+  M extends keyof Omit<Theme, 'components'> = keyof Omit<Theme, 'components'>,
+>(
+  name: M,
+  path: Theme[M] | number | undefined,
 ) => {
   const { theme } = useTheme()
   const { colorScheme } = useColorScheme()
 
-  if (name === 'layerStyles') name = 'styles.layerStyles' as T
+  if (name === 'layerStyles') name = 'styles.layerStyles' as M
 
-  if (name === 'textStyles') name = 'styles.textStyles' as T
+  if (name === 'textStyles') name = 'styles.textStyles' as M
 
-  if (name === 'transitionProperty') name = 'transitions.property' as T
+  if (name === 'transitionProperty') name = 'transitions.property' as M
 
-  if (name === 'transitionDuration') name = 'transitions.duration' as T
+  if (name === 'transitionDuration') name = 'transitions.duration' as M
 
-  if (name === 'transitionEasing') name = 'transitions.easing' as T
+  if (name === 'transitionEasing') name = 'transitions.easing' as M
 
-  const value: string | number | [string | number, string | number] | undefined = get(
-    theme,
-    `${name}.${path}`,
-  )
+  const value: Y | [Y, Y] | undefined = get(theme, `${name}.${path}`)
 
   if (isArray(value)) {
     const [lightValue, darkValue] = value

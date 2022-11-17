3b17566caf114f6087f21f1a25522588155f3f6a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-17 19:51:48 +0900

    add border darkMode

--- theme/src/tokens/colors.ts
+++ theme/src/tokens/colors.ts
@@ -10,7 +10,7 @@ export const colors = {
   warning: '#FF9800',
   danger: '#F44336',
   link: ' #1D94E0',
-  border: '#EBEBEB',
+  border: ['rgba(0, 0, 0, 0.08)', 'rgba(255, 255, 255, 0.06)'],
 
   whiteAlpha: {
     50: 'rgba(255, 255, 255, 0.04)',

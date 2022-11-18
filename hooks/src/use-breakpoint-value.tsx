7c949e5ea7140f839021202b13621bf85af6045c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-18 10:12:17 +0900

    add type assertion

--- hooks/src/use-breakpoint-value.tsx
+++ hooks/src/use-breakpoint-value.tsx
@@ -1,8 +1,8 @@
 import { ResponsiveObject } from '@yamada-ui/styled'
 import { useBreakpoint } from '.'
 
-export const useBreakpointValue = <T extends any>(values: ResponsiveObject<T>) => {
+export const useBreakpointValue = <T extends any>(values: ResponsiveObject<T>): T => {
   const breakpoint = useBreakpoint()
 
-  return values.hasOwnProperty(breakpoint) ? values[breakpoint] : values.base
+  return (values.hasOwnProperty(breakpoint) ? values[breakpoint] : values.base) as T
 }

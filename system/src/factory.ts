d2e2fa98eac3b0ec4aa147da1a47a534e0f22d42
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-26 18:39:33 +0900

    formatted

--- system/src/factory.ts
+++ system/src/factory.ts
@@ -4,14 +4,14 @@ const factory = () => {
   const cache = new Map<DOMElements, UIComponent<DOMElements>>()
 
   return new Proxy(styled, {
-    apply(target, thisArg, argArray: [DOMElements, StyledOptions]) {
-      return styled(...argArray)
+    apply: (target, thisArg, [el, options]: [DOMElements, StyledOptions]) => {
+      return styled(el, options)
     },
 
-    get(target, element: DOMElements) {
-      if (!cache.has(element)) cache.set(element, styled(element))
+    get: (target, el: DOMElements) => {
+      if (!cache.has(el)) cache.set(el, styled(el))
 
-      return cache.get(element)
+      return cache.get(el)
     },
   }) as unknown as UIFactory & HTMLUIComponents
 }

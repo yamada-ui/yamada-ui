9cad911fca08879db574cedcbcfd7704216e02dd
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-11-28 01:30:25 +0900

    add InputAddon

---
+++ components/forms/src/input-addon.tsx
@@ -0,0 +1,62 @@
+import { ui, forwardRef, CSSUIObject, HTMLUIProps } from '@yamada-ui/system'
+import { cx } from '@yamada-ui/utils'
+import { useInputGroup } from './'
+
+type InputAddonOptions = {
+  placement?: 'left' | 'right'
+}
+
+export type InputAddonProps = HTMLUIProps<'div'> & InputAddonOptions
+
+const InputAddon = forwardRef<InputAddonProps, 'div'>(
+  ({ className, placement = 'left', ...rest }, ref) => {
+    const styles = useInputGroup()
+
+    const placementStyles = {
+      left: {
+        me: '-1px',
+        borderEndRadius: 0,
+        borderEndColor: 'transparent',
+      },
+      right: {
+        ms: '-1px',
+        borderStartRadius: 0,
+        borderStartColor: 'transparent',
+      },
+    }
+
+    const css: CSSUIObject = {
+      flex: '0 0 auto',
+      w: 'auto',
+      display: 'flex',
+      alignItems: 'center',
+      whiteSpace: 'nowrap',
+      ...styles.addon,
+      ...placementStyles[placement],
+    }
+
+    return <ui.div ref={ref} className={cx('ui-input-addon', className)} __css={css} {...rest} />
+  },
+)
+
+export const InputLeftAddon = forwardRef<InputAddonProps, 'div'>(({ className, ...rest }, ref) => {
+  return (
+    <InputAddon
+      ref={ref}
+      className={cx('ui-input-addon-left', className)}
+      placement='left'
+      {...rest}
+    />
+  )
+})
+
+export const InputRightAddon = forwardRef<InputAddonProps, 'div'>(({ className, ...rest }, ref) => {
+  return (
+    <InputAddon
+      ref={ref}
+      className={cx('ui-input-addon-right', className)}
+      placement='right'
+      {...rest}
+    />
+  )
+})

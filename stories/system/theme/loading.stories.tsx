a618f8212fbaf16e012912e01d7720913ca460b6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-02 21:49:36 +0900

    add loading

--- stories/system/theme/loading.stories.tsx
+++ stories/system/theme/loading.stories.tsx
@@ -1,4 +1,17 @@
-import { UIProvider, extendConfig, Center, useLoading, Wrap, Button } from '@yamada-ui/react'
+import {
+  UIProvider,
+  extendConfig,
+  Center,
+  useLoading,
+  Wrap,
+  Button,
+  useTimeout,
+  Loading,
+  Motion,
+  Text,
+  isValidElement,
+  VStack,
+} from '@yamada-ui/react'
 import { FC, useEffect, useCallback } from 'react'
 
 export default {
@@ -47,7 +60,17 @@ export const useInitialState = () => {
       screen: {
         initialState: true,
         icon: {
-          variant: 'grid',
+          variant: 'dots',
+        },
+      },
+      page: {
+        icon: {
+          variant: 'dots',
+        },
+      },
+      background: {
+        icon: {
+          variant: 'dots',
         },
       },
     },
@@ -56,6 +79,119 @@ export const useInitialState = () => {
   return (
     <UIProvider config={config}>
       <AsyncApp />
+    </UIProvider>
+  )
+}
+
+export const useCustomLoading = () => {
+  const config = extendConfig({
+    loading: {
+      custom: {
+        component: ({ initialState, message, timeout, onFinish }) => {
+          useTimeout(onFinish, timeout)
+
+          return (
+            <Motion
+              initial={!initialState ? 'initial' : false}
+              animate='animate'
+              exit='exit'
+              variants={{
+                initial: {
+                  opacity: 0,
+                },
+                animate: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.4,
+                    ease: [0.4, 0, 0.2, 1],
+                  },
+                },
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.4,
+                    ease: [0.4, 0, 1, 1],
+                  },
+                },
+              }}
+              sx={{
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                zIndex: 9999,
+                bg: 'blackAlpha.600',
+                w: '100vw',
+                h: '100vh',
+                p: 'md',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Motion
+                initial={!initialState ? 'initial' : false}
+                animate='animate'
+                exit='exit'
+                variants={{
+                  initial: {
+                    opacity: 0,
+                    scale: 0.95,
+                  },
+                  animate: {
+                    opacity: 1,
+                    scale: 1,
+                    transition: {
+                      duration: 0.4,
+                      ease: [0.4, 0, 0.2, 1],
+                    },
+                  },
+                  exit: {
+                    opacity: 0,
+                    scale: 0.95,
+                    transition: {
+                      duration: 0.4,
+                      ease: [0.4, 0, 1, 1],
+                    },
+                  },
+                }}
+                sx={{
+                  bg: ['white', 'black'],
+                  maxW: 'md',
+                  p: 'md',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: 'sm',
+                  rounded: 'md',
+                  boxShadow: ['lg', 'dark-lg'],
+                }}
+              >
+                <Loading variant='dots' size='6xs' />
+                <VStack align='center' mb='md' gap='sm'>
+                  <Text>Downloading files…</Text>
+                  {message ? (
+                    isValidElement(message) ? (
+                      message
+                    ) : (
+                      <Text noOfLines={3}>{message}</Text>
+                    )
+                  ) : null}
+                </VStack>
+                <Button onClick={onFinish}>Switch to background</Button>
+              </Motion>
+            </Motion>
+          )
+        },
+      },
+    },
+  })
+
+  return (
+    <UIProvider config={config}>
+      <CustomApp />
     </UIProvider>
   )
 }
@@ -93,6 +229,18 @@ const AsyncApp: FC = () => {
         <Button onClick={() => screen.start()}>Start screen loading</Button>
         <Button onClick={() => page.start()}>Start page loading</Button>
         <Button onClick={() => background.start()}>Start background loading</Button>
+      </Wrap>
+    </Center>
+  )
+}
+
+const CustomApp: FC = () => {
+  const { custom } = useLoading()
+
+  return (
+    <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => custom.start({ timeout: 10000 })}>Start custom loading</Button>
       </Wrap>
     </Center>
   )

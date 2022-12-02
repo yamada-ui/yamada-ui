e57d76c96642c22c722d041b1c5cddada783a61f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-02 19:33:02 +0900

    add notice

---
+++ stories/system/theme/notice.stories.tsx
@@ -0,0 +1,42 @@
+import { UIProvider, extendConfig, Center, useNotice, Button } from '@yamada-ui/react'
+import { FC } from 'react'
+
+export default {
+  title: 'System / Theme / Notice',
+}
+
+export const basic = () => {
+  const config = extendConfig({
+    notice: {
+      options: {
+        placement: 'bottom-right',
+        limit: 3,
+      },
+    },
+  })
+
+  return (
+    <UIProvider config={config}>
+      <App />
+    </UIProvider>
+  )
+}
+
+const App: FC = () => {
+  const notice = useNotice()
+
+  return (
+    <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Button
+        onClick={() =>
+          notice({
+            title: '孫悟空',
+            description: 'オッス！オラ悟空！',
+          })
+        }
+      >
+        Show Notice
+      </Button>
+    </Center>
+  )
+}

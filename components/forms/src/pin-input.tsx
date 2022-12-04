fb159e38a4d162230607576490a085d8da198e49
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-04 21:52:23 +0900

    add PinInput

--- components/forms/src/pin-input.tsx
+++ components/forms/src/pin-input.tsx
@@ -8,32 +8,35 @@ import {
   ThemeProps,
   CSSUIProps,
 } from '@yamada-ui/core'
-import { createContext, cx } from '@yamada-ui/utils'
-import { useCallback, useState } from 'react'
+import { createDescendant, useControllableState } from '@yamada-ui/hooks'
+import { createContext, cx, handlerAll, mergeRefs } from '@yamada-ui/utils'
+import { ChangeEvent, KeyboardEvent, useCallback, useEffect, useId, useState } from 'react'
 import { useFormControlProps, FormControlOptions } from './'
 
 type PinInputOptions = {
-  autoFocus?: boolean
+  id?: string
+  type?: 'alphanumeric' | 'number'
+  placeholder?: string
   value?: string
   defaultValue?: string
   onChange?: (value: string) => void
   onComplete?: (value: string) => void
-  placeholder?: string
+  autoFocus?: boolean
   manageFocus?: boolean
   otp?: boolean
-  id?: string
-  type?: 'alphanumeric' | 'number'
   mask?: boolean
   focusBorderColor?: CSSUIProps<'unresponsive'>['borderColor']
   errorBorderColor?: CSSUIProps<'unresponsive'>['borderColor']
 }
 
-export type PinInputProps = HTMLUIProps<'div'> &
-  ThemeProps<'PinInput'> &
-  PinInputOptions &
-  FormControlOptions
-
-type PinInputContext = { styles: Record<string, CSSUIObject> }
+type PinInputContext = FormControlOptions & {
+  getPinInputFieldProps: (
+    props: PinInputFieldProps & {
+      index: number
+    },
+  ) => PinInputFieldProps
+  styles: Record<string, CSSUIObject>
+}
 
 const [PinInputProvider, usePinInput] = createContext<PinInputContext>({
   strict: false,
@@ -42,9 +45,28 @@ const [PinInputProvider, usePinInput] = createContext<PinInputContext>({
 
 export { usePinInput }
 
-export const PinInput = forwardRef<PinInputProps, 'div'>((props, ref) => {
+const { DescendantsContextProvider, useDescendants, useDescendant } =
+  createDescendant<HTMLInputElement>()
+
+const toArray = (value?: string) => value?.split('')
+
+const validate = (value: string, type: PinInputProps['type']) => {
+  const NUMERIC_REGEX = /^[0-9]+$/
+  const ALPHA_NUMERIC_REGEX = /^[a-zA-Z0-9]+$/i
+
+  const regex = type === 'alphanumeric' ? ALPHA_NUMERIC_REGEX : NUMERIC_REGEX
+
+  return regex.test(value)
+}
+
+export type PinInputProps = Omit<HTMLUIProps<'div'>, 'onChange'> &
+  ThemeProps<'PinInput'> &
+  PinInputOptions &
+  FormControlOptions
+
+export const PinInput = forwardRef<PinInputProps, 'div'>(({ id, ...props }, ref) => {
   const styles = useMultiComponentStyle('PinInput', props)
-  let {
+  const {
     className,
     autoFocus,
     value,
@@ -54,64 +76,171 @@ export const PinInput = forwardRef<PinInputProps, 'div'>((props, ref) => {
     placeholder = '○',
     manageFocus = true,
     otp = false,
-    id: idProp,
-    isDisabled,
-    isInvalid,
     type = 'number',
     mask,
+    isRequired,
+    isInvalid,
+    isReadOnly,
+    isDisabled,
     ...rest
   } = omitThemeProps(props)
 
-  rest = useFormControlProps(rest)
+  if (!id) id = useId()
+
+  const descendants = useDescendants()
 
   const [moveFocus, setMoveFocus] = useState<boolean>(true)
-  const [focusedIndex, setFocusedIndex] = useState<boolean>(-1)
+  const [focusedIndex, setFocusedIndex] = useState<number>(-1)
+
+  useEffect(() => {
+    if (!autoFocus) return
+
+    const firstValue = descendants.firstValue()
+
+    if (!firstValue) return
+
+    requestAnimationFrame(() => firstValue.node.focus())
+  }, [autoFocus, descendants])
 
   const [values, setValues] = useControllableState<string[]>({
-    defaultValue: toArray(defaultValue) || [],
     value: toArray(value),
+    defaultValue: toArray(defaultValue) || [],
     onChange: (values) => onChange?.(values.join('')),
   })
 
   const focusNext = useCallback(
     (index: number) => {
       if (!moveFocus || !manageFocus) return
-      const next = descendants.next(index, false)
-      if (next) {
-        requestAnimationFrame(() => {
-          next.node.focus()
-        })
-      }
+
+      const next = descendants.nextValue(index, false)
+
+      if (!next) return
+
+      requestAnimationFrame(() => next.node.focus())
     },
     [descendants, moveFocus, manageFocus],
   )
 
   const setValue = useCallback(
-    (value: string, index: number, handleFocus: boolean = true) => {
-      const nextValues = [...values]
+    (value: string, index: number, isFocus: boolean = true) => {
+      let nextValues = [...values]
+
       nextValues[index] = value
+
       setValues(nextValues)
+
+      nextValues = nextValues.filter(Boolean)
 
       const isComplete =
         value !== '' &&
         nextValues.length === descendants.count() &&
-        nextValues.every((inputValue) => inputValue != null && inputValue !== '')
+        nextValues.every((value) => value != null && value !== '')
 
       if (isComplete) {
         onComplete?.(nextValues.join(''))
-      } else {
-        if (handleFocus) focusNext(index)
+        descendants.value(index)?.node.blur()
+      } else if (isFocus) {
+        focusNext(index)
       }
     },
-    [values, setValues, focusNext, onComplete, descendants],
+    [values, setValues, descendants, onComplete, focusNext],
   )
 
-  const clear = useCallback(() => {
-    const values: string[] = Array(descendants.count()).fill('')
-    setValues(values)
-    const first = descendants.first()
-    first?.node?.focus()
-  }, [descendants, setValues])
+  const getNextValue = useCallback((value: string | undefined, eventValue: string) => {
+    let nextValue = eventValue
+
+    if (!value?.length) return nextValue
+
+    if (value[0] === eventValue.charAt(0)) {
+      nextValue = eventValue.charAt(1)
+    } else if (value[0] === eventValue.charAt(1)) {
+      nextValue = eventValue.charAt(0)
+    }
+
+    return nextValue
+  }, [])
+
+  const getPinInputFieldProps = useCallback(
+    ({ index, ...rest }: PinInputFieldProps & { index: number }): PinInputFieldProps => {
+      const onChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+        const eventValue = target.value
+        const currentValue = values[index]
+        const nextValue = getNextValue(currentValue, eventValue)
+
+        if (nextValue === '') {
+          setValue('', index)
+
+          return
+        }
+
+        if (eventValue.length > 2) {
+          if (!validate(eventValue, type)) return
+
+          const nextValue = eventValue.split('').filter((_, index) => index < descendants.count())
+
+          setValues(nextValue)
+
+          if (nextValue.length === descendants.count()) {
+            onComplete?.(nextValue.join(''))
+            descendants.value(index)?.node.blur()
+          }
+        } else {
+          if (validate(nextValue, type)) setValue(nextValue, index)
+
+          setMoveFocus(true)
+        }
+      }
+
+      const onKeyDown = ({ key, target }: KeyboardEvent<HTMLInputElement>) => {
+        if (key !== 'Backspace' || !manageFocus) return
+
+        if ((target as HTMLInputElement).value === '') {
+          const prevInput = descendants.prevValue(index, false)
+
+          if (!prevInput) return
+
+          setValue('', index - 1, false)
+          prevInput.node?.focus()
+          setMoveFocus(true)
+        } else {
+          setMoveFocus(false)
+        }
+      }
+
+      const onFocus = () => setFocusedIndex(index)
+
+      const onBlur = () => setFocusedIndex(-1)
+
+      return {
+        inputMode: type === 'number' ? 'numeric' : 'text',
+        type: mask ? 'password' : type === 'number' ? 'tel' : 'text',
+        ...rest,
+        id: `${id}-${index}`,
+        value: values[index] || '',
+        onChange: handlerAll(rest.onChange, onChange),
+        onKeyDown: handlerAll(rest.onKeyDown, onKeyDown),
+        onFocus: handlerAll(rest.onFocus, onFocus),
+        onBlur: handlerAll(rest.onBlur, onBlur),
+        autoComplete: otp ? 'one-time-code' : 'off',
+        placeholder: focusedIndex === index && !rest.readOnly ? '' : placeholder,
+      }
+    },
+    [
+      descendants,
+      focusedIndex,
+      getNextValue,
+      id,
+      manageFocus,
+      mask,
+      onComplete,
+      otp,
+      placeholder,
+      setValue,
+      setValues,
+      type,
+      values,
+    ],
+  )
 
   const css: CSSUIObject = {
     display: 'flex',
@@ -120,34 +249,35 @@ export const PinInput = forwardRef<PinInputProps, 'div'>((props, ref) => {
   }
 
   return (
-    <PinInputProvider
-      value={{
-        setValue,
-        setValues,
-        clear,
-        styles,
-      }}
-    >
-      <ui.div ref={ref} className={cx('ui-pin-input', className)} __css={css} {...rest} />
-    </PinInputProvider>
+    <DescendantsContextProvider value={descendants}>
+      <PinInputProvider
+        value={{ isRequired, isInvalid, isReadOnly, isDisabled, getPinInputFieldProps, styles }}
+      >
+        <ui.div ref={ref} className={cx('ui-pin-input', className)} __css={css} {...rest} />
+      </PinInputProvider>
+    </DescendantsContextProvider>
   )
 })
 
-export type PinInputFieldProps = HTMLUIProps<'input'>
+export type PinInputFieldProps = HTMLUIProps<'input'> & FormControlOptions
 
 export const PinInputField = forwardRef<PinInputFieldProps, 'input'>(
   ({ className, ...rest }, ref) => {
-    const { styles } = usePinInput()
+    const { isRequired, isInvalid, isReadOnly, isDisabled, getPinInputFieldProps, styles } =
+      usePinInput()
+    const { index, register } = useDescendant()
+
+    rest = useFormControlProps({ isRequired, isInvalid, isReadOnly, isDisabled, ...rest })
 
     const css: CSSUIObject = { ...styles.field }
 
     return (
       <ui.input
-        ref={ref}
+        ref={mergeRefs(register, ref)}
         className={cx('ui-pin-input-field', className)}
         __css={css}
-        {...rest}
-      ></ui.input>
+        {...getPinInputFieldProps({ index, ...rest })}
+      />
     )
   },
 )
--- stories/components/forms/pin-input.stories.tsx
+++ stories/components/forms/pin-input.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { PinInput, PinInputField } from '@yamada-ui/react'
+import { FormControl, PinInput, PinInputField, useLoading } from '@yamada-ui/react'
+import { useState } from 'react'
 
 export default {
   title: 'Components / Forms / PinInput',
@@ -14,5 +15,317 @@ export const basic: ComponentStory<typeof PinInput> = () => {
       <PinInputField />
       <PinInputField />
     </PinInput>
+  )
+}
+
+export const withSize: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput size='xs'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput size='sm'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput size='md'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput size='lg'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </>
+  )
+}
+
+export const withVariant: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput variant='outline'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput variant='filled'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput variant='flushed'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput variant='unstyled'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </>
+  )
+}
+
+export const withType: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput type='alphanumeric'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </>
+  )
+}
+
+export const withDefaultValue: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput defaultValue='1234'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput defaultValue='123'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </>
+  )
+}
+
+export const withBorderColor: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput focusBorderColor='green.500'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput isInvalid errorBorderColor='orange.500'>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+    </>
+  )
+}
+
+export const withOnComplete: ComponentStory<typeof PinInput> = () => {
+  const { page } = useLoading()
+
+  return (
+    <PinInput onComplete={() => page.start({ timeout: 5000 })}>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const useOneTimePassword: ComponentStory<typeof PinInput> = () => {
+  return (
+    <PinInput otp>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const maskingValue: ComponentStory<typeof PinInput> = () => {
+  return (
+    <PinInput mask>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const costomPlaceholder: ComponentStory<typeof PinInput> = () => {
+  return (
+    <PinInput placeholder='💩'>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const customControl: ComponentStory<typeof PinInput> = () => {
+  const { page } = useLoading()
+  const [value, setValue] = useState('')
+
+  const onChange = (value: string) => setValue(value)
+
+  const onComplete = () => page.start({ timeout: 5000 })
+
+  return (
+    <PinInput value={value} onChange={onChange} onComplete={onComplete}>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const disabledFocusManagement: ComponentStory<typeof PinInput> = () => {
+  return (
+    <PinInput manageFocus={false}>
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+      <PinInputField />
+    </PinInput>
+  )
+}
+
+export const isDisabled: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput isDisabled>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput>
+        <PinInputField isDisabled />
+        <PinInputField isDisabled />
+        <PinInputField isDisabled />
+        <PinInputField isDisabled />
+      </PinInput>
+
+      <FormControl
+        isDisabled
+        label='Please one-time password'
+        helperMessage='Just sent you a one-time password to your e-mail address.'
+        errorMessage='one-time password is required.'
+      >
+        <PinInput>
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </FormControl>
+    </>
+  )
+}
+
+export const isReadonly: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput isReadOnly>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput>
+        <PinInputField isReadOnly />
+        <PinInputField isReadOnly />
+        <PinInputField isReadOnly />
+        <PinInputField isReadOnly />
+      </PinInput>
+
+      <FormControl
+        isReadOnly
+        label='Please one-time password'
+        helperMessage='Just sent you a one-time password to your e-mail address.'
+        errorMessage='one-time password is required.'
+      >
+        <PinInput>
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </FormControl>
+    </>
+  )
+}
+
+export const isInvalid: ComponentStory<typeof PinInput> = () => {
+  return (
+    <>
+      <PinInput isInvalid>
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+        <PinInputField />
+      </PinInput>
+
+      <PinInput>
+        <PinInputField isInvalid />
+        <PinInputField isInvalid />
+        <PinInputField isInvalid />
+        <PinInputField isInvalid />
+      </PinInput>
+
+      <FormControl
+        isInvalid
+        label='Please one-time password'
+        helperMessage='Just sent you a one-time password to your e-mail address.'
+        errorMessage='one-time password is required.'
+      >
+        <PinInput>
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </FormControl>
+    </>
   )
 }

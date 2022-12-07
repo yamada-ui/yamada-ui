ba660faef097e34deea86498ad3484cbb3fa688e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-07 21:22:06 +0900

    formatted

--- hooks/src/use-controllable-state.tsx
+++ hooks/src/use-controllable-state.tsx
@@ -8,18 +8,15 @@ export type UseControllableStateProps<T extends any> = {
   onUpdate?: (prev: T, next: T) => boolean
 }
 
-export const useControllableState = <T extends any>({
-  value: _value,
-  defaultValue,
-  onChange: _onChange,
-  onUpdate: _onUpdate = (prev, next) => prev !== next,
-}: UseControllableStateProps<T>) => {
-  const onChange = useCallbackRef(_onChange)
-  const onUpdate = useCallbackRef(_onUpdate)
+export const useControllableState = <T extends any>(props: UseControllableStateProps<T>) => {
+  props.onUpdate = props.onUpdate ?? ((prev, next) => prev !== next)
 
-  const [uncontrolledState, setUncontrolledState] = useState(defaultValue as T)
-  const controlled = _value !== undefined
-  const value = controlled ? _value : uncontrolledState
+  const onChange = useCallbackRef(props.onChange)
+  const onUpdate = useCallbackRef(props.onUpdate)
+
+  const [uncontrolledState, setUncontrolledState] = useState(props.defaultValue as T)
+  const controlled = props.value !== undefined
+  const value = controlled ? (props.value as T) : uncontrolledState
 
   const setValue = useCallbackRef(
     (next: SetStateAction<T>) => {

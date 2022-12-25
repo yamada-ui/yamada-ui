e8af4174cca35637b60fbfc5852b159765c2857f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-25 18:34:54 +0900

    add AccordionPanel

---
+++ components/disclosure/src/accordion-panel.tsx
@@ -0,0 +1,52 @@
+import { ui, forwardRef, CSSUIObject, HTMLUIProps, WithTransitionProps } from '@yamada-ui/core'
+import { Collapse, CollapseProps } from '@yamada-ui/transitions'
+import { cx } from '@yamada-ui/utils'
+import { useAccordionItemContext, useAccordionContext } from './'
+
+export type AccordionPanelProps = Omit<WithTransitionProps<HTMLUIProps<'div'>>, 'isOpen'> &
+  Pick<CollapseProps, 'animationOpacity' | 'startingHeight' | 'endingHeight'>
+
+export const AccordionPanel = forwardRef<AccordionPanelProps, 'div'>(
+  (
+    {
+      className,
+      unmountOnExit,
+      animationOpacity,
+      startingHeight,
+      endingHeight,
+      transition,
+      transitionEnd,
+      delay,
+      duration,
+      ...rest
+    },
+    ref,
+  ) => {
+    const { isOpen, getPanelProps } = useAccordionItemContext()
+    const { styles } = useAccordionContext()
+
+    const css: CSSUIObject = { ...styles.panel }
+
+    return (
+      <Collapse
+        isOpen={isOpen}
+        {...{
+          unmountOnExit,
+          animationOpacity,
+          startingHeight,
+          endingHeight,
+          transition,
+          transitionEnd,
+          delay,
+          duration,
+        }}
+      >
+        <ui.div
+          {...getPanelProps(rest, ref)}
+          className={cx('ui-accordion-panel', className)}
+          __css={css}
+        />
+      </Collapse>
+    )
+  },
+)

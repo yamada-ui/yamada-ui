86c8ab56e4a4e0cda43b99b98fe695f0163f35eb
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-25 18:11:25 +0900

    add RadioGroup

--- components/forms/src/radio.tsx
+++ components/forms/src/radio.tsx
@@ -6,15 +6,10 @@ import {
   HTMLUIProps,
   ThemeProps,
 } from '@yamada-ui/core'
-import { trackFocusVisible, useControllableState } from '@yamada-ui/hooks'
-import { Flex, FlexProps } from '@yamada-ui/layouts'
+import { trackFocusVisible } from '@yamada-ui/hooks'
 import {
-  createContext,
   cx,
-  isObject,
-  mergeRefs,
   PropGetter,
-  DOMAttributes,
   useCallbackRef,
   omitObject,
   funcAll,
@@ -30,160 +25,15 @@ import {
   SyntheticEvent,
   useCallback,
   useEffect,
-  useId,
-  useRef,
   useState,
 } from 'react'
 import {
+  useRadioGroupContenxt,
   useFormControl,
   useFormControlProps,
   FormControlOptions,
   returnUseFormControlPropsMap,
 } from './'
-
-const isEvent = (value: any): value is { target: HTMLInputElement } =>
-  value && isObject(value) && isObject(value.target)
-
-export type UseRadioGroupProps = {
-  id?: string
-  name?: string
-  value?: string | number
-  defaultValue?: string | number
-  onChange?: (value: string | number) => void
-  isNative?: boolean
-}
-
-export const useRadioGroup = ({ id, name, isNative, ...props }: UseRadioGroupProps) => {
-  id = id ?? useId()
-  name = name ?? `radio-${id}`
-
-  props.onChange = useCallbackRef(props.onChange)
-
-  const [value, setValue] = useControllableState({
-    value: props.value,
-    defaultValue: props.defaultValue,
-    onChange: props.onChange,
-  })
-
-  const containerRef = useRef<any>(null)
-
-  const onFocus = useCallback(() => {
-    const container = containerRef.current
-
-    if (!container) return
-
-    let query = `input:not(:disabled):checked`
-
-    let firstInput = container.querySelector(query) as HTMLElement | undefined
-
-    if (firstInput) {
-      firstInput.focus()
-    } else {
-      query = `input:not(:disabled)`
-      firstInput = container.querySelector(query) as HTMLElement | undefined
-
-      firstInput?.focus()
-    }
-  }, [])
-
-  const onChange = useCallback(
-    (evOrValue: ChangeEvent<HTMLInputElement> | string | number) => {
-      const nextValue = isEvent(evOrValue) ? evOrValue.target.value : evOrValue
-
-      setValue(nextValue)
-    },
-    [setValue],
-  )
-
-  const getContainerProps: PropGetter = useCallback(
-    (props = {}, ref = null) => ({
-      ...props,
-      ref: mergeRefs(ref, containerRef),
-      role: 'group',
-    }),
-    [],
-  )
-
-  const getRadioProps: PropGetter<
-    DOMAttributes<HTMLInputElement> & { isChecked?: boolean },
-    Omit<DOMAttributes<HTMLInputElement>, 'onChange'> & {
-      onChange: (ev: ChangeEvent<HTMLInputElement> | string | number) => void
-    }
-  > = useCallback(
-    (props = {}, ref = null) => ({
-      ...props,
-      ref,
-      name,
-      [isNative ? 'checked' : 'isChecked']: value != null ? props.value === value : undefined,
-      onChange,
-    }),
-    [name, value, onChange, isNative],
-  )
-
-  return {
-    name,
-    value,
-    setValue,
-    onChange,
-    onFocus,
-    getContainerProps,
-    getRadioProps,
-  }
-}
-
-export type RadioGroupProps = ThemeProps<'Radio'> &
-  Omit<FlexProps, 'onChange'> &
-  UseRadioGroupProps &
-  FormControlOptions
-
-type RadioGroupContext = ThemeProps<'Radio'> &
-  FormControlOptions & {
-    name: string
-    value: string | number
-    onChange: (evOrValue: ChangeEvent<HTMLInputElement> | string | number) => void
-  }
-
-const [RadioGroupProvider, useRadioGroupContenxt] = createContext<RadioGroupContext | undefined>({
-  strict: false,
-  name: 'RadioGroupContext',
-})
-
-export const RadioGroup = forwardRef<RadioGroupProps, 'div'>(
-  (
-    { className, size, variant, colorStyle, children, direction = 'column', gap, ...props },
-    ref,
-  ) => {
-    const { name, value, onChange, getContainerProps } = useRadioGroup(props)
-    const { isRequired, isReadOnly, isDisabled, isInvalid } = useFormControl(props)
-
-    return (
-      <RadioGroupProvider
-        value={{
-          size,
-          variant,
-          colorStyle,
-          isRequired,
-          isReadOnly,
-          isDisabled,
-          isInvalid,
-          name,
-          value,
-          onChange,
-        }}
-      >
-        <Flex
-          ref={ref}
-          className={cx('ui-radio-group', className)}
-          direction={direction}
-          gap={gap ?? (direction === 'row' ? '1rem' : undefined)}
-          {...getContainerProps(omitObject(props, ['onChange']))}
-        >
-          {children}
-        </Flex>
-      </RadioGroupProvider>
-    )
-  },
-)
 
 export type UseRadioProps = FormControlOptions & {
   id?: string

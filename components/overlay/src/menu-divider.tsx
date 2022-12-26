feba34e12fc53f9846808e603720bccf80b9f5eb
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-12-26 16:36:54 +0900

    add MenuDivider

---
+++ components/overlay/src/menu-divider.tsx
@@ -0,0 +1,13 @@
+import { ui, forwardRef, HTMLUIProps, CSSUIObject } from '@yamada-ui/core'
+import { cx } from '@yamada-ui/utils'
+import { useMenu } from './'
+
+export type MenuDividerProps = HTMLUIProps<'hr'>
+
+export const MenuDivider = forwardRef(({ className, ...rest }, ref) => {
+  const { styles } = useMenu()
+
+  const css: CSSUIObject = { ...styles.divider }
+
+  return <ui.hr ref={ref} className={cx('ui-menu-divider', className)} __css={css} {...rest} />
+})

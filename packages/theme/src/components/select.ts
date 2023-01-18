cee2575dbce32ac94137897429e0a23f25bd4e2a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-01-18 23:14:06 +0900

    change style unstyled

--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -234,7 +234,7 @@ export const Select: ComponentMultiStyle = {
     unstyled: {
       field: {
         bg: 'transparent',
-        h: 'auto',
+        minH: 'auto',
         px: '0',
       },
     },

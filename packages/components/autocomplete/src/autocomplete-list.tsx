877deabe00617a5c744299828c1f38f93256994a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-01-20 19:23:29 +0900

    add AutocompleteList

---
+++ packages/components/autocomplete/src/autocomplete-list.tsx
@@ -0,0 +1,29 @@
+import { CSSUIObject, CSSUIProps, forwardRef, HTMLUIProps } from '@yamada-ui/core'
+import { PopoverContent } from '@yamada-ui/popover'
+import { cx } from '@yamada-ui/utils'
+import { useAutocompleteContext, useAutocompleteList } from './use-autocomplete'
+
+export type AutocompleteListProps = HTMLUIProps<'ul'>
+
+export const AutocompleteList = forwardRef<AutocompleteListProps, 'ul'>(
+  ({ className, w, width, minW, minWidth, ...rest }, ref) => {
+    const { styles } = useAutocompleteContext()
+
+    const { getListProps } = useAutocompleteList()
+
+    w = w ?? width ?? ((styles.list?.w ?? styles.list?.width) as CSSUIProps['w'])
+    minW = minW ?? minWidth ?? ((styles.list?.minW ?? styles.list?.minWidth) as CSSUIProps['minW'])
+
+    const css: CSSUIObject = { ...styles.list }
+
+    return (
+      <PopoverContent
+        className={cx('ui-autocomplete-list', className)}
+        w={w}
+        minW={minW}
+        __css={css}
+        {...getListProps(rest, ref)}
+      />
+    )
+  },
+)

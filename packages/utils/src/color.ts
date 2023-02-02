a1ed21aa85b5d476cbd287fa9a6108a76d14862e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-02-02 20:59:29 +0900

    add randomColor

--- packages/utils/src/color.ts
+++ packages/utils/src/color.ts
@@ -81,3 +81,59 @@ export const toneColor =
 
     return new TinyColor(raw)[isLighten ? 'lighten' : 'shade'](n).toString()
   }
+
+export const randomColor = ({ string, colors }: { string?: string; colors?: string[] } = {}) => {
+  const fallback = randomHex()
+
+  if (string && colors) return randomColorFromList(string, colors)
+
+  if (string && !colors) return randomColorFromString(string)
+
+  if (colors && !string) return randomFromList(colors)
+
+  return fallback
+}
+
+const randomHex = () =>
+  `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padEnd(6, '0')}`
+
+const randomColorFromString = (str: string) => {
+  let hash = 0
+
+  if (str.length === 0) return hash.toString()
+
+  for (let i = 0; i < str.length; i += 1) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+    hash = hash & hash
+  }
+
+  let color = '#'
+
+  for (let j = 0; j < 3; j += 1) {
+    const value = (hash >> (j * 8)) & 255
+
+    color += `00${value.toString(16)}`.substr(-2)
+  }
+
+  return color
+}
+
+const randomColorFromList = (str: string, list: string[]) => {
+  let index = 0
+
+  if (str.length === 0) return list[0]
+
+  for (let i = 0; i < str.length; i += 1) {
+    index = str.charCodeAt(i) + ((index << 5) - index)
+
+    index = index & index
+  }
+
+  index = ((index % list.length) + list.length) % list.length
+
+  return list[index]
+}
+
+const randomFromList = (list: string[]) => list[Math.floor(Math.random() * list.length)]

7c8af918f3ec3ee58e98d4ef6ed9816764a88f56
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-02-02 20:42:15 +0900

    add AvatarName

---
+++ packages/components/avatar/src/avatar-name.tsx
@@ -0,0 +1,23 @@
+import { ui, HTMLUIProps, CSSUIObject } from '@yamada-ui/core'
+import { FC } from 'react'
+import { AvatarProps, useAvatarContext } from './'
+
+const defaultFormat = (name: string) => {
+  const [firstName, lastName] = name.split(' ')
+
+  return firstName && lastName ? `${firstName.charAt(0)}${lastName.charAt(0)}` : firstName.charAt(0)
+}
+
+export type AvatarNameProps = HTMLUIProps<'div'> & Pick<AvatarProps, 'name' | 'format'>
+
+export const AvatarName: FC<AvatarNameProps> = ({ name, format = defaultFormat, ...rest }) => {
+  const styles = useAvatarContext()
+
+  const css: CSSUIObject = { ...styles.name }
+
+  return (
+    <ui.div className='ui-avatar-name' role='img' __css={css} {...rest}>
+      {name ? format(name) : null}
+    </ui.div>
+  )
+}

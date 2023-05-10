6239371041f0a30bebe82e2842c0ed27bed5a6cc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-10 16:02:50 +0900

    add Skeleton

--- packages/components/skeleton/src/skeleton.tsx
+++ packages/components/skeleton/src/skeleton.tsx
@@ -6,20 +6,98 @@ import {
   HTMLUIProps,
   ThemeProps,
   useComponentStyle,
+  CSSUIProps,
 } from '@yamada-ui/core'
-import { cx } from '@yamada-ui/utils'
+import { useAnimation } from '@yamada-ui/use-animation'
+import { usePrevious } from '@yamada-ui/use-previous'
+import { useValue } from '@yamada-ui/use-value'
+import { cx, useIsMounted } from '@yamada-ui/utils'
 
-type SkeletonOptions = {}
+type SkeletonOptions = {
+  startColor?: CSSUIProps['color']
+  endColor?: CSSUIProps['color']
+  isLoaded?: boolean
+  speed?: string | number
+  fadeDuration?: string | number
+}
 
 export type SkeletonProps = HTMLUIProps<'div'> & ThemeProps<'Skeleton'> & SkeletonOptions
 
 export const Skeleton = forwardRef<SkeletonProps, 'div'>((props, ref) => {
-  const styles = useComponentStyle('Skeleton', props)
-  const { className, ...rest } = omitThemeProps(props)
+  let [styles, mergedProps] = useComponentStyle('Skeleton', props)
+  const {
+    className,
+    startColor,
+    endColor,
+    fadeDuration = 0.4,
+    speed = 0.8,
+    isLoaded,
+    ...rest
+  } = omitThemeProps(mergedProps)
+  const isMounted = useIsMounted()
+
+  const prevIsLoaded = usePrevious(isLoaded)
+
+  const computedStartColor = useValue(startColor)
+  const computedEndColor = useValue(endColor)
+
+  const fadeIn = useAnimation({
+    keyframes: {
+      '0%': {
+        opacity: 0,
+      },
+      '100%': {
+        opacity: 1,
+      },
+    },
+    duration: typeof fadeDuration === 'string' ? fadeDuration : `${fadeDuration}s`,
+  })
+
+  const animation = useAnimation({
+    keyframes: {
+      '0%': {
+        borderColor: computedStartColor,
+        background: computedStartColor,
+      },
+      '100%': {
+        borderColor: computedEndColor,
+        background: computedEndColor,
+      },
+    },
+    duration: typeof speed === 'string' ? speed : `${speed}s`,
+    iterationCount: 'infinite',
+    direction: 'alternate',
+    timingFunction: 'linear',
+  })
 
   const css: CSSUIObject = {
+    boxShadow: 'none',
+    backgroundClip: 'padding-box',
+    cursor: 'default',
+    color: 'transparent',
+    pointerEvents: 'none',
+    userSelect: 'none',
+    '&::before, &::after, *': {
+      visibility: 'hidden',
+    },
     ...styles,
   }
 
-  return <ui.div ref={ref} className={cx('ui-skeleton', className)} __css={css} {...rest} />
+  if (isLoaded) {
+    const animation = !isMounted.current || prevIsLoaded ? 'none' : fadeIn
+
+    return (
+      <ui.div ref={ref} className={cx('ui-skeleton', className)} {...rest} animation={animation} />
+    )
+  } else {
+    return (
+      <ui.div
+        ref={ref}
+        className={cx('ui-skeleton', className)}
+        __css={css}
+        {...rest}
+        animation={animation}
+      />
+    )
+  }
 })
--- packages/theme/src/components/skeleton.ts
+++ packages/theme/src/components/skeleton.ts
@@ -1,11 +1,14 @@
 import { ComponentStyle } from '@yamada-ui/core'
 
 export const Skeleton: ComponentStyle = {
-  baseStyle: {},
-
-  variants: {},
-
-  sizes: {},
-
-  defaultProps: {},
+  baseStyle: {
+    background: ['gray.100', 'gray.800'],
+    borderColor: ['gray.400', 'gray.600'],
+    opacity: 0.7,
+    borderRadius: 'sm',
+  },
+  defaultProps: {
+    startColor: ['gray.100', 'gray.800'],
+    endColor: ['gray.400', 'gray.600'],
+  },
 }

a723c35afb553d21c51f5596e96a96c30680ee8a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-12 16:55:59 +0900

    add StepTitle

---
+++ packages/components/stepper/src/step-title.tsx
@@ -0,0 +1,23 @@
+import { ui, CSSUIObject, forwardRef, HTMLUIProps } from '@yamada-ui/core'
+import { cx } from '@yamada-ui/utils'
+import { useStepContext } from './step'
+import { useStepperContext } from './use-stepper'
+
+export type StepTitleProps = HTMLUIProps<'h3'>
+
+export const StepTitle = forwardRef<StepTitleProps, 'h3'>(({ className, ...rest }, ref) => {
+  const { styles } = useStepperContext()
+  const { status } = useStepContext()
+
+  const css: CSSUIObject = { ...styles.title }
+
+  return (
+    <ui.h3
+      ref={ref}
+      className={cx('ui-step-title', className)}
+      data-status={status}
+      __css={css}
+      {...rest}
+    />
+  )
+})

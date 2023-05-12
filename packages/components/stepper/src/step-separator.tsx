67ee8f5741e4b70b39efeecbd9cbc8e6fe104648
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-12 16:56:52 +0900

    add StepSeparator

---
+++ packages/components/stepper/src/step-separator.tsx
@@ -0,0 +1,29 @@
+import { ui, CSSUIObject, forwardRef, HTMLUIProps } from '@yamada-ui/core'
+import { cx } from '@yamada-ui/utils'
+import { useStepContext } from './step'
+import { useStepperContext } from './use-stepper'
+
+export type StepSeparatorProps = HTMLUIProps<'div'>
+
+export const StepSeparator = forwardRef<StepSeparatorProps, 'div'>(
+  ({ className, ...rest }, ref) => {
+    const { orientation, showLastSeparator, styles } = useStepperContext()
+    const { status, isLast } = useStepContext()
+
+    const css: CSSUIObject = { ...styles.separator }
+
+    if (isLast && !showLastSeparator) return null
+
+    return (
+      <ui.div
+        ref={ref}
+        className={cx('ui-step-separator', className)}
+        role='separator'
+        data-orientation={orientation}
+        data-status={status}
+        __css={css}
+        {...rest}
+      />
+    )
+  },
+)

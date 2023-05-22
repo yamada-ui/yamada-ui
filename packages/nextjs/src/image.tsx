971dd5a0dc61ff808ccdcbf38c97ec7d6439391b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-22 14:38:31 +0900

    add Image

---
+++ packages/nextjs/src/image.tsx
@@ -0,0 +1,23 @@
+import { ui, UIComponent, HTMLUIProps } from '@yamada-ui/core'
+import NextImage, { ImageProps as NextImageProps } from 'next/image'
+
+export type ImageProps = Omit<HTMLUIProps<'img'>, keyof NextImageProps> & NextImageProps
+
+export const Image: UIComponent<'img', ImageProps> = ui(NextImage, {
+  shouldForwardProp: (prop) =>
+    [
+      'src',
+      'alt',
+      'width',
+      'height',
+      'fill',
+      'loader',
+      'quality',
+      'priority',
+      'loading',
+      'placeholder',
+      'blurDataURL',
+      'unoptimized',
+      'onLoadingComplete',
+    ].includes(prop),
+})

b6920b1f4abfc8ce6aa61f51bbffb5eb8c7f0c01
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-25 11:48:56 +0900

    package.json include clean-package, tsup config

--- packages/components/focus-lock/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  clean: true,
-  format: ['cjs', 'esm'],
-  outExtension(ctx) {
-    return { js: `.${ctx.format}.js` }
-  },
-})

2bdf54e5e97dd86768cb02bc8a757dbeabebd2c4
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-26 12:35:03 +0900

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: ['packages/**/*.{ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  modulePathIgnorePatterns: ['<rootDir>/examples'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)?$': ['@swc-node/jest', { module: 'commonjs' }],
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './scripts/setup-test.ts'],
+  globals: {},
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+}
+
+export default config

82cb8b8e2911307829c21803ab181598467843c2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-26 14:08:49 +0900

    remove @testing-library/react-hooks

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
 export { act, fireEvent, screen, waitFor, renderHook } from '@testing-library/react'
-export * as ssr from '@testing-library/react-hooks/server'
 export * from './render'
 export * from './accessibility'
 export * from './focus'

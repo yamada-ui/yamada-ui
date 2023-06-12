245cd6d6ebd3c08908eee6147fd6210a344ff68c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-12 17:48:22 +0900

    feat: add useHover

---
+++ packages/hooks/use-hover/src/index.ts
@@ -0,0 +1,25 @@
+import { useRef, useEffect, useCallback, useState } from 'react'
+
+export const useHover = <T extends HTMLElement = HTMLDivElement>() => {
+  const [hovered, setHovered] = useState(false)
+  const ref = useRef<T>(null)
+
+  const onMouseEnter = useCallback(() => setHovered(true), [])
+  const onMouseLeave = useCallback(() => setHovered(false), [])
+
+  useEffect(() => {
+    const el = ref.current
+
+    if (el) {
+      el.addEventListener('mouseenter', onMouseEnter)
+      el.addEventListener('mouseleave', onMouseLeave)
+
+      return () => {
+        el.removeEventListener('mouseenter', onMouseEnter)
+        el.removeEventListener('mouseleave', onMouseLeave)
+      }
+    }
+  }, [onMouseEnter, onMouseLeave])
+
+  return { ref, hovered }
+}

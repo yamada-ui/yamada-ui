55276fdcc66a47b0881e267ecff19d20fa81e347
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-12 18:48:12 +0900

    feat: add useLocalStorage

---
+++ packages/hooks/use-local-storage/README.md
@@ -0,0 +1,30 @@
+# @yamada-ui/use-local-storage
+
+> This is an internal utility, not intended for public usage.
+
+## Installation
+
+```sh
+$ pnpm add @yamada-ui/use-local-storage
+```
+
+or
+
+```sh
+$ yarn add @yamada-ui/use-local-storage
+```
+
+or
+
+```sh
+$ npm install @yamada-ui/use-local-storage
+```
+
+## Contribution
+
+Wouldn't you like to contribute? That's amazing! We have prepared a [contribution guide](./CONTRIBUTING.md) to assist you.
+
+## Licence
+
+This package is licensed under the terms of the
+[MIT license](https://github.com/hirotomoyamada/yamada-ui/blob/main/LICENSE).
---
+++ packages/hooks/use-local-storage/src/index.ts
@@ -0,0 +1,114 @@
+import { useWindowEvent } from '@yamada-ui/use-window-event'
+import { isFunction } from '@yamada-ui/utils'
+import { useState, useCallback, useEffect } from 'react'
+
+export type StorageType = 'localStorage' | 'sessionStorage'
+
+export type StorageProps<T> = {
+  key: string
+  defaultValue?: T
+  getInitialValueInEffect?: boolean
+  serialize?: (value: T) => string
+  deserialize?: (value: string | undefined) => T
+}
+
+const serializeJSON = <T>(value: T, name: string) => {
+  try {
+    return JSON.stringify(value)
+  } catch (error) {
+    throw new Error(`useLocalStorage ${name}: Failed to serialize the value`)
+  }
+}
+
+const deserializeJSON = (value: string | undefined) => {
+  if (!value) return value
+
+  try {
+    return JSON.parse(value)
+  } catch {
+    return value
+  }
+}
+
+export function createStorage<T>(type: StorageType, name: string) {
+  const eventName = type === 'localStorage' ? 'ui-local-storage' : 'ui-session-storage'
+
+  return ({
+    key,
+    defaultValue = undefined,
+    getInitialValueInEffect = true,
+    deserialize = deserializeJSON,
+    serialize = (value: T) => serializeJSON(value, name),
+  }: StorageProps<T>) => {
+    const readStorageValue = useCallback(
+      (skipStorage?: boolean): T => {
+        if (
+          typeof window === 'undefined' ||
+          !(type in window) ||
+          window[type] === null ||
+          skipStorage
+        ) {
+          return (defaultValue ?? '') as T
+        }
+
+        const storageValue = window[type].getItem(key)
+
+        return storageValue !== null ? deserialize(storageValue) : ((defaultValue ?? '') as T)
+      },
+      [key, deserialize, defaultValue],
+    )
+
+    const [value, setValue] = useState<T>(readStorageValue(getInitialValueInEffect))
+
+    const setStorageValue = useCallback(
+      (valOrFunc: T | ((prevState: T) => T)) => {
+        if (isFunction(valOrFunc)) {
+          setValue((current) => {
+            const result = valOrFunc(current)
+
+            window[type].setItem(key, serialize(result))
+            window.dispatchEvent(
+              new CustomEvent(eventName, { detail: { key, value: valOrFunc(current) } }),
+            )
+
+            return result
+          })
+        } else {
+          window[type].setItem(key, serialize(valOrFunc))
+          window.dispatchEvent(new CustomEvent(eventName, { detail: { key, value: valOrFunc } }))
+
+          setValue(valOrFunc)
+        }
+      },
+      [key, serialize],
+    )
+
+    const removeStorageValue = useCallback(() => window[type].removeItem(key), [key])
+
+    useWindowEvent('storage', (event) => {
+      if (event.storageArea === window[type] && event.key === key)
+        setValue(deserialize(event.newValue ?? undefined))
+    })
+
+    useWindowEvent(eventName, (event) => {
+      if (event.detail.key === key) setValue(event.detail.value)
+    })
+
+    useEffect(() => {
+      if (defaultValue !== undefined && value === undefined) setStorageValue(defaultValue)
+    }, [defaultValue, value, setStorageValue])
+
+    useEffect(() => {
+      if (getInitialValueInEffect) setValue(readStorageValue())
+    }, [getInitialValueInEffect, readStorageValue])
+
+    return [
+      value === undefined ? defaultValue : value,
+      setStorageValue,
+      removeStorageValue,
+    ] as const
+  }
+}
+
+export const useLocalStorage = <T = string>(props: StorageProps<T>) =>
+  createStorage<T>('localStorage', 'use-local-storage')(props)

9dfaed476bf642a62de0d30f5a3f24999858099f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-13 03:03:40 +0900

    refactor: mac to macos

---
+++ .changeset/blue-dancers-reply.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/use-os': patch
+---
+
+Refactored the code.
--- packages/hooks/use-os/src/index.ts
+++ packages/hooks/use-os/src/index.ts
@@ -3,13 +3,13 @@ export type OS = 'undetermined' | 'macos' | 'ios' | 'windows' | 'android' | 'lin
 const getOS = (): OS => {
   const { userAgent } = window.navigator
 
-  const mac = /(Macintosh)|(MacIntel)|(MacPPC)|(Mac68K)/i
+  const macos = /(Macintosh)|(MacIntel)|(MacPPC)|(Mac68K)/i
   const windows = /(Win32)|(Win64)|(Windows)|(WinCE)/i
   const ios = /(iPhone)|(iPad)|(iPod)/i
   const android = /Android/i
   const linux = /Linux/i
 
-  if (mac.test(userAgent)) return 'macos'
+  if (macos.test(userAgent)) return 'macos'
   if (ios.test(userAgent)) return 'ios'
   if (windows.test(userAgent)) return 'windows'
   if (android.test(userAgent)) return 'android'

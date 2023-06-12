0b207040e6e309c60b99134c0d8bbfa05cbfc1d2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-12 18:08:40 +0900

    feat: add useOS

---
+++ packages/hooks/use-os/src/index.ts
@@ -0,0 +1,25 @@
+export type OS = 'undetermined' | 'macos' | 'ios' | 'windows' | 'android' | 'linux'
+
+const getOS = (): OS => {
+  const { userAgent } = window.navigator
+
+  const mac = /(Macintosh)|(MacIntel)|(MacPPC)|(Mac68K)/i
+  const windows = /(Win32)|(Win64)|(Windows)|(WinCE)/i
+  const ios = /(iPhone)|(iPad)|(iPod)/i
+  const android = /Android/i
+  const linux = /Linux/i
+
+  if (mac.test(userAgent)) return 'macos'
+  if (ios.test(userAgent)) return 'ios'
+  if (windows.test(userAgent)) return 'windows'
+  if (android.test(userAgent)) return 'android'
+  if (linux.test(userAgent)) return 'linux'
+
+  return 'undetermined'
+}
+
+export const useOS = (): OS => {
+  if (typeof window !== 'undefined') return getOS()
+
+  return 'undetermined'
+}

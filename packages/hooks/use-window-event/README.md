8fb8ef65d6865e509efe4625309f0d0d7e3084db
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-12 18:40:43 +0900

    feat: add useWindowEvent

---
+++ packages/hooks/use-window-event/README.md
@@ -0,0 +1,30 @@
+# @yamada-ui/use-window-event
+
+> This is an internal utility, not intended for public usage.
+
+## Installation
+
+```sh
+$ pnpm add @yamada-ui/use-window-event
+```
+
+or
+
+```sh
+$ yarn add @yamada-ui/use-window-event
+```
+
+or
+
+```sh
+$ npm install @yamada-ui/use-window-event
+```
+
+## Contribution
+
+Wouldn't you like to contribute? That's amazing! We have prepared a [contribution guide](./CONTRIBUTING.md) to assist you.
+
+## Licence
+
+This package is licensed under the terms of the
+[MIT license](https://github.com/hirotomoyamada/yamada-ui/blob/main/LICENSE).
---
+++ packages/hooks/use-window-event/src/index.ts
@@ -0,0 +1,23 @@
+import { useCallbackRef } from '@yamada-ui/utils'
+import { useEffect } from 'react'
+
+type Handler<E extends string> = E extends keyof WindowEventMap
+  ? (ev: WindowEventMap[E]) => void
+  : (ev: CustomEvent) => void
+type Options = boolean | AddEventListenerOptions
+
+export const useWindowEvent = <E extends string>(
+  event: E,
+  handler: Handler<E>,
+  options?: Options,
+) => {
+  const listener = useCallbackRef(handler) as unknown as EventListenerOrEventListenerObject
+
+  useEffect(() => {
+    window.addEventListener(event, listener, options)
+
+    return () => {
+      window.removeEventListener(event, listener, options)
+    }
+  }, [event, listener, options])
+}

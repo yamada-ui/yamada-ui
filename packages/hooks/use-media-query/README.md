75946ef45c717c2e98e7e1e3b2949c72a5bd76fc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-13 03:04:13 +0900

    feat: add useMediaQuery

---
+++ packages/hooks/use-media-query/README.md
@@ -0,0 +1,30 @@
+# @yamada-ui/use-media-query
+
+> This is an internal utility, not intended for public usage.
+
+## Installation
+
+```sh
+$ pnpm add @yamada-ui/use-media-query
+```
+
+or
+
+```sh
+$ yarn add @yamada-ui/use-media-query
+```
+
+or
+
+```sh
+$ npm install @yamada-ui/use-media-query
+```
+
+## Contribution
+
+Wouldn't you like to contribute? That's amazing! We have prepared a [contribution guide](./CONTRIBUTING.md) to assist you.
+
+## Licence
+
+This package is licensed under the terms of the
+[MIT license](https://github.com/hirotomoyamada/yamada-ui/blob/main/LICENSE).
---
+++ packages/hooks/use-media-query/src/index.ts
@@ -0,0 +1,72 @@
+import { useEnvironment } from '@yamada-ui/providers'
+import { isArray, isFunction } from '@yamada-ui/utils'
+import { useEffect, useState } from 'react'
+
+export type UseMediaQueryOptions = {
+  fallback?: boolean | boolean[]
+  ssr?: boolean
+}
+
+export const useMediaQuery = (
+  query: string | string[],
+  { ssr = true, fallback }: UseMediaQueryOptions = {},
+): boolean[] => {
+  const { getWindow } = useEnvironment()
+
+  const queries = isArray(query) ? query : [query]
+
+  let fallbackValues = isArray(fallback) ? fallback : [fallback]
+
+  fallbackValues = fallbackValues.filter((value) => value != null) as boolean[]
+
+  const [value, setValue] = useState(() => {
+    return queries.map((media, index) => ({
+      media,
+      matches: ssr ? !!fallbackValues[index] : getWindow().matchMedia(media).matches,
+    }))
+  })
+
+  useEffect(() => {
+    const win = getWindow()
+
+    setValue(
+      queries.map((media) => ({
+        media,
+        matches: win.matchMedia(media).matches,
+      })),
+    )
+
+    const mql = queries.map((query) => win.matchMedia(query))
+
+    const handler = (evt: MediaQueryListEvent) => {
+      setValue((prev) =>
+        prev.slice().map((item) => {
+          if (item.media === evt.media) return { ...item, matches: evt.matches }
+
+          return item
+        }),
+      )
+    }
+
+    mql.forEach((mql) => {
+      if (isFunction(mql.addListener)) {
+        mql.addListener(handler)
+      } else {
+        mql.addEventListener('change', handler)
+      }
+    })
+
+    return () => {
+      mql.forEach((mql) => {
+        if (isFunction(mql.removeListener)) {
+          mql.removeListener(handler)
+        } else {
+          mql.removeEventListener('change', handler)
+        }
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getWindow])
+
+  return value.map((item) => item.matches)
+}

7a9b238d83bca30fae4b879a4ca2c75d9364e0b2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-06-13 04:04:44 +0900

    feat: add useClipboard

---
+++ packages/hooks/use-clipboard/src/index.ts
@@ -0,0 +1,58 @@
+import { isNumber, isString } from '@yamada-ui/utils'
+import copy from 'copy-to-clipboard'
+import { useState, useCallback, useEffect } from 'react'
+
+export type UseClipboardOptions = {
+  timeout?: number
+  format?: string
+}
+
+export const useClipboard = (
+  defaultValue: string = '',
+  timeoutOrOptions: number | UseClipboardOptions = {},
+) => {
+  const [hasCopied, setHasCopied] = useState(false)
+
+  const [value, setValue] = useState(defaultValue)
+
+  useEffect(() => setValue(defaultValue), [defaultValue])
+
+  const { timeout = 1500, ...copyOptions } = isNumber(timeoutOrOptions)
+    ? { timeout: timeoutOrOptions }
+    : timeoutOrOptions
+
+  const onCopy = useCallback(
+    (newValue?: any) => {
+      if (!isString(newValue)) {
+        newValue = value
+      } else {
+        setValue(newValue)
+      }
+
+      const hasCopied = copy(newValue, copyOptions)
+
+      setHasCopied(hasCopied)
+    },
+    [value, copyOptions],
+  )
+
+  useEffect(() => {
+    let timeoutId: number | null = null
+
+    if (hasCopied)
+      timeoutId = window.setTimeout(() => {
+        setHasCopied(false)
+      }, timeout)
+
+    return () => {
+      if (timeoutId) window.clearTimeout(timeoutId)
+    }
+  }, [timeout, hasCopied])
+
+  return {
+    value,
+    setValue,
+    onCopy,
+    hasCopied,
+  }
+}
---
+++ stories/hooks/use-clipboard.stories.tsx
@@ -0,0 +1,70 @@
+import {
+  Button,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  HStack,
+  Input,
+  useClipboard,
+} from '@yamada-ui/react'
+
+export default {
+  title: 'Hooks / useClipboard',
+}
+
+export const basic = () => {
+  const { onCopy, value, setValue, hasCopied } = useClipboard()
+
+  return (
+    <>
+      <HStack gap='md'>
+        <Input
+          placeholder='text to be copied...'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={onCopy}>{hasCopied ? 'Copied!' : 'Copy'}</Button>
+      </HStack>
+
+      <Editable placeholder='Paste here'>
+        <EditablePreview width='full' />
+        <EditableInput />
+      </Editable>
+    </>
+  )
+}
+
+export const withTimeout = () => {
+  const { onCopy, value, setValue, hasCopied } = useClipboard('', 5000)
+
+  return (
+    <>
+      <HStack gap='md'>
+        <Input
+          placeholder='text to be copied...'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button onClick={onCopy}>{hasCopied ? 'Copied!' : 'Copy'}</Button>
+      </HStack>
+
+      <Editable placeholder='Paste here'>
+        <EditablePreview width='full' />
+        <EditableInput />
+      </Editable>
+    </>
+  )
+}
+
+export const directCopy = () => {
+  const { onCopy, hasCopied } = useClipboard()
+
+  const value = '孫悟空'
+
+  return (
+    <HStack gap='md'>
+      <Input value={value} isReadOnly />
+      <Button onClick={() => onCopy(value)}>{hasCopied ? 'Copied!' : 'Copy'}</Button>
+    </HStack>
+  )
+}

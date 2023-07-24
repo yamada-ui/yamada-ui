992342ac25e330807c6142bbaa88309226e7f7f2
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-24 12:07:49 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@9ed639a8ced27dab699d14e185aaf62c0df601db 🚀

--- assets/WithTooltip-I4CCNYWD-9e625156.js
+++
@@ -1,2 +0,0 @@
-import{W as l,W as s,a as f}from"./index-bca613e0.js";import"./iframe-07fed788.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-51e1fbf6.js";import"./index-a01a9712.js";import"./inheritsLoose-c82a83d4.js";import"./index-c8f542d6.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,f as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-I4CCNYWD-9e625156.js.map
---
+++ assets/notice.stories-afbba911.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-276775ef.js";import{e as n}from"./extend-config-fa1bfdf9.js";import{U as p}from"./ui-provider-b8ae2fed.js";import{u as c}from"./notice-ba0e8aa8.js";import{C as s}from"./center-f24092aa.js";import{B as a}from"./button-aae638aa.js";import"./index-1cdf6ce0.js";import"./index-1d8ff3ed.js";import"./factory-75cc801c.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-51e1fbf6.js";import"./environment-provider-f0cd95a3.js";import"./loading-provider-d366d231.js";import"./index-7320e9bd.js";import"./Combination-05e90cab.js";import"./tslib.es6-90f6c3e8.js";import"./motion-53506941.js";import"./motion-1ef152f4.js";import"./forward-ref-86842115.js";import"./loading-b1bbd31e.js";import"./index-71f4e961.js";import"./theme-8f32df8a.js";import"./index-7a67cca8.js";import"./index-cb49aa84.js";import"./icon-e7971e5d.js";import"./index-f3237c2b.js";import"./container-portal-1f9dc5e6.js";import"./index-a01a9712.js";import"./alert-185d7d3c.js";import"./use-component-style-9ee8741d.js";import"./close-button-a6b70db7.js";const J={title:"System / Theme / Notice"},o=()=>{const r=n({notice:{options:{placement:"bottom-right",limit:3}}});return t(p,{config:r,children:t(d,{})})},d=()=>{const r=c();return t(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:t(a,{onClick:()=>r({title:"孫悟空",description:"オッス！オラ悟空！"}),children:"Show Notice"})})};var i,e,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const config = extendConfig({
+    notice: {
+      options: {
+        placement: 'bottom-right',
+        limit: 3
+      }
+    }
+  });
+  return <UIProvider config={config}>
+      <App />
+    </UIProvider>;
+}`,...(m=(e=o.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const K=["basic"];export{K as __namedExportsOrder,o as basic,J as default};
+//# sourceMappingURL=notice.stories-afbba911.js.map

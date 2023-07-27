0c53300712160b1b4923eb5d211a6be5c6d99ab3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-27 11:58:43 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@82b48ebf1661b807382d8d7d033694f9c575b78f 🚀

---
+++ assets/configure.stories-5390e276.js
@@ -0,0 +1,20 @@
+import{j as o}from"./jsx-runtime-276775ef.js";import{e as g}from"./extend-config-1d57ae2c.js";import{U as a}from"./ui-provider-ca506ecd.js";import{C as d}from"./center-f24092aa.js";import{I as u}from"./image-152f5a48.js";import"./index-1cdf6ce0.js";import"./index-412d5463.js";import"./factory-75cc801c.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-51e1fbf6.js";import"./environment-provider-f0cd95a3.js";import"./loading-provider-8657b718.js";import"./index-7320e9bd.js";import"./Combination-05e90cab.js";import"./tslib.es6-90f6c3e8.js";import"./motion-53506941.js";import"./motion-1ef152f4.js";import"./forward-ref-86842115.js";import"./loading-b1bbd31e.js";import"./index-71f4e961.js";import"./theme-8f32df8a.js";import"./index-7a67cca8.js";import"./index-cb49aa84.js";import"./icon-e7971e5d.js";import"./index-f3237c2b.js";import"./container-portal-1f9dc5e6.js";import"./index-a01a9712.js";import"./notice-ba0e8aa8.js";import"./alert-185d7d3c.js";import"./use-component-style-9ee8741d.js";import"./close-button-a6b70db7.js";import"./use-image-21444f3a.js";const N={title:"System / Theme / Configure"},r=()=>{const e=g({});return o(a,{config:e,children:o(f,{})})},t=()=>o(a,{config:{},children:o(f,{})}),f=()=>o(d,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:o(u,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png"})});var i,n,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const config = extendConfig({
+    /**
+     * Define the config you want to customize, merging them with the default config
+     */
+  });
+  return <UIProvider config={config}>
+      <App />
+    </UIProvider>;
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var p,c,s;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const config = {
+    /**
+     * Define the config you want to customize
+     */
+  };
+  return <UIProvider config={config}>
+      <App />
+    </UIProvider>;
+}`,...(s=(c=t.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};const Q=["extendConfigure","overrideConfigure"];export{Q as __namedExportsOrder,N as default,r as extendConfigure,t as overrideConfigure};
+//# sourceMappingURL=configure.stories-5390e276.js.map
---
+++ assets/syntaxhighlighter-SIPZPD4I-043750bb.js
@@ -0,0 +1,2 @@
+import{S as n,c as d,s as f}from"./index-3cb74236.js";import"./iframe-f728eff1.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-51e1fbf6.js";import"./index-a01a9712.js";import"./inheritsLoose-c82a83d4.js";import"./index-c8f542d6.js";import"./index-356e4a49.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
+//# sourceMappingURL=syntaxhighlighter-SIPZPD4I-043750bb.js.map

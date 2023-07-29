ffe40bfe90771332cd571c30750833beaa5adcd3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-29 12:41:55 +0900

    feat: added useEventListener

---
+++ docs/hooks/use-event-listener.ts
@@ -0,0 +1,31 @@
+import { StringLiteral, runIfFunc, useCallbackRef } from '@yamada-ui/react'
+import { useEffect } from 'react'
+
+type DocumentOrElement = Document | HTMLElement | null
+
+export const useEventListener = <K extends keyof DocumentEventMap>(
+  event: K | StringLiteral,
+  handler?: (event: DocumentEventMap[K]) => void,
+  env?: (() => DocumentOrElement) | DocumentOrElement,
+  options?: boolean | AddEventListenerOptions,
+) => {
+  const listener = useCallbackRef(handler)
+
+  useEffect(() => {
+    const node = runIfFunc(env) ?? document
+
+    if (!handler) return
+
+    node.addEventListener(event, listener, options)
+
+    return () => {
+      node.removeEventListener(event, listener, options)
+    }
+  }, [event, env, options, listener, handler])
+
+  return () => {
+    const node = runIfFunc(env) ?? document
+
+    node.removeEventListener(event, listener, options)
+  }
+}

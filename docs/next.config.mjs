0dc9006fd665bdb682752938700c6fc27c7c1905
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-31 11:56:12 +0900

    refactor: convert next.config to es modules

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,4 @@
-const { withContentlayer } = require('next-contentlayer')
+import { withContentlayer } from 'next-contentlayer'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,4 +14,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withContentlayer(nextConfig)
+export default withContentlayer(nextConfig)

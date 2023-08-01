0ee8a76dd45af6cb4eb9d7af8c88ed94c615cf89
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-01 19:49:44 +0900

    refactor(contentlayer): simplify and optimize document tree generation

--- docs/utils/contentlayer.ts
+++ docs/utils/contentlayer.ts
@@ -1,57 +1,21 @@
-import { otherLocales } from './i18n'
-import { Data, DocWithChildren, Doc, allDocs } from 'contentlayer/generated'
+import { Data, DocWithChildren, Doc } from 'contentlayer/generated'
 
-const OTHER_LOCALES = `(${otherLocales.join('|')})`
+export const getTree = (docs: Doc[], parentPaths: string[] = []): DocWithChildren[] => {
+  const lv = parentPaths.length
 
-export const omitLocaleSlug = (path: string): string => {
-  const reg = new RegExp(`\(/index\)?.${OTHER_LOCALES}$`)
+  return docs
+    .filter(({ is_active, data }) => {
+      if (!is_active) return false
 
-  path = path.replace(reg, '')
+      let { paths } = data as Data
 
-  return path
-}
+      paths = paths.filter(Boolean)
 
-export const getLocale = (path: string): string => {
-  let locale = path.match(/(\.[^\.]*)$/)?.[1]
-
-  locale = locale?.replace(/\./, '')
-
-  return locale ?? 'en'
-}
-
-export const getTree =
-  (docs: Doc[], parentPaths: string[] = []) =>
-  (currentLocale: string): DocWithChildren[] => {
-    const lv = parentPaths.length
-
-    return docs
-      .filter(({ data }) => {
-        let { locale, paths } = data as Data
-
-        if (locale !== currentLocale) return false
-
-        paths = paths.filter(Boolean)
-
-        return paths.length === lv + 1 && paths.join('/').startsWith(parentPaths.join('/'))
-      })
-      .map((doc) => ({
-        ...doc,
-        children: getTree(docs, doc.data.paths)(currentLocale),
-      }))
-  }
-
-export const getPagination = (path: string) => {
-  let pagination: { prev?: Doc; next?: Doc } = {}
-
-  const c = allDocs.length
-
-  for (let i = 0; i < c; i++) {
-    const { slug } = allDocs[i]
-
-    if (slug !== path) continue
-
-    pagination = { prev: allDocs[i - 1], next: allDocs[i + 1] }
-  }
-
-  return pagination
+      return paths.length === lv + 1 && paths.join('/').startsWith(parentPaths.join('/'))
+    })
+    .sort((a, b) => a.order - b.order)
+    .map((doc) => ({
+      ...doc,
+      children: getTree(docs, doc.data.paths),
+    }))
 }

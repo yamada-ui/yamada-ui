703811a7ab2c326033ca5b29a51fd0d3aacd1270
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-01 19:47:36 +0900

    feat(utils): add flatten array function in array utils

--- docs/utils/array.ts
+++ docs/utils/array.ts
@@ -1,4 +1,18 @@
-export type MixedArray = string | Array<string | string[]>
+import { isArray } from '@yamada-ui/react'
+
+export type MixedArray = string | (string | string)[]
 
 export const toArray = (mixedArray: MixedArray) =>
   (Array.isArray(mixedArray) ? mixedArray.flat() : [mixedArray]).filter(Boolean)
+
+export const flattenArray = <T extends any = any>(array: T[], target: keyof T) => {
+  let result = []
+
+  array.forEach((item) => {
+    result = [...result, item]
+
+    if (isArray(item[target])) result = [...result, ...flattenArray(item[target], target)]
+  })
+
+  return result
+}

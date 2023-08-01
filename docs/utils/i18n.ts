7fbbd7ea5c963846eb02b12a5267e2a68d881258
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-01 19:49:02 +0900

    refactor(utils): update otherlocales to map before filtering

--- docs/utils/i18n.ts
+++ docs/utils/i18n.ts
@@ -1,5 +1,5 @@
 import { CONSTANT } from '../constant'
 
-export const otherLocales = CONSTANT.I18N.LOCALES.filter(
+export const otherLocales = CONSTANT.I18N.LOCALES.map(({ value }) => value).filter(
   (locale) => locale !== CONSTANT.I18N.DEFAULT_LOCALE,
 )

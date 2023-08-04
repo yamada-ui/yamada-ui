c8c21d88893a7a540a4c50524eb7ef9441602e25
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-04 16:20:10 +0900

    feat: added mdx styles

--- docs/theme/index.ts
+++ docs/theme/index.ts
@@ -1,11 +1,11 @@
 import { extendConfig, extendTheme } from '@yamada-ui/react'
 import { components } from './components'
 import { customConfig } from './config'
-import { globalStyle, resetStyle, layerStyles, textStyles } from './styles'
+import { globalStyle, resetStyle, layerStyles, textStyles, mdx } from './styles'
 import { tokens } from './tokens'
 
 export const customTheme = {
-  styles: { globalStyle, resetStyle, layerStyles, textStyles },
+  styles: { globalStyle, resetStyle, layerStyles, textStyles, mdx },
   components,
   ...tokens,
   semantics: {
---
+++ docs/theme/styles/mdx-styles.ts
@@ -0,0 +1,53 @@
+import { CSSUIObject } from '@yamada-ui/react'
+
+export const mdx: Record<string, CSSUIObject> = {
+  h1: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: '4xl' },
+  },
+  h2: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: '2xl' },
+  },
+  h3: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: 'xl' },
+  },
+  h4: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: 'md' },
+  },
+  h5: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: 'sm' },
+  },
+  h6: {
+    mt: '6',
+    mb: '4',
+    fontSize: { base: 'xs' },
+  },
+  p: {
+    my: '4',
+    'blockquote &': {
+      mt: '0',
+    },
+  },
+  strong: {
+    fontWeight: 'semibold',
+  },
+  code: {
+    color: ['red.500', 'red.300'],
+  },
+  a: {
+    color: 'link',
+    textDecoration: 'underline',
+  },
+  hr: {
+    my: '6',
+  },
+}

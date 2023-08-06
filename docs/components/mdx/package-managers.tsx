9b781b3d932ef40d2ab1f35db1a822781b7e85f7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-06 22:12:56 +0900

    refactor(package-managers): simplify package commands handling

--- docs/components/mdx/package-managers.tsx
+++ docs/components/mdx/package-managers.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs } from '@yamada-ui/react'
+import { Box, Tab, Tabs, isArray } from '@yamada-ui/react'
 import { Component } from 'mdx/types'
 import { themes } from 'prism-react-renderer'
 import { useState } from 'react'
@@ -6,50 +6,55 @@ import { Highlight } from './code-block'
 import { CopyButton } from 'components/forms'
 import { useConfigs } from 'contexts/configs-context'
 
-type PackageManagerName = 'npm' | 'yarn' | 'pnpm'
+const PACKAGE_COMMANDS = {
+  pnpm: 'pnpm add',
+  npm: 'npm installl',
+  yarn: 'yarn add',
+}
 
-export type PackageManagersProps = Partial<Record<PackageManagerName, string>>
+export type PackageManagersProps = { packageName: string | string[] }
 
-export const PackageManagers: Component<PackageManagersProps> = (props) => {
+export const PackageManagers: Component<PackageManagersProps> = ({ packageName }) => {
   const { colorScheme } = useConfigs()
-  const [name, setName] = useState<string>(Object.keys(props)[0] ?? 'pnpm')
+  const [selectedPackageName, setSelectedPackageName] = useState<string>('pnpm')
   const language = 'bash'
-  const code = props[name]
+  const code =
+    PACKAGE_COMMANDS[selectedPackageName] +
+    ' ' +
+    (isArray(packageName) ? packageName.join(' ') : packageName)
   const theme = themes.nightOwl
 
   return (
-    <>
-      <Box position='relative'>
-        <Box
-          rounded='md'
-          my='6'
-          bg={['zinc.800', 'zinc.900']}
-          sx={{ '& > div:last-child': { py: 'normal' } }}
-          overflow='hidden'
-        >
-          <Tabs bg={['whiteAlpha.200', 'whiteAlpha.100']}>
-            {Object.keys(props).map((name) => (
-              <Tab
-                key={name}
-                fontSize='xs'
-                color={['whiteAlpha.600', 'whiteAlpha.500']}
-                _selected={{
-                  color: ['white', 'white'],
-                  borderColor: [`${colorScheme}.400`, `${colorScheme}.300`],
-                }}
-                onClick={() => setName(name)}
-                _focusVisible={{ bg: 'whiteAlpha.50' }}
-              >
-                {name}
-              </Tab>
-            ))}
-          </Tabs>
+    <Box position='relative'>
+      <Box
+        rounded='md'
+        my='6'
+        bg={['zinc.800', 'zinc.900']}
+        sx={{ '& > div:last-child': { py: 'normal' } }}
+        overflow='hidden'
+      >
+        <Tabs bg={['whiteAlpha.200', 'whiteAlpha.100']}>
+          {Object.keys(PACKAGE_COMMANDS).map((name) => (
+            <Tab
+              key={name}
+              fontSize='xs'
+              color={['whiteAlpha.600', 'whiteAlpha.500']}
+              _selected={{
+                color: ['white', 'white'],
+                borderColor: [`${colorScheme}.400`, `${colorScheme}.300`],
+              }}
+              onClick={() => setSelectedPackageName(name)}
+              _focusVisible={{ bg: 'whiteAlpha.50' }}
+            >
+              {name}
+            </Tab>
+          ))}
+        </Tabs>
 
-          <Highlight {...{ code, language, theme }} />
-        </Box>
-
-        <CopyButton value={props[name]} position='absolute' top={'3.3rem'} right='4' />
+        <Highlight {...{ code, language, theme }} />
       </Box>
-    </>
+
+      <CopyButton value={code} position='absolute' top={'3.3rem'} right='4' />
+    </Box>
   )
 }

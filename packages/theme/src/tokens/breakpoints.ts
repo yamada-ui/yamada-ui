2fe1ff62c35ce3e9a986d37629d03a4b3e8bc88b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-09 09:18:28 +0900

    refactor(theme): adjust breakpoint values

--- packages/theme/src/tokens/breakpoints.ts
+++ packages/theme/src/tokens/breakpoints.ts
@@ -3,7 +3,7 @@ export type Breakpoints = typeof breakpoints
 export const breakpoints = {
   sm: '30em',
   md: '48em',
-  lg: '62em',
+  lg: '61em',
   xl: '80em',
-  '2xl': '96em',
+  '2xl': '90em',
 }

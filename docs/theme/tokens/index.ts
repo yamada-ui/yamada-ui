e4dbd570f4e27cfad2da29165cd9155bf77cc408
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-13 10:23:38 +0900

    feat(theme/tokens): add shadows to theme tokens

--- docs/theme/tokens/index.ts
+++ docs/theme/tokens/index.ts
@@ -1,4 +1,5 @@
 import { colors } from './colors'
 import { gradients } from './gradients'
+import { shadows } from './shadows'
 
-export const tokens = { colors, gradients }
+export const tokens = { colors, gradients, shadows }
---
+++ docs/theme/tokens/shadows.ts
@@ -0,0 +1,5 @@
+import { ThemeTokens } from '@yamada-ui/react'
+
+export const shadows: ThemeTokens = {
+  'inner-outline': 'inset 0 0 0 3px rgba(66, 153, 225, 0.6)',
+}

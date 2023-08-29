7b0bed2044736fe545b9a222af6788ca7005989e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-08-29 13:02:10 +0900

    fix(core): save themescheme in local storage or cookies

---
+++ .changeset/sharp-jars-double.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/core': patch
+---
+
+Fixed a bug that themeScheme was not saved in local storage or cookies.
---
+++ packages/core/src/providers/theme-script.tsx
@@ -0,0 +1,41 @@
+import { ThemeScheme } from '..'
+
+type ThemeSchemeScriptProps = {
+  type?: 'localStorage' | 'cookie'
+  initialThemeScheme?: ThemeScheme
+  storageKey?: string
+  nonce?: string
+}
+
+export const THEME_SCHEME_STORAGE_KEY = 'ui-theme-scheme'
+
+export const getThemeSchemeScript = ({
+  initialThemeScheme: init = 'light',
+  type = 'localStorage',
+  storageKey = THEME_SCHEME_STORAGE_KEY,
+}: Omit<ThemeSchemeScriptProps, 'nonce'>) => {
+  const isCookie = type === 'cookie'
+
+  const cookieScript = `(function(){try{var a=function(c){var o=document.documentElement;return o.dataset.mode=c},h='${init}',r='${storageKey}',t=document.cookie.match(new RegExp('(^| )'.concat(r,'=([^;]+)'))),c=t?t[2]:null;c?a(c):(document.cookie=''.concat(r,'=').concat(h,'; max-age=31536000; path=/'))}catch(a){}})()`
+
+  const localStorageScript = `(function(){try{var a=function(c){var o=document.documentElement;return o.dataset.mode=c},m='${init}',e='${storageKey}',t=localStorage.getItem(e);t?a(t):localStorage.setItem(e, m)}catch(a){}})()`
+
+  const fn = isCookie ? cookieScript : localStorageScript
+
+  return `!${fn}`.trim()
+}
+
+export const ThemeSchemeScript = ({
+  nonce,
+  ...rest
+}: ThemeSchemeScriptProps) => {
+  const html = getThemeSchemeScript(rest)
+
+  return (
+    <script
+      id='ui-theme-script'
+      nonce={nonce}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />
+  )
+}

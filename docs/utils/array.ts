31f559745abaaa870c59d2220fef341640c3f466
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-09-18 00:16:18 +0900

    refactor(utils/array): rename variable for clarity

--- docs/utils/array.ts
+++ docs/utils/array.ts
@@ -5,13 +5,15 @@ export type MixedArray = string | (string | string)[]
 export const toArray = (mixedArray: MixedArray) =>
   (Array.isArray(mixedArray) ? mixedArray.flat() : [mixedArray]).filter(Boolean)
 
-export const flattenArray = <T extends any = any>(array: T[], target: keyof T) => {
+export const flattenArray = <T extends any = any>(array: T[], key: keyof T) => {
   let result = []
 
   array.forEach((item) => {
     result = [...result, item]
 
-    if (isArray(item[target])) result = [...result, ...flattenArray(item[target], target)]
+    const targetItem = item[key]
+
+    if (isArray(targetItem)) result = [...result, ...flattenArray(targetItem, key)]
   })
 
   return result

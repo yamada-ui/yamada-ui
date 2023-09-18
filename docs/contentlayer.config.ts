1b091db5f0af5520d5459d18f2b8197b8526ee40
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-09-18 19:22:25 +0900

    refactor(contentlayer.config): update plugins and add remarkuicomponent

--- docs/contentlayer.config.ts
+++ docs/contentlayer.config.ts
@@ -1,5 +1,6 @@
 import { ComputedFields, defineDocumentType, makeSource } from 'contentlayer/source-files'
 import GithubSlugger from 'github-slugger'
+import remarkBreaks from 'remark-breaks'
 import remarkEmoji from 'remark-emoji'
 import remarkGfm from 'remark-gfm'
 import remarkSlug from 'remark-slug'
@@ -27,7 +28,7 @@ const getLocale = (path: string): string => {
   return includes(locales, locale) ? locale : CONSTANT.I18N.DEFAULT_LOCALE
 }
 
-const rehypeMdxCodeMeta: Plugin = () => (tree) => {
+const rehypeCodeMeta: Plugin = () => (tree) => {
   visit(tree, 'element', (node: any) => {
     if (node.tagName !== 'code' || !node.data) return
 
@@ -42,6 +43,101 @@ const rehypeMdxCodeMeta: Plugin = () => (tree) => {
     })
   })
 }
+
+const remarkUIComponent: Plugin = () => (tree) => {
+  visit(tree, 'paragraph', (node: any) => {
+    try {
+      const { name, attributes, children } = getValidChildren(node.children)
+
+      switch (name) {
+        case 'note':
+          insertElement({ name: 'Note', attributes, children })(node)
+
+          break
+
+        default:
+          break
+      }
+    } catch (e) {}
+  })
+}
+
+const getValidChildren = (
+  children: any[],
+): { name: string; attributes: any[]; children: any[] } => {
+  if (!children.length) throw new Error()
+
+  if (children.length === 1) {
+    const { type, value } = children[0]
+
+    if (type !== 'text' || !value) throw new Error()
+
+    const [, name, content] = value.match(/^:::(\w+)\s+([\s\S]*?)\s*:::$/m) ?? []
+
+    if (!name || !content) throw new Error()
+
+    const { attributes, resolvedContent } = getAttributes(content)
+
+    return { name, attributes, children: [{ type: 'text', value: resolvedContent }] }
+  } else {
+    const firstChild = children.at(0)
+    const lastChild = children.at(-1)
+
+    const [, name, firstChildContent] = firstChild.value.match(/^:::(\w+)\s+([\s\S]*?)\s*$/m) ?? []
+    const [, lastChildContent] = lastChild.value.match(/([\s\S]*?)\s*:::$/m) ?? []
+
+    if (!name) throw new Error()
+
+    const { attributes, resolvedContent } = getAttributes(firstChildContent)
+
+    if (resolvedContent) {
+      children[0].value = resolvedContent
+    } else {
+      children.shift()
+    }
+
+    if (lastChildContent) {
+      children[children.length - 1].value = lastChildContent
+    } else {
+      children.pop()
+    }
+
+    return { name, attributes, children }
+  }
+}
+
+const getAttributes = (content: string = ''): { attributes: any[]; resolvedContent: string } => {
+  const reg = /(\w+)=([^\s]+)(?=\s|$)/g
+
+  const attributes = [...content.matchAll(reg)].map(([, name, value]) => ({
+    type: 'mdxJsxAttribute',
+    name,
+    value: value.trim(),
+  }))
+
+  const resolvedContent = content.replace(reg, '').trim()
+
+  return { attributes, resolvedContent }
+}
+
+;[{ type: 'mdxJsxAttribute', name: 'status', value: 'error' }]
+
+const insertElement =
+  ({
+    name,
+    children = [],
+    attributes = [],
+  }: {
+    name: string
+    children: any[]
+    attributes?: any[]
+  }) =>
+  (node: any) => {
+    node.type = 'mdxJsxFlowElement'
+    node.name = name
+    node.attributes = attributes
+    node.children = children
+  }
 
 export const getTableOfContents = (raw: string, maxLv = Infinity) => {
   const slugger = new GithubSlugger()
@@ -118,7 +214,7 @@ export default makeSource({
   contentDirPath: 'contents',
   documentTypes: [Doc],
   mdx: {
-    rehypePlugins: [rehypeMdxCodeMeta],
-    remarkPlugins: [remarkSlug, remarkGfm, remarkEmoji],
+    rehypePlugins: [rehypeCodeMeta],
+    remarkPlugins: [remarkSlug, remarkGfm, remarkEmoji, remarkUIComponent, remarkBreaks],
   },
 })

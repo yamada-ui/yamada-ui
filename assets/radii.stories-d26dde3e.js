ec76f4b4024926bd4e95024aa166ef53bea0eba8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-09-19 00:27:52 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@670e164fe20c50d6f6eb222dded52d75444c5c4b 🚀

---
+++ assets/radii.stories-d26dde3e.js
@@ -0,0 +1,15 @@
+import{M as a}from"./index-eeb979de.js";import{j as t,a as m,F as i}from"./jsx-runtime-29545a09.js";import{u as s}from"./index-a1cf9e47.js";import"./iframe-80130c58.js";import"../sb-preview/runtime.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./index-d3ea75b5.js";import"./index-c4ced888.js";import"./index-356e4a49.js";function o(e){const r=Object.assign({h1:"h1",pre:"pre",code:"code"},s(),e.components);return m(i,{children:[t(a,{title:"Documents / Theme / Radii"}),`
+`,t(r.h1,{id:"radii",children:"Radii"}),`
+`,t(r.pre,{children:t(r.code,{className:"language-ts",children:`const radii = {
+  none: '0',
+  sm: '0.125rem',
+  base: '0.25rem',
+  md: '0.375rem',
+  lg: '0.5rem',
+  xl: '0.75rem',
+  '2xl': '1rem',
+  '3xl': '1.5rem',
+  full: '9999px',
+}
+`})})]})}function c(e={}){const{wrapper:r}=Object.assign({},s(),e.components);return r?t(r,{...e,children:t(o,{...e})}):o(e)}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const n={title:"Documents / Theme / Radii",tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:c};const y=["__page"];export{y as __namedExportsOrder,p as __page,n as default};
+//# sourceMappingURL=radii.stories-d26dde3e.js.map

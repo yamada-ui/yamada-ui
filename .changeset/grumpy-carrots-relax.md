264ab9e1221303a7bdf642fc014b1b21774c4c6e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-01 05:09:15 +0900

    feat(list): add item style to list component

---
+++ .changeset/grumpy-carrots-relax.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/theme': patch
+---
+
+Added item style to list component.
--- packages/theme/src/components/list.ts
+++ packages/theme/src/components/list.ts
@@ -3,6 +3,7 @@ import { ComponentMultiStyle } from '@yamada-ui/core'
 export const List: ComponentMultiStyle = {
   baseStyle: {
     container: {},
+    item: {},
     icon: {
       me: '2',
       display: 'inline-block',

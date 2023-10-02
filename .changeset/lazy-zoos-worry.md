bcdf6ae7a062bdd9a15a57073ecc588603b9573b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-02 20:53:51 +0900

    feat(core): add transformstyle and perspective css properties

---
+++ .changeset/lazy-zoos-worry.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/core': patch
+---
+
+Added `transformStyle` and `perspective` CSS properties.
--- packages/core/src/styles/transform.ts
+++ packages/core/src/styles/transform.ts
@@ -6,6 +6,8 @@ export const transform: Configs = {
   clipPath: true,
   transform: true,
   transformOrigin: true,
+  transformStyle: true,
+  perspective: true,
 }
 
 export type TransformProps<Y = 'responsive', M = 'colorMode'> = {
@@ -21,4 +23,12 @@ export type TransformProps<Y = 'responsive', M = 'colorMode'> = {
    * The CSS `transform-origin` property.
    */
   transformOrigin?: Token<CSS.Property.TransformOrigin | number, 'sizes', Y, M>
+  /**
+   * The CSS `transform-style` property.
+   */
+  transformStyle?: Token<CSS.Property.TransformStyle, unknown, Y, M>
+  /**
+   * The CSS `perspective` property.
+   */
+  perspective?: Token<CSS.Property.Perspective, unknown, Y, M>
 }

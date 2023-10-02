6dfe83b913709755ea6e5862000e248e0d18d306
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-02 13:22:14 +0900

    docs(animation.ja.mdx): update documentation for animation

--- docs/theme/tokens/index.ts
+++ docs/theme/tokens/index.ts
@@ -1,5 +1,6 @@
+import { animations } from './animations'
 import { colors } from './colors'
 import { gradients } from './gradients'
 import { shadows } from './shadows'
 
-export const tokens = { colors, gradients, shadows }
+export const tokens = { colors, gradients, shadows, animations }

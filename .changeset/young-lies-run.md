87dfbf126a9fba0d83b6367628a447cfc51005b0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-10 00:47:57 +0900

    feat(use-token): enhance value retrieval and checking

---
+++ .changeset/young-lies-run.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/use-token': minor
+---
+
+Enhance value retrieval and checking.
--- packages/hooks/use-token/src/index.ts
+++ packages/hooks/use-token/src/index.ts
@@ -1,9 +1,16 @@
 import { useColorMode, useTheme, Theme } from '@yamada-ui/core'
-import { getMemoizedObject as get, isArray } from '@yamada-ui/utils'
+import {
+  getMemoizedObject as get,
+  isArray,
+  isUndefined,
+} from '@yamada-ui/utils'
 
 export const useToken = <
   Y extends string | number = string,
-  M extends keyof Omit<Theme, 'components'> = keyof Omit<Theme, 'components'>,
+  M extends keyof Omit<Theme, 'components' | 'colorSchemes'> = keyof Omit<
+    Theme,
+    'components' | 'colorSchemes'
+  >,
 >(
   name: M,
   path: Theme[M] | number | undefined,
@@ -21,13 +28,42 @@ export const useToken = <
 
   if (name === 'transitionEasing') name = 'transitions.easing' as M
 
-  const value = get<Y | [Y, Y] | undefined>(theme, `${name}.${path}`)
+  let value = get<Y | [Y, Y] | undefined>(theme, `${name}.${path}`)
 
-  if (isArray(value)) {
-    const [lightValue, darkValue] = value
+  if (!isUndefined(value)) {
+    if (isArray(value)) {
+      const [lightValue, darkValue] = value
 
-    return colorMode === 'light' ? lightValue : darkValue
+      return colorMode === 'light' ? lightValue : darkValue
+    } else {
+      return value
+    }
   } else {
-    return value
+    value = get<Y | [Y, Y] | undefined>(theme, `semantics.${name}.${path}`)
+
+    if (isArray(value)) {
+      value = [
+        get<Y>(theme, `${name}.${value[0]}`) ?? value[0],
+        get<Y>(theme, `${name}.${value[1]}`) ?? value[1],
+      ]
+    } else {
+      value = get<Y | undefined>(theme, `${name}.${value}`) ?? value
+    }
+
+    if (isArray(value)) {
+      const [lightValue, darkValue] = value
+
+      value = colorMode === 'light' ? lightValue : darkValue
+
+      if (isArray(value)) {
+        const [lightValue, darkValue] = value as unknown as [Y, Y]
+
+        return colorMode === 'light' ? lightValue : darkValue
+      } else {
+        return value
+      }
+    } else {
+      return value
+    }
   }
 }

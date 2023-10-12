1bf828ddb9b966e1cb39559e489ce33c5c66c7c3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-13 01:24:45 +0900

    refactor(constants): renamed and added github url constants

--- docs/constant/sns.ts
+++ docs/constant/sns.ts
@@ -2,7 +2,8 @@ export const SNS = {
   GITHUB: {
     HIROTOMO_YAMADA: 'https://github.com/hirotomoyamada',
     YAMADA_UI: 'https://github.com/hirotomoyamada/yamada-ui',
-    EDIT_URL: 'https://github.com/hirotomoyamada/yamada-docs/tree/main/contents',
+    DOC_EDIT_URL: 'https://github.com/hirotomoyamada/yamada-docs/tree/main/contents',
+    UI_EDIT_URL: 'https://github.com/hirotomoyamada/yamada-ui/tree/main/packages',
   },
   TWITTER: {
     HIROTOMO_YAMADA: 'https://twitter.com/hirotomoyamada',

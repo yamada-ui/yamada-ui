8c80e0f4fa432ac775ab2e6114df67ec96a872e0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-15 19:35:40 +0900

    feat(theme): added info color and colorScheme

---
+++ .changeset/polite-turtles-swim.md
@@ -0,0 +1,5 @@
+---
+'@yamada-ui/theme': patch
+---
+
+Added info color and colorScheme.
--- packages/theme/src/semantics.ts
+++ packages/theme/src/semantics.ts
@@ -4,6 +4,7 @@ export const semantics: ThemeSemantics = {
   colors: {
     primary: 'blue.500',
     secondary: 'purple.500',
+    info: 'blue.500',
     success: 'emerald.500',
     warning: 'orange.500',
     danger: 'red.500',
@@ -12,6 +13,7 @@ export const semantics: ThemeSemantics = {
   colorSchemes: {
     primary: 'blue',
     secondary: 'purple',
+    info: 'blue',
     success: 'emerald',
     warning: 'orange',
     danger: 'red',

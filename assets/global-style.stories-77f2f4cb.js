819db08157510b2c09f6a7023b5ac961413d94ca
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-18 02:02:44 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@101c3c3c7db54bc2561d1e2460a3b5b68d38b7a9 🚀

---
+++ assets/global-style.stories-77f2f4cb.js
@@ -0,0 +1,16 @@
+import{a as o}from"./jsx-runtime-03b4ddbf.js";import{e as p}from"./extend-theme-8601c7af.js";import{U as l}from"./ui-provider-64e7f642.js";import{C as a}from"./center-a5003329.js";import{I as s}from"./image-a49b621b.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-03a2d05b.js";import"./theme-provider-1f5b74fc.js";import"./factory-3147849f.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./environment-provider-1aab5d43.js";import"./loading-provider-ca00ed13.js";import"./index-9c9a8873.js";import"./Combination-14beb162.js";import"./tslib.es6-90f6c3e8.js";import"./motion-d1722169.js";import"./motion-90200a2f.js";import"./forward-ref-8b7d2a6f.js";import"./loading-6f2def66.js";import"./index-d67a3cdd.js";import"./index-3fdaba91.js";import"./index-b3930670.js";import"./icon-733ea9be.js";import"./index-033cf86e.js";import"./container-portal-ffbf9c4d.js";import"./index-d3ea75b5.js";import"./notice-d95b4dfb.js";import"./alert-b974788e.js";import"./use-component-style-82d4d359.js";import"./close-button-83505c42.js";import"./use-image-aba5765f.js";const L={title:"System / Theme / Global Style"},t=()=>{const i=p({styles:{globalStyle:{}}})();return o(l,{theme:i,children:o(n,{})})},n=()=>o(a,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:o(s,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png"})});var r,e,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  const globalStyle: UIStyle = {
+    /**
+     * Define the style you want to customize
+     */
+  };
+  const theme = extendTheme({
+    styles: {
+      globalStyle
+    }
+  })();
+  return <UIProvider theme={theme}>
+      <App />
+    </UIProvider>;
+}`,...(m=(e=t.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const M=["customGlobalStyle"];export{M as __namedExportsOrder,t as customGlobalStyle,L as default};
+//# sourceMappingURL=global-style.stories-77f2f4cb.js.map
---
+++ assets/text-styles.stories-5ad67f22.js
@@ -0,0 +1,21 @@
+import{a as e}from"./jsx-runtime-03b4ddbf.js";import{H as m}from"./heading-95b94f14.js";import{e as s}from"./extend-theme-8601c7af.js";import{U as p}from"./ui-provider-64e7f642.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./forward-ref-8b7d2a6f.js";import"./use-component-style-82d4d359.js";import"./theme-provider-1f5b74fc.js";import"./factory-3147849f.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./index-03a2d05b.js";import"./environment-provider-1aab5d43.js";import"./loading-provider-ca00ed13.js";import"./index-9c9a8873.js";import"./Combination-14beb162.js";import"./tslib.es6-90f6c3e8.js";import"./motion-d1722169.js";import"./motion-90200a2f.js";import"./loading-6f2def66.js";import"./index-d67a3cdd.js";import"./index-3fdaba91.js";import"./index-b3930670.js";import"./icon-733ea9be.js";import"./index-033cf86e.js";import"./container-portal-ffbf9c4d.js";import"./index-d3ea75b5.js";import"./notice-d95b4dfb.js";import"./alert-b974788e.js";import"./close-button-83505c42.js";const D={title:"System / Theme / Text Styles"},t=()=>{const n=s({styles:{textStyles:{gradient:{w:"full",fontSize:"5xl",bgGradient:"linear(to-l, #7928CA, #FF0080)",bgClip:"text"}}}})();return e(p,{theme:n,children:e(m,{textStyle:"gradient",isTruncated:!0,children:"クリリンのことか……クリリンのことかーーーっ！！！！！"})})};var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  const textStyles: TextStyles = {
+    gradient: {
+      w: 'full',
+      fontSize: '5xl',
+      bgGradient: 'linear(to-l, #7928CA, #FF0080)',
+      bgClip: 'text'
+    }
+  };
+  const theme = extendTheme({
+    styles: {
+      textStyles
+    }
+  })();
+  return <UIProvider theme={theme}>
+      <Heading textStyle='gradient' isTruncated>
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </Heading>
+    </UIProvider>;
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const J=["useTextStyle"];export{J as __namedExportsOrder,D as default,t as useTextStyle};
+//# sourceMappingURL=text-styles.stories-5ad67f22.js.map

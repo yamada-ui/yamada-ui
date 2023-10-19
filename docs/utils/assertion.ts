c4c10cc67643c3b0425306c269d58b07c3088be9
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-19 12:51:54 +0900

    feat: added toCamelCase, toKebabCase

--- docs/utils/assertion.ts
+++ docs/utils/assertion.ts
@@ -5,3 +5,15 @@ export const toBoolean = (value: string | number | boolean) => {
 
   return Boolean(value)
 }
+
+export const toCamelCase = (value: string & {}) =>
+  value
+    .toLowerCase()
+    .replace(/-(.)/g, (_, group1) => group1.toUpperCase())
+    .replace(/^(.)/, (_, group1) => group1.toUpperCase())
+
+export const toKebabCase = (value: string & {}) =>
+  value
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
+    .toLowerCase()
+    .replace(/^-/, '')

9c9355333223b7e78570c825b41335d5b2b56c3f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-19 18:10:00 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@fd340299604f41efd5baa56e2e9abdb803d2c9a2 🚀

---
+++ assets/transitions.stories-3c640799.js
@@ -0,0 +1,30 @@
+import{M as a}from"./index-09af529c.js";import{a as r,j as i,F as c}from"./jsx-runtime-b08f8875.js";import{u as s}from"./index-b7a9f960.js";import"./iframe-28ba79c2.js";import"../sb-preview/runtime.js";import"./index-8ee6c85d.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-95ca0d59.js";import"./index-8bf7f4ad.js";import"./inheritsLoose-c82a83d4.js";import"./index-c4ced888.js";import"./index-356e4a49.js";function e(o){const n=Object.assign({h1:"h1",pre:"pre",code:"code"},s(),o.components);return i(c,{children:[r(a,{title:"Documents / Theme / Transitions"}),`
+`,r(n.h1,{id:"transitions",children:"Transitions"}),`
+`,r(n.pre,{children:r(n.code,{className:"language-ts",children:`const transitions = {
+  property: {
+    common:
+      'background-color, border-color, color, fill, stroke, opacity, box-shadow, transform',
+    colors: 'background-color, border-color, color, fill, stroke',
+    dimensions: 'width, height',
+    position: 'left, right, top, bottom',
+    background: 'background-color, background-image, background-position',
+  },
+
+  easing: {
+    'ease-in': 'cubic-bezier(0.4, 0, 1, 1)',
+    'ease-out': 'cubic-bezier(0, 0, 0.2, 1)',
+    'ease-in-out': 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+
+  duration: {
+    'ultra-fast': '50ms',
+    faster: '100ms',
+    fast: '150ms',
+    normal: '200ms',
+    slow: '300ms',
+    slower: '400ms',
+    'ultra-slow': '500ms',
+  },
+}
+`})})]})}function m(o={}){const{wrapper:n}=Object.assign({},s(),o.components);return n?r(n,{...o,children:r(e,{...o})}):e(o)}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const t={title:"Documents / Theme / Transitions",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:m};const y=["__page"];export{y as __namedExportsOrder,p as __page,t as default};
+//# sourceMappingURL=transitions.stories-3c640799.js.map

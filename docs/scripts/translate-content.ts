bba717123d375a401f1559cac4049e47a17eb819
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-23 03:27:52 +0900

    feat(scripts): added translate-content script

---
+++ docs/scripts/translate-content.ts
@@ -0,0 +1,147 @@
+import { existsSync } from 'fs'
+import { readFile, readdir, writeFile } from 'fs/promises'
+import chalk from 'chalk'
+import { program } from 'commander'
+import { ChatCompletionMessageParam } from 'openai/resources'
+import ora from 'ora'
+import { openai } from 'libs/openai'
+import { prettier } from 'libs/prettier'
+
+const LANG_MAP = {
+  ja: 'Japanese',
+  en: 'English',
+} as const
+
+type Option = { out?: string; lang?: keyof typeof LANG_MAP }
+
+const getPaths = async (path: string, lang: keyof typeof LANG_MAP): Promise<string[]> => {
+  try {
+    const dirents = await readdir(path, { withFileTypes: true })
+
+    const paths = await Promise.all(
+      dirents.flatMap(async (dirent) => {
+        if (dirent.isDirectory()) {
+          return await getPaths(`${path}/${dirent.name}`, lang)
+        } else {
+          return `${path}/${dirent.name}`
+        }
+      }),
+    )
+
+    let resolvedPaths = paths.flat()
+
+    resolvedPaths = resolvedPaths.filter((path) =>
+      lang === 'en' ? path.endsWith('.ja.mdx') : !path.endsWith('.ja.mdx'),
+    )
+
+    return resolvedPaths
+  } catch {
+    const isExist = existsSync(path)
+
+    return isExist ? [path] : []
+  }
+}
+
+const translateContent = async (content: string, lang: keyof typeof LANG_MAP): Promise<string> => {
+  const from = `from ${LANG_MAP[lang === 'en' ? 'ja' : 'en']}`
+  const to = `to ${LANG_MAP[lang]}`
+
+  const messages: ChatCompletionMessageParam[] = [
+    {
+      role: 'system',
+      content:
+        `Please translate the text of the mdx file that I will send you ${from} ${to}. Please note the following points:\n` +
+        `- The text you send will be saved as mdx. Therefore, except for the text to be translated, please output the contents of the sent mdx file as is.\n` +
+        `- Be sure to avoid translating sentences that don't need to be translated. e.g, variables, arguments, component names, etc.\n`,
+    },
+    { role: 'user', content },
+  ]
+
+  const { choices } = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages,
+    temperature: 0,
+  })
+
+  return choices[0].message?.content
+}
+
+program
+  .argument('<path>')
+  .option('-o, --out <path>')
+  .option('-l, --lang <lang>')
+  .action(async (targetPath: string, { out: outPath, lang: targetLang = 'en' }: Option) => {
+    const hrtime = process.hrtime()
+    const spinner = ora('Generating Yamada-UI theme typings').start()
+    const successes = new Map<string, string>()
+    const errors = new Map<string, string>()
+
+    try {
+      spinner.text = `Read files [1 / 4]...`
+
+      const resolvedPaths = await getPaths(targetPath, targetLang)
+
+      const totalCount = resolvedPaths.length
+
+      spinner.text = `Translate files [0 / ${totalCount}]...`
+
+      await Promise.all(
+        resolvedPaths.map(async (path) => {
+          try {
+            const hrtime = process.hrtime()
+
+            let content = await readFile(path, 'utf8')
+
+            content = await translateContent(content, targetLang)
+
+            content = await prettier(content)
+
+            outPath ??= path.replace(
+              new RegExp(`${targetLang === 'en' ? '.ja' : ''}\.mdx$`),
+              `${targetLang === 'en' ? '' : '.ja'}.mdx`,
+            )
+
+            await writeFile(outPath, content)
+
+            const [start, end] = process.hrtime(hrtime)
+            const duration = (Number(end - start) / 1e9).toFixed(2)
+
+            successes.set(outPath, duration)
+
+            spinner.text = `Translate files [${successes.size} / ${totalCount}]...`
+          } catch (e) {
+            errors.set(path, e.message)
+          }
+        }),
+      )
+
+      const [start, end] = process.hrtime(hrtime)
+      const duration = (Number(end - start) / 1e9).toFixed(2)
+
+      spinner.succeed(chalk.green(`Done in ${duration}s` + '\n'))
+
+      if (successes.size !== 0) {
+        console.log(
+          chalk.bgGreen(' Successes '),
+          '\n',
+          chalk.green(
+            ...Array.from(successes.entries()).map(([path, duration]) => `${path}: ${duration}s\n`),
+          ),
+        )
+      }
+
+      if (errors.size !== 0) {
+        console.log(
+          chalk.bgRed(' Errors '),
+          '\n',
+          chalk.red(
+            ...Array.from(errors.entries()).map(([path, duration]) => `${path}: ${duration}s\n`),
+          ),
+        )
+      }
+    } catch (e) {
+      spinner.fail(chalk.red(e.message))
+    }
+  })
+
+program.parse()

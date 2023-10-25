135977343e3d550ce98456d2d05c38d4ae9407ce
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-10-25 21:51:57 +0900

    refactor(translate-content): improved error handling and logging

--- docs/scripts/translate-content.ts
+++ docs/scripts/translate-content.ts
@@ -6,13 +6,14 @@ import { ChatCompletionMessageParam } from 'openai/resources'
 import ora from 'ora'
 import { openai } from 'libs/openai'
 import { prettier } from 'libs/prettier'
+import { wait } from 'utils/assertion'
 
 const LANG_MAP = {
   ja: 'Japanese',
   en: 'English',
 } as const
 
-type Option = { out?: string; lang?: keyof typeof LANG_MAP }
+type Option = { out?: string; lang?: keyof typeof LANG_MAP; logs?: boolean }
 
 const getPaths = async (path: string, lang: keyof typeof LANG_MAP): Promise<string[]> => {
   try {
@@ -44,112 +45,170 @@ const getPaths = async (path: string, lang: keyof typeof LANG_MAP): Promise<stri
 
 const splitContent = (content: string): string[] => content.split(/\n(?=## |### )/).filter(Boolean)
 
-const translateContent = async (content: string, lang: keyof typeof LANG_MAP): Promise<string> => {
-  const from = `from ${LANG_MAP[lang === 'en' ? 'ja' : 'en']}`
-  const to = `to ${LANG_MAP[lang]}`
+const translateContent = async ({
+  title,
+  content,
+  lang,
+  retry = 1,
+  onRetry,
+}: {
+  title: string
+  content: string
+  lang: keyof typeof LANG_MAP
+  retry?: number
+  onRetry?: (retry: number) => void
+}): Promise<string> => {
+  try {
+    const from = `from ${LANG_MAP[lang === 'en' ? 'ja' : 'en']}`
+    const to = `to ${LANG_MAP[lang]}`
 
-  const messages: ChatCompletionMessageParam[] = [
-    {
-      role: 'system',
-      content:
-        `Please translate the text of the mdx file that I will send you ${from} ${to}. Please note the following points:\n` +
-        `- The text you send will be saved as mdx. Therefore, except for the text to be translated, please output the contents of the sent mdx file as is.\n` +
-        `- Be sure to avoid translating sentences that don't need to be translated. e.g, variables, arguments, component names, etc.\n`,
-    },
-    { role: 'user', content },
-  ]
+    const messages: ChatCompletionMessageParam[] = [
+      {
+        role: 'system',
+        content:
+          `Please translate the text of the mdx file that I will send you ${from} ${to}. Please note the following points:\n` +
+          `- The text you send will be saved as mdx. Therefore, except for the text to be translated, please output the contents of the sent mdx file as is.\n` +
+          `- Be sure to avoid translating sentences that don't need to be translated. e.g, variables, arguments, component names, etc.\n`,
+      },
+      { role: 'user', content },
+    ]
 
-  const { choices } = await openai.chat.completions.create({
-    model: 'gpt-4',
-    messages,
-    temperature: 0,
-  })
+    const { choices } = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages,
+      temperature: 0,
+    })
 
-  return choices[0].message?.content
+    return choices[0].message?.content
+  } catch (e) {
+    onRetry?.(retry)
+
+    await wait(3000)
+
+    return await translateContent({ title, content, lang, retry: retry + 1, onRetry })
+  }
 }
 
 program
   .argument('<path>')
   .option('-o, --out <path>')
   .option('-l, --lang <lang>')
-  .action(async (targetPath: string, { out: outPath, lang: targetLang = 'en' }: Option) => {
-    const hrtime = process.hrtime()
-    const spinner = ora('Generating Yamada-UI theme typings').start()
-    const successes = new Map<string, string>()
-    const errors = new Map<string, string>()
+  .option('--logs')
+  .action(
+    async (targetPath: string, { out: outPath, lang: targetLang = 'en', logs: isLogs }: Option) => {
+      const hrtime = process.hrtime()
+      const spinner = !isLogs ? ora('Generating Yamada-UI theme typings').start() : undefined
+      const successes = new Map<string, string>()
+      const errors = new Map<string, string>()
 
-    try {
-      spinner.text = `Read files...`
+      try {
+        if (spinner) spinner.text = `Read files...`
 
-      const resolvedPaths = await getPaths(targetPath, targetLang)
+        const resolvedPaths = await getPaths(targetPath, targetLang)
 
-      const totalCount = resolvedPaths.length
+        const totalCount = resolvedPaths.length
 
-      spinner.text = `Translate files [0 / ${totalCount}]...`
+        if (spinner) spinner.text = `Translate files [0 / ${totalCount}]...`
 
-      await Promise.all(
-        resolvedPaths.map(async (path) => {
-          try {
-            const hrtime = process.hrtime()
+        await Promise.all(
+          resolvedPaths.map(async (path) => {
+            try {
+              const hrtime = process.hrtime()
 
-            let content = await readFile(path, 'utf8')
+              let content = await readFile(path, 'utf8')
 
-            const contents = await Promise.all(
-              splitContent(content).map((content) => translateContent(content, targetLang)),
-            )
+              const contents = await Promise.all(
+                splitContent(content).map((content) => {
+                  let title = content.match(/^(.*?)\n/)?.[1]
 
-            content = contents.join('\n')
+                  title = title?.replace(/#/g, '').trim()
 
-            content = await prettier(content)
+                  if (title === '---') title = 'Frontmatter'
 
-            const resolvedOutPath =
-              outPath ??
-              path.replace(
-                new RegExp(`${targetLang === 'en' ? '.ja' : ''}\.mdx$`),
-                `${targetLang === 'en' ? '' : '.ja'}.mdx`,
+                  if (isLogs)
+                    console.log(chalk.gray(`[${path}]`), chalk.blue(`Translate`), `${title}`)
+
+                  return translateContent({
+                    title,
+                    content,
+                    lang: targetLang,
+                    onRetry: (retry) => {
+                      if (isLogs)
+                        console.log(
+                          chalk.gray(`[${path}]`),
+                          chalk.yellow(`Retry ${retry}`),
+                          `${title}`,
+                        )
+                    },
+                  })
+                }),
               )
 
-            await writeFile(resolvedOutPath, content)
+              content = contents.join('\n')
 
-            const [start, end] = process.hrtime(hrtime)
-            const duration = (Number(end - start) / 1e9).toFixed(2)
+              content = await prettier(content)
 
-            successes.set(resolvedOutPath, duration)
+              const resolvedOutPath =
+                outPath ??
+                path.replace(
+                  new RegExp(`${targetLang === 'en' ? '.ja' : ''}\.mdx$`),
+                  `${targetLang === 'en' ? '' : '.ja'}.mdx`,
+                )
 
-            spinner.text = `Translate files [${successes.size} / ${totalCount}]...`
-          } catch (e) {
-            errors.set(path, e.message)
+              await writeFile(resolvedOutPath, content)
+
+              const [start, end] = process.hrtime(hrtime)
+              const duration = (Number(end - start) / 1e9).toFixed(2)
+
+              successes.set(resolvedOutPath, duration)
+
+              if (isLogs) console.log(chalk.gray(`[${path}]`), chalk.green(`Done ${duration}s`))
+
+              if (spinner) spinner.text = `Translate files [${successes.size} / ${totalCount}]...`
+            } catch (e) {
+              errors.set(path, e.message)
+            }
+          }),
+        )
+
+        const [start, end] = process.hrtime(hrtime)
+        const duration = (Number(end - start) / 1e9).toFixed(2)
+
+        if (spinner) spinner.succeed(chalk.green(`Done in ${duration}s` + '\n'))
+
+        if (!isLogs) {
+          if (successes.size !== 0) {
+            console.log(
+              chalk.bgGreen(' Successes '),
+              '\n',
+              chalk.green(
+                ...Array.from(successes.entries()).map(
+                  ([path, duration]) => `${path}: ${duration}s\n`,
+                ),
+              ),
+            )
           }
-        }),
-      )
 
-      const [start, end] = process.hrtime(hrtime)
-      const duration = (Number(end - start) / 1e9).toFixed(2)
-
-      spinner.succeed(chalk.green(`Done in ${duration}s` + '\n'))
-
-      if (successes.size !== 0) {
-        console.log(
-          chalk.bgGreen(' Successes '),
-          '\n',
-          chalk.green(
-            ...Array.from(successes.entries()).map(([path, duration]) => `${path}: ${duration}s\n`),
-          ),
-        )
+          if (errors.size !== 0) {
+            console.log(
+              chalk.bgRed(' Errors '),
+              '\n',
+              chalk.red(
+                ...Array.from(errors.entries()).map(
+                  ([path, duration]) => `${path}: ${duration}s\n`,
+                ),
+              ),
+            )
+          }
+        }
+      } catch (e) {
+        if (spinner) {
+          spinner.fail(chalk.red(e.message))
+        } else {
+          console.log(chalk.red(e.message))
+        }
       }
-
-      if (errors.size !== 0) {
-        console.log(
-          chalk.bgRed(' Errors '),
-          '\n',
-          chalk.red(
-            ...Array.from(errors.entries()).map(([path, duration]) => `${path}: ${duration}s\n`),
-          ),
-        )
-      }
-    } catch (e) {
-      spinner.fail(chalk.red(e.message))
-    }
-  })
+    },
+  )
 
 program.parse()

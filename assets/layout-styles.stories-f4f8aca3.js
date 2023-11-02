5f861d4fb3183db60ef37eba2a6bfa3b6431071f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-02 10:56:22 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@c1b4efe9e365b264710b8d6c426080110ddbcb5a 🚀

---
+++ assets/layout-styles.stories-f4f8aca3.js
@@ -0,0 +1,36 @@
+import{a as t}from"./jsx-runtime-b08f8875.js";import{e as m}from"./extend-theme-488f603a.js";import{U as l}from"./ui-provider-33bf6d72.js";import{C as a}from"./center-d172a620.js";import"./index-8ee6c85d.js";import"./_commonjsHelpers-de833af9.js";import"./index-704f2581.js";import"./theme-provider-1097130b.js";import"./factory-ab7c3580.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-95ca0d59.js";import"./environment-provider-c01394a3.js";import"./loading-provider-ee662396.js";import"./index-76f20168.js";import"./loading-0bce3a59.js";import"./index-6cdd3cb8.js";import"./index-2cfd973b.js";import"./index-a69af9f5.js";import"./icon-1c0bf186.js";import"./forward-ref-cf7188bd.js";import"./motion-2d0d8241.js";import"./motion-5447b2d8.js";import"./index-709acde9.js";import"./container-portal-d647f37d.js";import"./index-8bf7f4ad.js";import"./notice-c6d0d044.js";import"./alert-96f995e0.js";import"./use-component-style-07e84389.js";import"./close-button-b4ee3b21.js";const q={title:"System / Theme / Layer Styles"},e=()=>{const s=m({styles:{layerStyles:{masterRoshi:{position:"relative",m:"md",boxSize:"4xs",border:"6px solid #000",rounded:"full",bg:"#FFF",fontFamily:"serif",color:"#000",fontSize:"8xl",fontWeight:"bold",_after:{content:'""',position:"absolute",top:"50%",left:"50%",transform:"translate(-50%, -50%)",zIndex:-1,rounded:"md",bg:"#FF7F0B",boxSize:"3xs"}}}}})();return t(l,{theme:s,children:t(a,{layerStyle:"masterRoshi",children:"亀"})})};var r,o,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  const layerStyles: LayerStyles = {
+    masterRoshi: {
+      position: 'relative',
+      m: 'md',
+      boxSize: '4xs',
+      border: '6px solid #000',
+      rounded: 'full',
+      bg: '#FFF',
+      fontFamily: 'serif',
+      color: '#000',
+      fontSize: '8xl',
+      fontWeight: 'bold',
+      _after: {
+        content: '""',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: -1,
+        rounded: 'md',
+        bg: '#FF7F0B',
+        boxSize: '3xs'
+      }
+    }
+  };
+  const theme = extendTheme({
+    styles: {
+      layerStyles
+    }
+  })();
+  return <UIProvider theme={theme}>
+      <Center layerStyle='masterRoshi'>亀</Center>
+    </UIProvider>;
+}`,...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const w=["useLayerStyle"];export{w as __namedExportsOrder,q as default,e as useLayerStyle};
+//# sourceMappingURL=layout-styles.stories-f4f8aca3.js.map
---
+++ assets/use-loading.stories-393a7974.js
@@ -0,0 +1,168 @@
+import{a as n,j as c}from"./jsx-runtime-b08f8875.js";import{u as o}from"./loading-provider-ee662396.js";import{C as s}from"./center-d172a620.js";import{W as i}from"./flex-eedf9e68.js";import{B as t}from"./button-3d1799ed.js";import{T as h}from"./text-50bfeca2.js";import"./index-8ee6c85d.js";import"./_commonjsHelpers-de833af9.js";import"./index-76f20168.js";import"./factory-ab7c3580.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-95ca0d59.js";import"./loading-0bce3a59.js";import"./index-6cdd3cb8.js";import"./theme-provider-1097130b.js";import"./index-2cfd973b.js";import"./index-a69af9f5.js";import"./icon-1c0bf186.js";import"./forward-ref-cf7188bd.js";import"./motion-2d0d8241.js";import"./motion-5447b2d8.js";import"./index-709acde9.js";import"./container-portal-d647f37d.js";import"./index-8bf7f4ad.js";import"./use-component-style-07e84389.js";const on={title:"Hooks / useLoading"},k=a=>new Promise(e=>{setTimeout(e,a)}),d=()=>{const{screen:a,page:e,background:r}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:c(i,{gap:"md",children:[n(t,{onClick:()=>a.start(),children:"Start screen loading"}),n(t,{onClick:()=>e.start(),children:"Start page loading"}),n(t,{onClick:()=>r.start(),children:"Start background loading"})]})})},g=()=>{const{screen:a,page:e,background:r}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:c(i,{gap:"md",children:[n(t,{onClick:()=>a.start({duration:5e3}),children:"Start screen loading"}),n(t,{onClick:()=>e.start({duration:5e3}),children:"Start page loading"}),n(t,{onClick:()=>r.start({duration:5e3}),children:"Start background loading"})]})})},p=()=>{const{screen:a,page:e,background:r}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:c(i,{gap:"md",children:[n(t,{onClick:()=>a.start({message:"Loading",duration:5e3}),children:"Start screen loading"}),n(t,{onClick:()=>e.start({message:"Loading",duration:5e3}),children:"Start page loading"}),n(t,{onClick:()=>r.start({message:"Loading",duration:5e3}),children:"Start background loading"})]})})},l=()=>{const{screen:a,page:e,background:r}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:c(i,{gap:"md",children:[n(t,{onClick:()=>a.start({message:n(h,{color:"primary",children:"Loading"})}),children:"Start screen loading"}),n(t,{onClick:()=>e.start({message:n(h,{color:"primary",children:"Loading"})}),children:"Start page loading"}),n(t,{onClick:()=>r.start({message:n(h,{color:"primary",children:"Loading"})}),children:"Start background loading"})]})})},u=()=>{const{screen:a,page:e,background:r}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:c(i,{gap:"md",children:[n(t,{onClick:()=>{a.start({message:"Loading",duration:1e4}),setTimeout(()=>{a.update({message:"Please wait"})},5e3)},children:"Start screen loading"}),n(t,{onClick:()=>{e.start({message:"Loading",duration:1e4}),setTimeout(()=>{e.update({message:"Please wait"})},5e3)},children:"Start page loading"}),n(t,{onClick:()=>{r.start({message:"Loading",duration:1e4}),setTimeout(()=>{r.update({message:"Please wait"})},5e3)},children:"Start background loading"})]})})},m=()=>{const{page:a}=o();return n(s,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:n(t,{onClick:async()=>{a.start({message:"Get Data 1/3"}),await k(3e3),a.update({message:"Please wait 2/3"}),await k(3e3),a.update({message:"Almost done 3/3"}),await k(3e3),a.finish()},children:"Start page loading"})})};var w,C,S;d.parameters={...d.parameters,docs:{...(w=d.parameters)==null?void 0:w.docs,source:{originalSource:`() => {
+  const {
+    screen,
+    page,
+    background
+  } = useLoading();
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => screen.start()}>Start screen loading</Button>
+        <Button onClick={() => page.start()}>Start page loading</Button>
+        <Button onClick={() => background.start()}>
+          Start background loading
+        </Button>
+      </Wrap>
+    </Center>;
+}`,...(S=(C=d.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var x,B,b;g.parameters={...g.parameters,docs:{...(x=g.parameters)==null?void 0:x.docs,source:{originalSource:`() => {
+  const {
+    screen,
+    page,
+    background
+  } = useLoading();
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => screen.start({
+        duration: 5000
+      })}>
+          Start screen loading
+        </Button>
+        <Button onClick={() => page.start({
+        duration: 5000
+      })}>
+          Start page loading
+        </Button>
+        <Button onClick={() => background.start({
+        duration: 5000
+      })}>
+          Start background loading
+        </Button>
+      </Wrap>
+    </Center>;
+}`,...(b=(B=g.parameters)==null?void 0:B.docs)==null?void 0:b.source}}};var L,v,T;p.parameters={...p.parameters,docs:{...(L=p.parameters)==null?void 0:L.docs,source:{originalSource:`() => {
+  const {
+    screen,
+    page,
+    background
+  } = useLoading();
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => screen.start({
+        message: 'Loading',
+        duration: 5000
+      })}>
+          Start screen loading
+        </Button>
+        <Button onClick={() => page.start({
+        message: 'Loading',
+        duration: 5000
+      })}>
+          Start page loading
+        </Button>
+        <Button onClick={() => background.start({
+        message: 'Loading',
+        duration: 5000
+      })}>
+          Start background loading
+        </Button>
+      </Wrap>
+    </Center>;
+}`,...(T=(v=p.parameters)==null?void 0:v.docs)==null?void 0:T.source}}};var W,y,f;l.parameters={...l.parameters,docs:{...(W=l.parameters)==null?void 0:W.docs,source:{originalSource:`() => {
+  const {
+    screen,
+    page,
+    background
+  } = useLoading();
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => screen.start({
+        message: <Text color='primary'>Loading</Text>
+      })}>
+          Start screen loading
+        </Button>
+        <Button onClick={() => page.start({
+        message: <Text color='primary'>Loading</Text>
+      })}>
+          Start page loading
+        </Button>
+        <Button onClick={() => background.start({
+        message: <Text color='primary'>Loading</Text>
+      })}>
+          Start background loading
+        </Button>
+      </Wrap>
+    </Center>;
+}`,...(f=(y=l.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var P,D,M;u.parameters={...u.parameters,docs:{...(P=u.parameters)==null?void 0:P.docs,source:{originalSource:`() => {
+  const {
+    screen,
+    page,
+    background
+  } = useLoading();
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Wrap gap='md'>
+        <Button onClick={() => {
+        screen.start({
+          message: 'Loading',
+          duration: 10000
+        });
+        setTimeout(() => {
+          screen.update({
+            message: 'Please wait'
+          });
+        }, 5000);
+      }}>
+          Start screen loading
+        </Button>
+
+        <Button onClick={() => {
+        page.start({
+          message: 'Loading',
+          duration: 10000
+        });
+        setTimeout(() => {
+          page.update({
+            message: 'Please wait'
+          });
+        }, 5000);
+      }}>
+          Start page loading
+        </Button>
+
+        <Button onClick={() => {
+        background.start({
+          message: 'Loading',
+          duration: 10000
+        });
+        setTimeout(() => {
+          background.update({
+            message: 'Please wait'
+          });
+        }, 5000);
+      }}>
+          Start background loading
+        </Button>
+      </Wrap>
+    </Center>;
+}`,...(M=(D=u.parameters)==null?void 0:D.docs)==null?void 0:M.source}}};var j,A,F;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`() => {
+  const {
+    page
+  } = useLoading();
+  const getData = async () => {
+    page.start({
+      message: 'Get Data 1/3'
+    });
+    await wait(3000);
+    page.update({
+      message: 'Please wait 2/3'
+    });
+    await wait(3000);
+    page.update({
+      message: 'Almost done 3/3'
+    });
+    await wait(3000);
+    page.finish();
+  };
+  return <Center w='calc(100vw - 16px * 2)' h='calc(100vh - 16px * 2)'>
+      <Button onClick={getData}>Start page loading</Button>
+    </Center>;
+}`,...(F=(A=m.parameters)==null?void 0:A.docs)==null?void 0:F.source}}};const sn=["basic","withTimeout","withMessage","customMessage","updateMessage","asyncFunction"];export{sn as __namedExportsOrder,m as asyncFunction,d as basic,l as customMessage,on as default,u as updateMessage,p as withMessage,g as withTimeout};
+//# sourceMappingURL=use-loading.stories-393a7974.js.map

816bf4b1cf5abb589a7e7e8e8a5db1f2201810b6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-03 01:50:54 +0900

    feat(media-and-icons): added file and hash components

---
+++ docs/components/media-and-icons/hash.tsx
@@ -0,0 +1,25 @@
+import { Icon, IconProps } from '@yamada-ui/react'
+import { forwardRef } from 'react'
+
+export const Hash = forwardRef<SVGSVGElement, IconProps>(({ boxSize = '1em', ...rest }, ref) => {
+  return (
+    <Icon
+      ref={ref}
+      boxSize={boxSize}
+      stroke='currentColor'
+      fill='none'
+      fillRule='evenodd'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      viewBox='0 0 20 20'
+      aria-hidden='true'
+      focusable='false'
+      xmlns='http://www.w3.org/2000/svg'
+      {...rest}
+    >
+      <path d='M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z' />
+    </Icon>
+  )
+})
+
+Hash.displayName = 'Hash'

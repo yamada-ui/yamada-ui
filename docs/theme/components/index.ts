54ea2ad35509b001ecb188692c5bf2edda696795
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-03 15:55:20 +0900

    feat(theme/components): added Mark component

--- docs/theme/components/index.ts
+++ docs/theme/components/index.ts
@@ -1,4 +1,5 @@
 import { Alert } from './alert'
 import { Button } from './button'
+import { Mark } from './mark'
 
-export const components = { Button, Alert }
+export const components = { Button, Alert, Mark }

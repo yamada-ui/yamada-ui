9b1fee58ca02074fb0c51cd4d14b657135db2b6a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-19 01:24:04 +0900

    refactor(next.config): removed experimental esmExternals

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,9 +4,6 @@ import { withContentlayer } from "next-contentlayer"
 const nextConfig = {
   reactStrictMode: false,
   pageExtensions: ["page.jsx", "page.tsx"],
-  experimental: {
-    esmExternals: false,
-  },
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],

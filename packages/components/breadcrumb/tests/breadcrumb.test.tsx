79cb6d40dc26ede497831ae3af8d867f61d2b7c6
yuto ichinose <yuto.0526.sano@gmail.com>
2023-11-22 22:23:21 +0900

    fix: updated test

--- packages/components/breadcrumb/tests/breadcrumb.test.tsx
+++ packages/components/breadcrumb/tests/breadcrumb.test.tsx
@@ -51,4 +51,34 @@ describe("<Breadcrumb />", () => {
     const spanElement = container.querySelector(`[aria-current="page"]`)
     expect(spanElement).toBeInTheDocument()
   })
+
+  test("breadcrumb link has its href attribute correctly set", () => {
+    render(
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Link 1</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink href="#">Link 2</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>,
+    )
+    const breadcrumbLink = screen.getByText("Link 1")
+    expect(breadcrumbLink.getAttribute("href")).toBe("#")
+  })
+
+  test("current page link doesn't have href attribute set", () => {
+    render(
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="#">Link 1</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink href="#">Link 2</BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>,
+    )
+    const currentPageLink = screen.getByText("Link 2")
+    expect(currentPageLink.getAttribute("href")).toBe(null)
+  })
 })

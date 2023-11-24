a2e37dc3cf01e8fd89d9e66cde2845e17946ce5b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-24 14:58:25 +0000

    Deploying to gh-pages from @ hirotomoyamada/yamada-ui@d8525ed6e0e05c989b079dd2a336b9bcdc340932 🚀

---
+++ assets/colors.stories-a8812601.js
@@ -0,0 +1,51 @@
+import{M as p}from"./index-b3399f00.js";import{J as o,C as i,a}from"./components-f5848f13.js";import{a as e,j as t,F as h}from"./jsx-runtime-b08f8875.js";import{u as s}from"./index-b7a9f960.js";import{G as l}from"./grid-e09d6dfe.js";import{d as r}from"./index-262e28b0.js";import"./iframe-f95f70db.js";import"../sb-preview/runtime.js";import"./index-8ee6c85d.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-95ca0d59.js";import"./index-8bf7f4ad.js";import"./index-13a69483.js";import"./index-356e4a49.js";import"./index-7c0031f6.js";import"./ui-provider-2160461d.js";import"./environment-provider-d44107bb.js";import"./factory-2d442159.js";import"./theme-provider-78d359c6.js";import"./loading-provider-ff9a5329.js";import"./index-161daff9.js";import"./component-911fdb88.js";import"./loading-9c6ba408.js";import"./index-7636bab8.js";import"./index-a8e328c0.js";import"./index-0e02ae64.js";import"./icon-a76117e5.js";import"./forward-ref-cf7188bd.js";import"./motion-f1cc01af.js";import"./motion-5447b2d8.js";import"./index-709acde9.js";import"./container-portal-8be8bf32.js";import"./notice-a21d04a0.js";import"./alert-45c11755.js";import"./use-component-style-b6002d28.js";import"./close-button-7784ce89.js";import"./container-72b57892.js";import"./stack-57e4b232.js";import"./box-262adc2c.js";import"./text-c7eceae7.js";function d(m){const n=Object.assign({h1:"h1",h2:"h2"},s(),m.components);return t(h,{children:[e(p,{title:"Documents / Theme / Colors"}),`
+`,e(n.h1,{id:"colors",children:"Colors"}),`
+`,e(n.h2,{id:"black--white",children:"Black & White"}),`
+`,e(o,{children:t(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:[e(i,{name:"white",value:r.colors.white}),e(i,{name:"black",value:r.colors.black})]})}),`
+`,e(n.h2,{id:"standard",children:"Standard"}),`
+`,e(o,{children:t(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:[e(i,{name:"primary",value:r.colors.primary}),e(i,{name:"secondary",value:r.colors.secondary}),e(i,{name:"warning",value:r.colors.warning}),e(i,{name:"danger",value:r.colors.danger}),e(i,{name:"link",value:r.colors.link}),e(i,{name:"border",value:r.colors.border})]})}),`
+`,e(n.h2,{id:"alphas",children:"Alphas"}),`
+`,e(o,{children:t(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:[e(a,{name:"whiteAlpha",colors:r.colors}),e(a,{name:"blackAlpha",colors:r.colors})]})}),`
+`,e(n.h2,{id:"gray",children:"Gray"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"gray",colors:r.colors})})}),`
+`,e(n.h2,{id:"zinc",children:"Zinc"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"zinc",colors:r.colors})})}),`
+`,e(n.h2,{id:"neutral",children:"Neutral"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"neutral",colors:r.colors})})}),`
+`,e(n.h2,{id:"stone",children:"Stone"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"stone",colors:r.colors})})}),`
+`,e(n.h2,{id:"red",children:"Red"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"red",colors:r.colors})})}),`
+`,e(n.h2,{id:"rose",children:"Rose"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"rose",colors:r.colors})})}),`
+`,e(n.h2,{id:"pink",children:"Pink"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"pink",colors:r.colors})})}),`
+`,e(n.h2,{id:"orange",children:"Orange"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"orange",colors:r.colors})})}),`
+`,e(n.h2,{id:"amber",children:"Amber"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"amber",colors:r.colors})})}),`
+`,e(n.h2,{id:"yellow",children:"Yellow"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"yellow",colors:r.colors})})}),`
+`,e(n.h2,{id:"lime",children:"Lime"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"lime",colors:r.colors})})}),`
+`,e(n.h2,{id:"green",children:"Green"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"green",colors:r.colors})})}),`
+`,e(n.h2,{id:"emerald",children:"Emerald"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"emerald",colors:r.colors})})}),`
+`,e(n.h2,{id:"teal",children:"Teal"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"teal",colors:r.colors})})}),`
+`,e(n.h2,{id:"cyan",children:"Cyan"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"cyan",colors:r.colors})})}),`
+`,e(n.h2,{id:"sky",children:"Sky"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"sky",colors:r.colors})})}),`
+`,e(n.h2,{id:"blue",children:"Blue"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"blue",colors:r.colors})})}),`
+`,e(n.h2,{id:"indigo",children:"Indigo"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"indigo",colors:r.colors})})}),`
+`,e(n.h2,{id:"violet",children:"Violet"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"violet",colors:r.colors})})}),`
+`,e(n.h2,{id:"purple",children:"Purple"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"purple",colors:r.colors})})}),`
+`,e(n.h2,{id:"fuchsia",children:"Fuchsia"}),`
+`,e(o,{children:e(l,{templateColumns:"repeat(3, 1fr)",gap:"md",children:e(a,{name:"fuchsia",colors:r.colors})})})]})}function u(m={}){const{wrapper:n}=Object.assign({},s(),m.components);return n?e(n,{...m,children:e(d,{...m})}):d(m)}const g=()=>{throw new Error("Docs-only story")};g.parameters={docsOnly:!0};const c={title:"Documents / Theme / Colors",tags:["stories-mdx"],includeStories:["__page"]};c.parameters=c.parameters||{};c.parameters.docs={...c.parameters.docs||{},page:u};const ne=["__page"];export{ne as __namedExportsOrder,g as __page,c as default};
+//# sourceMappingURL=colors.stories-a8812601.js.map

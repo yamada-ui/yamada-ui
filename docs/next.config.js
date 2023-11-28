d96b1e93b23abf9700620a32a1ab4a2d4c9304ca
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-28 11:47:28 +0900

    refactor: changed module system to commonjs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from "next-contentlayer"
+const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,4 +11,4 @@ const nextConfig = {
   },
 }
 
-export default withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig)

25dc1d8e277ec4735245906aaf264c7b57b02c3f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-29 02:34:00 +0900

    feat: added Inter font and responsive meta tags

--- docs/pages/_app.page.tsx
+++ docs/pages/_app.page.tsx
@@ -4,11 +4,18 @@ import {
   themeSchemeManager,
 } from "@yamada-ui/react"
 import type { AppProps } from "next/app"
+import { Inter } from "next/font/google"
 import Head from "next/head"
 import type { FC } from "react"
-
 import { I18nProvider } from "contexts/i18n-context"
 import { theme, config } from "theme"
+
+const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin", "latin-ext"],
+  style: "normal",
+  display: "block",
+})
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const { cookies } = pageProps
@@ -16,6 +23,8 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <title>Yamada UI</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
@@ -35,7 +44,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         }
       >
         <I18nProvider>
-          <Component {...pageProps} />
+          <Component {...{ ...pageProps, inter }} />
         </I18nProvider>
       </UIProvider>
     </>

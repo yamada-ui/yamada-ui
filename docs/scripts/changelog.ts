e65a348aa02a24391b59cfd1719a371521cdfd66
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-06 05:47:49 +0900

    refactor(scripts): updated changelog script logic

--- docs/scripts/changelog.ts
+++ docs/scripts/changelog.ts
@@ -1,70 +1,108 @@
-import { readFile, readdir, writeFile } from "fs/promises"
+import { writeFile } from "fs/promises"
 import path from "path"
+import { Octokit } from "@octokit/rest"
 import chalk from "chalk"
+import { config } from "dotenv"
 import type { GrayMatterFile } from "gray-matter"
 import matter from "gray-matter"
 import { CONSTANT } from "constant"
 import { prettier } from "libs/prettier"
 
+config()
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+
 type Input = string | Buffer
+type MdxFile = { name: string; version: number[]; data: Data; content: Content }
 type Data = GrayMatterFile<Input>["data"]
 type Content = GrayMatterFile<Input>["content"]
 type Locale = (typeof LOCALES)[number]
 
-const DIR_PATH = path.join("contents", "changelog")
+const SOURCE_PATH = path.join(".changelog")
+const DIST_PATH = path.join("contents", "changelog")
 const LOCALES = CONSTANT.I18N.LOCALES.map(({ value }) => value)
 const LOCALE_MENU_MAP = {
   en: "Changelog",
   ja: "変更履歴",
 }
+const REPO_REQUEST_PARAMETERS = {
+  owner: "hirotomoyamada",
+  repo: "yamada-ui",
+  path: SOURCE_PATH,
+  ref: "main",
+}
 
-const getVersions = (fileNames: string[]) =>
-  fileNames
-    .map((fileName) => {
-      if (fileName.startsWith("index")) return
+const getMdxFiles = async () => {
+  const { data } = await octokit.repos.getContent(REPO_REQUEST_PARAMETERS)
 
-      return fileName
+  const mdxFiles: Omit<MdxFile, "version">[] = []
+
+  if (Array.isArray(data)) {
+    await Promise.all(
+      data.map(async ({ name, path }) => {
+        if (name.startsWith("manifest")) return
+
+        try {
+          const res = await octokit.repos.getContent({
+            ...REPO_REQUEST_PARAMETERS,
+            path,
+          })
+
+          if ("content" in res.data) {
+            const file = Buffer.from(res.data.content, "base64").toString(
+              "utf-8",
+            )
+
+            const { data, content } = matter(file)
+
+            mdxFiles.push({ name, data, content })
+          }
+        } catch (e) {
+          console.log(`[props]: Not found ${name}`)
+        }
+      }),
+    )
+  }
+
+  const resolvedMdxFiles = mdxFiles
+    .map((item) => ({
+      ...item,
+      version: item.name
         .replace("v", "")
         .replace(".mdx", "")
         .split(".")
-        .map((n) => parseInt(n))
-    })
+        .map((n) => parseInt(n)),
+    }))
     .filter(Boolean)
     .sort((a, b) => {
-      for (let i = 0; i < Math.max(a.length, b.length); i++) {
-        if ((a[i] || 0) > (b[i] || 0)) return -1
-        if ((a[i] || 0) < (b[i] || 0)) return 1
+      for (let i = 0; i < Math.max(a.version.length, b.version.length); i++) {
+        if ((a.version[i] || 0) > (b.version[i] || 0)) return -1
+        if ((a.version[i] || 0) < (b.version[i] || 0)) return 1
       }
 
       return 0
     })
 
-const versionToFileName = (version: number[]) => `v${version.join(".")}.mdx`
+  return resolvedMdxFiles
+}
 
-const generateMdxFiles = (fileNames: string[]) =>
+const generateMdxFiles = (mdxFiles: MdxFile[]) =>
   Promise.all(
-    fileNames.map(async (fileName, index) => {
-      const filePath = path.join(DIR_PATH, fileName)
-      const { data, content } = await getMdxFile(filePath)
+    mdxFiles.map(async ({ name, data, content }, index) => {
+      const outPath = path.join(DIST_PATH, name)
 
       data.table_of_contents_max_lv = 2
       data.order = index + 1
 
-      await writeMdxFile(filePath, data, content)
+      await writeMdxFile(outPath, data, content)
 
-      console.log(`[changelog]: Generated ${fileName}`)
+      console.log(chalk.green(`[changelog]: Generated ${name}`))
 
       if (index !== 0) return
 
       await writeMdxIndexFiles(data, content)
     }),
   )
-
-const getMdxFile = async (path: string) => {
-  const file = await readFile(path, "utf8")
-
-  return matter(file)
-}
 
 const writeMdxFile = async (path: string, data: Data, content: Content) => {
   let file = matter.stringify(content, data)
@@ -88,7 +126,7 @@ const writeMdxIndexFiles = async (data: Data, content: Content) => {
       data.menu = LOCALE_MENU_MAP[locale]
 
       await writeMdxFile(
-        path.join(DIR_PATH, getMdxFileName("index", locale)),
+        path.join(DIST_PATH, getMdxFileName("index", locale)),
         data,
         content,
       )
@@ -103,11 +141,9 @@ const writeMdxIndexFiles = async (data: Data, content: Content) => {
 }
 
 const main = async () => {
-  const fileNames = await readdir(DIR_PATH)
-  const versions = getVersions(fileNames)
-  const resolvedFileNames = versions.map(versionToFileName)
+  const mdxFiles = await getMdxFiles()
 
-  await generateMdxFiles(resolvedFileNames)
+  await generateMdxFiles(mdxFiles)
 }
 
 main()

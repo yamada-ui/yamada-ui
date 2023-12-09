86c4d139bfbda9af2ffadfccc8543f37341b2187
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-09 20:37:15 +0900

    refactor(calendar): removed unused export types

---
+++ .changeset/six-rings-think.md
@@ -0,0 +1,5 @@
+---
+"@yamada-ui/calendar": patch
+---
+
+Removed unnecessary export types.
--- packages/components/calendar/src/index.ts
+++ packages/components/calendar/src/index.ts
@@ -1,6 +1,5 @@
 export { Calendar } from "./calendar"
 export type { CalendarProps } from "./calendar"
-export type { UseCalendarProps } from "./use-calendar"
 export {
   isMonthInRange,
   isSameMonth,
@@ -10,7 +9,6 @@ export {
   isAfterMaxMonth,
   isBeforeMinMonth,
 } from "./use-calendar"
-export type { WeekdayProps, DayProps } from "./month"
 export { DatePicker } from "./date-picker"
 export type { DatePickerProps } from "./date-picker"
 export { MonthPicker } from "./month-picker"

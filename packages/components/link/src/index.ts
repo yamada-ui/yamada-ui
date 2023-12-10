d306d2cc58b8e3e524dd32cb42ed3ef2eaaaffa7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-10 10:12:16 +0900

    refactor: renamed linkBox files to link-box

--- packages/components/link/src/index.ts
+++ packages/components/link/src/index.ts
@@ -1,4 +1,4 @@
 export { Link } from "./link"
 export type { LinkProps } from "./link"
-export { LinkBox, LinkOverlay } from "./linkBox"
-export type { LinkBoxProps, LinkOverlayProps } from "./linkBox"
+export { LinkBox, LinkOverlay } from "./link-box"
+export type { LinkBoxProps, LinkOverlayProps } from "./link-box"

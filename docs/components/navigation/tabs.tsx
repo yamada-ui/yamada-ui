9d086f1283e104fad46984ba3b99f3097c54322a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-15 08:01:42 +0900

    refactor(components): update tabs with new UI library

--- docs/components/navigation/tabs.tsx
+++ docs/components/navigation/tabs.tsx
@@ -1,11 +1,11 @@
-import type { StackProps } from "@yamada-ui/react"
-import { forwardRef, HStack, dataAttr, Box, Center } from "@yamada-ui/react"
-import Link from "next/link"
+import type { BoxProps } from "@yamada-ui/react"
+import { forwardRef, Box, Tabs as UITabs, Tab } from "@yamada-ui/react"
 import { useRouter } from "next/router"
 import { memo } from "react"
+import { NextLink } from "./next-link"
 import { usePage } from "contexts/page-context"
 
-export type TabsProps = StackProps
+export type TabsProps = BoxProps
 
 export const Tabs = memo(
   forwardRef<TabsProps, "div">(({ ...rest }, ref) => {
@@ -13,46 +13,27 @@ export const Tabs = memo(
     const { asPath } = useRouter()
 
     return documentTabs.length ? (
-      <Box as="nav" overflowX="auto" mt="normal">
-        <HStack
-          ref={ref}
+      <Box ref={ref} as="nav" mt="normal" {...rest}>
+        <UITabs
           as="ul"
           index={documentTabs.findIndex(({ slug }) => slug === asPath)}
-          gap="0"
-          borderColor="inherit"
-          borderBottomWidth="1px"
-          {...rest}
         >
           {documentTabs.map(({ title, slug }) => (
-            <Box as="li" key={slug}>
-              <Center
-                as={Link}
-                href={slug}
-                data-selected={dataAttr(asPath === slug)}
-                py="2"
-                px="4"
-                borderColor="transparent"
-                borderBottomWidth="1px"
-                borderBottomStyle="solid"
-                marginBottom="-1px"
-                whiteSpace="nowrap"
-                transitionProperty="common"
-                transitionDuration="normal"
-                _hover={{ opacity: 0.7 }}
-                _focus={{ outline: "none" }}
-                _focusVisible={{ zIndex: 1, boxShadow: "outline" }}
-                _selected={{
-                  color: [`primary.600`, `primary.300`],
-
-                  borderColor: "currentColor",
-                  _hover: { opacity: 1 },
+            <Tab as="li" key={slug} p="0">
+              <NextLink
+                color="inherit"
+                _hover={{
+                  textDecoration: "inherit",
                 }}
+                href={slug}
+                py="sm"
+                px="md"
               >
                 {title}
-              </Center>
-            </Box>
+              </NextLink>
+            </Tab>
           ))}
-        </HStack>
+        </UITabs>
       </Box>
     ) : null
   }),

8dd6e3453ab5acc8d5cf4692c2e5976550193f69
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-18 01:05:07 +0900

    refactor: updated theme management implementation

--- examples/nextjs-pages/pages/_app.tsx
+++ examples/nextjs-pages/pages/_app.tsx
@@ -1,26 +1,18 @@
 import type { AppProps } from 'next/app'
-import {
-  UIProvider,
-  colorModeCookieStorageManager,
-  colorModeSSRManager,
-  themeSchemeCookieStorageManager,
-  themeSchemeSSRManager,
-} from '@yamada-ui/react'
+import { UIProvider, createColorModeManager, createThemeSchemeManager } from '@yamada-ui/react'
 import { theme, config } from 'theme'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const { cookies } = pageProps
-  const resolvedColorModeManager =
-    typeof cookies === 'string' ? colorModeSSRManager(cookies) : colorModeCookieStorageManager
-  const resolvedThemeSchemeManager =
-    typeof cookies === 'string' ? themeSchemeSSRManager(cookies) : themeSchemeCookieStorageManager
+  const colorModeManager = createColorModeManager('ssr', cookies)
+  const themeSchemeManager = createThemeSchemeManager('ssr', cookies)
 
   return (
     <UIProvider
       theme={theme}
       config={config}
-      colorModeManager={resolvedColorModeManager}
-      themeSchemeManager={resolvedThemeSchemeManager}
+      colorModeManager={colorModeManager}
+      themeSchemeManager={themeSchemeManager}
     >
       <Component {...pageProps} />
     </UIProvider>

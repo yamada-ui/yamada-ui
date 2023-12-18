45826f6a9a95795656fef11bbfe46849d8a09fe0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-18 14:01:36 +0900

    refactor(scripts): replaced ora with clack prompts

--- scripts/mattermost.ts
+++ scripts/mattermost.ts
@@ -1,23 +1,34 @@
 import { readFile } from "fs/promises"
+import * as p from "@clack/prompts"
+import c from "chalk"
 import { config } from "dotenv"
 import fetch from "node-fetch"
-import ora from "ora"
 
 config()
 
 const main = async () => {
-  const spinner = ora("Send a message to Mattermost").start()
+  p.intro(c.magenta(`Send a message to Mattermost`))
+
+  const s = p.spinner()
 
   try {
+    const start = process.hrtime.bigint()
+
     const url = process.env.MATTERMOST_WEBHOOK_URL
 
     if (!url) throw new Error("Missing Mattermost Webhook URL\n")
 
+    s.start(`Getting the changelog`)
+
     const manifest = await readFile(".changelog/manifest.json", "utf8")
 
-    let [{ version }] = JSON.parse(manifest)
+    const [{ version }] = JSON.parse(manifest)
 
     const text = `A new version of Yamada UI has been released😎\n\nversion: [${version}](https://github.com/hirotomoyamada/yamada-ui/blob/main/.changelog/v${version}.mdx)`
+
+    s.stop(`Got the changelog`)
+
+    s.start(`Sending to Mattermost`)
 
     const data = {
       username: "GitHub",
@@ -31,15 +42,18 @@ const main = async () => {
 
     const { ok } = await fetch(url, { method: "POST", headers, body })
 
-    if (ok) {
-      spinner.succeed("Successfully sent message to Mattermost\n")
-    } else {
-      throw new Error("Failed to send message to Mattermost\n")
-    }
+    if (!ok) throw new Error("Failed to send message to Mattermost\n")
+
+    s.stop(`Sent to Mattermost`)
+
+    const end = process.hrtime.bigint()
+    const duration = (Number(end - start) / 1e9).toFixed(2)
+
+    p.outro(c.green(`Done in ${duration}s\n`))
   } catch (e) {
-    if (e instanceof Error) spinner.fail(e.message)
-  } finally {
-    spinner.stop()
+    s.stop(`An error occurred`, 500)
+
+    p.cancel(c.red(e instanceof Error ? e.message : "Message is missing"))
   }
 }
 

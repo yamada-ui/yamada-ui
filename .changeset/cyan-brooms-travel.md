194c9e2d37920f9a68ec2e8bd8f5776eb948d777
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-24 06:14:28 +0900

    feat: added ping animation to Avatar and Indicator

---
+++ .changeset/cyan-brooms-travel.md
@@ -0,0 +1,6 @@
+---
+"@yamada-ui/indicator": minor
+"@yamada-ui/avatar": minor
+---
+
+Added ping animation.
--- packages/components/avatar/src/avatar-badge.tsx
+++ packages/components/avatar/src/avatar-badge.tsx
@@ -1,5 +1,6 @@
-import type { HTMLUIProps, CSSUIObject } from "@yamada-ui/core"
+import type { HTMLUIProps, CSSUIObject, AnimationStyle } from "@yamada-ui/core"
 import { ui, forwardRef } from "@yamada-ui/core"
+import { useAnimation } from "@yamada-ui/use-animation"
 import { cx } from "@yamada-ui/utils"
 import { useAvatarContext } from "./avatar"
 
@@ -10,6 +11,36 @@ type AvatarBadgeOptions = {
    * @default 'bottom-end'
    */
   placement?: "top-start" | "top-end" | "bottom-start" | "bottom-end"
+  /**
+   * If `true`, make an element scale and fade like a radar ping or ripple of water.
+   *
+   * @default false
+   */
+  ping?: boolean
+  /**
+   * It is used for the color of the ping animation.
+   *
+   * @default '["blackAlpha.400", "whiteAlpha.500"]''
+   */
+  pingColor?: HTMLUIProps<"div">["backgroundColor"]
+  /**
+   * It is used for the scale of the ping animation.
+   *
+   * @default 1.8
+   */
+  pingScale?: number
+  /**
+   * It is used for the count of the ping animation.
+   *
+   * @default "infinite"
+   */
+  pingCount?: AnimationStyle["iterationCount"]
+  /**
+   * It is used for the duration of the ping animation.
+   *
+   * @default "1.4s"
+   */
+  pingDuration?: AnimationStyle["direction"]
 }
 
 export type AvatarBadgeProps = HTMLUIProps<"div"> & AvatarBadgeOptions
@@ -41,8 +72,34 @@ const placementStyles: Record<
 }
 
 export const AvatarBadge = forwardRef<AvatarBadgeProps, "div">(
-  ({ className, boxSize = "1em", placement = "bottom-end", ...rest }, ref) => {
+  (
+    {
+      className,
+      boxSize = "1em",
+      placement = "bottom-end",
+      ping,
+      pingColor = ["blackAlpha.400", "whiteAlpha.500"],
+      pingDuration = "1.4s",
+      pingCount = "infinite",
+      pingScale = 1.8,
+      children,
+      ...rest
+    },
+    ref,
+  ) => {
     const styles = useAvatarContext()
+    const animation = useAnimation({
+      keyframes: {
+        "75%, 100%": {
+          transform: `scale(${pingScale})`,
+          opacity: 0,
+        },
+      },
+      fillMode: "forwards",
+      duration: pingDuration,
+      timingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+      iterationCount: pingCount,
+    })
 
     const css: CSSUIObject = {
       position: "absolute",
@@ -60,7 +117,24 @@ export const AvatarBadge = forwardRef<AvatarBadgeProps, "div">(
         boxSize={boxSize}
         __css={css}
         {...rest}
-      />
+      >
+        {children}
+
+        {ping ? (
+          <ui.div
+            className="ui-avatar__badge__ping"
+            __css={{
+              position: "absolute",
+              boxSize: "full",
+              rounded: "full",
+              opacity: 0.75,
+              zIndex: -1,
+              bg: pingColor,
+            }}
+            animation={animation}
+          />
+        ) : null}
+      </ui.div>
     )
   },
 )

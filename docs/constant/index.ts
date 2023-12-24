d57037c90ce199d20e3e68cd2d3bc3553a4270cc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-12-24 12:41:38 +0900

    feat(components): added contributors component and data

--- docs/constant/index.ts
+++ docs/constant/index.ts
@@ -1,5 +1,7 @@
+import { CONTRIBUTORS } from "./contributors"
+import { CORE_MEMBERS } from "./core-members"
 import { I18N } from "./i18n"
 import { SEO } from "./seo"
 import { SNS } from "./sns"
 
-export const CONSTANT = { SNS, SEO, I18N }
+export const CONSTANT = { SNS, SEO, I18N, CONTRIBUTORS, CORE_MEMBERS }

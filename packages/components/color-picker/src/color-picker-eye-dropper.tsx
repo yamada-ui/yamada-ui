154a9b172b9d6bd83a68b4ff401c777927c4e32f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-01-09 15:47:01 +0900

    refactor(components): extracted EyeDropperIcon component

--- packages/components/color-picker/src/color-picker-eye-dropper.tsx
+++ packages/components/color-picker/src/color-picker-eye-dropper.tsx
@@ -2,8 +2,10 @@ import type { IconButtonProps } from "@yamada-ui/button"
 import { IconButton } from "@yamada-ui/button"
 import { forwardRef } from "@yamada-ui/core"
 import type { CSSUIObject } from "@yamada-ui/core"
+import type { IconProps } from "@yamada-ui/icon"
 import { Icon } from "@yamada-ui/icon"
 import { cx } from "@yamada-ui/utils"
+import type { FC } from "react"
 import { useColorPickerContext } from "./use-color-picker"
 
 export type ColorPickerEyeDropperProps = IconButtonProps
@@ -29,20 +31,25 @@ export const ColorPickerEyeDropper = forwardRef<
       __css={css}
       {...getEyeDropperProps(rest, ref)}
     >
-      <Icon
-        className="ui-color-picker__eye-dropper__icon"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        __css={{ ...styles.eyeDropperIcon }}
-      >
-        <path d="m2 22 1-1h3l9-9" />
-        <path d="M3 21v-3l9-9" />
-        <path d="m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z" />
-      </Icon>
+      <EyeDropperIcon className="ui-color-picker__eye-dropper__icon" />
     </IconButton>
   )
 })
+
+export const EyeDropperIcon: FC<IconProps> = ({ ...rest }) => {
+  return (
+    <Icon
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...rest}
+    >
+      <path d="m2 22 1-1h3l9-9" />
+      <path d="M3 21v-3l9-9" />
+      <path d="m15 6 3.4-3.4a2.1 2.1 0 1 1 3 3L18 9l.4.4a2.1 2.1 0 1 1-3 3l-3.8-3.8a2.1 2.1 0 1 1 3-3l.4.4Z" />
+    </Icon>
+  )
+}

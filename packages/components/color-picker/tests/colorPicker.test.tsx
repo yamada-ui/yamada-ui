e1f68186a2214294ea563d222e03c3813daec466
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-07 19:35:54 +0900

    test(color-picker): simplified and removed tests

--- packages/components/color-picker/tests/colorPicker.test.tsx
+++ packages/components/color-picker/tests/colorPicker.test.tsx
@@ -3,36 +3,29 @@ import { ColorPicker } from "../src"
 
 describe("<ColorPicker />", () => {
   test("ColorPicker renders correctly", async () => {
-    render(<ColorPicker data-testid="color" />)
-    const colorPicker = screen.getByTestId("color")
+    render(<ColorPicker data-testid="colorPicker" />)
+
+    const colorPicker = screen.getByTestId("colorPicker")
+
     expect(colorPicker).toBeInTheDocument()
   })
 
   test("ColorPicker changes color on selecting a new color", () => {
-    render(<ColorPicker data-testid="color" />)
-    const colorPicker = screen.getByTestId("color")
-    fireEvent.change(colorPicker, { target: { value: "#ff0000" } })
-    expect(colorPicker).toHaveValue("#ff0000")
+    render(<ColorPicker data-testid="colorPicker" />)
+
+    const colorPicker = screen.getByTestId("colorPicker")
+    const value = "#ff0000"
+
+    fireEvent.change(colorPicker, { target: { value } })
+
+    expect(colorPicker).toHaveValue(value)
   })
 
-  test("ColorPicker renders with the correct initial color", () => {
-    render(<ColorPicker color="#00ff00" data-testid="color" />)
-    const colorPicker = screen.getByTestId("color")
-    expect(colorPicker).toHaveValue("#00ff00")
-  })
+  test("ColorPicker renders with the correct initial value", () => {
+    render(<ColorPicker defaultValue="#00ff00" data-testid="colorPicker" />)
 
-  test("ColorPicker displays a color preview", () => {
-    render(<ColorPicker data-testid="color" />)
-    const colorPreview = screen.getByTestId("color")
-    expect(colorPreview).toBeInTheDocument()
-  })
+    const colorPicker = screen.getByTestId("colorPicker")
 
-  test("ColorPicker resets to initial color on reset button click", () => {
-    render(<ColorPicker color="#00ff00" data-testid="color" />)
-    const colorPicker = screen.getByTestId("color")
-    fireEvent.change(colorPicker, { target: { value: "#ff0000" } })
-    const resetButton = screen.getByRole("button", { name: /reset/i })
-    fireEvent.click(resetButton)
     expect(colorPicker).toHaveValue("#00ff00")
   })
 })

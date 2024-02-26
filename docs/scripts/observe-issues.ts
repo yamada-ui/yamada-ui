b36bba4a65433040b2a3da6541efdff8397ea191
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-02-26 16:48:39 +0900

    ci: added discord webhook env variable

--- docs/scripts/observe-issues.ts
+++ docs/scripts/observe-issues.ts
@@ -1,4 +1,5 @@
 import { Octokit } from "@octokit/rest"
+import { isObject } from "@yamada-ui/react"
 import { config } from "dotenv"
 
 type Issue = Awaited<
@@ -18,6 +19,16 @@ const INFORMATION_COMMENT = (id: string) =>
     `Hi, Thanks for the working on this issue!`,
     `We will return this issue assignment to initial state.`,
     `If you are working on this issue and would like to be reassigned, please contact us.`,
+  ].join("\n\n")
+const DISCORD_HELP_WANTED_COMMENT = (
+  number: number,
+  title: string,
+  html_url: string,
+) =>
+  [
+    `<@&1202956318718304276>`,
+    `Help!, I need somebody, Help!, not just anybody,\nHelp!, you know I need someone, Help!`,
+    `[${number}: ${title}](${html_url})`,
   ].join("\n\n")
 
 config()
@@ -40,7 +51,6 @@ const getIssues = async () => {
     const { data } = await octokit.issues.listForRepo({
       ...COMMON_PARAMS,
       state: "open",
-      assignee: "*",
       per_page: perPage,
       page,
     })
@@ -52,33 +62,74 @@ const getIssues = async () => {
     page++
   } while (count === perPage)
 
+  issues = issues.filter(({ pull_request }) => !pull_request)
+
   return issues
 }
 
-const filterIssues = async (issues: Issue[], collaborators: Collaborator[]) => {
-  const collaboratorIds = collaborators.map(({ login }) => login)
+const addHelpWantedLabel = async (issues: Issue[]) => {
+  const url = process.env.DISCORD_HELP_WANTED_WEBHOOK_URL
 
-  issues = issues.filter(({ pull_request }) => !pull_request)
+  if (!url) throw new Error("Missing Discord Webhook URL\n")
 
-  issues = issues.filter(
-    ({ assignees }) =>
-      assignees?.some(({ login }) => !collaboratorIds.includes(login)),
+  await Promise.all(
+    issues.map(
+      async ({ number, title, labels, created_at, assignee, html_url }) => {
+        if (assignee) return
+
+        if (labels.every((label) => isObject(label) && label.name !== "bug"))
+          return
+
+        if (
+          labels.some(
+            (label) =>
+              isObject(label) &&
+              [
+                "fixed",
+                "discussion",
+                "help wanted",
+                "question",
+                "ongoing",
+                "wontfix",
+                "duplicate",
+                "confirm",
+              ].includes(label.name ?? ""),
+          )
+        )
+          return
+
+        const createdTimestamp = new Date(created_at)
+        const limitTimestamp = new Date(Date.now() - 3 * 24 * 60 * 60 * 1000)
+
+        if (createdTimestamp > limitTimestamp) return
+
+        await octokit.issues.addLabels({
+          ...COMMON_PARAMS,
+          issue_number: number,
+          labels: ["help wanted"],
+        })
+
+        console.log("Added label", number, title)
+
+        const content = DISCORD_HELP_WANTED_COMMENT(number, title, html_url)
+
+        await sendDiscordChannel(url, content)
+
+        console.log("Send discord", number, title)
+      },
+    ),
   )
+}
 
-  const resolvedIssues = (
-    await Promise.all(
-      issues.map(async (issue) => {
-        const { created_at } = await getAssignedEvent(issue.number)
+const sendDiscordChannel = async (url: string, content: string) => {
+  const data = { username: "GitHub", content }
 
-        const eventTimestamp = new Date(created_at)
-        const limitTimestamp = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000)
+  const headers = { "Content-Type": "application/json" }
+  const body = JSON.stringify(data)
 
-        if (eventTimestamp <= limitTimestamp) return issue
-      }),
-    )
-  ).filter(Boolean) as Issue[]
+  const { ok } = await fetch(url, { method: "POST", headers, body })
 
-  return resolvedIssues
+  if (!ok) throw new Error("Failed to send message to Discord\n")
 }
 
 const getAssignedEvent = async (issue_number: number) => {
@@ -92,10 +143,26 @@ const getAssignedEvent = async (issue_number: number) => {
   return assignedEvents.at(-1) as Event
 }
 
-const clearAssignIssue = async (issues: Issue[]) =>
+const clearAssignIssues = async (
+  issues: Issue[],
+  collaborators: Collaborator[],
+) => {
+  const collaboratorIds = collaborators.map(({ login }) => login)
+
+  issues = issues.filter(({ assignees }) =>
+    assignees?.some(({ login }) => !collaboratorIds.includes(login)),
+  )
+
   await Promise.all(
     issues.map(async ({ number, title, assignee }) => {
       if (!assignee) return
+
+      const { created_at } = await getAssignedEvent(number)
+
+      const eventTimestamp = new Date(created_at)
+      const limitTimestamp = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000)
+
+      if (eventTimestamp > limitTimestamp) return
 
       await octokit.issues.createComment({
         ...COMMON_PARAMS,
@@ -114,6 +181,7 @@ const clearAssignIssue = async (issues: Issue[]) =>
       return
     }),
   )
+}
 
 const main = async () => {
   try {
@@ -121,9 +189,8 @@ const main = async () => {
 
     const issues = await getIssues()
 
-    const resolvedIssues = await filterIssues(issues, collaborators)
-
-    await clearAssignIssue(resolvedIssues)
+    await addHelpWantedLabel(issues)
+    await clearAssignIssues(issues, collaborators)
   } catch (e) {
     if (e instanceof Error) console.log(e.message)
   }

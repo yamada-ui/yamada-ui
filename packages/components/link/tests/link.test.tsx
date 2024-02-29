3b92271d421e93a4144e9ab9aa33f43fe7e6fe83
taroj1205 <taroj1205@gmail.com>
2024-02-29 21:23:08 +1300

    test(link): enhance test coverage

--- packages/components/link/tests/link.test.tsx
+++ packages/components/link/tests/link.test.tsx
@@ -1,5 +1,5 @@
 import { a11y, render, screen } from "@yamada-ui/test"
-import { Link } from "../src"
+import { Link, LinkBox, LinkOverlay } from "../src"
 
 describe("<Link />", () => {
   test("Link renders correctly", async () => {
@@ -29,5 +29,69 @@ describe("<Link />", () => {
     )
     expect(link).toHaveAttribute("rel", "noopener")
     expect(link).toHaveTextContent("Welcome page")
+  })
+})
+
+describe("<LinkOverlay />", () => {
+  test("renders correctly", () => {
+    render(
+      <LinkOverlay href="https://hirotomoyamada.github.io/yamada-ui/?path=/docs/documents-welcome--docs">
+        Welcome page
+      </LinkOverlay>,
+    )
+    const link = screen.getByRole("link")
+
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute(
+      "href",
+      "https://hirotomoyamada.github.io/yamada-ui/?path=/docs/documents-welcome--docs",
+    )
+    expect(link).toHaveTextContent("Welcome page")
+  })
+
+  test("opens link in a new tab when isExternal is true", () => {
+    render(
+      <LinkOverlay
+        href="https://hirotomoyamada.github.io/yamada-ui/?path=/docs/documents-welcome--docs"
+        isExternal
+      >
+        Welcome page
+      </LinkOverlay>,
+    )
+    const link = screen.getByRole("link")
+
+    expect(link).toHaveAttribute("target", "_blank")
+    expect(link).toHaveAttribute("rel", "noopener")
+  })
+
+  test("applies custom className", () => {
+    render(
+      <LinkOverlay
+        href="https://hirotomoyamada.github.io/yamada-ui/?path=/docs/documents-welcome--docs"
+        className="custom-class"
+      >
+        Welcome page
+      </LinkOverlay>,
+    )
+    const link = screen.getByRole("link")
+
+    expect(link).toHaveClass("custom-class")
+  })
+})
+
+describe("<LinkBox />", () => {
+  test("renders correctly", () => {
+    render(<LinkBox>Welcome page Box</LinkBox>)
+    const box = screen.getByText("Welcome page Box")
+
+    expect(box).toBeInTheDocument()
+    expect(box).toHaveTextContent("Welcome page Box")
+  })
+
+  test("applies custom className", () => {
+    render(<LinkBox className="custom-class">Welcome page Box</LinkBox>)
+    const box = screen.getByText("Welcome page Box")
+
+    expect(box).toHaveClass("custom-class")
   })
 })

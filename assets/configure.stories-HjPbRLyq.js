bbfa682ab34f42bbd14c39f31e32d4cdf602e99c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-03 10:08:24 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@54aed7c5a2fa85dbbc8f2076e7a5514cba9a1144 🚀

---
+++ assets/configure.stories-HjPbRLyq.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-TtYKBvr-.js";import{U as a}from"./ui-provider-0EaMHHa0.js";import{e as g}from"./extend-config-HRsKAVtD.js";import{C as u}from"./center-buHHRxIa.js";import{I as d}from"./image-JRY3unre.js";import"./index-IybTgENJ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-rmcOHXeV.js";import"./theme-provider-rjxHA_Gz.js";import"./factory-APG2CWDq.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-_OkO9XJu.js";import"./environment-provider-ebV35j6x.js";import"./motion-RGBG165B.js";import"./loading-provider-sj5IPeRo.js";import"./index-dH9witMp.js";import"./Combination-UEGTaxpD.js";import"./loading-Z6ToCT2V.js";import"./index-1lNNAzjq.js";import"./index-yCED6MhK.js";import"./index-immFGolW.js";import"./icon-7S-xPcKO.js";import"./forward-ref-6T0UNPU-.js";import"./motion-YDUrcbbR.js";import"./index-dKw0visz.js";import"./container-portal-o5r_vKh6.js";import"./index-xIHD0DlW.js";import"./notice-ie1hmQHC.js";import"./alert-t99HKK4L.js";import"./use-component-style-XlqdEMGL.js";import"./close-button-ai1MnZ7G.js";import"./use-ripple-tuvEI28x.js";import"./use-image-AuCul83v.js";const Q={title:"System / Theme / Configure"},o=()=>{const e=g({});return r(a,{config:e,children:r(f,{})})},t=()=>r(a,{config:{},children:r(f,{})}),f=()=>r(u,{w:"calc(100vw - 16px * 2)",h:"calc(100vh - 16px * 2)",children:r(d,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/yamada-ui/yamada-ui/main/logo/logo-colored@2x.png"})});var i,n,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const config = extendConfig({
+    /**
+     * Define the config you want to customize, merging them with the default config
+     */
+  });
+  return <UIProvider config={config}>
+      <App />
+    </UIProvider>;
+}`,...(m=(n=o.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var p,c,s;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const config = {
+    /**
+     * Define the config you want to customize
+     */
+  };
+  return <UIProvider config={config}>
+      <App />
+    </UIProvider>;
+}`,...(s=(c=t.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};const R=["extendConfigure","overrideConfigure"];export{R as __namedExportsOrder,Q as default,o as extendConfigure,t as overrideConfigure};
--- assets/markdown.stories-O0wsPoyg.js
+++ assets/markdown.stories-O0wsPoyg.js
@@ -63,7 +63,7 @@ Status is error.
 :::
 `,Ic=`
 <p align="center">
-  <img src="https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png" alt="Yamada UI" width="480" />
+  <img src="https://raw.githubusercontent.com/yamada-ui/yamada-ui/main/logo/logo-colored@2x.png" alt="Yamada UI" width="480" />
 </p>
 
 <p align="center">
@@ -73,9 +73,9 @@ Status is error.
   </a>
   <img alt="NPM Downloads" src="https://img.shields.io/npm/dm/@yamada-ui/react.svg?style=flat"/>
   <a href="https://github.com/yamada-ui/yamada-ui/blob/main/LICENSE">
-    <img alt="MIT License" src="https://img.shields.io/github/license/hirotomoyamada/yamada-ui"/>
+    <img alt="MIT License" src="https://img.shields.io/github/license/yamada-ui/yamada-ui"/>
   </a>
-  <img alt="Github Stars" src="https://img.shields.io/github/stars/hirotomoyamada/yamada-ui" />
+  <img alt="Github Stars" src="https://img.shields.io/github/stars/yamada-ui/yamada-ui" />
   <a href="https://discord.gg/H7V5RfEDTR">
     <img alt="MIT License" src="https://img.shields.io/badge/Chat%20on-Discord-%235865f2"/>
   </a>

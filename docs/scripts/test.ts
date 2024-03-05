681de773688b057a78fae60dcc8e0531a723b710
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-05 17:59:37 +0900

    ci: added test workflow and script

---
+++ docs/scripts/test.ts
@@ -0,0 +1,30 @@
+import { Octokit } from "@octokit/rest"
+import { config } from "dotenv"
+
+const COMMON_PARAMS = { owner: "yamada-ui", repo: "yamada-docs" }
+
+config()
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+
+const main = async () => {
+  try {
+    const { data: collaboratorsA } = await octokit.repos.listCollaborators({
+      ...COMMON_PARAMS,
+    })
+
+    console.log(collaboratorsA)
+
+    const { data: collaboratorsB } = await octokit.repos.listCollaborators({
+      ...COMMON_PARAMS,
+      affiliation: "all",
+      per_page: 100,
+    })
+
+    console.log(collaboratorsB)
+  } catch (e) {
+    if (e instanceof Error) console.log(e.message)
+  }
+}
+
+main()

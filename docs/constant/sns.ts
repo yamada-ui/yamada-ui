70cdf3a954722573ba9dfb3f6b63d6d4e9e496cc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-07 16:18:14 +0900

    chore: updated Hirotomo's GitHub URL

--- docs/constant/sns.ts
+++ docs/constant/sns.ts
@@ -1,6 +1,6 @@
 export const SNS = {
   GITHUB: {
-    HIROTOMO_YAMADA: "https://github.com/yamada-ui",
+    HIROTOMO_YAMADA: "https://github.com/hirotomoyamada",
     YAMADA_UI: "https://github.com/yamada-ui/yamada-ui",
     DOC_EDIT_URL: "https://github.com/yamada-ui/yamada-docs/tree/main/contents",
     UI_EDIT_URL: "https://github.com/yamada-ui/yamada-ui/tree/main/packages",

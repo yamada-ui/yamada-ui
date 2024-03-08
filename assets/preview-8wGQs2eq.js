0c62d85a3745baac88e4fce55709e98c69fa5f4c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-08 19:16:34 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@32b301c71fd7c8deae5e891c7f64598bcce03c16 🚀

--- assets/preview-8wGQs2eq.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-84rv2eIm.js","./iframe-_CdpSPWj.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-mO45BXRy.js","./_basePickBy-W3dyMIi7.js","./mapValues-fvZQFbEN.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-KmmwFJNj.js","./inheritsLoose-Y9jOMJLd.js","./index-PPLHz8o0.js","./index-4libyTQa.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-_CdpSPWj.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-84rv2eIm.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12]),import.meta.url);return new r}}};export{a as parameters};
---
+++ assets/welcome.stories-jgo0Y3ag.js
@@ -0,0 +1,24 @@
+import{M as p}from"./index-wZoY38C2.js";import{J as a}from"./components-SK8bSf3b.js";import{j as o,a as e,F as s}from"./jsx-runtime-5BUNAZ9W.js";import{useMDXComponents as m}from"./index-_VGcpBFS.js";import{C as c}from"./center-vtSpiasz.js";import{I as d}from"./image-0vy852U8.js";import"./iframe-wxmot68Z.js";import"../sb-preview/runtime.js";import"./_basePickBy-W3dyMIi7.js";import"./mapValues-fvZQFbEN.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-4g5l5LRQ.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-KmmwFJNj.js";import"./index-jmm5gWkb.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-PPLHz8o0.js";import"./index-4libyTQa.js";import"./index-hQyOJv3L.js";import"./isPlainObject-mmZlXdLr.js";import"./ui-provider-IY1FvQAY.js";import"./index-xNRvIn36.js";import"./theme-provider-bBWP4weD.js";import"./factory-d4qha7R6.js";import"./environment-provider-zx8rJJd-.js";import"./motion-heeYkzTb.js";import"./loading-provider-TwU9g3-b.js";import"./index-c3ZI6J-D.js";import"./Combination-DfugsOYv.js";import"./loading-a2VmdJrN.js";import"./index-gPPkT-3X.js";import"./index-NfIPy_Y6.js";import"./index-Ar4QPYE3.js";import"./icon-hF0n87st.js";import"./forward-ref-A-8Arhkk.js";import"./motion-v7t8L39m.js";import"./index-eMn_GZVX.js";import"./container-portal-0Gk-FyfS.js";import"./notice-WyKhFkM1.js";import"./alert-vFvawrLQ.js";import"./use-component-style-d9n8R3cE.js";import"./close-button-vaxwjIzp.js";import"./use-ripple-WbBjk5fz.js";import"./container-2JHhdvL6.js";import"./stack-wIOq24gw.js";import"./box-gsgHkkli.js";import"./text-puUo1HSE.js";import"./use-image-uDLoSF_I.js";function n(t){const r=Object.assign({h1:"h1",ol:"ol",li:"li",code:"code",pre:"pre",p:"p"},m(),t.components);return e(s,{children:[o(p,{title:"Documents / Welcome"}),`
+`,o(a,{children:o(c,{mb:"16",children:o(d,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png"})})}),`
+`,o(r.h1,{id:"usage",children:"Usage"}),`
+`,e(r.ol,{children:[`
+`,e(r.li,{children:["Wrap your application with the ",o(r.code,{children:"UIProvider"})," provided"]}),`
+`]}),`
+`,o(r.pre,{children:o(r.code,{className:"language-tsx",children:`import { UIProvider } from "@yamada-ui/react"
+
+const App = ({ children }) => {
+  return <UIProvider>{children}</UIProvider>
+}
+
+export default App
+`})}),`
+`,o(r.p,{children:"Yamada-ui supports light and dark modes by default"}),`
+`,e(r.ol,{start:"2",children:[`
+`,o(r.li,{children:"Now you can start using components like so"}),`
+`]}),`
+`,o(r.pre,{children:o(r.code,{className:"language-tsx",children:`import { Text } from "@yamada-ui/react"
+
+const Example = () => {
+  return <Text>ギャルのパンティーおくれーーーっ！！！！！</Text>
+}
+`})})]})}function l(t={}){const{wrapper:r}=Object.assign({},m(),t.components);return r?o(r,{...t,children:o(n,{...t})}):n(t)}const u=()=>{throw new Error("Docs-only story")};u.parameters={docsOnly:!0};const i={title:"Documents / Welcome",tags:["stories-mdx"],includeStories:["__page"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:l};const pr=["__page"];export{pr as __namedExportsOrder,u as __page,i as default};

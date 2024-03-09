85f159ea2f83327a4dacc2e6e9f62ef93f7101d7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-09 12:55:50 +0900

    feat(header): added 'System' color mode option

--- examples/remix/app/components/layouts/header.tsx
+++ examples/remix/app/components/layouts/header.tsx
@@ -140,6 +140,9 @@ const ColorModeButton: FC<ColorModeButtonProps> = memo(
             <MenuOptionItem value="dark" closeOnSelect>
               Dark
             </MenuOptionItem>
+            <MenuOptionItem value="system" closeOnSelect>
+              System
+            </MenuOptionItem>
           </MenuOptionGroup>
         </MenuList>
       </Menu>
--- examples/remix/app/root.tsx
+++ examples/remix/app/root.tsx
@@ -8,18 +8,14 @@ import {
   useLoaderData,
 } from "@remix-run/react"
 import {
-  COLOR_MODE_STORAGE_KEY,
   ColorModeScript,
-  THEME_SCHEME_STORAGE_KEY,
   ThemeSchemeScript,
   UIProvider,
   createColorModeManager,
   createThemeSchemeManager,
 } from "@yamada-ui/react"
 import theme, { config } from "./theme"
-import { ClientOnly } from "remix-utils/client-only"
 import { json, LoaderFunction } from "@remix-run/node"
-import { useMemo } from "react"
 
 const parseCookie = (cookie: string, key: string) => {
   const match = cookie.match(new RegExp(`(^| )${key}=([^;]+)`))
@@ -40,31 +36,11 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function App() {
   let { cookies } = useLoaderData<{ cookies: string }>()
 
-  if (typeof document !== "undefined") cookies = document.cookie
-
   const colorModeManager = createColorModeManager("ssr", cookies)
   const themeSchemeManager = createThemeSchemeManager("ssr", cookies)
 
-  const colorMode = useMemo(
-    () =>
-      parseCookie(cookies, COLOR_MODE_STORAGE_KEY) ?? config.initialColorMode,
-    [],
-  )
-
-  const themeScheme = useMemo(
-    () =>
-      parseCookie(cookies, THEME_SCHEME_STORAGE_KEY) ??
-      config.initialThemeScheme,
-    [],
-  )
-
   return (
-    <html
-      lang="en"
-      data-mode={colorMode}
-      data-theme={themeScheme}
-      style={{ colorScheme: colorMode }}
-    >
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -72,23 +48,17 @@ export default function App() {
         <Links />
       </head>
 
-      <body className={`ui-${colorMode}`}>
-        <ClientOnly fallback={<></>}>
-          {() => (
-            <>
-              <ColorModeScript
-                type="cookie"
-                nonce="testing"
-                initialColorMode={config.initialColorMode}
-              />
-              <ThemeSchemeScript
-                type="cookie"
-                nonce="testing"
-                initialThemeScheme={config.initialThemeScheme}
-              />
-            </>
-          )}
-        </ClientOnly>
+      <body>
+        <ColorModeScript
+          type="cookie"
+          nonce="testing"
+          initialColorMode={config.initialColorMode}
+        />
+        <ThemeSchemeScript
+          type="cookie"
+          nonce="testing"
+          initialThemeScheme={config.initialThemeScheme}
+        />
 
         <UIProvider
           config={config}

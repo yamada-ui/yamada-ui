cf6425065f114412ce589b4c263df79778d357be
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-12 19:05:24 +0900

    feat(support): added support section in UI

--- docs/pages/index.page.tsx
+++ docs/pages/index.page.tsx
@@ -1,7 +1,22 @@
-import { HStack, Heading, Text, VStack } from "@yamada-ui/react"
+import {
+  Center,
+  HStack,
+  Heading,
+  Image,
+  Spacer,
+  Text,
+  VStack,
+  Wrap,
+} from "@yamada-ui/react"
 import type { InferGetStaticPropsType, NextPage } from "next"
 import { Section } from "components/layouts"
-import { SEO, Arrow, Github } from "components/media-and-icons"
+import {
+  SEO,
+  Arrow,
+  Github,
+  OpenCollective,
+  Patreon,
+} from "components/media-and-icons"
 import { NextLinkButton } from "components/navigation"
 import { CONSTANT } from "constant"
 import { useI18n } from "contexts/i18n-context"
@@ -71,6 +86,199 @@ const Page: NextPage<PageProps> = ({ documentTree }) => {
             </NextLinkButton>
           </HStack>
         </Section>
+
+        <Center bg={["primary.500", "primary.600"]} m="0 calc(50% - 50vw)">
+          <Section maxW="9xl" px={{ base: "lg", md: "md" }}>
+            <VStack alignItems="center" gap="lg">
+              <VStack alignItems="center" maxW="2xl" color="white">
+                <Heading size="2xl">{t("home.support.title")}</Heading>
+                <Text textAlign="center">{t("home.support.description")}</Text>
+              </VStack>
+
+              <VStack
+                alignItems="center"
+                maxW="2xl"
+                gap={{ base: "lg", md: "md" }}
+              >
+                <HStack
+                  w="full"
+                  p={{ base: "normal", md: "md" }}
+                  bg="white"
+                  rounded="md"
+                  flexDirection={{ base: "row", sm: "column" }}
+                >
+                  <HStack color="black">
+                    <OpenCollective boxSize="12" />
+
+                    <VStack gap="0">
+                      <Heading as="h2" fontSize="lg">
+                        Open Collective
+                      </Heading>
+
+                      <Text color="blackAlpha.700">
+                        {t("home.support.open-collective")}
+                      </Text>
+                    </VStack>
+                  </HStack>
+
+                  <Spacer display={{ base: "block", sm: "none" }} />
+
+                  <NextLinkButton
+                    w={{ base: "auto", sm: "full" }}
+                    href="https://opencollective.com/yamada-ui"
+                    isExternal
+                    colorScheme="primary"
+                    px="lg"
+                  >
+                    Sponsor
+                  </NextLinkButton>
+                </HStack>
+
+                <HStack
+                  w="full"
+                  p={{ base: "normal", md: "md" }}
+                  bg="white"
+                  rounded="md"
+                  flexDirection={{ base: "row", sm: "column" }}
+                >
+                  <HStack color="black">
+                    <Patreon boxSize="12" />
+
+                    <VStack gap="0">
+                      <Heading as="h2" fontSize="lg">
+                        Patreon
+                      </Heading>
+
+                      <Text color="blackAlpha.700">
+                        {t("home.support.patreon")}
+                      </Text>
+                    </VStack>
+                  </HStack>
+
+                  <Spacer display={{ base: "block", sm: "none" }} />
+
+                  <NextLinkButton
+                    w={{ base: "auto", sm: "full" }}
+                    href="https://www.patreon.com/hirotomoyamada"
+                    isExternal
+                    colorScheme="primary"
+                    px="lg"
+                  >
+                    Sponsor
+                  </NextLinkButton>
+                </HStack>
+              </VStack>
+
+              {CONSTANT.SPONSORS.PLATINUM.length ? (
+                <>
+                  <Heading as="h3" size="lg">
+                    {t("home.support.platinum-sponsors")}
+                  </Heading>
+
+                  <Wrap
+                    w="full"
+                    maxW="6xl"
+                    justifyContent="center"
+                    gap={{ base: "lg", md: "md" }}
+                    var={[
+                      {
+                        name: "space",
+                        token: "spaces",
+                        value: { base: "lg", md: "md" },
+                      },
+                    ]}
+                  >
+                    {CONSTANT.SPONSORS.PLATINUM.map(
+                      ({ href, src, alt, ...rest }, index) => (
+                        <Center
+                          key={index}
+                          as="a"
+                          href={href}
+                          target="_blank"
+                          flexBasis={{
+                            base: "calc((100% - var(--ui-space) * 2) / 3)",
+                            md: "calc((100% - var(--ui-space) * 1) / 2)",
+                            sm: "100%",
+                          }}
+                          maxW={{
+                            base: "calc((100% - var(--ui-space) * 2) / 3)",
+                            md: "calc((100% - var(--ui-space) * 1) / 2)",
+                            sm: "100%",
+                          }}
+                          bg="white"
+                          rounded="md"
+                          p="normal"
+                        >
+                          <Image
+                            boxSize="full"
+                            src={src}
+                            alt={alt}
+                            {...rest}
+                            colorMode="light"
+                          />
+                        </Center>
+                      ),
+                    )}
+                  </Wrap>
+                </>
+              ) : null}
+
+              {CONSTANT.SPONSORS.GOLD.length ? (
+                <>
+                  <Heading as="h3" size="lg">
+                    {t("home.support.gold-sponsors")}
+                  </Heading>
+
+                  <Wrap
+                    w="full"
+                    maxW="6xl"
+                    justifyContent="center"
+                    gap={{ base: "lg", md: "md" }}
+                    var={[
+                      {
+                        name: "space",
+                        token: "spaces",
+                        value: { base: "lg", md: "md" },
+                      },
+                    ]}
+                  >
+                    {CONSTANT.SPONSORS.GOLD.map(
+                      ({ href, src, alt, ...rest }, index) => (
+                        <Center
+                          key={index}
+                          as="a"
+                          href={href}
+                          target="_blank"
+                          flexBasis={{
+                            base: "calc((100% - var(--ui-space) * 3) / 4)",
+                            md: "calc((100% - var(--ui-space) * 2) / 3)",
+                            sm: "calc((100% - var(--ui-space) * 1) / 2)",
+                          }}
+                          maxW={{
+                            base: "calc((100% - var(--ui-space) * 3) / 4)",
+                            md: "calc((100% - var(--ui-space) * 2) / 3)",
+                            sm: "calc((100% - var(--ui-space) * 1) / 2)",
+                          }}
+                          bg="white"
+                          rounded="md"
+                          p="normal"
+                        >
+                          <Image
+                            boxSize="full"
+                            src={src}
+                            alt={alt}
+                            {...rest}
+                            colorMode="light"
+                          />
+                        </Center>
+                      ),
+                    )}
+                  </Wrap>
+                </>
+              ) : null}
+            </VStack>
+          </Section>
+        </Center>
       </TopLayout>
     </PageProvider>
   )

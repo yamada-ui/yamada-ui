865f1c63f8d0ae6f9133358bde70fd3a623d4def
suzukisan22 <getty2017@gmail.com>
2024-03-12 22:05:20 +0900

    test: date-picker

--- packages/components/calendar/tests/calendar.test.tsx
+++ packages/components/calendar/tests/calendar.test.tsx
@@ -1,5 +1,5 @@
 import { a11y, act, fireEvent, render } from "@yamada-ui/test"
-import { Calendar } from "../src"
+import { Calendar, DatePicker } from "../src"
 
 describe("<Calendar />", () => {
   test("Calendar renders correctly", async () => {
@@ -669,5 +669,32 @@ describe("<Calendar />", () => {
       )
       expect(monthWeekday).toBe(null)
     })
+  })
+})
+
+describe("<DatePicker />", () => {
+  test("DatePicker renders correctly", async () => {
+    const { container } = render(<DatePicker placeholder="basic" />)
+    await a11y(container)
+  })
+
+  test("should change selected date", async () => {
+    const { container } = render(
+      <DatePicker
+        placeholder="basic"
+        today
+        defaultValue={new Date(new Date().setDate(1))}
+      />,
+    )
+
+    const selectDate = new Date(new Date().setDate(7))
+    const dateStr = new Date(selectDate.setHours(0, 0, 0, 0)).toString()
+    const selectBtn = container.querySelector(`button[data-value="${dateStr}"]`)
+
+    act(() => {
+      fireEvent.click(selectBtn!)
+    })
+
+    expect(selectBtn).toHaveAttribute("data-selected")
   })
 })

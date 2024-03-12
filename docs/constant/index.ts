9a4907d944ef5a91290dd56b891bfabfb5ae70fd
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-12 19:05:02 +0900

    feat(sponsors): added sponsors constant and import

--- docs/constant/index.ts
+++ docs/constant/index.ts
@@ -3,5 +3,6 @@ import { CORE_MEMBERS } from "./core-members"
 import { I18N } from "./i18n"
 import { SEO } from "./seo"
 import { SNS } from "./sns"
+import { SPONSORS } from "./sponsors"
 
-export const CONSTANT = { SNS, SEO, I18N, CONTRIBUTORS, CORE_MEMBERS }
+export const CONSTANT = { SNS, SEO, I18N, CONTRIBUTORS, CORE_MEMBERS, SPONSORS }
---
+++ docs/constant/sponsors.ts
@@ -0,0 +1,12 @@
+export const SPONSORS = {
+  PLATINUM: [
+    {
+      src: "/sponsors/avap.svg",
+      alt: "Avap Co., Ltd.",
+      href: "https://avap.co.jp",
+    },
+  ],
+  GOLD: [],
+  SILVER: [],
+  BRONZE: [],
+}

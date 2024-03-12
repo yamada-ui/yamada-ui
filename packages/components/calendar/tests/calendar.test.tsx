bac1452aa8ebece6825d6ccca8a2121b81091d92
suzukisan22 <getty2017@gmail.com>
2024-03-12 22:06:54 +0900

    test: multi-date-picker

--- packages/components/calendar/tests/calendar.test.tsx
+++ packages/components/calendar/tests/calendar.test.tsx
@@ -1,5 +1,5 @@
 import { a11y, act, fireEvent, render } from "@yamada-ui/test"
-import { Calendar, DatePicker } from "../src"
+import { Calendar, DatePicker, MultiDatePicker } from "../src"
 
 describe("<Calendar />", () => {
   test("Calendar renders correctly", async () => {
@@ -696,5 +696,45 @@ describe("<DatePicker />", () => {
     })
 
     expect(selectBtn).toHaveAttribute("data-selected")
+  })
+})
+
+describe("<MultiDatePicker />", () => {
+  test("MultiDatePicker renders correctly", async () => {
+    const { container } = render(<MultiDatePicker placeholder="basic" />)
+    await a11y(container)
+  })
+
+  test("should change selected dates", async () => {
+    const { container } = render(
+      <MultiDatePicker
+        placeholder="basic"
+        today
+        defaultValue={[new Date(new Date().setDate(1))]}
+      />,
+    )
+    const selectDate = new Date(new Date().setDate(7))
+    const dateStr = new Date(selectDate.setHours(0, 0, 0, 0)).toString()
+    const selectBtn = container.querySelector(`button[data-value="${dateStr}"]`)
+
+    act(() => {
+      fireEvent.click(selectBtn!)
+    })
+
+    expect(selectBtn).toHaveAttribute("data-selected")
+
+    const selectDateSecond = new Date(new Date().setDate(14))
+    const dateStrSecond = new Date(
+      selectDateSecond.setHours(0, 0, 0, 0),
+    ).toString()
+    const selectBtnSecond = container.querySelector(
+      `button[data-value="${dateStrSecond}"]`,
+    )
+
+    act(() => {
+      fireEvent.click(selectBtnSecond!)
+    })
+
+    expect(selectBtnSecond).toHaveAttribute("data-selected")
   })
 })

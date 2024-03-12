825069d56d3e3a97bcbfd5ee59f58a7b20f34909
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-12 19:00:09 +0900

    refactor(components): centralize sponsors data

--- docs/components/mdx/sponsors.tsx
+++ docs/components/mdx/sponsors.tsx
@@ -1,25 +1,30 @@
 import { Image, Wrap, ui } from "@yamada-ui/react"
 import type { FlexProps, ImageProps } from "@yamada-ui/react"
 import type { FC } from "react"
+import { CONSTANT } from "constant"
 
 type Item = ImageProps & { href: string }
 
-export type SponsorsProps = FlexProps & { type: string; items: Item[] }
+export type SponsorsProps = FlexProps & { type: string }
 
-export const Sponsors: FC<SponsorsProps> = ({ type, items = [], ...rest }) => {
+export const Sponsors: FC<SponsorsProps> = ({ type, ...rest }) => {
   let h: string
+  let items: Item[] = []
 
   switch (type) {
     case "platinum":
       h = "20"
+      items = CONSTANT.SPONSORS.PLATINUM
       break
 
     case "gold":
       h = "16"
+      items = CONSTANT.SPONSORS.GOLD
       break
 
     default:
       h = "12"
+      items = CONSTANT.SPONSORS.SILVER
       break
   }
 

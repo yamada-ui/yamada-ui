68d4616fda44dba89d7f2946cc65a3090fd90386
108yen <kazuking.1911@gmail.com>
2024-03-12 21:03:49 +0900

    refactor(examples/hono): remove production script

--- examples/hono/src/index.tsx
+++ examples/hono/src/index.tsx
@@ -12,11 +12,7 @@ app.get("*", (c) => {
           <meta charSet="utf-8" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <link rel="icon" href="/favicon.svg" />
-          {import.meta.env.PROD ? (
-            <script type="module" src="/static/client.js"></script>
-          ) : (
-            <script type="module" src="/src/client.tsx"></script>
-          )}
+          <script type="module" src="/src/client.tsx"></script>
         </head>
         <body>
           <div id="root"></div>

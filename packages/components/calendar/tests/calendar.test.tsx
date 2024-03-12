e3739c91af71310ce34a05a4859ae7c451728962
suzukisan22 <getty2017@gmail.com>
2024-03-12 22:07:32 +0900

    test: range-date-picker

--- packages/components/calendar/tests/calendar.test.tsx
+++ packages/components/calendar/tests/calendar.test.tsx
@@ -1,5 +1,5 @@
 import { a11y, act, fireEvent, render } from "@yamada-ui/test"
-import { Calendar, DatePicker, MultiDatePicker } from "../src"
+import { Calendar, DatePicker, MultiDatePicker, RangeDatePicker } from "../src"
 
 describe("<Calendar />", () => {
   test("Calendar renders correctly", async () => {
@@ -736,5 +736,45 @@ describe("<MultiDatePicker />", () => {
     })
 
     expect(selectBtnSecond).toHaveAttribute("data-selected")
+  })
+})
+
+describe("<RangeDatePicker />", () => {
+  test("RangeDatePicker renders correctly", async () => {
+    const { container } = render(<RangeDatePicker placeholder="basic" />)
+    await a11y(container)
+  })
+
+  test("should render dates of range", async () => {
+    const { container } = render(
+      <RangeDatePicker
+        placeholder="basic"
+        today
+        defaultValue={[new Date(new Date().setDate(1))]}
+      />,
+    )
+    const selectDateFrom = new Date(new Date().setDate(7))
+    const dateStrFrom = new Date(selectDateFrom.setHours(0, 0, 0, 0)).toString()
+    const selectBtnFrom = container.querySelector(
+      `button[data-value="${dateStrFrom}"]`,
+    )
+
+    act(() => {
+      fireEvent.click(selectBtnFrom!)
+    })
+
+    expect(selectBtnFrom).toHaveAttribute("data-selected")
+
+    const selectDateTo = new Date(new Date().setDate(14))
+    const dateStrTo = new Date(selectDateTo.setHours(0, 0, 0, 0)).toString()
+    const selectBtnTo = container.querySelector(
+      `button[data-value="${dateStrTo}"]`,
+    )
+
+    act(() => {
+      fireEvent.click(selectBtnTo!)
+    })
+
+    expect(selectBtnTo).toHaveAttribute("data-selected")
   })
 })

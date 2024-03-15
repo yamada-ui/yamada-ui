4c2f7a9afc0387bc300bfcba7f9fd509b6c01046
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-16 00:02:57 +0900

    test(infinite-scroll-area): added tests for loading state

--- packages/components/infinite-scroll-area/tests/infinite-scroll-area.test.tsx
+++ packages/components/infinite-scroll-area/tests/infinite-scroll-area.test.tsx
@@ -1,9 +1,44 @@
 import { a11y, render } from "@yamada-ui/test"
+import { Fragment } from "react/jsx-runtime"
 import { InfiniteScrollArea } from "../src"
 
 describe("<InfiniteScrollArea />", () => {
+  const defaultIntersectionObserver = global.IntersectionObserver
+
+  beforeAll(() => {
+    global.IntersectionObserver = class IntersectionObserver {
+      constructor(public callback: Function) {}
+
+      root = null
+      rootMargin = ""
+      thresholds = []
+
+      observe() {}
+
+      disconnect() {}
+
+      unobserve() {}
+
+      takeRecords() {
+        return []
+      }
+    }
+  })
+
+  afterAll(() => {
+    global.IntersectionObserver = defaultIntersectionObserver
+  })
+
   test("InfiniteScrollArea renders correctly", async () => {
-    const { container } = render(<InfiniteScrollArea />)
+    const { container } = render(
+      <InfiniteScrollArea loading={<>Loading…</>}>
+        {Array(50)
+          .fill(0)
+          .map((_, index) => (
+            <Fragment key={index}>{index}</Fragment>
+          ))}
+      </InfiniteScrollArea>,
+    )
     await a11y(container)
   })
 })

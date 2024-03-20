78ca5a73758a38124d1643e345f268f799dc6a16
108yen <kazuking.1911@gmail.com>
2024-03-20 15:46:05 +0900

    feat(theme): added radar-chart theme

--- packages/theme/src/components/radar-chart.ts
+++ packages/theme/src/components/radar-chart.ts
@@ -1,11 +1,21 @@
-import type { ComponentMultiStyle } from "@yamada-ui/core"
+import { mergeMultiStyle, type ComponentMultiStyle } from "@yamada-ui/core"
+import { LineChart } from "./line-chart"
 
-export const RadarChart: ComponentMultiStyle = {
-  baseStyle: {},
-
-  variants: {},
-
-  sizes: {},
-
-  defaultProps: {},
-}
+//TODO: RadarChartがないというかほかのところにもない。chartのまちがい？
+export const RadarChart: ComponentMultiStyle = mergeMultiStyle(LineChart, {
+  baseStyle: {
+    radar: {},
+    polarGrid: {
+      stroke: ["blackAlpha.400", "whiteAlpha.400"],
+      strokeWidth: 1,
+    },
+    polarAngleAxis: {
+      stroke: ["blackAlpha.400", "whiteAlpha.400"],
+      strokeWidth: 1,
+    },
+    polarRadiusAxis: {
+      stroke: ["blackAlpha.400", "whiteAlpha.400"],
+      strokeWidth: 1,
+    },
+  },
+})({ omit: ["line", "grid"] })

cbc8974dc6173372158dceb164f63bb05340e428
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-03-31 03:30:48 +0900

    feat(components): added Heading to theme exports

--- docs/theme/components/index.ts
+++ docs/theme/components/index.ts
@@ -1,6 +1,7 @@
 import { Alert } from "./alert"
 import { Button } from "./button"
+import { Heading } from "./heading"
 import { Mark } from "./mark"
 import { SegmentedControl } from "./segmented-control"
 
-export const components = { Button, Alert, Mark, SegmentedControl }
+export const components = { Button, Alert, Mark, SegmentedControl, Heading }

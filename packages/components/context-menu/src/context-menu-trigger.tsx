069207882e066639f5f9b94b86d8e3870124fac0
suzukisan22 <getty2017@gmail.com>
2024-04-05 22:40:09 +0900

    feat: set Anchor when click

--- packages/components/context-menu/src/context-menu-trigger.tsx
+++ packages/components/context-menu/src/context-menu-trigger.tsx
@@ -1,20 +1,39 @@
 import type { HTMLUIProps, CSSUIObject } from "@yamada-ui/core"
 import { ui, forwardRef } from "@yamada-ui/core"
-import { PopoverTrigger } from "@yamada-ui/react"
+import { PopoverAnchor, PopoverTrigger } from "@yamada-ui/react"
 import { cx } from "@yamada-ui/utils"
+import { useState } from "react"
 
 type ContextMenuTriggerProps = HTMLUIProps<"div">
 export const ContextMenuTrigger = forwardRef<ContextMenuTriggerProps, "div">(
   ({ children, className, as: As, ...rest }, ref) => {
     const Component = As ?? Button
+    const [point, setPoint] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+
+    const handleContextMenu = (event: MouseEvent) => {
+      setPoint({ x: event.clientX, y: event.clientY })
+    }
+
     return (
-      <PopoverTrigger>
-        <Component ref={ref} className={cx("ui-menu", className)} {...rest}>
-          <ui.span __css={{ pointerEvents: "none", flex: "1 1 auto", minW: 0 }}>
-            {children}
-          </ui.span>
-        </Component>
-      </PopoverTrigger>
+      <div>
+        <PopoverAnchor>
+          <div style={{ position: "fixed", top: point.y, left: point.x }}></div>
+        </PopoverAnchor>
+        <PopoverTrigger>
+          <Component
+            ref={ref}
+            className={cx("ui-menu", className)}
+            onContextMenu={handleContextMenu}
+            {...rest}
+          >
+            <ui.span
+              __css={{ pointerEvents: "none", flex: "1 1 auto", minW: 0 }}
+            >
+              {children}
+            </ui.span>
+          </Component>
+        </PopoverTrigger>
+      </div>
     )
   },
 )
--- stories/components/overlay/context-menu.stories.tsx
+++ stories/components/overlay/context-menu.stories.tsx
@@ -5,7 +5,6 @@ import {
   ContextMenuTrigger,
   ContextMenuItem,
   ContextMenuDivider,
-  Button,
 } from "@yamada-ui/react"
 
 type Story = StoryFn<typeof ContextMenu>
@@ -19,16 +18,40 @@ export default meta
 
 export const basic: Story = () => {
   return (
-    <ContextMenu>
-      <ContextMenuTrigger as={Button}>Right Click Here</ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem>Undo</ContextMenuItem>
-        <ContextMenuItem>Redo</ContextMenuItem>
-        <ContextMenuDivider />
-        <ContextMenuItem disabled>Cut</ContextMenuItem>
-        <ContextMenuItem>Copy</ContextMenuItem>
-        <ContextMenuItem>Paste</ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <div style={{ textAlign: "center" }}>
+      <ContextMenu>
+        <ContextMenuTrigger style={triggerStyle}>
+          Right Click Here
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuItem>Undo</ContextMenuItem>
+          <ContextMenuItem>Redo</ContextMenuItem>
+          <ContextMenuDivider />
+          <ContextMenuItem disabled>Cut</ContextMenuItem>
+          <ContextMenuItem>Copy</ContextMenuItem>
+          <ContextMenuItem>Paste</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+    </div>
   )
+}
+
+const triggerStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: 200,
+  height: 100,
+  border: "2px dashed $black",
+  borderRadius: 6,
+  backgroundColor: "rgba(0, 0, 0, 0.1)",
+
+  "&:focus": {
+    outline: "none",
+    boxShadow: "0 0 0 2px rgba(0, 0, 0, 0.5)",
+  },
+
+  '&[data-state="open"]': {
+    backgroundColor: "lightblue",
+  },
 }

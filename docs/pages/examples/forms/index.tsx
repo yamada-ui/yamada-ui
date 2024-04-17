edfa1ed4ff5fd2aec4702b023aea5477049252df
kaitobq <kaitobq520@gmail.com>
2024-04-17 12:12:06 +0900

    feat(forms): remove commentout

--- docs/pages/examples/forms/index.tsx
+++ docs/pages/examples/forms/index.tsx
@@ -2,7 +2,6 @@ import { Divider, HStack, Heading, Text, VStack } from "@yamada-ui/react"
 import { memo, useCallback, useState } from "react"
 import { SelectedForm } from "./selectedForm"
 import { Sidebar } from "./sidebar"
-// import { useI18n } from "contexts/i18n-context"
 
 export type menuType =
   | "profile"
@@ -12,15 +11,11 @@ export type menuType =
   | "display"
 
 export const Forms = memo(() => {
-  // const { t } = useI18n()
   const [selectedMenu, setSelectedMenu] = useState<menuType>("profile")
   const onChangeMenu = useCallback((menu: menuType) => {
     setSelectedMenu(menu)
   }, [])
   return (
-    // <Center minH="lg">
-    //   <Text color="muted">Coming soon!</Text>
-    // </Center>
     <>
       <VStack
         divider={<Divider />}
@@ -41,16 +36,9 @@ export const Forms = memo(() => {
         </HStack>
         <HStack alignItems="flex-start">
           <Sidebar onChangeMenu={onChangeMenu} />
-          {/* <VStack divider={<Divider />}> */}
           <SelectedForm selectedMenu={selectedMenu} />
-          {/* </VStack> */}
         </HStack>
-        {/* <Preset display={{ base: "none", md: "block" }} /> */}
       </VStack>
-      {/* <Resizable>
-        <ResizableItem>nav</ResizableItem>
-        <ResizableItem>nav</ResizableItem>
-      </Resizable> */}
     </>
   )
 })

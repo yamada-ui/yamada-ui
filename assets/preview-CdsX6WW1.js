d4429ebb1235f281c6058da45b5df2ea017edcf7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-04-21 19:54:01 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@201f42a4207d44f0385ce849d5e006946bfcb65a 🚀

---
+++ assets/preview-CdsX6WW1.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-BDv166pv.js","./iframe-Do04OwxS.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-D01vo1oH.js","./_basePickBy-BjZHHVk7.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js","./mapValues-SH5BukUW.js","./_baseUniq-B-QDgpM1.js","./index-Bgp06z-1.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./iframe-Do04OwxS.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),d={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-BDv166pv.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{d as parameters};
---
+++ assets/switch.stories-D9IgDKiK.js
@@ -0,0 +1,106 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{u as oe}from"./index-rYLD436_.js";import{r as se}from"./index-CBqU2yxZ.js";import{u as Z,C as $}from"./index.esm-CEuCMWHm.js";import{j as r,c as ne}from"./components-DbxF7Jmi.js";import{H as ie,V as ee}from"./stack-DoT5LYu9.js";import{L as ae}from"./form-control-DzLz1dOu.js";import{W as ce}from"./flex-BWDrtgeb.js";import{B as re}from"./button-ByzKvAA6.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BRIizmwy.js";import"./factory-HTeyK_fA.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js";import"./index-CgzoytyC.js";import"./_basePickBy-BjZHHVk7.js";import"./iframe-Do04OwxS.js";import"../sb-preview/runtime.js";import"./index-BtM5VmRH.js";import"./mapValues-SH5BukUW.js";import"./isPlainObject-BuOqb7Pk.js";import"./index-DrFu-skq.js";import"./ui-provider-uCX0VjQR.js";import"./index-DAq9QMov.js";import"./theme-provider-DvYDGHdP.js";import"./environment-provider-d2aKwQWw.js";import"./motion-JgkBPv6g.js";import"./loading-provider-CizUmNU_.js";import"./index-B0FPZzFH.js";import"./Combination-DlzjZhkT.js";import"./loading-DnyjtoRI.js";import"./icon-B1_QQpZG.js";import"./index-dkYwPDS2.js";import"./forward-ref-DuAegr0M.js";import"./use-component-style-CaxTiwrP.js";import"./motion-DzQfhwIS.js";import"./index-_tcQm2Fk.js";import"./container-portal-CXToMjiT.js";import"./notice-BZoviSZm.js";import"./alert-B-zE3qzJ.js";import"./close-button-CsU1qDnP.js";import"./use-ripple-C7PrLSTl.js";import"./container-DCsdu_m8.js";import"./box-BNFaYC6y.js";import"./text-sgxCCRlS.js";import"./index-08h5wHR4.js";import"./index-DRoJptwM.js";import"./index-CVU6I4PY.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-CcLJ6ORU.js";import"./index-_rFdYV1q.js";import"./index-NNY8n33j.js";import"./index-CI2IYPpt.js";import"./index-Cv5NyirI.js";import"./slide-fade-Dwa26ik0.js";import"./utils-Cf-6jYhN.js";import"./scale-fade-DPj_1rnM.js";import"./index-D4YPUvP0.js";import"./index-CpvBRLJe.js";import"./select-DqS5OFQm.js";import"./option-DSs6ggHa.js";import"./index-D4PdZs2E.js";import"./popover-content-CjL3x1dA.js";import"./index-BBR8_Uz5.js";import"./index-AbGqtBFc.js";import"./index-B84wXCVI.js";const kr={title:"Components / Forms / Switch",component:r},i=()=>e.jsx(r,{children:"basic"}),a=()=>e.jsx(r,{isReverse:!0,children:"basic"}),c=()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{size:"sm",children:"small size"}),e.jsx(r,{size:"md",children:"medium size"}),e.jsx(r,{size:"lg",children:"large size"})]}),m=()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{variant:"thick",children:"thick"}),e.jsx(r,{variant:"thin",children:"thin"})]}),l=()=>{const t=se.useId();return e.jsxs(ie,{gap:"sm",children:[e.jsx(ae,{htmlFor:t,userSelect:"none",children:"Please Click"}),e.jsx(r,{id:t})]})},d=()=>e.jsx(ce,{gap:"md",children:ne.map(t=>e.jsx(r,{colorScheme:t,defaultIsChecked:!0,children:t},t))}),u=()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{isDisabled:!0,children:"disabled"}),e.jsx(r,{isDisabled:!0,defaultIsChecked:!0,children:"disabled"})]}),p=()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{isReadOnly:!0,children:"read only"}),e.jsx(r,{isReadOnly:!0,defaultIsChecked:!0,children:"read only"})]}),h=()=>{const[t,{toggle:o}]=oe(!1);return e.jsx(r,{isChecked:t,onChange:o,children:"custom control"})},S=()=>{const{control:t,handleSubmit:o,watch:b}=Z(),g=s=>console.log("submit:",s);return console.log("watch:",b()),e.jsxs(ee,{as:"form",onSubmit:o(g),children:[e.jsx($,{name:"switch",control:t,render:({field:{value:s,...n}})=>e.jsx(r,{isChecked:s,...n,children:"Dark mode"})}),e.jsx(re,{type:"submit",alignSelf:"flex-end",children:"Submit"})]})},w=()=>{const t={switch:!0},{control:o,handleSubmit:b,watch:g}=Z({defaultValues:t}),s=n=>console.log("submit:",n);return console.log("watch:",g()),e.jsxs(ee,{as:"form",onSubmit:b(s),children:[e.jsx($,{name:"switch",control:o,render:({field:{value:n,...te}})=>e.jsx(r,{isChecked:n,...te,children:"Dark mode"})}),e.jsx(re,{type:"submit",alignSelf:"flex-end",children:"Submit"})]})};var f,x,k;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`() => {
+  return <Switch>basic</Switch>;
+}`,...(k=(x=i.parameters)==null?void 0:x.docs)==null?void 0:k.source}}};var j,C,D;a.parameters={...a.parameters,docs:{...(j=a.parameters)==null?void 0:j.docs,source:{originalSource:`() => {
+  return <Switch isReverse>basic</Switch>;
+}`,...(D=(C=a.parameters)==null?void 0:C.docs)==null?void 0:D.source}}};var y,z,F;c.parameters={...c.parameters,docs:{...(y=c.parameters)==null?void 0:y.docs,source:{originalSource:`() => {
+  return <>
+      <Switch size="sm">small size</Switch>
+      <Switch size="md">medium size</Switch>
+      <Switch size="lg">large size</Switch>
+    </>;
+}`,...(F=(z=c.parameters)==null?void 0:z.docs)==null?void 0:F.source}}};var V,v,R;m.parameters={...m.parameters,docs:{...(V=m.parameters)==null?void 0:V.docs,source:{originalSource:`() => {
+  return <>
+      <Switch variant="thick">thick</Switch>
+      <Switch variant="thin">thin</Switch>
+    </>;
+}`,...(R=(v=m.parameters)==null?void 0:v.docs)==null?void 0:R.source}}};var H,B,I;l.parameters={...l.parameters,docs:{...(H=l.parameters)==null?void 0:H.docs,source:{originalSource:`() => {
+  const id = useId();
+  return <HStack gap="sm">
+      <Label htmlFor={id} userSelect="none">
+        Please Click
+      </Label>
+      <Switch id={id} />
+    </HStack>;
+}`,...(I=(B=l.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var L,W,O;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Switch key={colorScheme} colorScheme={colorScheme} defaultIsChecked>
+          {colorScheme}
+        </Switch>)}
+    </Wrap>;
+}`,...(O=(W=d.parameters)==null?void 0:W.docs)==null?void 0:O.source}}};var E,P,_;u.parameters={...u.parameters,docs:{...(E=u.parameters)==null?void 0:E.docs,source:{originalSource:`() => {
+  return <>
+      <Switch isDisabled>disabled</Switch>
+      <Switch isDisabled defaultIsChecked>
+        disabled
+      </Switch>
+    </>;
+}`,...(_=(P=u.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};var q,A,G;p.parameters={...p.parameters,docs:{...(q=p.parameters)==null?void 0:q.docs,source:{originalSource:`() => {
+  return <>
+      <Switch isReadOnly>read only</Switch>
+      <Switch isReadOnly defaultIsChecked>
+        read only
+      </Switch>
+    </>;
+}`,...(G=(A=p.parameters)==null?void 0:A.docs)==null?void 0:G.source}}};var J,K,M;h.parameters={...h.parameters,docs:{...(J=h.parameters)==null?void 0:J.docs,source:{originalSource:`() => {
+  const [isChecked, {
+    toggle
+  }] = useBoolean(false);
+  return <Switch isChecked={isChecked} onChange={toggle}>
+      custom control
+    </Switch>;
+}`,...(M=(K=h.parameters)==null?void 0:K.docs)==null?void 0:M.source}}};var N,Q,T;S.parameters={...S.parameters,docs:{...(N=S.parameters)==null?void 0:N.docs,source:{originalSource:`() => {
+  type Data = {
+    switch: boolean;
+  };
+  const {
+    control,
+    handleSubmit,
+    watch
+  } = useForm<Data>();
+  const onSubmit: SubmitHandler<Data> = data => console.log("submit:", data);
+  console.log("watch:", watch());
+  return <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Controller name="switch" control={control} render={({
+      field: {
+        value,
+        ...rest
+      }
+    }) => <Switch isChecked={value} {...rest}>
+            Dark mode
+          </Switch>} />
+
+      <Button type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </VStack>;
+}`,...(T=(Q=S.parameters)==null?void 0:Q.docs)==null?void 0:T.source}}};var U,X,Y;w.parameters={...w.parameters,docs:{...(U=w.parameters)==null?void 0:U.docs,source:{originalSource:`() => {
+  type Data = {
+    switch: boolean;
+  };
+  const defaultValues: Data = {
+    switch: true
+  };
+  const {
+    control,
+    handleSubmit,
+    watch
+  } = useForm<Data>({
+    defaultValues
+  });
+  const onSubmit: SubmitHandler<Data> = data => console.log("submit:", data);
+  console.log("watch:", watch());
+  return <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Controller name="switch" control={control} render={({
+      field: {
+        value,
+        ...rest
+      }
+    }) => <Switch isChecked={value} {...rest}>
+            Dark mode
+          </Switch>} />
+
+      <Button type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </VStack>;
+}`,...(Y=(X=w.parameters)==null?void 0:X.docs)==null?void 0:Y.source}}};const jr=["basic","withReverse","withSize","withVariant","withLabel","withColorScheme","isDisabled","isReadonly","customControl","reactHookForm","reactHookFormWithDefaultValue"];export{jr as __namedExportsOrder,i as basic,h as customControl,kr as default,u as isDisabled,p as isReadonly,S as reactHookForm,w as reactHookFormWithDefaultValue,d as withColorScheme,l as withLabel,a as withReverse,c as withSize,m as withVariant};

e106d82dc135419171a77ffbf78dab29d6577710
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-04-22 13:06:53 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@aa514f05d8d02c654085b092e09bcd5b14d7165b 🚀

--- assets/gradients-CG2e7WOY.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as r}from"./index-DSz_1G2r.js";import{M as i}from"./index-D01vo1oH.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Do04OwxS.js";import"../sb-preview/runtime.js";import"./_basePickBy-BjZHHVk7.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js";import"./index-BtM5VmRH.js";import"./mapValues-SH5BukUW.js";import"./_baseUniq-B-QDgpM1.js";import"./index-Bgp06z-1.js";import"./index-DrFu-skq.js";function o(e){const n={code:"code",h1:"h1",pre:"pre",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Documents / Theme / Gradients"}),`
-`,t.jsx(n.h1,{id:"gradients",children:"Gradients"}),`
-`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-ts",children:`Not defined
-`})})]})}function D(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{D as default};

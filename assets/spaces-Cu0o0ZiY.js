d24efe413ec1e98788e189306a042f8160bf9007
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-04-29 15:26:21 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@960304f82baa3e08b5339a2dcf060b06111881b6 🚀

---
+++ assets/spaces-Cu0o0ZiY.js
@@ -0,0 +1,52 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as d}from"./index-DSz_1G2r.js";import{M as l}from"./index-BikkqsnC.js";import{d as c}from"./index-CvP1BksN.js";import{J as t}from"./components-FUxVYqaM.js";import{r as h}from"./index-CBqU2yxZ.js";import{G as o}from"./grid-wbCsFuRM.js";import{T as m}from"./text-BhMssTMP.js";import{B as e}from"./box-yN_xBWT7.js";import"./iframe-CAs8TCe8.js";import"../sb-preview/runtime.js";import"./_basePickBy-BkKH0Y6v.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js";import"./index-BtM5VmRH.js";import"./_commonjsHelpers-BosuxZz1.js";import"./mapValues-SH5BukUW.js";import"./_baseUniq-B-QDgpM1.js";import"./index-BfTvW27s.js";import"./index-DrFu-skq.js";import"./theme-provider-CvYGGDsf.js";import"./factory-C06z1xU-.js";import"./index-m_mncMBS.js";import"./index-BmdQaUM6.js";import"./isPlainObject-BuOqb7Pk.js";import"./ui-provider-vXlncXPg.js";import"./environment-provider-B_N8N04Z.js";import"./motion-JgkBPv6g.js";import"./loading-provider-Be7iT3ZK.js";import"./index-CvMCbKV5.js";import"./Combination-DlzjZhkT.js";import"./loading-lRr_nN3q.js";import"./icon-CyBNnypl.js";import"./index-wE-5yW0t.js";import"./forward-ref-DuAegr0M.js";import"./use-component-style-CZ1o4dYw.js";import"./motion-CogyOzDV.js";import"./index-_tcQm2Fk.js";import"./container-portal-BBLTjt9V.js";import"./notice-BYlSvlZx.js";import"./alert-MzCcELZD.js";import"./close-button-CQgmrLu1.js";import"./use-ripple-CjJeY8ag.js";import"./container-LqI6I_bV.js";import"./stack-PKnyx2CQ.js";import"./index-CSu8YGE2.js";import"./index-DEC9cry9.js";import"./form-control-1mH_ZNvi.js";import"./flex-CHxlxuCK.js";import"./index-CVS1BtgT.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-CT56U8RG.js";import"./index-dOiiHaRb.js";import"./index-DJUiO7A8.js";import"./index-K9dgHeEC.js";import"./index-DLCjGraH.js";import"./slide-fade-BpU2SDOY.js";import"./utils-CK4Vuz3E.js";import"./scale-fade-Db1JbvCk.js";import"./index-B4gSFzwB.js";import"./index-CpvBRLJe.js";import"./select-Do5Etv8Z.js";import"./option-BNGYYNfE.js";import"./index-DCJ79jOd.js";import"./popover-content-WzgJs_27.js";import"./index-DWI-gbI8.js";import"./index-rYLD436_.js";import"./index-Dv3jTNze.js";import"./index-Ce5pLeDf.js";function p(i){const n={code:"code",h1:"h1",h2:"h2",pre:"pre",...d(),...i.components};return r.jsxs(r.Fragment,{children:[r.jsx(l,{title:"Documents / Theme / Spaces"}),`
+`,r.jsx(n.h1,{id:"spaces",children:"Spaces"}),`
+`,r.jsx(n.pre,{children:r.jsx(n.code,{className:"language-ts",children:`const spaces = {
+  xs: "0.25rem",
+  sm: "0.5rem",
+  md: "1rem",
+  normal: "1.5rem",
+  lg: "2rem",
+  xl: "3rem",
+  "2xl": "4rem",
+  "3xl": "6rem",
+  "4xl": "8rem",
+
+  px: "1px",
+  0.5: "0.125rem",
+  1: "0.25rem",
+  1.5: "0.375rem",
+  2: "0.5rem",
+  2.5: "0.625rem",
+  3: "0.75rem",
+  3.5: "0.875rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  9: "2.25rem",
+  10: "2.5rem",
+  12: "3rem",
+  14: "3.5rem",
+  16: "4rem",
+  20: "5rem",
+  24: "6rem",
+  28: "7rem",
+  32: "8rem",
+  36: "9rem",
+  40: "10rem",
+  44: "11rem",
+  48: "12rem",
+  52: "13rem",
+  56: "14rem",
+  60: "15rem",
+  64: "16rem",
+  72: "18rem",
+  80: "20rem",
+  96: "24rem",
+}
+`})}),`
+`,r.jsx(n.h2,{id:"token",children:"Token"}),`
+`,r.jsx(t,{children:r.jsxs(o,{templateColumns:"5rem 5rem 5rem 1fr",my:"md",gap:"md",children:[r.jsx(m,{m:"0",children:"Name"}),r.jsx(m,{m:"0",children:"Rem"}),r.jsx(m,{m:"0",children:"Pixel"}),r.jsx(e,{}),r.jsx(m,{m:"0",children:"xs"}),r.jsx(m,{m:"0",children:"0.25rem"}),r.jsx(m,{m:"0",children:"4px"}),r.jsx(e,{maxW:"0.25rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"sm"}),r.jsx(m,{m:"0",children:"0.5rem"}),r.jsx(m,{m:"0",children:"8px"}),r.jsx(e,{maxW:"0.5rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"md"}),r.jsx(m,{m:"0",children:"1rem"}),r.jsx(m,{m:"0",children:"16px"}),r.jsx(e,{maxW:"1rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"normal"}),r.jsx(m,{m:"0",children:"1.5rem"}),r.jsx(m,{m:"0",children:"24px"}),r.jsx(e,{maxW:"1.5rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"lg"}),r.jsx(m,{m:"0",children:"2rem"}),r.jsx(m,{m:"0",children:"32px"}),r.jsx(e,{maxW:"2rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"xl"}),r.jsx(m,{m:"0",children:"3rem"}),r.jsx(m,{m:"0",children:"48px"}),r.jsx(e,{maxW:"3rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"2xl"}),r.jsx(m,{m:"0",children:"4rem"}),r.jsx(m,{m:"0",children:"64px"}),r.jsx(e,{maxW:"4rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"3xl"}),r.jsx(m,{m:"0",children:"6rem"}),r.jsx(m,{m:"0",children:"96px"}),r.jsx(e,{maxW:"6rem",h:"8xs",bg:"red.500"}),r.jsx(m,{m:"0",children:"4xl"}),r.jsx(m,{m:"0",children:"8rem"}),r.jsx(m,{m:"0",children:"128px"}),r.jsx(e,{maxW:"8rem",h:"8xs",bg:"red.500"})]})}),`
+`,r.jsx(n.h2,{id:"default",children:"Default"}),`
+`,r.jsx(t,{children:r.jsxs(o,{templateColumns:"5rem 5rem 5rem 1fr",my:"md",gap:"md",children:[r.jsx(m,{m:"0",children:"Name"}),r.jsx(m,{m:"0",children:"Rem"}),r.jsx(m,{m:"0",children:"Pixel"}),r.jsx(e,{}),r.jsx(m,{m:"0",children:"px"}),r.jsx(m,{m:"0",children:"0.0625rem"}),r.jsx(m,{m:"0",children:"1px"}),r.jsx(e,{maxW:"1px",h:"8xs",bg:"green.500"}),Object.entries(c.spaces).filter(([x])=>!isNaN(Number(x))).sort(([x],[s])=>x-s).map(([x,s])=>r.jsxs(h.Fragment,{children:[r.jsx(m,{m:"0",children:x}),r.jsx(m,{m:"0",children:s}),r.jsx(m,{m:"0",children:`${parseFloat(s)*16}px`}),r.jsx(e,{maxW:s,h:"8xs",bg:"green.500"})]},x))]})})]})}function Sr(i={}){const{wrapper:n}={...d(),...i.components};return n?r.jsx(n,{...i,children:r.jsx(p,{...i})}):p(i)}export{Sr as default};
---
+++ assets/welcome-Brk5kDCZ.js
@@ -0,0 +1,24 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{useMDXComponents as m}from"./index-DSz_1G2r.js";import{M as p}from"./index-BikkqsnC.js";import{J as n}from"./components-FUxVYqaM.js";import{C as e}from"./center-Dhb4ktwc.js";import{I as s}from"./image-DkW0wnak.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CAs8TCe8.js";import"../sb-preview/runtime.js";import"./_basePickBy-BkKH0Y6v.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js";import"./index-BtM5VmRH.js";import"./mapValues-SH5BukUW.js";import"./_baseUniq-B-QDgpM1.js";import"./index-BfTvW27s.js";import"./index-DrFu-skq.js";import"./index-m_mncMBS.js";import"./factory-C06z1xU-.js";import"./index-BmdQaUM6.js";import"./isPlainObject-BuOqb7Pk.js";import"./ui-provider-vXlncXPg.js";import"./index-CvP1BksN.js";import"./theme-provider-CvYGGDsf.js";import"./environment-provider-B_N8N04Z.js";import"./motion-JgkBPv6g.js";import"./loading-provider-Be7iT3ZK.js";import"./index-CvMCbKV5.js";import"./Combination-DlzjZhkT.js";import"./loading-lRr_nN3q.js";import"./icon-CyBNnypl.js";import"./index-wE-5yW0t.js";import"./forward-ref-DuAegr0M.js";import"./use-component-style-CZ1o4dYw.js";import"./motion-CogyOzDV.js";import"./index-_tcQm2Fk.js";import"./container-portal-BBLTjt9V.js";import"./notice-BYlSvlZx.js";import"./alert-MzCcELZD.js";import"./close-button-CQgmrLu1.js";import"./use-ripple-CjJeY8ag.js";import"./container-LqI6I_bV.js";import"./stack-PKnyx2CQ.js";import"./box-yN_xBWT7.js";import"./text-BhMssTMP.js";import"./index-CSu8YGE2.js";import"./index-DEC9cry9.js";import"./form-control-1mH_ZNvi.js";import"./flex-CHxlxuCK.js";import"./index-CVS1BtgT.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-CT56U8RG.js";import"./index-dOiiHaRb.js";import"./index-DJUiO7A8.js";import"./index-K9dgHeEC.js";import"./index-DLCjGraH.js";import"./slide-fade-BpU2SDOY.js";import"./utils-CK4Vuz3E.js";import"./scale-fade-Db1JbvCk.js";import"./index-B4gSFzwB.js";import"./index-CpvBRLJe.js";import"./select-Do5Etv8Z.js";import"./option-BNGYYNfE.js";import"./index-DCJ79jOd.js";import"./popover-content-WzgJs_27.js";import"./index-DWI-gbI8.js";import"./index-rYLD436_.js";import"./index-Dv3jTNze.js";import"./index-Ce5pLeDf.js";import"./use-image-CasKR8m8.js";function i(t){const o={code:"code",h1:"h1",li:"li",ol:"ol",p:"p",pre:"pre",...m(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{title:"Documents / Welcome"}),`
+`,r.jsx(n,{children:r.jsx(e,{mb:"16",children:r.jsx(s,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png"})})}),`
+`,r.jsx(o.h1,{id:"usage",children:"Usage"}),`
+`,r.jsxs(o.ol,{children:[`
+`,r.jsxs(o.li,{children:["Wrap your application with the ",r.jsx(o.code,{children:"UIProvider"})," provided"]}),`
+`]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { UIProvider } from "@yamada-ui/react"
+
+const App = ({ children }) => {
+  return <UIProvider>{children}</UIProvider>
+}
+
+export default App
+`})}),`
+`,r.jsx(o.p,{children:"Yamada-ui supports light and dark modes by default"}),`
+`,r.jsxs(o.ol,{start:"2",children:[`
+`,r.jsx(o.li,{children:"Now you can start using components like so"}),`
+`]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { Text } from "@yamada-ui/react"
+
+const Example = () => {
+  return <Text>ギャルのパンティーおくれーーーっ！！！！！</Text>
+}
+`})})]})}function Dr(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{Dr as default};

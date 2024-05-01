fcd30446e6cc2b074fb3e2e1e90b09c837172347
koralle <mkhd7930@gmail.com>
2024-05-01 21:34:28 +0900

    fix: typo

--- packages/hooks/use-media-query/tests/index.test.ts
+++ packages/hooks/use-media-query/tests/index.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from "@yamada-ui/test"
 import { useMediaQuery } from "../src"
 
 describe("useMediaQuery", () => {
-  test("`(prerers-color-scheme: dark)` should be truthy", () => {
+  test("`(prefers-color-scheme: dark)` should be truthy", () => {
     const { result } = renderHook(() =>
       useMediaQuery("(prefers-color-scheme: dark)"),
     )

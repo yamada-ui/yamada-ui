bd5681a4b98f31fd515aac96680cdbfd07ff3ea6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-03 14:16:39 +0900

    chore(docs): updated ignored-build-step script

--- docs/ignored-build-step.sh
+++ docs/ignored-build-step.sh
@@ -2,11 +2,11 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-git remote -v
-
 if [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then
   exit 1;
 else
+  git fetch git@github.com:yamada-ui/yamada-ui.git main
+
   git diff FETCH_HEAD HEAD --quiet -- ./docs
 
   if [ $? -eq 0 ]; then

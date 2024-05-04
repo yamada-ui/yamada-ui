544a8c79b177e0cb9ee964a3cd9870264a700263
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-04 09:39:33 +0900

    refactor: use variable for target branch in script

--- docs/ignored-build-step.sh
+++ docs/ignored-build-step.sh
@@ -2,21 +2,23 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then
-  echo "Run because the committed branch is 'main'."
+TARGET_BRANCH="main"
+
+if [ "$VERCEL_GIT_COMMIT_REF" == "$TARGET_BRANCH" ]; then
+  echo "Run because the committed branch is '$TARGET_BRANCH'."
   exit 1
 else
-  git fetch https://github.com/yamada-ui/yamada-ui.git main
+  git fetch https://github.com/yamada-ui/yamada-ui.git $TARGET_BRANCH:$TARGET_BRANCH
 
-  git_diff_result=$(git diff --name-only $(git merge-base HEAD main) HEAD)
+  git_diff_result=$(git diff --name-only $(git merge-base HEAD $TARGET_BRANCH) HEAD)
 
   echo "$git_diff_result"
 
   if echo "$git_diff_result" | grep -q "^docs/"; then
-    echo "Run because './docs' is included in the difference with the 'main' branch."
+    echo "Run because './docs' is included in the difference with the '$TARGET_BRANCH' branch."
     exit 1
   else
-    echo "Run was canceled because './docs' was not included in the difference with the 'main' branch."
+    echo "Run was canceled because './docs' was not included in the difference with the '$TARGET_BRANCH' branch."
     exit 0
   fi
 fi

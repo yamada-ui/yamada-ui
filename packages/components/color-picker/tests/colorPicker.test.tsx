f3c1b24e4df7128f1b7c325a3ae2072cb530185d
David Sevikian <david.sevikian.work@gmail.com>
2024-05-06 18:36:10 +0400

    test(color-picker): test EyeDropper render

--- packages/components/color-picker/tests/colorPicker.test.tsx
+++ packages/components/color-picker/tests/colorPicker.test.tsx
@@ -1,6 +1,28 @@
 import { render, screen, fireEvent } from "@yamada-ui/test"
 import { ColorPicker } from "../src"
 
+type WindowWithEyeDropper = Window &
+  typeof globalThis & {
+    EyeDropper: any
+  }
+
+const mockEyeDropper = vi.fn()
+
+const setupEyeDropperMock = () => {
+  const originalEyeDropper = ((window as WindowWithEyeDropper).EyeDropper(
+    window as WindowWithEyeDropper,
+  ).EyeDropper = mockEyeDropper)
+
+  const utils = render(<ColorPicker />)
+
+  return { ...utils, originalEyeDropper }
+}
+
+const resetEyeDropperMock = () => {
+  // @ts-ignore
+  window.EyeDropper = undefined
+}
+
 describe("<ColorPicker />", () => {
   test("ColorPicker renders correctly", async () => {
     render(<ColorPicker data-testid="colorPicker" />)
@@ -27,5 +49,23 @@ describe("<ColorPicker />", () => {
     const colorPicker = screen.getByTestId("colorPicker")
 
     expect(colorPicker).toHaveValue("#00ff00")
+  })
+
+  test("ColorPicker renders with eye dropper", () => {
+    setupEyeDropperMock()
+
+    render(
+      <ColorPicker
+        defaultValue="#00ff00"
+        data-testid="colorPicker"
+        withEyeDropper
+      />,
+    )
+
+    const colorPicker = screen.getByTestId("colorPicker")
+
+    expect(colorPicker).toHaveValue("#00ff00")
+
+    resetEyeDropperMock()
   })
 })

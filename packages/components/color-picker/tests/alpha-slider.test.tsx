369a24d0190c920715461136c544638300c4f297
David Sevikian <david.sevikian.work@gmail.com>
2024-05-06 18:36:28 +0400

    test(color-picker): test AlphaSlider

---
+++ packages/components/color-picker/tests/alpha-slider.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@yamada-ui/test"
+import { AlphaSlider } from "../src"
+
+describe("<AlphaSlider />", () => {
+  test("AlphaSlider renders correctly", async () => {
+    render(<AlphaSlider data-testid="alphaSlider" />)
+
+    const alphaSlider = screen.getByTestId("alphaSlider")
+
+    expect(alphaSlider).toBeInTheDocument()
+  })
+
+  test("AlphaSlider renders overlays correctly with default props", async () => {
+    render(<AlphaSlider data-testid="alphaSlider" />)
+    const thumb = screen.getByRole("slider")
+
+    expect(thumb).toHaveAttribute("aria-label", "Slider thumb")
+    expect(thumb).toHaveAttribute("aria-valuemax", "1")
+    expect(thumb).toHaveAttribute("aria-valuemin", "0")
+    expect(thumb).toHaveAttribute("aria-valuenow", "0.5")
+  })
+
+  test("AlphaSlider renders correctly with custom props", () => {
+    render(<AlphaSlider data-testid="alphaSlider" min={0.2} max={0.8} />)
+
+    const alphaSlider = screen.getByTestId("alphaSlider")
+
+    expect(alphaSlider).toHaveClass("ui-alpha-slider")
+
+    const thumb = screen.getByRole("slider")
+
+    expect(thumb).toHaveAttribute("aria-valuemax", "0.8")
+    expect(thumb).toHaveAttribute("aria-valuemin", "0.2")
+    expect(thumb).toHaveAttribute("aria-valuenow", "0.5")
+  })
+})

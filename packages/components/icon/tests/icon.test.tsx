d15a9d910ed5bb62647648364a9c7619aa6766dd
suzukisan22 <getty2017@gmail.com>
2024-05-20 23:22:55 +0900

    fix: fix test

--- packages/components/icon/tests/icon.test.tsx
+++ packages/components/icon/tests/icon.test.tsx
@@ -1,13 +1,8 @@
 import { a11y } from "@yamada-ui/test"
-import { Md3dRotation } from "react-icons/md"
 import { Icon } from "../src"
 
 describe("<Icon/>", () => {
   test("passes a11y test", async () => {
     await a11y(<Icon />)
-  })
-
-  test("passes a11y test given a third-party icon", async () => {
-    await a11y(<Icon as={Md3dRotation} />)
   })
 })

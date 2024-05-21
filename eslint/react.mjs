8710d645fca8b1b4125e76a54fdb0c5b98e2a168
koralle <mkhd7930@gmail.com>
2024-05-21 21:52:28 +0900

    chore: update `eslint/react.mjs`

--- eslint/react.mjs
+++ eslint/react.mjs
@@ -1,39 +1,24 @@
-import { FlatCompat } from "@eslint/eslintrc"
 import pluginReact from "eslint-plugin-react"
 import pluginReactHooks from "eslint-plugin-react-hooks"
 
-const reactCompat = new FlatCompat({
-  recommendedConfig: pluginReact.configs.recommended,
-})
-
-const reactHooksCompat = new FlatCompat({
-  recommendedConfig: pluginReactHooks.configs.recommended,
-})
-
-export const reactConfig = reactCompat.config({
-  plugins: ["react"],
-  settings: {
-    react: {
-      version: "detect",
-    },
+export const reactConfig = {
+  plugins: {
+    react: pluginReact,
+    "react-hooks": pluginReactHooks,
   },
   rules: {
+    ...pluginReact.configs.recommended.rules,
+    ...pluginReactHooks.configs.recommended.rules,
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
     "react/no-unescaped-entities": "off",
     "react/jsx-curly-brace-presence": "error",
     "react/jsx-no-leaked-render": "error",
+    "react-hooks/rules-of-hooks": "off",
   },
-})
-
-export const reactHooksConfig = reactHooksCompat.config({
-  plugins: ["react-hooks"],
   settings: {
     react: {
       version: "detect",
     },
   },
-  rules: {
-    "react-hooks/rules-of-hooks": "off",
-  },
-})
+}

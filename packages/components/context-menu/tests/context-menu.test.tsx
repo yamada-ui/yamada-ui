7fc1460dab013339e3b457c37a78fc73b506b4ef
suzukisan22 <getty2017@gmail.com>
2024-05-21 22:39:33 +0900

    test: add test

--- packages/components/context-menu/tests/context-menu.test.tsx
+++ packages/components/context-menu/tests/context-menu.test.tsx
@@ -1,9 +1,28 @@
-import { a11y, render } from "@yamada-ui/test"
-import { ContextMenu } from "../src"
+import { MenuList, MenuItem, MenuDivider } from "@yamada-ui/menu"
+import { render, fireEvent } from "@yamada-ui/test"
+import { Text } from "@yamada-ui/typography"
+import { ContextMenu, ContextMenuTrigger } from "../src"
 
 describe("<ContextMenu />", () => {
   test("ContextMenu renders correctly", async () => {
-    const { container } = render(<ContextMenu />)
-    await a11y(container)
+    const { getByText, findByText } = render(
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <Text style={{ textAlign: "center" }}>Right Click Here</Text>
+        </ContextMenuTrigger>
+        <MenuList>
+          <MenuItem>Undo</MenuItem>
+          <MenuItem>Redo</MenuItem>
+          <MenuDivider />
+          <MenuItem isDisabled>Cut</MenuItem>
+          <MenuItem>Copy</MenuItem>
+          <MenuItem>Paste</MenuItem>
+        </MenuList>
+      </ContextMenu>,
+    )
+
+    await fireEvent.contextMenu(getByText("Right Click Here"))
+    const undoText = await findByText("Undo")
+    expect(undoText).toBeVisible()
   })
 })

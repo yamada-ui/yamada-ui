c750a5de5b44812f1f3af4e4c792c306296eefad
koralle <mkhd7930@gmail.com>
2024-05-21 21:52:47 +0900

    chore: update `eslint.config.mjs`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import { config as tseslintConfig } from "typescript-eslint"
 import { tsESLintConfig } from "./eslint/typescript.mjs"
-import { reactConfig, reactHooksConfig } from "./eslint/react.mjs"
+import { reactConfig } from "./eslint/react.mjs"
 import { vitestConfig } from "./eslint/vitest.mjs"
 import { testingLibraryConfig } from "./eslint/testing-library.mjs"
 import { importConfig } from "./eslint/import.mjs"
+
+const exts = "{js,jsx,cjs,mjs,ts,tsx,cts,mts,d.ts}"
 
 export default tseslintConfig(
   {
@@ -19,29 +21,23 @@ export default tseslintConfig(
     ],
   },
   {
-    files: [
-      "scripts/**/*.{ts,tsx,cts,mts,d.ts}",
-      "packages/**/*.{ts,tsx,cts,mts,d.ts}",
-    ],
+    files: [`scripts/**/*.${exts}`, `packages/**/*.${exts}`],
     ...tsESLintConfig,
   },
-  ...importConfig.map((config) => ({
-    files: [
-      "scripts/**/*.{ts,tsx,cts,mts,d.ts}",
-      "packages/**/*.{ts,tsx,cts,mts,d.ts}",
-    ],
-    ...config,
-  })),
-  ...reactConfig.map((config) => ({
-    files: ["packages/**/*.{ts,tsx,cts,mts,d.ts}"],
-    ...config,
-  })),
-  ...reactHooksConfig.map((config) => ({
-    files: ["packages/**/*.{ts,tsx,cts,mts,d.ts}"],
-    ...config,
-  })),
   {
-    files: ["scripts/**/*", "packages/cli/**/*", "stories/**/*"],
+    files: [`scripts/**/*.${exts}`, `packages/**/*.${exts}`],
+    ...importConfig,
+  },
+  {
+    files: [`packages/**/*.${exts}`],
+    ...reactConfig,
+  },
+  {
+    files: [
+      `scripts/**/*.${exts}`,
+      `packages/cli/**/*.${exts}`,
+      `stories/**/*.${exts}`,
+    ],
     rules: {
       "no-console": "off",
     },

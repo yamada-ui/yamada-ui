d3b6f4c6fc6db3d037e9ecfeef470742a707115d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-05-22 14:41:15 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@7dd0d0112342e754231429ef07c75c43ed664ec8 🚀

---
+++ assets/badge.stories-DdmTeCkG.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{c as i}from"./components-CBA57vA9.js";import{B as e}from"./badge-BeoIsf5S.js";import{W as a}from"./flex-DIqIlCFh.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BBt5KR5O.js";import"./factory-DcjttzXA.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-D3Fv6YO_.js";import"./index-CMUyB3_7.js";import"./_basePickBy-BarplhU5.js";import"./iframe-CXMMJmKd.js";import"../sb-preview/runtime.js";import"./index-CqS9fqXy.js";import"./component-IvP1ueaU.js";import"./mapValues-SH5BukUW.js";import"./isPlainObject-BuOqb7Pk.js";import"./index-DrFu-skq.js";import"./ui-provider-BZpNq46F.js";import"./index-5hxhpjTX.js";import"./theme-provider-blvefFsb.js";import"./environment-provider-99yZV1sY.js";import"./motion-BNFRb361.js";import"./loading-provider-Cota65l7.js";import"./index-D-LSkzaE.js";import"./Combination-nrDmEbEK.js";import"./loading-D2RTdWov.js";import"./icon-i9DNsLAz.js";import"./index-ZcQ9dmS1.js";import"./forward-ref-Dr5Hqd9a.js";import"./use-component-style-Bt4oJcRH.js";import"./motion-CGV1aZSj.js";import"./index-DSHhgXSs.js";import"./container-portal-CZlBTm5D.js";import"./notice-DPHnfhmo.js";import"./alert-BLe23aCX.js";import"./close-button--PP8EEnm.js";import"./use-ripple-DGdpgE18.js";import"./container-C43KVyGu.js";import"./stack-hhk-VzsX.js";import"./box-DseOx6RT.js";import"./text-B7cjXMz4.js";import"./index-BguKd_Cb.js";import"./index-CutY5rpZ.js";import"./form-control-BtOL3irc.js";import"./index-C93wcPdP.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-DVJ9CG7S.js";import"./index-BEz7kc7x.js";import"./index-BynWHtcw.js";import"./index-CfyBxFcd.js";import"./index-DsuEdmfO.js";import"./index-gtcoX668.js";import"./slide-fade-BCrKcD4j.js";import"./utils-qy47cExs.js";import"./scale-fade-DdhoTvDk.js";import"./index-BqfSQEBY.js";import"./index-3VUv5hbS.js";import"./select-DShv-Prl.js";import"./option-Gl-FshK3.js";import"./index-Chh_-GY8.js";import"./popover-content-BuqQEBR6.js";import"./index-CnGadzrd.js";import"./index-CBqzus2r.js";import"./index-kKcgrWTG.js";const Cr={title:"Components / Data Display / Badge",component:e},m=()=>o.jsx(a,{gap:"md",children:i.map(r=>o.jsx(e,{colorScheme:r,children:r},r))}),t=()=>o.jsx(a,{gap:"md",children:i.map(r=>o.jsx(e,{variant:"solid",colorScheme:r,children:r},r))}),p=()=>o.jsx(a,{gap:"md",children:i.map(r=>o.jsx(e,{variant:"outline",colorScheme:r,children:r},r))});var s,c,n;m.parameters={...m.parameters,docs:{...(s=m.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} colorScheme={colorScheme}>
+          {colorScheme}
+        </Badge>)}
+    </Wrap>;
+}`,...(n=(c=m.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var d,l,u;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} variant="solid" colorScheme={colorScheme}>
+          {colorScheme}
+        </Badge>)}
+    </Wrap>;
+}`,...(u=(l=t.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var h,g,S;p.parameters={...p.parameters,docs:{...(h=p.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} variant="outline" colorScheme={colorScheme}>
+          {colorScheme}
+        </Badge>)}
+    </Wrap>;
+}`,...(S=(g=p.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};const Or=["subtle","solid","outline"];export{Or as __namedExportsOrder,Cr as default,p as outline,t as solid,m as subtle};
---
+++ assets/pagination.stories-C-002rFO.js
@@ -0,0 +1,78 @@
+import{j as t}from"./jsx-runtime-X2b_N9AH.js";import{I as a,f as e}from"./fontawesome-icon-DpaxtiZU.js";import{r as lt}from"./index-uCp2LrAq.js";import{c as S}from"./components-CBA57vA9.js";import{P as o}from"./pagination-Bg5QHM8A.js";import{W as pt}from"./flex-DIqIlCFh.js";import{V as w}from"./stack-hhk-VzsX.js";import"./index-D3ylJrlI.js";import"./_commonjsHelpers-BosuxZz1.js";import"./icon-i9DNsLAz.js";import"./index-ZcQ9dmS1.js";import"./theme-provider-blvefFsb.js";import"./factory-DcjttzXA.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-D3Fv6YO_.js";import"./forward-ref-Dr5Hqd9a.js";import"./index-BBt5KR5O.js";import"./index-CMUyB3_7.js";import"./_basePickBy-BarplhU5.js";import"./iframe-CXMMJmKd.js";import"../sb-preview/runtime.js";import"./index-CqS9fqXy.js";import"./component-IvP1ueaU.js";import"./mapValues-SH5BukUW.js";import"./isPlainObject-BuOqb7Pk.js";import"./index-DrFu-skq.js";import"./ui-provider-BZpNq46F.js";import"./index-5hxhpjTX.js";import"./environment-provider-99yZV1sY.js";import"./motion-BNFRb361.js";import"./loading-provider-Cota65l7.js";import"./index-D-LSkzaE.js";import"./Combination-nrDmEbEK.js";import"./loading-D2RTdWov.js";import"./use-component-style-Bt4oJcRH.js";import"./motion-CGV1aZSj.js";import"./index-DSHhgXSs.js";import"./container-portal-CZlBTm5D.js";import"./notice-DPHnfhmo.js";import"./alert-BLe23aCX.js";import"./close-button--PP8EEnm.js";import"./use-ripple-DGdpgE18.js";import"./container-C43KVyGu.js";import"./box-DseOx6RT.js";import"./text-B7cjXMz4.js";import"./index-BguKd_Cb.js";import"./index-CutY5rpZ.js";import"./form-control-BtOL3irc.js";import"./index-C93wcPdP.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-DVJ9CG7S.js";import"./index-BEz7kc7x.js";import"./index-BynWHtcw.js";import"./index-CfyBxFcd.js";import"./index-DsuEdmfO.js";import"./index-gtcoX668.js";import"./slide-fade-BCrKcD4j.js";import"./utils-qy47cExs.js";import"./scale-fade-DdhoTvDk.js";import"./index-BqfSQEBY.js";import"./index-3VUv5hbS.js";import"./select-DShv-Prl.js";import"./option-Gl-FshK3.js";import"./index-Chh_-GY8.js";import"./popover-content-BuqQEBR6.js";import"./index-CnGadzrd.js";import"./index-CBqzus2r.js";import"./index-kKcgrWTG.js";const Do={title:"Components / Navigation / Pagination",component:o},n=()=>t.jsx(o,{total:10}),s=()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{total:10,size:"xs"}),t.jsx(o,{total:10,size:"sm"}),t.jsx(o,{total:10,size:"md"}),t.jsx(o,{total:10,size:"lg"}),t.jsx(o,{total:10,size:"xl"})]}),i=()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{total:10,variant:"solid"}),t.jsx(o,{total:10,variant:"outline"}),t.jsx(o,{total:10,variant:"ghost"}),t.jsx(o,{total:10,variant:"unstyled"})]}),c=()=>t.jsxs(pt,{w:"full",gap:"md",children:[t.jsx(w,{w:"auto",children:S.map(r=>t.jsx(o,{total:10,variant:"solid",colorScheme:r},r))}),t.jsx(w,{w:"auto",children:S.map(r=>t.jsx(o,{total:10,variant:"outline",colorScheme:r},r))}),t.jsx(w,{w:"auto",children:S.map(r=>t.jsx(o,{total:10,variant:"ghost",colorScheme:r},r))})]}),m=()=>t.jsx(o,{defaultPage:3,total:10}),l=()=>t.jsx(o,{total:77}),p=()=>t.jsx(o,{total:10,withEdges:!0}),u=()=>t.jsx(o,{total:77,siblings:3}),d=()=>t.jsx(o,{total:77,boundaries:3}),g=()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{total:10,variant:"solid",isDisabled:!0}),t.jsx(o,{total:10,variant:"outline",isDisabled:!0}),t.jsx(o,{total:10,variant:"ghost",isDisabled:!0}),t.jsx(o,{total:10,variant:"unstyled",isDisabled:!0})]}),h=()=>t.jsx(o,{total:10,withControls:!1}),P=()=>{const[r,mt]=lt.useState(1);return t.jsx(o,{page:r,total:10,onChange:mt})},x=()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{total:10,controlProps:{children:t.jsx(a,{icon:e})}}),t.jsx(o,{total:10,controlPrevProps:{children:t.jsx(a,{icon:e})}}),t.jsx(o,{total:10,controlNextProps:{children:t.jsx(a,{icon:e})}})]}),j=()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{total:10,withEdges:!0,edgeProps:{children:t.jsx(a,{icon:e})}}),t.jsx(o,{total:10,withEdges:!0,edgeFirstProps:{children:t.jsx(a,{icon:e})}}),t.jsx(o,{total:10,withEdges:!0,edgeLastProps:{children:t.jsx(a,{icon:e})}})]});var v,f,b;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`() => {
+  return <Pagination total={10} />;
+}`,...(b=(f=n.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var C,E,z;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`() => {
+  return <>
+      <Pagination total={10} size="xs" />
+      <Pagination total={10} size="sm" />
+      <Pagination total={10} size="md" />
+      <Pagination total={10} size="lg" />
+      <Pagination total={10} size="xl" />
+    </>;
+}`,...(z=(E=s.parameters)==null?void 0:E.docs)==null?void 0:z.source}}};var D,k,V;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`() => {
+  return <>
+      <Pagination total={10} variant="solid" />
+      <Pagination total={10} variant="outline" />
+      <Pagination total={10} variant="ghost" />
+      <Pagination total={10} variant="unstyled" />
+    </>;
+}`,...(V=(k=i.parameters)==null?void 0:k.docs)==null?void 0:V.source}}};var B,I,y;c.parameters={...c.parameters,docs:{...(B=c.parameters)==null?void 0:B.docs,source:{originalSource:`() => {
+  return <Wrap w="full" gap="md">
+      <VStack w="auto">
+        {colorSchemes.map(colorScheme => <Pagination key={colorScheme} total={10} variant="solid" colorScheme={colorScheme} />)}
+      </VStack>
+
+      <VStack w="auto">
+        {colorSchemes.map(colorScheme => <Pagination key={colorScheme} total={10} variant="outline" colorScheme={colorScheme} />)}
+      </VStack>
+
+      <VStack w="auto">
+        {colorSchemes.map(colorScheme => <Pagination key={colorScheme} total={10} variant="ghost" colorScheme={colorScheme} />)}
+      </VStack>
+    </Wrap>;
+}`,...(y=(I=c.parameters)==null?void 0:I.docs)==null?void 0:y.source}}};var F,W,N;m.parameters={...m.parameters,docs:{...(F=m.parameters)==null?void 0:F.docs,source:{originalSource:`() => {
+  return <Pagination defaultPage={3} total={10} />;
+}`,...(N=(W=m.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};var L,T,_;l.parameters={...l.parameters,docs:{...(L=l.parameters)==null?void 0:L.docs,source:{originalSource:`() => {
+  return <Pagination total={77} />;
+}`,...(_=(T=l.parameters)==null?void 0:T.docs)==null?void 0:_.source}}};var O,R,q;p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:`() => {
+  return <Pagination total={10} withEdges />;
+}`,...(q=(R=p.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};var A,G,H;u.parameters={...u.parameters,docs:{...(A=u.parameters)==null?void 0:A.docs,source:{originalSource:`() => {
+  return <Pagination total={77} siblings={3} />;
+}`,...(H=(G=u.parameters)==null?void 0:G.docs)==null?void 0:H.source}}};var J,K,M;d.parameters={...d.parameters,docs:{...(J=d.parameters)==null?void 0:J.docs,source:{originalSource:`() => {
+  return <Pagination total={77} boundaries={3} />;
+}`,...(M=(K=d.parameters)==null?void 0:K.docs)==null?void 0:M.source}}};var Q,U,X;g.parameters={...g.parameters,docs:{...(Q=g.parameters)==null?void 0:Q.docs,source:{originalSource:`() => {
+  return <>
+      <Pagination total={10} variant="solid" isDisabled />
+      <Pagination total={10} variant="outline" isDisabled />
+      <Pagination total={10} variant="ghost" isDisabled />
+      <Pagination total={10} variant="unstyled" isDisabled />
+    </>;
+}`,...(X=(U=g.parameters)==null?void 0:U.docs)==null?void 0:X.source}}};var Y,Z,$;h.parameters={...h.parameters,docs:{...(Y=h.parameters)==null?void 0:Y.docs,source:{originalSource:`() => {
+  return <Pagination total={10} withControls={false} />;
+}`,...($=(Z=h.parameters)==null?void 0:Z.docs)==null?void 0:$.source}}};var tt,ot,rt;P.parameters={...P.parameters,docs:{...(tt=P.parameters)==null?void 0:tt.docs,source:{originalSource:`() => {
+  const [page, onChange] = useState<number>(1);
+  return <Pagination page={page} total={10} onChange={onChange} />;
+}`,...(rt=(ot=P.parameters)==null?void 0:ot.docs)==null?void 0:rt.source}}};var at,et,nt;x.parameters={...x.parameters,docs:{...(at=x.parameters)==null?void 0:at.docs,source:{originalSource:`() => {
+  return <>
+      <Pagination total={10} controlProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+      <Pagination total={10} controlPrevProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+      <Pagination total={10} controlNextProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+    </>;
+}`,...(nt=(et=x.parameters)==null?void 0:et.docs)==null?void 0:nt.source}}};var st,it,ct;j.parameters={...j.parameters,docs:{...(st=j.parameters)==null?void 0:st.docs,source:{originalSource:`() => {
+  return <>
+      <Pagination total={10} withEdges edgeProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+      <Pagination total={10} withEdges edgeFirstProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+      <Pagination total={10} withEdges edgeLastProps={{
+      children: <Icon icon={faPoo} />
+    }} />
+    </>;
+}`,...(ct=(it=j.parameters)==null?void 0:it.docs)==null?void 0:ct.source}}};const ko=["basic","withSize","withVariant","withColorScheme","withDefaultPage","withTotal","withEdge","withSiblings","withBoundaries","withDisabled","disabledControlButton","customControl","customControlButton","customEdgeButton"];export{ko as __namedExportsOrder,n as basic,P as customControl,x as customControlButton,j as customEdgeButton,Do as default,h as disabledControlButton,d as withBoundaries,c as withColorScheme,m as withDefaultPage,g as withDisabled,p as withEdge,u as withSiblings,s as withSize,l as withTotal,i as withVariant};

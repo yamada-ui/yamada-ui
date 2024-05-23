24d8a058e35df910318982f8c4ee9792576ebfa7
108yen <kazuking.1911@gmail.com>
2024-05-23 12:52:48 +0900

    test(rating): added hover test

--- packages/components/rating/tests/rating.test.tsx
+++ packages/components/rating/tests/rating.test.tsx
@@ -1,12 +1,30 @@
-import { a11y, render, screen } from "@yamada-ui/test"
+import { a11y, fireEvent, render, screen } from "@yamada-ui/test"
 import { Rating } from "../src"
 
 describe("<Rating />", () => {
-  test("Rating renders correctly", async () => {
+  const defaultGetBoundingClientRect =
+    window.HTMLElement.prototype.getBoundingClientRect
+
+  beforeAll(() => {
+    window.HTMLElement.prototype.getBoundingClientRect = () => {
+      return {
+        left: 16,
+        width: 100,
+        height: 20,
+      } as DOMRect
+    }
+  })
+
+  afterAll(() => {
+    window.HTMLElement.prototype.getBoundingClientRect =
+      defaultGetBoundingClientRect
+  })
+
+  test("rating renders correctly", async () => {
     await a11y(<Rating />)
   })
 
-  test("Rating renders correctly with value", () => {
+  test("rating renders correctly with value", () => {
     render(<Rating value={4} data-testid="rating" />)
     const filledIcons = screen
       .getByTestId("rating")
@@ -34,13 +52,36 @@ describe("<Rating />", () => {
     )
   })
 
-  test("Disabled Rating renders correctly", () => {
+  test("disabled Rating renders correctly", () => {
     render(<Rating isDisabled data-testid="rating" />)
     expect(screen.getByTestId("rating")).toHaveAttribute("disabled")
   })
 
-  test("Readonly Rating renders correctly", () => {
+  test("readonly Rating renders correctly", () => {
     render(<Rating isReadOnly data-testid="rating" />)
     expect(screen.getByTestId("rating")).toHaveAttribute("readonly")
+  })
+
+  test("should be filled to the point of hovering", async () => {
+    const { container } = render(
+      <Rating width={100} height={20} data-testid="rating" />,
+    )
+
+    const rating = screen.getByTestId("rating")
+
+    const items = container.querySelectorAll(".ui-rating__item")
+
+    for (let i = 1; i < items.length; i++) {
+      expect(items[i]).not.toHaveAttribute("data-filled")
+    }
+
+    fireEvent.mouseMove(rating, { clientX: 50, clientY: 10 })
+
+    for (let i = 1; i < 3; i++) {
+      expect(items[i]).toHaveAttribute("data-filled")
+    }
+    for (let i = 3; i < items.length; i++) {
+      expect(items[i]).not.toHaveAttribute("data-filled")
+    }
   })
 })

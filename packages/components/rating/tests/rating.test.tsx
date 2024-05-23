d32a878d9b8cb638abfd52f4f21b199bb2e01a5a
108yen <kazuking.1911@gmail.com>
2024-05-23 18:26:33 +0900

    test(rating): fix `HTMLElement.matcher` error

--- packages/components/rating/tests/rating.test.tsx
+++ packages/components/rating/tests/rating.test.tsx
@@ -13,11 +13,13 @@ describe("<Rating />", () => {
         height: 20,
       } as DOMRect
     }
+    vi.spyOn(HTMLElement.prototype, "matches").mockImplementation(() => true)
   })
 
   afterAll(() => {
     window.HTMLElement.prototype.getBoundingClientRect =
       defaultGetBoundingClientRect
+    vi.clearAllMocks()
   })
 
   test("rating renders correctly", async () => {
@@ -25,28 +27,34 @@ describe("<Rating />", () => {
   })
 
   test("rating renders correctly with value", () => {
-    render(<Rating value={4} data-testid="rating" />)
-    const filledIcons = screen
-      .getByTestId("rating")
-      .querySelectorAll("label[data-filled]")
-    expect(filledIcons).toHaveLength(4 + 1)
+    const { container } = render(<Rating value={4} />)
+
+    const items = container.querySelectorAll(".ui-rating__item")
+
+    for (let i = 1; i < 5; i++) {
+      expect(items[i]).toHaveAttribute("data-filled")
+    }
   })
 
   test("should render with the correct color scheme", () => {
     render(
       <Rating colorScheme="purple" defaultValue={3} data-testid="rating" />,
     )
+
     const filledIcon = screen
       .getByTestId("rating")
       .querySelectorAll("label[data-filled]")[1]
+
     expect(filledIcon).toHaveClass("ui-rating__item css-14gxg28")
   })
 
   test("should render with the correct size", () => {
     render(<Rating size="sm" defaultValue={3} data-testid="rating" />)
+
     const filledIcon = screen
       .getByTestId("rating")
       .querySelectorAll("label[data-filled]")[1]
+
     expect(filledIcon.getElementsByTagName("div")[0]).toHaveClass(
       "ui-rating__item__icon css-sqp1dq",
     )
@@ -54,11 +62,13 @@ describe("<Rating />", () => {
 
   test("disabled Rating renders correctly", () => {
     render(<Rating isDisabled data-testid="rating" />)
+
     expect(screen.getByTestId("rating")).toHaveAttribute("disabled")
   })
 
   test("readonly Rating renders correctly", () => {
     render(<Rating isReadOnly data-testid="rating" />)
+
     expect(screen.getByTestId("rating")).toHaveAttribute("readonly")
   })
 

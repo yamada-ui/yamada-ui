ed2f41fff3419bbcfe1c57281e9ba6138562dca4
108yen <kazuking.1911@gmail.com>
2024-05-29 21:26:39 +0900

    test(select): refactor existing tests

--- packages/components/select/tests/select.test.tsx
+++ packages/components/select/tests/select.test.tsx
@@ -3,181 +3,238 @@ import { Select, Option, OptionGroup, MultiSelect } from "../src"
 
 describe("<Select />", () => {
   test("Select renders correctly", async () => {
-    const { container } = render(
-      <>
-        <Select>
+    await a11y(
+      <Select>
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+      </Select>,
+    )
+  })
+
+  test("should render select", async () => {
+    const { user } = render(
+      <Select role="combobox">
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+      </Select>,
+    )
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).toHaveTextContent(/one/i)
+    expect(options[1]).toHaveTextContent(/two/i)
+  })
+
+  test("should render select with option group", async () => {
+    const { user } = render(
+      <Select role="combobox">
+        <OptionGroup label="Numbers">
           <Option value="one">One</Option>
-          <Option value="two">Two</Option>
-        </Select>
-      </>,
-    )
-    await a11y(container)
-  })
-
-  test("should render select", () => {
-    render(
-      <Select data-testid="Select" placeholder="Select numbers">
-        <Option data-testid="Option" value="one">
-          One
-        </Option>
-        <Option value="two">Two</Option>
-      </Select>,
-    )
-    expect(screen.getByTestId("Select")).toBeInTheDocument()
-    expect(screen.getByTestId("Select")).toHaveTextContent("Select numbers")
-    expect(screen.getByTestId("Option")).toHaveTextContent("One")
-  })
-
-  test("should render select with option group", () => {
-    render(
-      <Select data-testid="Select">
-        <OptionGroup data-testid="OptionGroup" label="Numbers">
-          <Option data-testid="Option" value="one">
-            One
-          </Option>
-          <Option value="two">Two</Option>
         </OptionGroup>
+        <Option value="two">Two</Option>
       </Select>,
     )
-    expect(screen.getByTestId("OptionGroup")).toHaveAttribute(
-      "data-label",
-      "Numbers",
-    )
-    expect(screen.getByText("Numbers")).toBeInTheDocument()
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const select = screen.getByRole("select")
+    expect(select).toBeInTheDocument()
+    expect(select).toHaveTextContent(/numbers/i)
+    expect(select).toHaveTextContent(/one/i)
   })
 
-  test("should render select with placeholder included in options", () => {
-    render(
+  test("should render select with placeholder", async () => {
+    const { user } = render(
+      <Select role="combobox" placeholder="Select numbers">
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+      </Select>,
+    )
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+    expect(input).toHaveTextContent(/select numbers/i)
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).toHaveTextContent(/select numbers/i)
+  })
+
+  test("should render correctly with placeholder not included in options", async () => {
+    const { user } = render(
       <Select
-        data-testid="Select"
-        placeholder="Numbers"
-        placeholderInOptions={true}
+        role="combobox"
+        placeholder="Select numbers"
+        placeholderInOptions={false}
       >
-        <Option data-testid="Option" value="one">
-          One
-        </Option>
+        <Option value="one">One</Option>
         <Option value="two">Two</Option>
       </Select>,
     )
-    expect(screen.getAllByText("Numbers")).toHaveLength(2)
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+    expect(input).toHaveTextContent(/select numbers/i)
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).not.toHaveTextContent(/select numbers/i)
   })
 
-  test("should disable select", () => {
+  test("should be disable", () => {
     render(
-      <Select data-testid="Select" placeholder="Numbers" isDisabled>
-        <Option data-testid="Option" value="one">
-          One
-        </Option>
+      <Select role="combobox" isDisabled>
+        <Option value="one">One</Option>
         <Option value="two">Two</Option>
       </Select>,
     )
-    expect(screen.getByTestId("Select")).toHaveAttribute("disabled")
+
+    const input = screen.getByRole("combobox")
+    expect(input).toHaveAttribute("disabled")
+    expect(input).toHaveAttribute("aria-disabled", "true")
   })
 
-  test("should be read only", () => {
+  test("should be readonly", () => {
     render(
-      <Select data-testid="Select" placeholder="Numbers" isReadOnly>
-        <Option data-testid="Option" value="one">
-          One
-        </Option>
+      <Select role="combobox" placeholder="Numbers" isReadOnly>
+        <Option value="one">One</Option>
         <Option value="two">Two</Option>
       </Select>,
     )
-    expect(screen.getByTestId("Select")).toHaveAttribute("readonly")
+
+    const input = screen.getByRole("combobox")
+    expect(input).toHaveAttribute("readonly")
+    expect(input).toHaveAttribute("data-readonly")
   })
 
-  test("should disable option", () => {
-    render(
-      <Select data-testid="Select">
-        <Option data-testid="Option" value="one" isDisabled>
+  test("should be disable option", async () => {
+    const { user } = render(
+      <Select role="combobox">
+        <Option value="one" isDisabled>
           One
         </Option>
         <Option value="two">Two</Option>
       </Select>,
     )
-    expect(screen.getByTestId("Option")).toHaveAttribute(
-      "aria-disabled",
-      "true",
-    )
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).toHaveTextContent(/one/i)
+    expect(options[0]).toHaveAttribute("data-disabled")
+    expect(options[0]).toHaveAttribute("aria-disabled", "true")
   })
 })
 
 describe("<MultiSelect />", () => {
-  test("MultiSelect renders correctly", async () => {
-    const { container } = render(
-      <>
-        <MultiSelect>
-          <Option value="one">One</Option>
-          <Option value="two">Two</Option>
-        </MultiSelect>
-      </>,
+  test("should pass a11y test", async () => {
+    await a11y(
+      <MultiSelect>
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+      </MultiSelect>,
     )
-    await a11y(container)
+  })
+
+  test("MultiSelect renders correctly", async () => {
+    const { user } = render(
+      <MultiSelect role="combobox">
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+      </MultiSelect>,
+    )
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).toHaveTextContent(/one/i)
+    expect(options[1]).toHaveTextContent(/two/i)
   })
 
   test("when items passed to props, renders correctly", async () => {
-    const { container } = render(
+    const { user } = render(
       <MultiSelect
+        role="combobox"
         items={[
-          { label: "One", value: "One" },
-          { label: "Two", value: "Two" },
+          { label: "One", value: "one" },
+          { label: "Two", value: "two" },
         ]}
       />,
     )
-    await a11y(container)
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const options = screen.getAllByRole("select-item")
+    expect(options[0]).toHaveTextContent(/one/i)
+    expect(options[1]).toHaveTextContent(/two/i)
   })
 
   test("when nested items passed to props, renders correctly", async () => {
-    const { container } = render(
+    const { user } = render(
       <MultiSelect
+        role="combobox"
         items={[
           {
-            label: "One",
-            items: [
-              { label: "Nested One-One", value: "Nested One-One" },
-              { label: "Nested One-Two", value: "Nested One-Two" },
-            ],
+            label: "Numbers",
+            items: [{ label: "One", value: "one" }],
           },
           {
             label: "Two",
-            items: [
-              { label: "Nested Two-One", value: "Nested Two-One" },
-              { label: "Nested Two-Two", value: "Nested Two-Two" },
-            ],
+            value: "two",
           },
         ]}
       />,
     )
-    await a11y(container)
+
+    const input = screen.getByRole("combobox")
+    expect(input).toBeInTheDocument()
+
+    await user.click(input)
+
+    const select = screen.getByRole("select")
+    expect(select).toBeInTheDocument()
+    expect(select).toHaveTextContent(/numbers/i)
+    expect(select).toHaveTextContent(/one/i)
   })
 
   test("when items clicked twice, renders correctly", async () => {
     const { user } = render(
-      <MultiSelect data-testid="MultiSelect">
-        <Option data-testid="Option-One" value="one">
-          One
-        </Option>
-        <Option data-testid="Option-Two" value="two">
-          Two
-        </Option>
-        <Option data-testid="Option-Three" value="three">
-          Three
-        </Option>
+      <MultiSelect role="combobox">
+        <Option value="one">One</Option>
+        <Option value="two">Two</Option>
+        <Option value="three">Three</Option>
       </MultiSelect>,
     )
 
-    const multiSelect = screen.getByTestId("MultiSelect")
+    const input = screen.getByRole("combobox")
 
-    await user.click(multiSelect)
+    await user.click(input)
 
-    const optionOne = screen.getByTestId("Option-One")
+    const option1 = screen.getByText(/one/i)
+    await user.click(option1)
 
-    await user.click(optionOne)
+    const option2 = screen.getByText(/two/i)
+    await user.click(option2)
 
-    const optionTwo = screen.getByTestId("Option-Two")
-
-    await user.click(optionTwo)
-
-    expect(screen.getByText("One,")).toBeInTheDocument()
+    expect(input).toHaveTextContent(/one/i)
+    expect(input).toHaveTextContent(/two/i)
   })
 })

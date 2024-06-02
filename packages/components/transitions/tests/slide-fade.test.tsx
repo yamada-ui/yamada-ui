bd74d6150db6ba1dd47dd9c0114b724fba78b187
108yen <kazuking.1911@gmail.com>
2024-06-02 19:23:09 +0900

    test(transitions): added `slide-fade` test

--- packages/components/transitions/tests/slide-fade.test.tsx
+++ packages/components/transitions/tests/slide-fade.test.tsx
@@ -1,18 +1,41 @@
-import { render } from "@yamada-ui/test"
+import { waitFor } from "@testing-library/react"
+import { useDisclosure, Button } from "@yamada-ui/react"
+import { a11y, render, screen } from "@yamada-ui/test"
 import { SlideFade } from "../src"
 describe("<SlideFade />", () => {
-  test("renders correctly", () => {
-    const { getByText } = render(<SlideFade>Test</SlideFade>)
-
-    getByText("Test")
+  test("passes a11y test", async () => {
+    await a11y(<SlideFade />)
   })
 
-  test("renders correctly with false reverse prop", () => {
-    render(<SlideFade reverse />)
+  test("fade-in and fade-out work correctly", async () => {
+    const SlideFadeTestComponent = () => {
+      const { isOpen, onToggle } = useDisclosure()
+
+      return (
+        <>
+          <Button onClick={onToggle}>button</Button>
+          <SlideFade isOpen={isOpen}>slide fade test</SlideFade>
+        </>
+      )
+    }
+
+    const { user } = render(<SlideFadeTestComponent />)
+
+    const slideFade = await screen.findByText(/slide fade test/i)
+    expect(slideFade).not.toBeVisible()
+
+    const button = await screen.findByRole("button", { name: /button/i })
+
+    await user.click(button)
+    await waitFor(() => expect(slideFade).toBeVisible())
+
+    await user.click(button)
+    await waitFor(() => expect(slideFade).not.toBeVisible())
   })
 
   test("default offset is set correctly", () => {
     const { getByTestId } = render(<SlideFade data-testid="slide-fade" />)
+
     expect(getByTestId("slide-fade")).toHaveStyle(
       "transform: translateX(0px) translateY(8px) translateZ(0);",
     )
@@ -22,6 +45,7 @@ describe("<SlideFade />", () => {
     const { getByTestId } = render(
       <SlideFade offsetX={10} data-testid="slide-fade" />,
     )
+
     expect(getByTestId("slide-fade")).toHaveStyle(
       "transform: translateX(10px) translateY(8px) translateZ(0);",
     )
@@ -31,6 +55,7 @@ describe("<SlideFade />", () => {
     const { getByTestId } = render(
       <SlideFade offsetY={10} data-testid="slide-fade" />,
     )
+
     expect(getByTestId("slide-fade")).toHaveStyle(
       "transform: translateX(0px) translateY(10px) translateZ(0);",
     )

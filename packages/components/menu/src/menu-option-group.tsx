63867d42df3972001d867625b0b0d2327614f1f4
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-03 16:14:56 +0900

    fix: fix className to BEM in `Menu`

--- packages/components/menu/src/menu-option-group.tsx
+++ packages/components/menu/src/menu-option-group.tsx
@@ -93,7 +93,7 @@ export const MenuOptionGroup = forwardRef(
     return (
       <MenuGroup
         ref={ref}
-        className={cx("ui-menu__item--group--option", className)}
+        className={cx("ui-menu__item-group--option", className)}
         {...omitObject(rest, ["value", "onChange"])}
       >
         {cloneChildren}

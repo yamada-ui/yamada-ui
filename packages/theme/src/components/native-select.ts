8b3ff08fe9f13a900c792afef42bcbb896906ff0
zonoryo17 <p.naka.0p0@gmail.com>
2024-06-06 23:54:22 +0900

    refactor: made native-select component headless

--- packages/theme/src/components/native-select.ts
+++ packages/theme/src/components/native-select.ts
@@ -4,11 +4,16 @@ import { Input } from "./input"
 
 export const NativeSelect: ComponentMultiStyle = mergeMultiStyle(Input, {
   baseStyle: {
-    container: {},
+    container: {
+      position: "relative",
+      w: "100%",
+      h: "fit-content",
+    },
     field: {
       cursor: "pointer",
       pr: "8",
       pb: "px",
+      pe: "2rem",
       _focus: {
         zIndex: "unset",
       },
@@ -17,6 +22,13 @@ export const NativeSelect: ComponentMultiStyle = mergeMultiStyle(Input, {
       },
     },
     icon: {
+      position: "absolute",
+      display: "inline-flex",
+      alignItems: "center",
+      justifyContent: "center",
+      pointerEvents: "none",
+      top: "50%",
+      transform: "translateY(-50%)",
       w: "6",
       py: "2",
       color: ["blackAlpha.600", "whiteAlpha.700"],

dafe62f7fcf1f37b3d7ae4d6f159d836ef2cdfc0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-06-08 21:28:28 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@82d0d6b463525d5a7dbe38149bd0ef25571fa769 🚀

--- assets/center.stories-fysmovMH.js
+++
@@ -1,9 +0,0 @@
-import{j as o}from"./jsx-runtime-X2b_N9AH.js";import{I as p,o as d}from"./fontawesome-icon-C1r1eWas.js";import{C as t}from"./center-C0juVNGk.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D3ylJrlI.js";import"./icon-DcmOu1fx.js";import"./index-CdvFIPz6.js";import"./theme-provider-DuMcMORV.js";import"./factory-C0k1XOS4.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xaNYqS-v.js";import"./forward-ref-Dr5Hqd9a.js";const H={title:"Components / Layouts / Center",component:t},r=()=>o.jsx(t,{minW:"xs",minH:"xs",rounded:"md",bg:"primary",color:"white",children:"Center"}),e=()=>o.jsx(t,{minW:"6xs",minH:"6xs",rounded:"full",bg:"gray.500",color:"white",children:o.jsx(p,{icon:d,size:"3xl"})});var n,s,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
-  return <Center minW="xs" minH="xs" rounded="md" bg="primary" color="white">
-      Center
-    </Center>;
-}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var m,a,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
-  return <Center minW="6xs" minH="6xs" rounded="full" bg="gray.500" color="white">
-      <Icon icon={faSkull} size="3xl" />
-    </Center>;
-}`,...(c=(a=e.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};const S=["basic","iconWithFrame"];export{S as __namedExportsOrder,r as basic,H as default,e as iconWithFrame};

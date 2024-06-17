b8bf321cd35255f6385d0d0349cf91f37c484b29
koralle <mkhd7930@gmail.com>
2024-06-17 21:39:49 +0900

    feat(switch): add `switch` role

--- packages/components/switch/src/switch.tsx
+++ packages/components/switch/src/switch.tsx
@@ -116,6 +116,7 @@ export const Switch = forwardRef<SwitchProps, "input">((props, ref) => {
       {...rest}
     >
       <ui.input
+        role="switch"
         className={cx("ui-switch__input", className)}
         {...getInputProps(inputProps, ref)}
       />

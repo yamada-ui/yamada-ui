cef4c763ee8b158e57fc6a190be40043d45c649d
k-wakamatsu-tms <69659176+k-wakamatsu-tms@users.noreply.github.com>
2024-06-19 18:11:13 +0900

    test: add keyboard navigation for multi-autocomplete component

--- packages/components/autocomplete/tests/multi-autocomplete.test.tsx
+++ packages/components/autocomplete/tests/multi-autocomplete.test.tsx
@@ -1,7 +1,11 @@
+import type { AutocompleteItem } from "@yamada-ui/react"
 import { AutocompleteOption, MultiAutocomplete } from "@yamada-ui/react"
 import { fireEvent, render, screen, waitFor } from "@yamada-ui/test"
 
 describe("<MultiAutoComplete />", () => {
+  const AUTOCOMPLETE_CLASS = ".ui-multi-autocomplete"
+  const AUTOCOMPLETE_ITEM_ROLE = "autocomplete-item"
+
   describe("renders correctly", () => {
     test("default", async () => {
       const { container } = render(
@@ -42,6 +46,136 @@ describe("<MultiAutoComplete />", () => {
       )
 
       expect(screen.getByText("option1,")).toBeInTheDocument()
+    })
+  })
+
+  describe("first item is selected", () => {
+    const ITEMS: AutocompleteItem[] = [
+      {
+        label: "option1",
+        value: "option1",
+      },
+      {
+        label: "option2",
+        value: "option2",
+      },
+      {
+        label: "option3",
+        value: "option3",
+      },
+    ]
+
+    test("focuses first enabled item when the first item is selected", async () => {
+      const { user, container } = render(
+        <MultiAutocomplete
+          placeholder="Select Option"
+          items={ITEMS}
+          defaultValue={["option1", "option2"]}
+          omitSelectedValues
+        />,
+      )
+
+      const autocomplete = container.querySelector(AUTOCOMPLETE_CLASS)
+      expect(autocomplete).toBeInTheDocument()
+
+      await user.click(autocomplete!)
+
+      const optionElements = screen.getAllByRole(AUTOCOMPLETE_ITEM_ROLE)
+      expect(optionElements).toHaveLength(3)
+
+      await user.keyboard("{Escape>}{ArrowDown>}")
+      await waitFor(() =>
+        expect(optionElements[2]).toHaveAttribute("data-focus"),
+      )
+    })
+
+    test("focuses first item when the first item is not selected", async () => {
+      const { user, container } = render(
+        <MultiAutocomplete
+          placeholder="Select Option"
+          items={ITEMS}
+          defaultValue={["option3"]}
+          omitSelectedValues
+        />,
+      )
+
+      const autocomplete = container.querySelector(AUTOCOMPLETE_CLASS)
+      expect(autocomplete).toBeInTheDocument()
+
+      await user.click(autocomplete!)
+
+      const optionElements = screen.getAllByRole(AUTOCOMPLETE_ITEM_ROLE)
+      expect(optionElements).toHaveLength(3)
+
+      await user.keyboard("{Escape>}{ArrowDown>}")
+      await waitFor(() =>
+        expect(optionElements[0]).toHaveAttribute("data-focus"),
+      )
+    })
+  })
+
+  describe("last item is selected", () => {
+    const ITEMS: AutocompleteItem[] = [
+      {
+        label: "option1",
+        value: "option1",
+      },
+      {
+        label: "option2",
+        value: "option2",
+      },
+      {
+        label: "option3",
+        value: "option3",
+      },
+    ]
+
+    test("focuses last enabled item when the last item is selected", async () => {
+      const { user, container } = render(
+        <MultiAutocomplete
+          placeholder="Select Option"
+          items={ITEMS}
+          defaultValue={["option1", "option3"]}
+          omitSelectedValues
+        />,
+      )
+
+      const autocomplete = container.querySelector(AUTOCOMPLETE_CLASS)
+      expect(autocomplete).toBeInTheDocument()
+
+      await user.click(autocomplete!)
+
+      const optionElements = screen.getAllByRole(AUTOCOMPLETE_ITEM_ROLE)
+      expect(optionElements).toHaveLength(3)
+
+      await user.keyboard("{Escape>}{ArrowUp>}")
+      await waitFor(() =>
+        expect(optionElements[1]).toHaveAttribute("data-focus"),
+      )
+    })
+
+    test("focuses last item when the last item is not selected", async () => {
+      const { user, container } = render(
+        <MultiAutocomplete
+          placeholder="Select Option"
+          items={ITEMS}
+          defaultValue={["option1"]}
+          omitSelectedValues
+        />,
+      )
+
+      const autocomplete = container.querySelector(AUTOCOMPLETE_CLASS)
+      expect(autocomplete).toBeInTheDocument()
+
+      await user.click(autocomplete!)
+
+      const optionElements = screen.getAllByRole(AUTOCOMPLETE_ITEM_ROLE)
+      expect(optionElements).toHaveLength(3)
+
+      await user.keyboard("{Escape>}{ArrowUp>}")
+      await waitFor(() =>
+        expect(optionElements[2]).toHaveAttribute("data-focus"),
+      )
     })
   })
 })

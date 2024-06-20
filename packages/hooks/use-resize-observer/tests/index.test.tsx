1d0764b2ce9e0add921759cfd1ac4555d671a7cc
kurakke <kcanggua@gmail.com>
2024-06-20 23:00:42 +0900

    test: add tests for use-resize-observer hook

--- packages/hooks/use-resize-observer/tests/index.test.tsx
+++ packages/hooks/use-resize-observer/tests/index.test.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@yamada-ui/react"
 import { render, screen, waitFor } from "@yamada-ui/test"
-import { useResizeObserver } from "../src"
+import { useElementSize, useResizeObserver } from "../src"
 
 describe("useResizeObserver", () => {
   const defaultResizeObserver = global.ResizeObserver
@@ -46,5 +46,52 @@ describe("useResizeObserver", () => {
     const button = await screen.findByRole("button")
 
     await waitFor(() => expect(button.textContent).toBe("400 x 320"))
+  })
+})
+
+describe("useElementSize", () => {
+  const defaultResizeObserver = global.ResizeObserver
+
+  beforeAll(() => {
+    global.ResizeObserver = class ResizeObserver {
+      constructor(cb: ResizeObserverCallback) {
+        ;(() => {
+          cb(
+            [
+              {
+                contentRect: {
+                  height: 200,
+                  width: 150,
+                },
+              },
+            ] as ResizeObserverEntry[],
+            this,
+          )
+        })()
+      }
+      observe = vi.fn()
+      unobserve = vi.fn()
+      disconnect = vi.fn()
+    }
+  })
+
+  afterAll(() => {
+    global.ResizeObserver = defaultResizeObserver
+  })
+
+  const ButtonWithSize = () => {
+    const { ref, width, height } = useElementSize()
+    return (
+      <Button ref={ref}>
+        {width} x {height}
+      </Button>
+    )
+  }
+
+  test("returns width and height correctly", async () => {
+    render(<ButtonWithSize />)
+    const button = await screen.findByRole("button")
+
+    expect(button.textContent).toBe("150 x 200")
   })
 })

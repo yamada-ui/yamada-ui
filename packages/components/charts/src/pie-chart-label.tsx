d1b453120c592ff63d29639a48551d0ff229758c
108yen <kazuking.1911@gmail.com>
2024-07-07 11:30:54 +0900

    refactor(pie-chart): refactor radius

--- packages/components/charts/src/pie-chart-label.tsx
+++ packages/components/charts/src/pie-chart-label.tsx
@@ -2,7 +2,6 @@ import { ui } from "@yamada-ui/core"
 import { cx, isUndefined } from "@yamada-ui/utils"
 
 const RADIAN = Math.PI / 180
-//TODO: considar to which distance is this. (center or outerRadius)
 const DEFAULT_LABEL_OFFSET = 22
 
 export type PieChartLabelProps = {
@@ -12,6 +11,7 @@ export type PieChartLabelProps = {
   midAngle?: number
   innerRadius?: number
   outerRadius?: number
+  middleRadius?: number
   percent?: number
   value?: number
   labelOffset?: number
@@ -28,6 +28,7 @@ export const pieChartLabel: (props: PieChartLabelProps) => React.ReactNode = ({
   midAngle,
   innerRadius,
   outerRadius,
+  middleRadius,
   percent,
   value,
   labelOffset: labelOffsetProp,
@@ -38,10 +39,9 @@ export const pieChartLabel: (props: PieChartLabelProps) => React.ReactNode = ({
 }) => {
   const labelOffset =
     labelOffsetProp ?? (outerRadius - innerRadius) * 0.5 + DEFAULT_LABEL_OFFSET
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5
 
-  const x = cxProp + (radius + labelOffset) * Math.cos(-midAngle * RADIAN)
-  const y = cyProp + (radius + labelOffset) * Math.sin(-midAngle * RADIAN)
+  const x = cxProp + (middleRadius + labelOffset) * Math.cos(-midAngle * RADIAN)
+  const y = cyProp + (middleRadius + labelOffset) * Math.sin(-midAngle * RADIAN)
 
   const textAnchor = x > cxProp ? "start" : x < cxProp ? "end" : "middle"
   const displayLabel = () => {
@@ -70,4 +70,23 @@ export const pieChartLabel: (props: PieChartLabelProps) => React.ReactNode = ({
       {displayLabel()}
     </ui.text>
   )
+}
+
+export type PieChartLabelLineProps = {
+  className?: string
+  cx?: number
+  cy?: number
+  innerRadius?: number
+  midAngle?: number
+  middleRadius?: number
+  outerRadius?: number
+  points?: Array<{ x: number; y: number }>
+  stroke?: string
+  strokeWidth?: number
+}
+
+export const pieChartLabelLine: (
+  props: PieChartLabelLineProps,
+) => React.ReactElement<SVGElement> = ({}) => {
+  return <ui.path />
 }

3ef7c308ebcdf8a7e72202afd8cda61369913018
Kazuhiro Kumagai <78473460+cidkumagai@users.noreply.github.com>
2024-07-09 22:17:26 +0900

    test: enhance test coverage for segmented-control

---
+++ .changeset/funny-dolls-jump.md
@@ -0,0 +1,5 @@
+---
+"@yamada-ui/segmented-control": patch
+---
+
+Enhance Test Coverage for `@yamada-ui/segmented-control`
--- packages/components/segmented-control/tests/segmented-control.test.tsx
+++ packages/components/segmented-control/tests/segmented-control.test.tsx
@@ -1,4 +1,4 @@
-import { a11y, render, screen } from "@yamada-ui/test"
+import { a11y, fireEvent, render, screen } from "@yamada-ui/test"
 import type { SegmentedControlItem } from "../src"
 import { SegmentedControl, SegmentedControlButton } from "../src"
 
@@ -76,5 +76,67 @@ describe("<SegmentedControl />", () => {
         expect(input).toBeDisabled()
       },
     )
+  })
+
+  test("focus moves correctly between buttons", async () => {
+    const { user } = render(
+      <SegmentedControl>
+        <SegmentedControlButton value="one" data-testid="firstButton">
+          One
+        </SegmentedControlButton>
+        <SegmentedControlButton value="two" data-testid="secondButton" disabled>
+          Two
+        </SegmentedControlButton>
+        <SegmentedControlButton value="three" data-testid="thirdButton">
+          Three
+        </SegmentedControlButton>
+      </SegmentedControl>,
+    )
+    const firstButton = screen.getByTestId("firstButton")
+    const secondButton = screen.getByTestId("secondButton")
+    const thirdButton = screen.getByTestId("thirdButton")
+
+    expect(firstButton).toHaveAttribute("data-checked")
+
+    await user.click(secondButton)
+
+    expect(secondButton).not.toHaveAttribute("data-checked")
+
+    await user.click(thirdButton)
+
+    expect(thirdButton).toHaveAttribute("data-checked")
+  })
+
+  test("SegmentedControl with non-SegmentedControlButton children renders correctly", async () => {
+    const { container } = render(
+      <SegmentedControl data-testid="SegmentedControl" isDisabled>
+        <option>one</option>
+        <option>two</option>
+        <option>three</option>
+      </SegmentedControl>,
+    )
+
+    await a11y(container)
+  })
+
+  test("focus moves to the next element when the focused element is disabled", async () => {
+    render(
+      <SegmentedControl>
+        <SegmentedControlButton value="one">One</SegmentedControlButton>
+        <SegmentedControlButton value="two" data-testid="secondButton" disabled>
+          Two
+        </SegmentedControlButton>
+        <SegmentedControlButton value="three" data-testid="thirdButton">
+          Three
+        </SegmentedControlButton>
+      </SegmentedControl>,
+    )
+
+    const secondButton = screen.getByTestId("secondButton")
+    const thirdButton = screen.getByTestId("thirdButton")
+
+    fireEvent.focus(secondButton)
+
+    expect(thirdButton).toHaveAttribute("data-focus")
   })
 })

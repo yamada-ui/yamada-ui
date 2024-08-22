df9f7efc3bc103bd00aba6904ff77c2f33aa8ea8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-08-22 21:46:55 +0900

    feat(create-yamada-app): uncommented prompts in config

--- packages/create-yamada-app/src/utils/templates/next/prompts.ts
+++ packages/create-yamada-app/src/utils/templates/next/prompts.ts
@@ -1,15 +1,15 @@
-// import c from "chalk"
+import c from "chalk"
 import type { Prompts } from "../index.types"
 
 export const prompts: Prompts = {
-  // appRouter: {
-  //   type: "confirm",
-  //   message: `Would you like to use ${c.magenta("App Router")}?`,
-  //   initialValue: true,
-  // },
-  // eslint: {
-  //   type: "confirm",
-  //   message: `Would you like to use ${c.magenta("ESLint")}?`,
-  //   initialValue: true,
-  // },
+  appRouter: {
+    type: "confirm",
+    message: `Would you like to use ${c.magenta("App Router")}?`,
+    initialValue: true,
+  },
+  eslint: {
+    type: "confirm",
+    message: `Would you like to use ${c.magenta("ESLint")}?`,
+    initialValue: true,
+  },
 }

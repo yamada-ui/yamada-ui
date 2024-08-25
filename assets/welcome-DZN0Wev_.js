07a23acb49801a9ba8c2a21e48d5c0a0fb043154
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-08-25 04:48:11 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@990c71bafb405c75bf0ab45a57617995335706bc 🚀

---
+++ assets/welcome-DZN0Wev_.js
@@ -0,0 +1,24 @@
+import{j as r}from"./jsx-runtime-Nms4Y4qS.js";import{useMDXComponents as m}from"./index-DlpNa54Y.js";import{ae as p}from"./index-BErZCdJI.js";import{J as e}from"./components-Dlk9ss1G.js";import{C as n}from"./center-BjLpYenC.js";import{I as s}from"./image-tQpfIbvg.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BZqsWled.js";import"../sb-preview/runtime.js";import"./index-8jxD160K.js";import"./index-DxjWwZXO.js";import"./extends-CF3RwP-h.js";import"./inheritsLoose-DR8r8Ogv.js";import"./_baseAssignValue-qKg1-Mlh.js";import"./_getPrototype-CVMNbDHg.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";import"./index-l3-ow9aL.js";import"./factory-R2Y4PRbz.js";import"./index-DwVKC9Jw.js";import"./theme-provider-DgHPuWDh.js";import"./ui-provider-3Lcv-Den.js";import"./index-BBB69wBL.js";import"./motion-Bd6WkMzw.js";import"./loading-provider-BBa-LEqd.js";import"./index-LK_sHYyG.js";import"./Combination-CONbhR2A.js";import"./loading-CS9bmOQf.js";import"./icon-Cag4izmS.js";import"./index-8JqQBj0s.js";import"./use-component-style-DhC_P_n8.js";import"./forward-ref-BmTAT9U5.js";import"./motion-Dpi5adfX.js";import"./index-Ms_xn5rh.js";import"./container-portal-3TEhbyIR.js";import"./notice-DbwXzKJ4.js";import"./alert-Biq7199x.js";import"./close-button-B0zD6pHy.js";import"./use-ripple-07NCTLKG.js";import"./container-Sid-IIgR.js";import"./stack-BylotlZw.js";import"./box-DE78NN7h.js";import"./text-B1bw_vjj.js";import"./checkbox-KFALWN6w.js";import"./index-id8kYb0w.js";import"./index-DsxzWwQ3.js";import"./form-control-qaT0mOOy.js";import"./flex-Bv58BnJQ.js";import"./index-kM831JbP.js";import"./event-imqIgfLG.js";import"./number-49BHn0Cl.js";import"./tooltip-ByAxuh-Q.js";import"./index-S8W3Ewfq.js";import"./index-H-Woa-k_.js";import"./index-jR8x9-Ez.js";import"./index-DZFlsgPM.js";import"./index-DDGSjvsI.js";import"./slide-fade-BgnG1T0R.js";import"./utils-B6NRY1gi.js";import"./scale-fade-BYdP9kSX.js";import"./index-DarizNyh.js";import"./index-Bqz3rL1X.js";import"./select-D4na6yw1.js";import"./option-BcpxVDv7.js";import"./index-BgQAyYS-.js";import"./popover-content-CSBEfcVB.js";import"./index--1NhZ_DV.js";import"./index-Du0-7Fls.js";import"./index-cDSTOvqi.js";import"./use-image-DKAXBp7a.js";function i(t){const o={code:"code",h1:"h1",li:"li",ol:"ol",p:"p",pre:"pre",...m(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(p,{title:"Documents / Welcome"}),`
+`,r.jsx(e,{children:r.jsx(n,{mb:"16",children:r.jsx(s,{w:"full",maxW:"xl",src:"https://raw.githubusercontent.com/hirotomoyamada/yamada-ui/main/logo/logo-colored@2x.png"})})}),`
+`,r.jsx(o.h1,{id:"usage",children:"Usage"}),`
+`,r.jsxs(o.ol,{children:[`
+`,r.jsxs(o.li,{children:["Wrap your application with the ",r.jsx(o.code,{children:"UIProvider"})," provided"]}),`
+`]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { UIProvider } from "@yamada-ui/react"
+
+const App = ({ children }) => {
+  return <UIProvider>{children}</UIProvider>
+}
+
+export default App
+`})}),`
+`,r.jsx(o.p,{children:"Yamada-ui supports light and dark modes by default"}),`
+`,r.jsxs(o.ol,{start:"2",children:[`
+`,r.jsx(o.li,{children:"Now you can start using components like so"}),`
+`]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-tsx",children:`import { Text } from "@yamada-ui/react"
+
+const Example = () => {
+  return <Text>ギャルのパンティーおくれーーーっ！！！！！</Text>
+}
+`})})]})}function Dr(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{Dr as default};

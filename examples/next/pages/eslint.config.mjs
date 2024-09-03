d9f72a9d81c1d3ac32c3c9d907dd7f19be344dca
koralle <mkhd7930@gmail.com>
2024-09-04 00:15:09 +0900

    chore(examples/next/pages): add eslint-related packages

---
+++ examples/next/pages/eslint.config.mjs
@@ -0,0 +1,112 @@
+import { fixupPluginRules } from '@eslint/compat'
+import eslint from '@eslint/js'
+import pluginNext from '@next/eslint-plugin-next'
+import pluginImport from 'eslint-plugin-import'
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+import {
+  config as tseslintConfig,
+  configs as tseslintConfigs,
+  parser as tseslintParser,
+} from 'typescript-eslint'
+
+export default tseslintConfig(
+  {
+    ignores: ['**/dist', '**/node_modules', '**/.next'],
+  },
+  eslint.configs.recommended,
+  ...tseslintConfigs.recommended,
+  {
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    plugins: {
+      import: fixupPluginRules(pluginImport),
+    },
+    rules: {
+      'import/no-anonymous-default-export': 'warn',
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            order: 'asc',
+          },
+        },
+      ],
+      'import/named': 'off',
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.mts', '.cts', '.tsx', '.d.ts'],
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+  {
+    plugins: {
+      react: pluginReact,
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      '@next/next': fixupPluginRules(pluginNext),
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+      'react/no-unknown-property': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-no-target-blank': 'off',
+      '@next/next/no-img-element': 'off',
+      '@next/next/no-title-in-document-head': 'off',
+      '@next/next/no-assign-module-variable': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    plugins: {
+      'jsx-a11y': fixupPluginRules(pluginJsxA11y),
+    },
+    rules: {
+      'jsx-a11y/alt-text': [
+        'warn',
+        {
+          elements: ['img'],
+          img: ['Image'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+    },
+  },
+)

f4ab3e8b449645c05b87e43fbb5124dbef81e51b
koralle <mkhd7930@gmail.com>
2024-09-04 01:33:02 +0900

    chore(docs): resolve duplication of import-path

--- docs/scripts/mdx/map.ts
+++ docs/scripts/mdx/map.ts
@@ -1,8 +1,7 @@
 import { writeFileSync } from "fs"
 import path from "path"
 import type { Document } from "mdx"
-import type { DocumentMap } from "./utils"
-import { MDX_PATH, type EventName } from "./utils"
+import type { DocumentMap, MDX_PATH, type EventName } from "./utils"
 
 let MAP: DocumentMap = {}
 let TIMEOUT_ID: NodeJS.Timeout | null = null

a81de92a7cbcbae4c3dc5e8832f2cbae7a6d83f7
Kae <kaehehehe@gmail.com>
2024-09-07 21:21:54 +0900

    feat: implement Rotate component

---
+++ packages/components/swap/src/rotate.tsx
@@ -0,0 +1,48 @@
+import type { ThemeProps } from "@yamada-ui/core"
+import { omitThemeProps, useComponentStyle } from "@yamada-ui/core"
+import type { MotionProps, WithTransitionProps } from "@yamada-ui/motion"
+import { motion, motionForwardRef, useMotionAnimation } from "@yamada-ui/motion"
+import { cx } from "@yamada-ui/utils"
+import { useState } from "react"
+import type { SwapElements } from "./swap"
+
+type RotateProps = WithTransitionProps<MotionProps> &
+  ThemeProps<"Rotate"> &
+  SwapElements
+
+export const Rotate = motionForwardRef<RotateProps, "div">((props, ref) => {
+  const [style, mergedProps] = useComponentStyle("Rotate", props)
+  const [currentElement, setCurrentElement] = useState<"from" | "to">("from")
+  const controls = useMotionAnimation()
+
+  let { rotate, duration, className, ...rest } = omitThemeProps(mergedProps)
+
+  const { from, to } = mergedProps
+
+  const onClick = async () => {
+    await controls.start({
+      opacity: 0,
+      rotate: typeof rotate === "string" ? rotate : "360deg",
+    })
+    setCurrentElement(currentElement === "from" ? "to" : "from")
+    await controls.start({ opacity: 1, rotate: "0deg" })
+  }
+
+  return (
+    <motion.div
+      ref={ref}
+      custom={rotate}
+      className={cx(`ui-airy__${currentElement}`, className)}
+      onClick={onClick}
+      animate={controls}
+      initial={{ opacity: 1, rotate: "0deg" }}
+      transition={{
+        duration: duration ? duration : 0.3,
+      }}
+      __css={style}
+      {...rest}
+    >
+      {currentElement === "from" ? from : to}
+    </motion.div>
+  )
+})

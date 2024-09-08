42b1f772fadc1cac4ab1059663a0274a7bb3dc8b
Kae <kaehehehe@gmail.com>
2024-09-08 14:56:52 +0900

    feat: update stories

--- stories/components/motion/swap.stories.tsx
+++ stories/components/motion/swap.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryFn } from "@storybook/react"
-import { Swap } from "@yamada-ui/react"
+import { Apple, Cherry, Menu, Moon, Sun, X } from "@yamada-ui/lucide"
+import { Avatar, Card, Center, Swap, Text } from "@yamada-ui/react"
 
 type Story = StoryFn<typeof Swap>
 
@@ -10,6 +11,153 @@ const meta: Meta<typeof Swap> = {
 
 export default meta
 
-export const basic: Story = () => {
-  return <Swap></Swap>
+export const airy: Story = () => {
+  return (
+    <Center w="calc(100vw - 16px * 2)" h="calc(100vh - 16px * 2)">
+      <Swap
+        from={
+          <Text w="xs" fontSize="5xl">
+            ON
+          </Text>
+        }
+        to={
+          <Text w="xs" fontSize="5xl">
+            OFF
+          </Text>
+        }
+        animation="airy"
+        style={{
+          userSelect: "none",
+          cursor: "pointer",
+        }}
+      />
+
+      <Swap
+        from={<Menu fontSize="50px" />}
+        to={<X fontSize="50px" />}
+        animation="airy"
+      />
+    </Center>
+  )
+}
+
+export const rotate: Story = () => {
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Swap
+        from={<Sun fontSize="50px" />}
+        to={<Moon fontSize="50px" />}
+        animation="rotate"
+        rotate="45deg"
+        duration={0.1}
+        style={{
+          cursor: "pointer",
+        }}
+      />
+
+      <Swap
+        from={<Apple fontSize="50px" />}
+        to={<Cherry fontSize="50px" />}
+        animation="rotate"
+        rotate="360deg"
+        style={{
+          cursor: "pointer",
+        }}
+      />
+    </Center>
+  )
+}
+
+const BackOfCard = () => {
+  return (
+    <Card
+      borderRadius="xl"
+      bg="black"
+      w="xs"
+      h="sm"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      border="1px solid gray"
+    >
+      <Avatar
+        size="xl"
+        name="Hirotomo Yamada"
+        src="https://avatars.githubusercontent.com/u/84060430?v=4"
+      />
+    </Card>
+  )
+}
+
+const FrontOfCard = () => {
+  return (
+    <Card
+      borderRadius="xl"
+      bg="white"
+      w="xs"
+      h="sm"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      border="1px solid gray"
+    >
+      <Text fontSize="xl" color="black">
+        Yamada UI
+      </Text>
+    </Card>
+  )
+}
+
+export const horizontalFlip: Story = () => {
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Swap
+        from={<BackOfCard />}
+        to={<FrontOfCard />}
+        animation="horizontalFlip"
+      />
+
+      <Swap
+        from={<Apple fontSize="50px" />}
+        to={<Cherry fontSize="50px" />}
+        animation="horizontalFlip"
+        style={{
+          cursor: "pointer",
+        }}
+      />
+    </Center>
+  )
+}
+
+export const verticalFlip: Story = () => {
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Swap
+        from={<BackOfCard />}
+        to={<FrontOfCard />}
+        animation="verticalFlip"
+      />
+
+      <Swap
+        from={<Apple fontSize="50px" />}
+        to={<Cherry fontSize="50px" />}
+        animation="verticalFlip"
+      />
+    </Center>
+  )
 }

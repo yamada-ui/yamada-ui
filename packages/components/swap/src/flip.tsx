61bbd3d0e5e8ad0ce9d2e3a8fd063bf0addf47bf
Kae <kaehehehe@gmail.com>
2024-09-08 14:54:07 +0900

    feat: improve flip css styles and implement error handling for for dimension mismatches

--- packages/components/swap/src/flip.tsx
+++ packages/components/swap/src/flip.tsx
@@ -1,9 +1,9 @@
-import type { ThemeProps } from "@yamada-ui/core"
+import type { CSSUIObject, ThemeProps } from "@yamada-ui/core"
 import { useComponentStyle } from "@yamada-ui/core"
 import type { MotionProps, WithTransitionProps } from "@yamada-ui/motion"
 import { motionForwardRef, motion } from "@yamada-ui/motion"
 import { cx } from "@yamada-ui/utils"
-import { useState } from "react"
+import { useLayoutEffect, useRef, useState } from "react"
 import type { SwapElements } from "./swap"
 
 type FlipDirection = "horizontal" | "vertical"
@@ -54,9 +54,21 @@ const flipMotion: FlipMotion = {
   },
 }
 
+const flipStyle: CSSUIObject = {
+  position: "absolute",
+  top: 0,
+  backfaceVisibility: "hidden",
+}
+
 export const Flip = motionForwardRef<FlipProps, "div">((props, ref) => {
   const [style, mergedProps] = useComponentStyle("Flip", props)
   const [isVisible, setIsVisible] = useState(false)
+  const [dimensions, setDimensions] = useState<{
+    width?: number
+    height?: number
+  }>({})
+  const fromRef = useRef<HTMLDivElement | null>(null)
+  const toRef = useRef<HTMLDivElement | null>(null)
 
   const { from, to, flipDirection, className, ...rest } = mergedProps
 
@@ -64,46 +76,65 @@ export const Flip = motionForwardRef<FlipProps, "div">((props, ref) => {
     setIsVisible((prev) => !prev)
   }
 
+  useLayoutEffect(() => {
+    const fromElement = fromRef?.current
+    const toElement = toRef?.current
+
+    if (!fromElement || !toElement) return
+
+    const fromWidth = fromElement.offsetWidth
+    const fromHeight = fromElement.offsetHeight
+    const toWidth = toElement.offsetWidth
+    const toHeight = toElement.offsetHeight
+
+    // to element, from elementのwidth, heightが同じでないとアニメーションの挙動をおかしくあるから
+    // 下記のエラー処理を追加しました
+    if (fromWidth !== toWidth || fromHeight !== toHeight) {
+      throw new Error(
+        `Dimensions do not match: 
+        "from" element (Width: ${fromWidth}px, Height: ${fromHeight}px) 
+        does not match "to" element (Width: ${toWidth}px, Height: ${toHeight}px). 
+        Please ensure both elements have the same dimensions.`,
+      )
+    }
+
+    setDimensions({ width: fromWidth, height: fromHeight })
+  }, [fromRef, toRef])
+
   return (
     <motion.div
       ref={ref}
-      style={{
+      className={cx(`ui-swap__${flipDirection}-flip`, className)}
+      __css={{
         position: "relative",
-        perspective: 1000, // 원근감을 주기 위한 설정
+        w: dimensions.width ? `${dimensions.width}px` : "auto",
+        h: dimensions.height ? `${dimensions.height}px` : "auto",
+        ...style,
       }}
+      {...rest}
     >
       <motion.div
+        ref={fromRef}
         custom={isVisible}
         className={cx(`ui-swap__${flipDirection}-flip-from`, className)}
         onClick={switchVisibility}
         variants={flipMotion[flipDirection].from}
         initial="initial"
         animate="animate"
-        __css={style}
-        style={{
-          position: "absolute",
-          top: 0,
-          backfaceVisibility: "hidden",
-        }}
-        {...rest}
+        __css={flipStyle}
       >
         {from}
       </motion.div>
 
       <motion.div
+        ref={toRef}
         custom={isVisible}
         className={cx(`ui-swap__${flipDirection}-flip-to`, className)}
         onClick={switchVisibility}
         variants={flipMotion[flipDirection].to}
         initial="initial"
         animate="animate"
-        __css={style}
-        style={{
-          position: "absolute",
-          top: 0,
-          backfaceVisibility: "hidden",
-        }}
-        {...rest}
+        __css={flipStyle}
       >
         {to}
       </motion.div>

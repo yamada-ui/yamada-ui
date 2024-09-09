88ac56df043dcefe98fbe6485cad1dbbc0997dcb
Yuto Urashima <yuto.urashima24@gmail.com>
2024-09-09 09:36:12 +0900

    test: add test for AlphaSlider component with pointer events

--- packages/components/color-picker/tests/alpha-slider.test.tsx
+++ packages/components/color-picker/tests/alpha-slider.test.tsx
@@ -53,4 +53,33 @@ describe("<AlphaSlider />", () => {
     fireEvent.keyDown(sliderThumb, { key: "ArrowRight" })
     expect(onChange).not.toHaveBeenCalled()
   })
+
+  test("AlphaSlider component with pointer events", async () => {
+    const onChange = vi.fn()
+    const onChangeStart = vi.fn()
+    const onChangeEnd = vi.fn()
+
+    const { min, max, step } = { min: 0, max: 1, step: 0.01 }
+
+    const { user } = render(
+      <AlphaSlider
+        min={min}
+        max={max}
+        step={step}
+        onChangeStart={onChangeStart}
+        onChangeEnd={onChangeEnd}
+        onChange={onChange}
+      />,
+    )
+
+    const sliderThumb = screen.getByRole("slider")
+
+    await user.tab()
+    await user.pointer({ keys: "[MouseLeft>]", target: sliderThumb })
+    await user.pointer({ keys: "[/MouseLeft]", target: sliderThumb })
+
+    expect(onChangeStart).toHaveBeenCalledWith(0.5)
+    expect(onChange).toHaveBeenCalledWith(0)
+    expect(onChangeEnd).toHaveBeenCalledWith(0)
+  })
 })

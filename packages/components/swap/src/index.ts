728635e5a52478a1eea383fdc53899772b124cbc
Kae <kaehehehe@gmail.com>
2024-09-15 16:24:51 +0900

    chore: remove swap-related files

--- packages/components/swap/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Swap } from "./swap"
-export type { SwapProps } from "./swap"
--- packages/components/swap/tests/swap.test.tsx
+++
@@ -1,8 +0,0 @@
-import { a11y } from "@yamada-ui/test"
-import { Swap } from "../src"
-
-describe("<Swap />", () => {
-  test("Swap renders correctly", async () => {
-    await a11y(<Swap />)
-  })
-})

5a8cd1a278346866882bbf6bd18a0ae28a6ebe08
Kae <kaehehehe@gmail.com>
2024-09-15 16:20:58 +0900

    feat: create flip story

--- stories/components/transitions/flip.stories.tsx
+++ stories/components/transitions/flip.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryFn } from "@storybook/react"
-import { Flip } from "@yamada-ui/react"
+import { Apple, Cherry } from "@yamada-ui/lucide"
+import { Flip, Avatar, Card, Center, Text } from "@yamada-ui/react"
 
 type Story = StoryFn<typeof Flip>
 
@@ -10,6 +11,94 @@ const meta: Meta<typeof Flip> = {
 
 export default meta
 
+const FrontOfCard = () => {
+  return (
+    <Card
+      borderRadius="xl"
+      bg="white"
+      w="xs"
+      h="sm"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      border="1px solid gray"
+    >
+      <Text fontSize="xl" color="black">
+        Yamada UI
+      </Text>
+    </Card>
+  )
+}
+
+const BackOfCard = () => {
+  return (
+    <Card
+      borderRadius="xl"
+      bg="black"
+      w="xs"
+      h="sm"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      border="1px solid gray"
+    >
+      <Avatar
+        size="xl"
+        name="Hirotomo Yamada"
+        src="https://avatars.githubusercontent.com/u/84060430?v=4"
+      />
+    </Card>
+  )
+}
+
 export const basic: Story = () => {
-  return <Flip></Flip>
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Flip
+        from={<BackOfCard />}
+        to={<FrontOfCard />}
+        transition={{
+          type: "spring",
+          stiffness: 20,
+          damping: 10,
+        }}
+      />
+
+      <Flip
+        from={<Apple fontSize="6xl" />}
+        to={<Cherry fontSize="6xl" />}
+        style={{
+          cursor: "pointer",
+        }}
+      />
+    </Center>
+  )
+}
+
+export const verticalFlip: Story = () => {
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Flip
+        from={<BackOfCard />}
+        to={<FrontOfCard />}
+        flipDirection="vertical"
+      />
+
+      <Flip
+        from={<Apple fontSize="6xl" />}
+        to={<Cherry fontSize="6xl" />}
+        flipDirection="vertical"
+      />
+    </Center>
+  )
 }

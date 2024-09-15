432eed1be94da27f721a004cc071766b0ef4489e
Kae <kaehehehe@gmail.com>
2024-09-15 15:37:44 +0900

    feat: create rotate story

--- stories/components/transitions/rotate.stories.tsx
+++ stories/components/transitions/rotate.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryFn } from "@storybook/react"
-import { Rotate } from "@yamada-ui/react"
+import { Apple, Cherry, Moon, Sun } from "@yamada-ui/lucide"
+import { Rotate, Center } from "@yamada-ui/react"
 
 type Story = StoryFn<typeof Rotate>
 
@@ -11,5 +12,33 @@ const meta: Meta<typeof Rotate> = {
 export default meta
 
 export const basic: Story = () => {
-  return <Rotate></Rotate>
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Rotate
+        from={<Sun fontSize="50px" />}
+        to={<Moon fontSize="50px" />}
+        rotate="45deg"
+        transition={{
+          duration: 0.1,
+        }}
+        style={{
+          cursor: "pointer",
+        }}
+      />
+
+      <Rotate
+        from={<Apple fontSize="6xl" />}
+        to={<Cherry fontSize="6xl" />}
+        rotate="360deg"
+        style={{
+          cursor: "pointer",
+        }}
+      />
+    </Center>
+  )
 }

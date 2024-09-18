2ee59b51b128f7080af4144c24595552cb3f62d6
Kae <kaehehehe@gmail.com>
2024-09-18 21:15:42 +0900

    feat: add customControl story to rotate component

--- stories/components/transitions/rotate.stories.tsx
+++ stories/components/transitions/rotate.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryFn } from "@storybook/react"
 import { Apple, Cherry, Moon, Sun } from "@yamada-ui/lucide"
+import type { RotateAnimationElement } from "@yamada-ui/react"
 import { Rotate, Center } from "@yamada-ui/react"
+import { useState } from "react"
 
 type Story = StoryFn<typeof Rotate>
 
@@ -56,6 +58,34 @@ export const withRotate: Story = () => {
         from={<Apple fontSize="6xl" />}
         to={<Cherry fontSize="6xl" />}
         rotate={-360}
+      />
+    </Center>
+  )
+}
+
+export const customControl: Story = () => {
+  const [currentElement, onChange] = useState<RotateAnimationElement>("to")
+
+  const onChangeRotateAnimation = () => {
+    console.log(`current element is ${currentElement}`)
+    onChange((prev) => (prev === "from" ? "to" : "from"))
+  }
+
+  return (
+    <Center
+      w="calc(100vw - 16px * 2)"
+      h="calc(100vh - 16px * 2)"
+      display="flex"
+      gap="4xl"
+    >
+      <Rotate
+        initialElement={currentElement}
+        onChange={onChangeRotateAnimation}
+        from={<Sun fontSize="50px" />}
+        to={<Moon fontSize="50px" />}
+        style={{
+          cursor: "pointer",
+        }}
       />
     </Center>
   )

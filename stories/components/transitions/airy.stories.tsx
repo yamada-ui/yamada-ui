3928db806302c1121e91ea9bd308d174ebc86154
Kae <kaehehehe@gmail.com>
2024-09-18 20:52:05 +0900

    feat: add customControl story

--- stories/components/transitions/airy.stories.tsx
+++ stories/components/transitions/airy.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryFn } from "@storybook/react"
 import { Menu, X } from "@yamada-ui/lucide"
+import type { AiryCurrentElement } from "@yamada-ui/react"
 import { Airy, Text, Center } from "@yamada-ui/react"
+import { useState } from "react"
 
 type Story = StoryFn<typeof Airy>
 
@@ -54,6 +56,35 @@ export const withDuration: Story = () => {
         to={<X fontSize="6xl" />}
         duration={0.6}
       />
+    </Center>
+  )
+}
+
+export const customControl: Story = () => {
+  const [currentElement, onChange] = useState<AiryCurrentElement>("to")
+
+  const onChangeAiryAnimation = () => {
+    console.log(`current element is ${currentElement}`)
+    onChange((prev) => (prev === "from" ? "to" : "from"))
+  }
+  return (
+    <Center w="calc(100vw - 16px * 2)" h="calc(100vh - 16px * 2)">
+      <Airy
+        initialElement={currentElement}
+        onChange={onChangeAiryAnimation}
+        from={
+          <Text w="xs" fontSize="5xl">
+            ON
+          </Text>
+        }
+        to={
+          <Text w="xs" fontSize="5xl">
+            OFF
+          </Text>
+        }
+      />
+
+      <Airy from={<Menu fontSize="6xl" />} to={<X fontSize="6xl" />} />
     </Center>
   )
 }

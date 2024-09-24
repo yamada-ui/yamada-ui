c2e6c8c3134ce11ef6d95892393850c897bc162a
Kazuhiro Kumagai <78473460+cidkumagai@users.noreply.github.com>
2024-09-24 17:53:51 +0900

    chore: fix import

--- packages/components/accordion/src/accordion-label.tsx
+++ packages/components/accordion/src/accordion-label.tsx
@@ -1,9 +1,9 @@
-import type { CSSUIObject, HTMLUIProps } from "@yamada-ui/core"
+import type { CSSUIObject, FC, HTMLUIProps } from "@yamada-ui/core"
 import { ui, forwardRef } from "@yamada-ui/core"
 import type { IconProps } from "@yamada-ui/icon"
 import { ChevronIcon } from "@yamada-ui/icon"
 import { cx } from "@yamada-ui/utils"
-import type { FC, PropsWithChildren, ReactNode } from "react"
+import type { PropsWithChildren, ReactNode } from "react"
 import {
   useAccordionContext,
   useAccordionItemContext,
@@ -129,3 +129,6 @@ const AccordionIcon: FC<PropsWithChildren<IconProps>> = ({
     />
   )
 }
+
+AccordionIcon.displayName = "AccordionIcon"
+AccordionIcon.__ui__ = "AccordionIcon"

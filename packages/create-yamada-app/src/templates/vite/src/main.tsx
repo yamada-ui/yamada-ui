407e266c146f1eaf73322fd3c3df2683eb8c7e72
fffk10 <warafffk10@gmail.com>
2024-09-29 00:47:47 +0900

    feat: create-yamada-app update vite template

--- packages/create-yamada-app/src/templates/vite/src/main.tsx
+++ packages/create-yamada-app/src/templates/vite/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
-import "./index.css"
+import { UIProvider } from "@yamada-ui/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <UIProvider>
+      <App />
+    </UIProvider>
   </StrictMode>,
 )
--- packages/create-yamada-app/src/templates/vite/vite.config.ts
+++ packages/create-yamada-app/src/templates/vite/vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
+import { defaultConfig, getColorModeScript } from "@yamada-ui/react"
 import react from "@vitejs/plugin-react-swc"
 
+function injectScript(): Plugin {
+  return {
+    name: "vite-plugin-inject-scripts",
+    transformIndexHtml(html, _) {
+      const content = getColorModeScript({
+        initialColorMode: defaultConfig.initialColorMode,
+      })
+
+      return html.replace("<body>", `<body><script>${content}</script>`)
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), injectScript()],
 })

04c1322966307be792462865b3b5b208d3c680cc
fffk10 <warafffk10@gmail.com>
2024-09-29 02:46:48 +0900

    chore: remove library globals and fix vite.config.ts

--- packages/create-yamada-app/src/templates/vite/vite.config.ts
+++ packages/create-yamada-app/src/templates/vite/vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig, Plugin } from "vite"
-import { defaultConfig, getColorModeScript } from "@yamada-ui/react"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
-function injectScript(): Plugin {
-  return {
-    name: "vite-plugin-inject-scripts",
-    transformIndexHtml(html, _) {
-      const content = getColorModeScript({
-        initialColorMode: defaultConfig.initialColorMode,
-      })
-
-      return html.replace("<body>", `<body><script>${content}</script>`)
-    },
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), injectScript()],
+  plugins: [react()],
 })

82b6773205f2e33bef7c4fb7420a37a66fc4b432
taiki <tnakamura@ucsd.edu>
2024-09-29 06:37:56 -0700

    feat: set display name and UI for segmented-controll

--- packages/components/segmented-control/src/segmented-control.tsx
+++ packages/components/segmented-control/src/segmented-control.tsx
@@ -9,6 +9,7 @@ import type {
 import {
   ui,
   forwardRef,
+  FC,
   useComponentMultiStyle,
   omitThemeProps,
 } from "@yamada-ui/core"
@@ -31,7 +32,6 @@ import {
 import type {
   ChangeEvent,
   ChangeEventHandler,
-  FC,
   FocusEventHandler,
   ReactElement,
 } from "react"
@@ -375,6 +375,9 @@ export const SegmentedControl = forwardRef<SegmentedControlProps, "div">(
   },
 )
 
+SegmentedControl.displayName = "SegmentedControl"
+SegmentedControl.__ui__ = "SegmentedControl"
+
 interface SegmentedControlButtonOptions {
   /**
    * The value of the segmented control button.
@@ -463,6 +466,7 @@ export const SegmentedControlButton = forwardRef<
 )
 
 SegmentedControlButton.displayName = "SegmentedControlButton"
+SegmentedControlButton.__ui__ = "SegmentedControlButton"
 
 interface SegmentedControlCursorProps extends MotionProps {
   className?: string
@@ -498,3 +502,6 @@ const SegmentedControlCursor: FC<SegmentedControlCursorProps> = ({
     />
   )
 }
+
+SegmentedControlCursor.displayName = "SegmentedControlCursor"
+SegmentedControlCursor.__ui__ = "SegmentedControlCursor"

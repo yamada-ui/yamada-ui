50881ad65019879e80bd7d60c73a75b593b562ef
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-10-19 16:23:07 +0900

    refactor(test): remove focus and cookieStorage modules

--- packages/test/src/mocks/cookie.ts
+++
@@ -1,6 +0,0 @@
-export function cookieStorage(key: string, value: null | string) {
-  Object.defineProperty(document, "cookie", {
-    value: value ? `${key}=${value}` : "",
-    writable: true,
-  })
-}

e4f8212ecbb2702673c82696870d89dc01473cb4
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-10-22 19:01:45 +0900

    refactor(pagination): update prop handling, forward refs

--- packages/components/pagination/src/pagination-item.tsx
+++ packages/components/pagination/src/pagination-item.tsx
@@ -1,6 +1,6 @@
-import type { CSSUIObject, FC } from "@yamada-ui/core"
-import type { ComponentPropsWithoutRef, ReactNode } from "react"
-import { ui } from "@yamada-ui/core"
+import type { CSSUIObject, HTMLUIProps } from "@yamada-ui/core"
+import type { ReactNode } from "react"
+import { forwardRef, ui } from "@yamada-ui/core"
 import { Ripple, useRipple } from "@yamada-ui/ripple"
 import { cx, dataAttr } from "@yamada-ui/utils"
 import {
@@ -38,7 +38,7 @@ interface PaginationItemOptions {
 }
 
 export interface PaginationItemProps
-  extends ComponentPropsWithoutRef<"button">,
+  extends Omit<HTMLUIProps<"button">, "page">,
     PaginationItemOptions {}
 
 const iconMap: {
@@ -51,51 +51,60 @@ const iconMap: {
   prev: <PaginationPrevIcon />,
 }
 
-export const PaginationItem: FC<PaginationItemProps> = ({
-  className,
-  children,
-  disableRipple,
-  isActive,
-  isDisabled,
-  page,
-  ...rest
-}) => {
-  const styles = usePaginationContext()
-  const { onPointerDown, ...rippleProps } = useRipple({
-    ...rest,
-    isDisabled: disableRipple || isDisabled,
-  })
+export const PaginationItem = forwardRef<PaginationItemProps, "button">(
+  (
+    { className, children, disableRipple, isActive, isDisabled, page, ...rest },
+    ref,
+  ) => {
+    const styles = usePaginationContext()
+    const isEllipsis = page === "ellipsis"
+    const { onPointerDown, ...rippleProps } = useRipple({
+      ...rest,
+      isDisabled: disableRipple || isDisabled || isEllipsis,
+    })
 
-  children ??= iconMap[page] ?? page
+    children ??= iconMap[page] ?? page
 
-  const css: CSSUIObject = {
-    alignItems: "center",
-    display: "flex",
-    justifyContent: "center",
-    overflow: "hidden",
-    position: "relative",
-    userSelect: "none",
-    ...styles.item,
-    ...styles[page],
-  }
+    const css: CSSUIObject = {
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+      overflow: "hidden",
+      position: "relative",
+      userSelect: "none",
+      ...styles.item,
+      ...styles[page],
+    }
 
-  return (
-    <ui.button
-      type="button"
-      className={cx("ui-pagination__item", className)}
-      data-disabled={dataAttr(isDisabled)}
-      data-selected={dataAttr(isActive)}
-      disabled={isDisabled}
-      tabIndex={page !== "ellipsis" ? 0 : -1}
-      __css={css}
-      {...rest}
-      onPointerDown={onPointerDown}
-    >
-      {children}
+    const Component = ui[isEllipsis ? "span" : "button"]
 
-      <Ripple isDisabled={disableRipple || isDisabled} {...rippleProps} />
-    </ui.button>
-  )
-}
+    return (
+      <Component
+        ref={ref}
+        {...(!isEllipsis
+          ? {
+              "data-disabled": dataAttr(isDisabled),
+              "data-selected": dataAttr(isActive),
+              disabled: isDisabled,
+            }
+          : {})}
+        className={cx(
+          "ui-pagination__item",
+          isEllipsis ? "ui-pagination__item--ellipsis" : undefined,
+          className,
+        )}
+        tabIndex={!isEllipsis ? 0 : -1}
+        __css={css}
+        {...rest}
+        onPointerDown={onPointerDown}
+      >
+        {children}
+
+        <Ripple isDisabled={disableRipple || isDisabled} {...rippleProps} />
+      </Component>
+    )
+  },
+)
+
 PaginationItem.displayName = "PaginationItem"
 PaginationItem.__ui__ = "PaginationItem"

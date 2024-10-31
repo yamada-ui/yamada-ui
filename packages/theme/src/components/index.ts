0ae6ea5840ab1102bb7d7698be28aa2b25b48483
nathanlao11 <nathanl@emonster.ca>
2024-10-31 15:54:27 -0700

    feat(components): add for component
    fix(stories): color of text
    
    fix: remove package in changeset
    
    fix(for): added test to handle undefined each and ensure each is array when do length check
    
    refactor(components/for): simplify and optimize rendering logic

--- packages/theme/src/components/index.ts
+++ packages/theme/src/components/index.ts
@@ -33,6 +33,7 @@ import { Fieldset } from "./fieldset"
 import { FileButton } from "./file-button"
 import { FileInput } from "./file-input"
 import { Flip } from "./flip"
+import { For } from "./for"
 import { FormControl } from "./form-control"
 import { Heading } from "./heading"
 import { HueSlider } from "./hue-slider"
@@ -130,6 +131,7 @@ export const components = {
   FileButton,
   FileInput,
   Flip,
+  For,
   FormControl,
   Heading,
   HueSlider,

402df3562f95dcd2899a038b7aadb60a8409991e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-11-13 21:17:20 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@3604c34d5f7e5870fa9c6186d8ecefdebea83152 🚀

---
+++ assets/badge.stories-BCenpkDJ.js
@@ -0,0 +1,25 @@
+import{j as o}from"./jsx-runtime-CfatFE5O.js";import{c as i}from"./components-BSKWN5WN.js";import{B as e}from"./badge-DHGGeCwR.js";import{t as s}from"./factory-COau3w21.js";import{W as c}from"./flex-BgJeD3no.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BcE809qD.js";import"./index-BTMolVUU.js";import"./index-12mLNQJN.js";import"./theme-provider-CNI9L2WW.js";import"./ui-provider-4e9Xb-Lo.js";import"./index-B6xYib6C.js";import"./proxy-Bq47Fk52.js";import"./loading-provider-CW1g4LYM.js";import"./index-CA_fCdqy.js";import"./Combination-CkmEMpJG.js";import"./loading-DYtJ9fc2.js";import"./icon-BodRPJCf.js";import"./use-component-style-CLSKeq_H.js";import"./use-var-YCfkKbSC.js";import"./forward-ref-D13m8o2p.js";import"./memo-CTsy6qLS.js";import"./index-r0TXmcNt.js";import"./portal-CgvdJ0pj.js";import"./index-ZuzByk-F.js";import"./factory-ep9rrzy9.js";import"./notice-0nBeSUXU.js";import"./alert-Br92M73P.js";import"./triangle-alert-D-rdrXiJ.js";import"./createLucideIcon-Czt4prMK.js";import"./close-button-EOT3XTCt.js";import"./use-ripple-D0covPjN.js";import"./container-9UL7bQcu.js";import"./stack-DfTvLKXJ.js";import"./box-Co1KKng-.js";import"./text-BnztNdZ-.js";import"./checkbox-Dlwy-uIR.js";import"./index-CRS5qAAq.js";import"./form-control-Y8qD5MxJ.js";import"./index-DPt_fcIQ.js";import"./index-BCBmayc8.js";import"./event-C_48urmU.js";import"./number-CcP_arM8.js";import"./tooltip-DqnSdWX7.js";import"./index-qyuJ8aiN.js";import"./index-Blij8Q7p.js";import"./index-CYDuQAfY.js";import"./index-J4x_JfHv.js";import"./index-BjGf4nWH.js";import"./slide-fade-DzakJmAy.js";import"./forward-ref-2BKBy0Yi.js";import"./utils-COso-ZNO.js";import"./scale-fade-DAfdo16Z.js";import"./index-Btt-sfjQ.js";import"./index-B5Z2HpMN.js";import"./select-DiVSeaVw.js";import"./select-list-CJRoYFPx.js";import"./index-C58se0b8.js";import"./popover-trigger-C0a8uwwS.js";import"./index-a5RVC8cz.js";import"./index-gGKStiao.js";import"./index-Bt_V34Vb.js";const Or={component:e,title:"Components / Data Display / Badge"},t=()=>o.jsx(c,{gap:"md",children:i.map(r=>o.jsx(e,{colorScheme:r,children:s(r)},r))}),m=()=>o.jsx(c,{gap:"md",children:i.map(r=>o.jsx(e,{colorScheme:r,variant:"surface",children:s(r)},r))}),p=()=>o.jsx(c,{gap:"md",children:i.map(r=>o.jsx(e,{colorScheme:r,variant:"solid",children:s(r)},r))}),a=()=>o.jsx(c,{gap:"md",children:i.map(r=>o.jsx(e,{colorScheme:r,variant:"outline",children:s(r)},r))});var n,l,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} colorScheme={colorScheme}>
+          {toTitleCase(colorScheme)}
+        </Badge>)}
+    </Wrap>;
+}`,...(d=(l=t.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var u,h,g;m.parameters={...m.parameters,docs:{...(u=m.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} colorScheme={colorScheme} variant="surface">
+          {toTitleCase(colorScheme)}
+        </Badge>)}
+    </Wrap>;
+}`,...(g=(h=m.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var S,x,B;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} colorScheme={colorScheme} variant="solid">
+          {toTitleCase(colorScheme)}
+        </Badge>)}
+    </Wrap>;
+}`,...(B=(x=p.parameters)==null?void 0:x.docs)==null?void 0:B.source}}};var f,j,W;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      {colorSchemes.map(colorScheme => <Badge key={colorScheme} colorScheme={colorScheme} variant="outline">
+          {toTitleCase(colorScheme)}
+        </Badge>)}
+    </Wrap>;
+}`,...(W=(j=a.parameters)==null?void 0:j.docs)==null?void 0:W.source}}};const Rr=["subtle","surface","solid","outline"];export{Rr as __namedExportsOrder,Or as default,a as outline,p as solid,t as subtle,m as surface};

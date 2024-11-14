ebb6423ad926d4c2d3c5661e01976da593ff31e8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-11-14 01:01:05 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@aed8828daa0027777124b6cad38c6d5fd86c09ad 🚀

---
+++ assets/center.stories-Dnp8lwaU.js
@@ -0,0 +1,9 @@
+import{j as o}from"./jsx-runtime-CfatFE5O.js";import{S as p}from"./skull-D-VNsKQN.js";import{C as t}from"./center-CSG2P-cD.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./icon-BodRPJCf.js";import"./use-component-style-CLSKeq_H.js";import"./factory-COau3w21.js";import"./theme-provider-CNI9L2WW.js";import"./use-var-YCfkKbSC.js";import"./forward-ref-D13m8o2p.js";import"./createLucideIcon-Czt4prMK.js";const y={component:t,title:"Components / Layouts / Center"},r=()=>o.jsx(t,{bg:"primary",color:"white",minH:"xs",minW:"xs",rounded:"md",children:"Center"}),e=()=>o.jsx(t,{bg:"gray.500",color:"white",minH:"6xs",minW:"6xs",rounded:"full",children:o.jsx(p,{fontSize:"3xl"})});var n,s,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <Center bg="primary" color="white" minH="xs" minW="xs" rounded="md">
+      Center
+    </Center>;
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var m,a,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <Center bg="gray.500" color="white" minH="6xs" minW="6xs" rounded="full">
+      <SkullIcon fontSize="3xl" />
+    </Center>;
+}`,...(c=(a=e.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};const w=["basic","iconWithFrame"];export{w as __namedExportsOrder,r as basic,y as default,e as iconWithFrame};

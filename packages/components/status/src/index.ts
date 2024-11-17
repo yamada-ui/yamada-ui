604208c13d90f0c665616c7bd8c0f097b3c1b498
taisei miyazaki <>
2024-11-17 20:11:38 +0900

    feat(status): add  component

---
+++ packages/components/status/src/index.ts
@@ -0,0 +1,2 @@
+export { Status } from "./status"
+export type { StatusProps } from "./status"
---
+++ stories/components/data-display/status.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryFn } from "@storybook/react"
+import { For, HStack, Stack, Status } from "@yamada-ui/react"
+
+type Story = StoryFn<typeof Status>
+
+const meta: Meta<typeof Status> = {
+  component: Status,
+  title: "Components / Data Display / Status",
+}
+
+export default meta
+
+export const basic: Story = () => {
+  return (
+    <HStack>
+      <Status value="info" />
+      <Status value="success" />
+      <Status value="warning" />
+      <Status value="error" />
+    </HStack>
+  )
+}
+
+export const withLabel: Story = () => {
+  return (
+    <HStack>
+      <Status value="info">Info</Status>
+      <Status value="success">Success</Status>
+      <Status value="warning">Warning</Status>
+      <Status value="error">Error</Status>
+    </HStack>
+  )
+}
+
+export const withSize: Story = () => {
+  return (
+    <Stack>
+      <For each={["sm", "md", "lg"]}>
+        {(size) => (
+          <HStack>
+            <Status size={size} value="info">
+              Info
+            </Status>
+
+            <Status size={size} value="success">
+              Success
+            </Status>
+
+            <Status size={size} value="warning">
+              Warning
+            </Status>
+
+            <Status size={size} value="error">
+              Error
+            </Status>
+          </HStack>
+        )}
+      </For>
+    </Stack>
+  )
+}

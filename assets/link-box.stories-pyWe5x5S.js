0f4d167e10ce9b7db601f8224eb2760f949eb34f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-11-18 17:41:16 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@489169b359776b7a424912975f5672d315e4d4d7 🚀

---
+++ assets/link-box.stories-pyWe5x5S.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-CfatFE5O.js";import{ae as A,p as _,a as E,c as g}from"./factory-BPYpkDor.js";import{f as j}from"./forward-ref-D13m8o2p.js";import{a as T}from"./use-component-style-CYL4T5vM.js";import{o as N}from"./theme-provider-Bsm-0rKu.js";import{u as O}from"./use-var-DoLXLJEg.js";import{T as t}from"./text-Y1d9IDh4.js";import{H as y}from"./heading-CBcUDjDf.js";import{L as D}from"./link-DA6gJ6VP.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";const[S,z]=_({name:"LinkBoxContext",errorMessage:`useLinkBox returned is 'undefined'. Seems you forgot to wrap the components in "<LinkBox />"`}),v=j(({href:a,target:d,className:o,children:m,external:r,isExternal:l,rel:n,...c},x)=>{const{styles:p,variableProps:w}=z();r??(r=l);const C={position:"static",_before:{content:'""',cursor:"inherit",display:"block",height:"100%",left:0,position:"absolute",top:0,width:"100%",zIndex:0,...w},...p.overlay};return e.jsx(E.a,{ref:x,href:a,target:r?"_blank":d,className:g("ui-link-box__overlay",o),rel:r?"noopener":n,__css:C,...c,children:m})}),h=j((a,d)=>{const[o,m]=T("LinkBox",a),{className:r,children:l,...n}=N(m),[c,x]=O(n,A),p={"a[href]:not(.ui-link-box__overlay), abbr[title]":{position:"relative",zIndex:"fallback(yamcha, 1)"},position:"relative",vars:c,...o.container};return e.jsx(S,{value:{styles:o,variableProps:x},children:e.jsx(E.div,{ref:d,className:g("ui-link-box",r),__css:p,...n,children:l})})}),K={component:h,title:"Components / Navigation / LinkBox"},s=()=>e.jsxs(h,{as:"article",border:"1px solid",borderColor:"inherit",boxShadow:"md",maxW:"sm",p:"md",rounded:"md",children:[e.jsx(t,{as:"span",children:"鳥山明"}),e.jsx(y,{size:"md",my:"sm",children:e.jsx(v,{href:"https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB",external:!0,children:"ドラゴンボール"})}),e.jsx(t,{children:"『ドラゴンボール』（DRAGON BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。"})]}),i=()=>e.jsxs(h,{as:"article",border:"1px solid",borderColor:"inherit",boxShadow:"md",maxW:"sm",p:"md",rounded:"md",children:[e.jsx(t,{as:"span",children:"鳥山明"}),e.jsx(y,{size:"md",my:"sm",children:e.jsx(v,{href:"https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB",external:!0,children:"ドラゴンボール"})}),e.jsx(t,{mb:"sm",children:"『ドラゴンボール』（DRAGON BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。"}),e.jsx(D,{href:"https://dragon-ball-official.com/",external:!0,fontWeight:"bold",children:"オフィシャルサイト"})]});var u,k,B;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  return <LinkBox as="article" border="1px solid" borderColor="inherit" boxShadow="md" maxW="sm" p="md" rounded="md">
+      <Text as="span">鳥山明</Text>
+
+      <Heading size="md" my="sm">
+        <LinkOverlay href="https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB" external>
+          ドラゴンボール
+        </LinkOverlay>
+      </Heading>
+
+      <Text>
+        『ドラゴンボール』（DRAGON
+        BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。
+      </Text>
+    </LinkBox>;
+}`,...(B=(k=s.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};var b,f,L;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`() => {
+  return <LinkBox as="article" border="1px solid" borderColor="inherit" boxShadow="md" maxW="sm" p="md" rounded="md">
+      <Text as="span">鳥山明</Text>
+
+      <Heading size="md" my="sm">
+        <LinkOverlay href="https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB" external>
+          ドラゴンボール
+        </LinkOverlay>
+      </Heading>
+
+      <Text mb="sm">
+        『ドラゴンボール』（DRAGON
+        BALL）は、鳥山明による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて1984年51号から1995年25号まで連載された。略称は『DB』。
+      </Text>
+
+      <Link href="https://dragon-ball-official.com/" external fontWeight="bold">
+        オフィシャルサイト
+      </Link>
+    </LinkBox>;
+}`,...(L=(f=i.parameters)==null?void 0:f.docs)==null?void 0:L.source}}};const Q=["basic","withNestedLink"];export{Q as __namedExportsOrder,s as basic,K as default,i as withNestedLink};
--- assets/link.stories-BWcy0-q2.js
+++
@@ -1,9 +0,0 @@
-import{j as E}from"./jsx-runtime-CfatFE5O.js";import{L as e}from"./link-DC23bmla.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./forward-ref-D13m8o2p.js";import"./use-component-style-CYL4T5vM.js";import"./factory-BPYpkDor.js";import"./theme-provider-Bsm-0rKu.js";const l={component:e,title:"Components / Navigation / Link"},i=()=>E.jsx(e,{href:"https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB",children:"ドラゴンボール - wikipedia"}),r=()=>E.jsx(e,{href:"https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB",isExternal:!0,children:"ドラゴンボール - wikipedia"});var t,a,s;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
-  return <Link href="https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB">
-      ドラゴンボール - wikipedia
-    </Link>;
-}`,...(s=(a=i.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var o,n,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  return <Link href="https://ja.wikipedia.org/wiki/%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB" isExternal>
-      ドラゴンボール - wikipedia
-    </Link>;
-}`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const C=["basic","useBlank"];export{C as __namedExportsOrder,i as basic,l as default,r as useBlank};

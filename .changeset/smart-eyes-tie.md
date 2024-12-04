6b6e3e4d73759d1facd9e9939790c530e2ff5c7a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-12-04 16:41:49 +0900

    feat(changeset): add new features and update dependencies

---
+++ .changeset/smart-eyes-tie.md
@@ -0,0 +1,5 @@
+---
+"@yamada-ui/popover": patch
+---
+
+Simplified component rendering logic.
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,37 +1,82 @@
-import React from "react"
-import { Decorator, Parameters } from "@storybook/react"
+import type { DocsContainerProps } from "@storybook/blocks"
+import type { Preview } from "@storybook/react"
+import type { FC, PropsWithChildren } from "react"
+import { DocsContainer } from "@storybook/blocks"
+import { addons } from "@storybook/preview-api"
 import { themes } from "@storybook/theming"
-import { UITheme } from "./theme"
-import { StoryProvider, DocsContainer } from "./components"
+import { UIProvider, useColorMode, VStack } from "@yamada-ui/react"
+import { useEffect, useState } from "react"
+import { DARK_MODE_EVENT_NAME, useDarkMode } from "storybook-dark-mode"
 import { A11Y_RULES } from "./constant"
+import { customThemes } from "./themes"
 
-export const parameters: Parameters = {
-  darkMode: {
-    light: { ...themes.light, ...UITheme.light },
-    dark: { ...themes.dark, ...UITheme.dark },
-  },
-  options: {
-    storySort: {
-      order: ["Documents", ["Welcome", "*"], "Components", "Hooks", "System"],
-    },
-  },
-  a11y: {
-    config: {
-      rules: A11Y_RULES,
-    },
-  },
-  backgrounds: { disable: true },
-  controls: { expanded: true },
-  docs: { container: DocsContainer },
-  layout: "fullscreen",
+const channel = addons.getChannel()
+
+const App: FC<PropsWithChildren> = ({ children }) => {
+  const { changeColorMode } = useColorMode()
+  const darkMode = useDarkMode()
+  const colorMode = darkMode ? "dark" : "light"
+
+  useEffect(() => {
+    changeColorMode(colorMode)
+  }, [colorMode])
+
+  return (
+    <VStack align="start" p="md">
+      {children}
+    </VStack>
+  )
 }
 
-export const decorators: Decorator[] = [
-  (Story) => {
-    return (
-      <StoryProvider>
-        <Story />
-      </StoryProvider>
-    )
+export const preview: Preview = {
+  parameters: {
+    a11y: { config: { rules: A11Y_RULES } },
+    backgrounds: { disable: true },
+    controls: { expanded: true },
+    darkMode: { ...customThemes },
+    docs: {
+      container: ({
+        children,
+        theme,
+        ...rest
+      }: PropsWithChildren<DocsContainerProps>) => {
+        const [darkMode, setDarkMode] = useState(false)
+        const colorMode = darkMode ? "dark" : "light"
+
+        theme = themes[colorMode]
+
+        useEffect(() => {
+          channel.on(DARK_MODE_EVENT_NAME, setDarkMode)
+
+          return () => channel.off(DARK_MODE_EVENT_NAME, setDarkMode)
+        }, [channel, setDarkMode])
+
+        return (
+          <DocsContainer theme={theme} {...rest}>
+            <UIProvider colorMode={colorMode}>{children}</UIProvider>
+          </DocsContainer>
+        )
+      },
+    },
+    layout: "fullscreen",
+    options: {
+      storySort: {
+        order: ["Documents", ["Welcome", "*"], "Components", "Hooks", "System"],
+      },
+    },
   },
-]
+
+  decorators: [
+    (Story) => {
+      return (
+        <UIProvider>
+          <App>
+            <Story />
+          </App>
+        </UIProvider>
+      )
+    },
+  ],
+}
+
+export default preview

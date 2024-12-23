9e9449ed9498befab1385898238505f89d67e314
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-12-23 21:54:54 +0900

    feat(utils): added toArray and updated types in array functions

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,3 +1,9 @@
-export function filterEmpty<T extends any[]>(array: T) {
+export function filterEmpty<Y extends any[]>(array: Y) {
   return array.filter((value) => value != null)
+}
+
+export function toArray<Y>(mixedArray: Y) {
+  return (Array.isArray(mixedArray) ? mixedArray.flat() : [mixedArray]).filter(
+    Boolean,
+  ) as Y extends any[] ? Y[number][] : Y[]
 }

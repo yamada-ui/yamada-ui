ed372f7803fd82b1ef0f353368792e00f85f4e08
takaaki chida <takaaki.chida@findy.co.jp>
2025-01-06 22:32:23 +0900

    refactor: add baseStyle for autocomplete

--- packages/react/src/theme/components/autocomplete.ts
+++ packages/react/src/theme/components/autocomplete.ts
@@ -3,4 +3,20 @@ import { mergeMultiStyle } from "../../core"
 import { Select } from "./select"
 
 export const Autocomplete: ComponentMultiStyle<"Autocomplete"> =
-  mergeMultiStyle(Select)()
+  mergeMultiStyle(Select, {
+    baseStyle: {
+      container: {
+        h: "fit-content",
+        w: "100%",
+      },
+      field: {
+        alignItems: "center",
+        cursor: "text",
+        display: "flex",
+        pe: "2rem",
+      },
+      inner: {
+        position: "relative",
+      },
+    },
+  })()

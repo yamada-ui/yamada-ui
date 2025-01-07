60d25a9631c7658ac3f044b396e72ca75a9aa898
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-01-07 22:45:08 +0900

    chore(cspell.json): add new names to dictionary

--- packages/react/src/components/avatar/avatar.tsx
+++ packages/react/src/components/avatar/avatar.tsx
@@ -1,186 +1,83 @@
-import type { HTMLAttributeReferrerPolicy, ReactElement } from "react"
-import type { CSSUIObject, FC, HTMLUIProps, ThemeProps } from "../../core"
-import type { ImageProps, UseImageProps } from "../image"
-import { cloneElement, useState } from "react"
-import { omitThemeProps, ui, useComponentMultiStyle } from "../../core"
-import { createContext, cx, dataAttr, handlerAll } from "../../utils"
+import type { ReactElement } from "react"
+import type { HTMLUIProps, ThemeProps } from "../../core"
+import type { AvatarStyle } from "./avatar.style"
+import type { UseAvatarProps } from "./use-avatar"
+import type { UseAvatarGroupProps } from "./use-avatar-group"
+import { useMemo } from "react"
+import { createSlotComponent, ui } from "../../core"
 import { UserRoundIcon } from "../icon"
-import { useImage } from "../image"
-import { AvatarName } from "./avatar-name"
+import { avatarStyle } from "./avatar.style"
+import { useAvatar } from "./use-avatar"
+import { useAvatarGroup } from "./use-avatar-group"
 
-interface AvatarContext {
-  [key: string]: CSSUIObject | undefined
-}
-
-export const [AvatarContext, useAvatarContext] = createContext<AvatarContext>({
-  name: "AvatarContext",
-  errorMessage: `useAvatarContext returned is 'undefined'. Seems you forgot to wrap the components in "<Avatar />"`,
-})
-
-interface AvatarOptions {
-  /**
-   * The name of the person in the avatar.
-   *
-   * - If `src` has loaded, the name will be used as the `alt` attribute of the `img`
-   * - If `src` is not loaded, the name will be used to create the initials
-   */
-  name?: string
-  /**
-   * The image url of the avatar.
-   */
-  src?: ImageProps["src"]
-  /**
-   * List of sources to use for different screen resolutions.
-   */
-  srcSet?: ImageProps["srcSet"]
-  /**
-   * The `HTMLImageElement` property `alt`.
-   */
-  alt?: HTMLUIProps<"img">["alt"]
-  /**
-   * Function to get the initials to display.
-   */
-  format?: (name: string) => string
+export interface AvatarProps
+  extends HTMLUIProps,
+    ThemeProps<AvatarStyle>,
+    UseAvatarProps {
   /**
    * The avatar icon to use.
    */
   icon?: ReactElement
-  /**
-   * If `true`, opt out of the avatar's `fallback` logic and renders the `img` at all times.
-   *
-   * @default false
-   */
-  ignoreFallback?: ImageProps["ignoreFallback"]
-  /**
-   * Defines loading strategy.
-   */
-  loading?: ImageProps["loading"]
-  /**
-   * Defining which referrer is sent when fetching the resource.
-   */
-  referrerPolicy?: HTMLAttributeReferrerPolicy
 }
 
-export type AvatarProps = AvatarOptions &
-  HTMLUIProps<"span"> &
-  Pick<UseImageProps, "crossOrigin" | "onError" | "onLoad"> &
-  ThemeProps<"Avatar">
+export const {
+  component,
+  PropsContext: AvatarPropsContext,
+  usePropsContext: useAvatarPropsContext,
+  withContext,
+  withProvider,
+  useRootComponentProps,
+} = createSlotComponent<AvatarProps, AvatarStyle>("avatar", avatarStyle)
 
 /**
  * `Avatar` is a component that displays a profile picture or an icon with initials representing a user.
  *
  * @see Docs https://yamada-ui.com/components/media-and-icons/avatar
  */
-export const Avatar: FC<AvatarProps> = (props) => {
-  const [styles, mergedProps] = useComponentMultiStyle("Avatar", props)
-  const {
-    name,
-    src,
-    srcSet,
-    alt,
-    className,
-    borderRadius = "fallback(full, 9999px)",
-    children,
-    crossOrigin,
-    format,
-    icon,
-    ignoreFallback,
-    loading,
-    referrerPolicy,
-    rounded = "fallback(full, 9999px)",
-    onError,
-    onLoad,
-    ...rest
-  } = omitThemeProps(mergedProps)
-
-  const [loaded, setLoaded] = useState<boolean>(false)
+export const Avatar = withProvider<"div", AvatarProps>((props) => {
+  const { getFallbackProps, getImageProps, getRootProps } = useAvatar(props)
 
   return (
-    <AvatarContext value={styles}>
-      <ui.span
-        className={cx("ui-avatar", className)}
-        data-loaded={dataAttr(loaded)}
-        borderRadius={borderRadius}
-        rounded={rounded}
-        __css={styles.container}
-        {...rest}
-      >
-        <AvatarImage
-          name={name}
-          src={src}
-          srcSet={srcSet}
-          alt={alt}
-          borderRadius={borderRadius}
-          crossOrigin={crossOrigin}
-          format={format}
-          icon={icon}
-          ignoreFallback={ignoreFallback}
-          loading={loading}
-          referrerPolicy={referrerPolicy}
-          rounded={rounded}
-          onError={onError}
-          onLoad={handlerAll(onLoad, () => setLoaded(true))}
-        />
+    <ui.div {...getRootProps()}>
+      <AvatarFallback {...getFallbackProps()} />
+      <AvatarImage {...getImageProps()} />
+    </ui.div>
+  )
+}, "root")({ icon: <UserRoundIcon fontSize="1.2em" /> })
+
+interface AvatarImageProps extends HTMLUIProps<"img"> {}
+
+const AvatarImage = withContext<"img", AvatarImageProps>("img", "image")()
+
+interface AvatarFallbackProps extends HTMLUIProps {}
+
+const AvatarFallback = withContext<"div", AvatarFallbackProps>(
+  "div",
+  "fallback",
+)()
+
+export interface AvatarGroupProps
+  extends HTMLUIProps,
+    ThemeProps<AvatarStyle>,
+    UseAvatarGroupProps {}
+
+export const AvatarGroup = component<"div", AvatarGroupProps>((props) => {
+  const [, { colorScheme, size, variant, shape, ...rest }] =
+    useRootComponentProps(props, "group", {
+      transferProps: ["variant", "colorScheme", "size", "shape"],
+    })
+  const { children, excess, getRootProps } = useAvatarGroup(rest)
+  const context = useMemo(
+    () => ({ colorScheme, size, variant, shape }),
+    [variant, size, colorScheme, shape],
+  )
+
+  return (
+    <AvatarPropsContext value={context}>
+      <ui.div {...getRootProps()}>
+        {excess > 0 ? <Avatar fallback={`+${excess}`} /> : null}
         {children}
-      </ui.span>
-    </AvatarContext>
+      </ui.div>
+    </AvatarPropsContext>
   )
-}
-
-Avatar.displayName = "Avatar"
-Avatar.__ui__ = "Avatar"
-
-interface AvatarImageProps
-  extends ImageProps,
-    Pick<AvatarProps, "format" | "icon" | "ignoreFallback" | "name"> {}
-
-const AvatarImage: FC<AvatarImageProps> = ({
-  name,
-  src,
-  srcSet,
-  alt,
-  borderRadius,
-  crossOrigin,
-  format,
-  icon = <UserRoundIcon boxSize="calc(100% / 1.5)" />,
-  ignoreFallback,
-  loading,
-  referrerPolicy,
-  rounded,
-  onError,
-  onLoad,
-}) => {
-  const status = useImage({ src, crossOrigin, ignoreFallback, onError, onLoad })
-
-  const loaded = status === "loaded"
-
-  const fallback = !src || !loaded
-
-  if (fallback)
-    return name ? (
-      <AvatarName name={name} format={format} />
-    ) : (
-      cloneElement(icon, { "aria-label": alt ?? "Avatar Icon", role: "img" })
-    )
-
-  return (
-    <ui.img
-      src={src}
-      srcSet={srcSet}
-      alt={alt ?? name}
-      className="ui-avatar__image"
-      borderRadius={borderRadius}
-      loading={loading}
-      referrerPolicy={referrerPolicy}
-      rounded={rounded}
-      __css={{
-        height: "100%",
-        objectFit: "cover",
-        width: "100%",
-      }}
-    />
-  )
-}
-
-AvatarImage.displayName = "AvatarImage"
-AvatarImage.__ui__ = "AvatarImage"
+}, "group")()

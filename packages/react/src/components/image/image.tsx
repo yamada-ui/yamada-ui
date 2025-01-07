76e8ac1b766dd1ebc9100982225ff50dba2a104a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-01-07 22:44:15 +0900

    refactor(image): streamline components, update tests

--- packages/react/src/components/image/image.tsx
+++ packages/react/src/components/image/image.tsx
@@ -1,89 +1,27 @@
-import type { ReactElement } from "react"
-import type { CSSUIProps, FC, HTMLUIProps, ThemeProps } from "../../core"
-import type { UseImageProps } from "./use-image"
-import { isValidElement, useMemo } from "react"
-import { omitThemeProps, ui, useComponentStyle } from "../../core"
-import { cx } from "../../utils"
-import { shouldShowFallbackImage, useImage } from "./use-image"
+import type { CSSProps, FC, HTMLUIProps, ThemeProps } from "../../core"
+import type { ImageStyle } from "./image.style"
+import { createComponent } from "../../core"
+import { imageStyle } from "./image.style"
 
-interface ImageOptions {
-  /**
-   * Fallback image `src` or element to show if image is loading or image fails.
-   */
-  fallback?: ReactElement | string
-  /**
-   * - beforeLoadOrError: loads the fallbackImage while loading the src.
-   * - onError: loads the fallbackImage only if there is an error fetching the src.
-   *
-   * @default "beforeLoadOrError"
-   */
-  fallbackStrategy?: "beforeLoadOrError" | "onError"
+export interface ImageProps extends HTMLUIProps<"img">, ThemeProps<ImageStyle> {
   /**
    * The CSS `object-fit` property.
    */
-  fit?: CSSUIProps["objectFit"]
+  fit?: CSSProps["objectFit"]
 }
 
-export interface ImageProps
-  extends Omit<HTMLUIProps<"img">, keyof UseImageProps>,
-    ThemeProps<"Image">,
-    UseImageProps,
-    ImageOptions {}
+export const {
+  PropsContext: ImagePropsContext,
+  usePropsContext: useImagePropsContext,
+  withContext,
+} = createComponent<ImageProps, ImageStyle>("image", imageStyle)
 
 /**
  * `Image` is a component that displays images with fallback support.
  *
  * @see Docs https://yamada-ui.com/components/media-and-icons/image
  */
-export const Image: FC<ImageProps> = (props) => {
-  const [styles, mergedProps] = useComponentStyle("Image", props)
-  const {
-    src,
-    srcSet,
-    className,
-    crossOrigin,
-    fallback,
-    fallbackStrategy = "beforeLoadOrError",
-    fit: objectFit,
-    ignoreFallback: _ignoreFallback,
-    loading,
-    referrerPolicy,
-    onError,
-    onLoad,
-    ...rest
-  } = omitThemeProps(mergedProps)
-  const ignoreFallback = loading != null || _ignoreFallback || !fallback
-  const css = useMemo(() => ({ ...styles, objectFit }), [styles, objectFit])
-  const status = useImage({ ...props, ignoreFallback })
-  const fallbackImage = shouldShowFallbackImage(status, fallbackStrategy)
-
-  if (fallbackImage) {
-    if (isValidElement(fallback)) {
-      return fallback
-    } else {
-      return (
-        <ui.img
-          src={fallback as string | undefined}
-          className={cx("ui-image--fallback", className)}
-          __css={css}
-          {...(ignoreFallback ? { ...rest, onError, onLoad } : rest)}
-        />
-      )
-    }
-  }
-
-  return (
-    <ui.img
-      src={src}
-      srcSet={srcSet}
-      className={cx("ui-image", className)}
-      crossOrigin={crossOrigin}
-      loading={loading}
-      referrerPolicy={referrerPolicy}
-      __css={css}
-      {...(ignoreFallback ? { ...rest, onError, onLoad } : rest)}
-    />
-  )
-}
-
-Image.__ui__ = "Image"
+export const Image: FC<ImageProps> = withContext("img")(
+  undefined,
+  ({ fit, ...rest }) => ({ objectFit: fit, ...rest }),
+)

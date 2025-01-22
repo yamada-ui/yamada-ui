f0187f57d25a47babe895ced83ae2354a332837b
takaaki chida <takaaki.chida@findy.co.jp>
2025-01-22 22:24:37 +0900

    refactor: move styles to theme for multi date picker

--- packages/react/src/theme/components/multi-date-picker.ts
+++ packages/react/src/theme/components/multi-date-picker.ts
@@ -3,4 +3,20 @@ import { mergeMultiStyle } from "../../core"
 import { DatePicker } from "./date-picker"
 
 export const MultiDatePicker: ComponentMultiStyle<"MultiDatePicker"> =
-  mergeMultiStyle(DatePicker)()
+  mergeMultiStyle(DatePicker, {
+    baseStyle: {
+      container: {
+        h: "fit-content",
+        w: "100%",
+      },
+      field: {
+        alignItems: "center",
+        display: "flex",
+        flexWrap: "wrap",
+        pe: "2rem",
+      },
+      inner: {
+        position: "relative",
+      },
+    },
+  })()

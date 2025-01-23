c93a1a4df8f380091f6546162d4e7aa938a5085b
taroj1205 <taroj1205@gmail.com>
2025-01-24 10:47:04 +1300

    fix(close-button): simplify close-button using icon-button

--- packages/react/src/components/close-button/close-button.tsx
+++ packages/react/src/components/close-button/close-button.tsx
@@ -1,66 +1,12 @@
-import type { ElementType } from "react"
-import type { HTMLUIProps, ThemeProps } from "../../core"
-import type { CloseButtonStyle } from "./close-button.style"
-import { useCallback, useMemo, useRef } from "react"
-import { createComponent, ui } from "../../core"
-import { createContext, cx, mergeRefs } from "../../utils"
+import { createComponent } from "../../core"
 import { XIcon } from "../icon"
-import { Ripple, useRipple } from "../ripple"
-import { closeButtonStyle } from "./close-button.style"
-
-interface CloseButtonContext extends Pick<CloseButtonOptions, "isRounded"> {}
-
-const [CloseButtonContext] = createContext<CloseButtonContext>({
-  name: "CloseButtonContext",
-})
-
-const useCloseButtonType = (value?: ElementType) => {
-  const buttonRef = useRef(!value)
-
-  const ref = useCallback((node: HTMLElement | null) => {
-    if (node) buttonRef.current = node.tagName === "BUTTON"
-  }, [])
-
-  const type = buttonRef.current ? "button" : undefined
-
-  return { ref, type } as const
-}
-
-interface CloseButtonOptions {
-  /**
-   * If `true`, the button is disabled.
-   *
-   * @default false
-   */
-  disabled?: boolean
-  /**
-   * If `true`, disable ripple effects when pressing a element.
-   *
-   * @default false
-   */
-  disableRipple?: boolean
-  /**
-   * If true, the button is full rounded.
-   *
-   * @default false
-   */
-  isRounded?: boolean
-}
-
-export interface CloseButtonProps
-  extends HTMLUIProps<"button">,
-    ThemeProps<CloseButtonStyle>,
-    CloseButtonOptions {}
+import { IconButton, type IconButtonProps } from "../button"
 
 export const {
-  component,
-  PropsContext: CloseButtonPropsContext,
-  usePropsContext: useCloseButtonPropsContext,
+  PropsContext: IconButtonPropsContext,
+  usePropsContext: useIconButtonPropsContext,
   withContext,
-} = createComponent<CloseButtonProps, CloseButtonStyle>(
-  "close-button",
-  closeButtonStyle,
-)
+} = createComponent<IconButtonProps>("close-button")
 
 /**
  * `CloseButton` is a component used primarily to trigger the close functionality of a component.
@@ -68,44 +14,12 @@ export const {
  * @see Docs https://yamada-ui.com/components/other/close-button
  */
 
-export const CloseButton = withContext(
-  ({
-    ref,
-    as,
-    className,
-    children,
-    disabled,
-    disableRipple,
-    isRounded,
-    ...rest
-  }) => {
-    const { ref: closeButtonRef, type } = useCloseButtonType(as)
-    const { onClick, ...rippleProps } = useRipple({
-      ...rest,
-      disabled: disableRipple || disabled,
-    })
-
-    const context = useMemo(() => ({ isRounded }), [isRounded])
-
-    return (
-      <CloseButtonContext.Provider value={context}>
-        <ui.button
-          ref={mergeRefs(ref, closeButtonRef)}
-          as={as}
-          type={type}
-          className={cx("ui-close-button", className)}
-          aria-label="Close"
-          disabled={disabled}
-          {...rest}
-          onClick={onClick}
-        >
-          {children || <XIcon boxSize="2em" />}
-
-          <Ripple {...rippleProps} />
-        </ui.button>
-      </CloseButtonContext.Provider>
-    )
-  },
-)()
-
-CloseButton.__ui__ = "CloseButton"
+export const CloseButton = withContext(IconButton)(
+  undefined,
+  ({ children, icon, ...rest }) => ({
+    children: children || icon || <XIcon />,
+    variant: "ghost",
+    colorScheme: "gray",
+    ...rest,
+  }),
+)

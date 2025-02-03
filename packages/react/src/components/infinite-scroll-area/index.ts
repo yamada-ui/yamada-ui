b3cdec9e17b8f164ce9a4c4f5a5385c301b2b842
108yen <kazuking.1911@gmail.com>
2025-02-03 19:45:14 +0900

    refactor(infinite-scroll-area): updated component structure

---
+++ packages/react/src/components/infinite-scroll-area/index.ts
@@ -0,0 +1,8 @@
+export {
+  InfiniteScrollArea,
+  InfiniteScrollAreaPropsContext,
+  useInfiniteScrollAreaPropsContext,
+} from "./infinite-scroll-area"
+export type { InfiniteScrollAreaProps } from "./infinite-scroll-area"
+
+export * from "./infinite-scroll-area.style"

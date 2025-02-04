33577862244e0bdef0d02d65f3121107f0d4a0e5
108yen <kazuking.1911@gmail.com>
2025-02-05 00:01:21 +0900

    refactor(scroll-area): separate logic

--- packages/react/src/components/scroll-area/index.ts
+++ packages/react/src/components/scroll-area/index.ts
@@ -1,8 +1,11 @@
 export {
   ScrollArea,
-  scrollAreaPropsContext,
+  ScrollAreaPropsContext,
   useScrollAreaPropsContext,
 } from "./scroll-area"
 export type { ScrollAreaProps } from "./scroll-area"
 
 export * from "./scroll-area.style"
+
+export { useScrollArea } from "./use-scroll-area"
+export type { UseScrollAreaProps, UseScrollAreaReturn } from "./use-scroll-area"

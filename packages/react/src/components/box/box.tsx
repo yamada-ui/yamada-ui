35121822934866eed989fe11239e427443e76197
bmthd <jougennotuki67@gmail.com>
2025-02-19 00:30:14 +0900

    refactor(box): replace ui with createComponent and add context support

--- packages/react/src/components/box/box.tsx
+++ packages/react/src/components/box/box.tsx
@@ -1,13 +1,17 @@
 import type { HTMLUIProps } from "../../core"
-import { ui } from "../../core"
+import { createComponent } from "../../core"
 
 export interface BoxProps extends HTMLUIProps {}
+
+export const {
+  PropsContext: BoxPropsContext,
+  usePropsContext: useBoxPropsContext,
+  withContext,
+} = createComponent<BoxProps>("box")
 
 /**
  * `Box` is the most abstract component on which all other components are built. By default, it renders a `div` element.
  *
  * @see Docs https://yamada-ui.com/components/layouts/box
  */
-export const Box = ui("div")
-
-Box.__ui__ = "Box"
+export const Box = withContext("div")()

1fc805ca8ae0c976d6f226e5acb0b917a5750c64
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-02-21 06:11:31 +0900

    refactor(react): updated LucideIcon component props

--- packages/react/src/components/icon/lucide-icon.tsx
+++ packages/react/src/components/icon/lucide-icon.tsx
@@ -3,14 +3,12 @@ import type { IconProps } from "./icon"
 import { Icon as OriginalIcon } from "lucide-react"
 import { Icon, withContext } from "./icon"
 
-interface LucideIconOptions {
+export interface LucideIconProps extends IconProps {
   /**
    * The icon of the [Lucide lab](https://github.com/lucide-icons/lucide-lab).
    */
-  icon: IconNode
+  iconNode?: IconNode
 }
-
-export interface LucideIconProps extends IconProps, LucideIconOptions {}
 
 /**
  * `LucideIcon` is a component for using [Lucide lab](https://github.com/lucide-icons/lucide-lab) icons.

51a2c39a570a46752e5a6f087a22a70943486ee5
108yen <kazuking.1911@gmail.com>
2025-02-27 21:41:22 +0900

    refactor(number-input): updated component structure

---
+++ packages/react/src/components/number-input/index.ts
@@ -0,0 +1,10 @@
+export { NumberInput } from "./number-input"
+export type { NumberInputProps } from "./number-input"
+
+export * from "./number-input.style"
+
+export { useNumberInput } from "./use-number-input"
+export type {
+  UseNumberInputProps,
+  UseNumberInputReturn,
+} from "./use-number-input"

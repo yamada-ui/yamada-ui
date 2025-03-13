5e81efd97e07f2f10053802c3a542b07e23a2a11
108yen <kazuking.1911@gmail.com>
2025-03-13 15:20:02 +0900

    refactor(reorder): updated component structure

---
+++ packages/react/src/components/reorder/index.ts
@@ -0,0 +1,15 @@
+export {
+  ReorderItem,
+  ReorderPropsContext,
+  ReorderRoot,
+  ReorderTrigger,
+  useReorderPropsContext,
+} from "./reorder"
+export type {
+  ReorderGenerateItem,
+  ReorderItemProps,
+  ReorderRootProps,
+  ReorderTriggerProps,
+} from "./reorder"
+
+export * as Reorder from "./namespace"
---
+++ packages/react/src/components/reorder/namespace.ts
@@ -0,0 +1,13 @@
+export {
+  ReorderItem as Item,
+  ReorderPropsContext as PropsContext,
+  ReorderRoot as Root,
+  ReorderTrigger as Trigger,
+  useReorderPropsContext as usePropsContext,
+} from "./reorder"
+export type {
+  ReorderGenerateItem as GenerateItem,
+  ReorderItemProps as ItemProps,
+  ReorderRootProps as RootProps,
+  ReorderTriggerProps as TriggerProps,
+} from "./reorder"

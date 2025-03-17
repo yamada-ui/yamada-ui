ac1b57ff60375c4b5891823da64fea94cf494e39
108yen <kazuking.1911@gmail.com>
2025-03-17 14:05:35 +0900

    refactor(resizable): updated component structure

---
+++ packages/react/src/components/resizable/index.ts
@@ -0,0 +1,17 @@
+export {
+  ResizableItem,
+  ResizablePropsContext,
+  ResizableRoot,
+  ResizableTrigger,
+  useResizablePropsContext,
+} from "./resizable"
+export type {
+  ResizableItemProps,
+  ResizableRootProps,
+  ResizableTriggerProps,
+} from "./resizable"
+export type { ResizableItemControl, ResizableStorage } from "./use-resizable"
+
+export * from "./resizable.style"
+
+export * as Resizable from "./namespace"

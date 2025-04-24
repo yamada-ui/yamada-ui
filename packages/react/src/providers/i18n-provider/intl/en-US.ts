69e0f335e402563532ff29186470dcbcd74f5426
yuto ichinose <yuto.0526.sano@gmail.com>
2025-04-24 10:16:34 +0900

    feat(breadcrumb): add i18n support for breadcrumb labels in English and Japanese

--- packages/react/src/providers/i18n-provider/intl/en-US.ts
+++ packages/react/src/providers/i18n-provider/intl/en-US.ts
@@ -2,4 +2,8 @@ export default {
   avatar: {
     "Avatar Icon": "Avatar Icon",
   },
+  breadcrumb: {
+    Breadcrumb: "Breadcrumb",
+    "Breadcrumb Ellipsis": "Ellipsis",
+  },
 } as const
--- packages/react/src/providers/i18n-provider/intl/ja-JP.ts
+++ packages/react/src/providers/i18n-provider/intl/ja-JP.ts
@@ -2,4 +2,8 @@ export default {
   avatar: {
     "Avatar Icon": "アバターアイコン",
   },
+  breadcrumb: {
+    Breadcrumb: "パンくずリスト",
+    "Breadcrumb Ellipsis": "省略記号",
+  },
 } as const

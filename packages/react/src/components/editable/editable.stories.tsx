a19d538c17131c9240404b8e5c7b87b719e0e78d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-04-29 16:17:55 +0900

    feat(avatar): added avatar group components and stories

--- packages/react/src/components/editable/editable.stories.tsx
+++ packages/react/src/components/editable/editable.stories.tsx
@@ -3,10 +3,10 @@ import type { SubmitHandler } from "react-hook-form"
 import { Controller, useForm } from "react-hook-form"
 import { Editable } from "."
 import { Button } from "../button"
-import { ButtonGroup } from "../button-group"
+import { ButtonGroup } from "../button"
+import { IconButton } from "../button"
 import { Field } from "../field"
 import { CheckIcon, PencilIcon, XIcon } from "../icon"
-import { IconButton } from "../icon-button"
 import { VStack } from "../stack"
 
 type Story = StoryFn<typeof Editable.Root>

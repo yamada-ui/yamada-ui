479673dd20c7a79580ba1ae00c1e9fb1d938e30c
koralle <mkhd7930@gmail.com>
2025-06-11 21:15:30 +0900

    feat(storybook): remove `getAbsolutePath`

--- packages/react/storybook/main.ts
+++ packages/react/storybook/main.ts
@@ -1,25 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite"
-import { createRequire } from "node:module"
-import { dirname, join } from "node:path"
-
-const require = createRequire(import.meta.url)
 
 export const config: StorybookConfig = {
   addons: [
-    getAbsolutePath("@storybook/addon-docs"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("storybook-dark-mode"),
-    getAbsolutePath("storybook-i18n"),
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "storybook-dark-mode",
+    "storybook-i18n",
   ],
   core: { disableTelemetry: true },
-  framework: getAbsolutePath("@storybook/react-vite"),
+  framework: "@storybook/react-vite",
   // TODO: Remove legacy-components
   stories: ["../src/!(legacy-components)/**/*.@(mdx|stories.@(tsx))"],
   typescript: { reactDocgen: false },
 }
 
 export default config
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")))
-}

ebce41e4732d6f3bb51f5f9c3e336356962d0fb6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-06-25 08:58:55 +0900

    refactor(eslint-config): removed custom react plugin and rules

--- .eslint/react.ts
+++ .eslint/react.ts
@@ -2,46 +2,10 @@ import type { TSESLint } from "@typescript-eslint/utils"
 import reactPlugin from "eslint-plugin-react"
 import { sharedFiles } from "./shared"
 
-const noDebugRule: TSESLint.LooseRuleDefinition = {
-  create(context) {
-    return {
-      JSXAttribute(node: any) {
-        if (node.name && node.name.name === "__debug") {
-          context.report({
-            messageId: "noDebugProp",
-            node,
-          })
-        }
-      },
-    }
-  },
-  meta: {
-    type: "problem",
-    docs: {
-      description: "Warn when using __debug prop",
-    },
-    messages: {
-      noDebugProp:
-        "`__debug` prop is for debugging only and should not be used in production.",
-    },
-    schema: [],
-  },
-}
-
-const reactCustomPlugin: TSESLint.FlatConfig.Plugin = {
-  meta: {
-    name: "eslint-custom-plugin-react",
-    version: "1.0.0",
-  },
-  rules: {
-    "no-debug": noDebugRule,
-  },
-}
-
 export const reactConfig: TSESLint.FlatConfig.Config = {
   name: "eslint/react",
   files: sharedFiles,
-  plugins: { "custom-react": reactCustomPlugin, react: reactPlugin },
+  plugins: { react: reactPlugin },
   rules: {
     ...reactPlugin.configs.recommended.rules,
 
@@ -57,8 +21,6 @@ export const reactConfig: TSESLint.FlatConfig.Config = {
     "react/jsx-pascal-case": "error",
     "react/react-in-jsx-scope": "off",
     "react/self-closing-comp": "error",
-
-    "custom-react/no-debug": "warn",
   },
   settings: { react: { version: "detect" } },
 }

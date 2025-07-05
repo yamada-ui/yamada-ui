f0507cc009767987a3bd29c1c7bc9c56f43154c7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-06 05:05:01 +0900

    feat(header): added navigation links and new icon component

--- website/app/[locale]/docs/[...slug]/header.tsx
+++ website/app/[locale]/docs/[...slug]/header.tsx
@@ -1,10 +1,25 @@
 import type { Doc } from "#velite"
-import { Heading, Text, VStack } from "@yamada-ui/react"
+import {
+  ButtonGroup,
+  GithubIcon,
+  Heading,
+  Text,
+  VStack,
+} from "@yamada-ui/react"
+import { useTranslations } from "next-intl"
+import { NextLinkButton, StorybookIcon } from "@/components"
 import { mdToHtml } from "@/utils/string"
 
 export interface HeaderProps extends Doc {}
 
-export function Header({ description, title }: HeaderProps) {
+export function Header({
+  style,
+  description,
+  source,
+  storybook,
+  title,
+}: HeaderProps) {
+  const t = useTranslations("docs")
   return (
     <VStack gap="md" mb="xl">
       <VStack gap="sm">
@@ -12,6 +27,41 @@ export function Header({ description, title }: HeaderProps) {
 
         <Text color="fg.emphasized">{mdToHtml(description)}</Text>
       </VStack>
+
+      {storybook || source || style ? (
+        <ButtonGroup as="nav" size="xs" variant="outline" gap="sm">
+          {source ? (
+            <NextLinkButton
+              href={source}
+              color="fg.emphasized"
+              external
+              startIcon={<GithubIcon />}
+            >
+              {t("source")}
+            </NextLinkButton>
+          ) : null}
+          {style ? (
+            <NextLinkButton
+              href={style}
+              color="fg.emphasized"
+              external
+              startIcon={<GithubIcon />}
+            >
+              {t("style")}
+            </NextLinkButton>
+          ) : null}
+          {storybook ? (
+            <NextLinkButton
+              href={storybook}
+              color="fg.emphasized"
+              external
+              startIcon={<StorybookIcon />}
+            >
+              {t("storybook")}
+            </NextLinkButton>
+          ) : null}
+        </ButtonGroup>
+      ) : null}
     </VStack>
   )
 }

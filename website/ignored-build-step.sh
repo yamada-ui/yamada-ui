fbb3176b9cf5b5ed2bdcb063bc9e422a5f8ab915
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-07 18:31:03 +0900

    chore(website): added ignored-build-step.sh script

---
+++ website/ignored-build-step.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+TARGET_BRANCH="main"
+
+if [ "$VERCEL_GIT_COMMIT_REF" == "$TARGET_BRANCH" ]; then
+  git_diff_result=$(git diff --name-only HEAD^ HEAD)
+else
+  git fetch https://github.com/yamada-ui/yamada-ui.git $TARGET_BRANCH:$TARGET_BRANCH
+
+  git_diff_result=$(git diff --name-only $(git merge-base HEAD $TARGET_BRANCH) HEAD)
+fi
+
+echo "$git_diff_result"
+
+if echo "$git_diff_result" | grep -q "^website/"; then
+  echo "Run because './website' is included in the difference."
+  exit 1
+else
+  echo "Run was canceled because './website' was not included in the difference."
+  exit 0
+fi

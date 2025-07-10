bf42eedb80b0c4860b215e8697cb544816c3773c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-10 15:37:57 +0900

    build: updated build script conditions for www changes

--- www/ignored-build-step.sh
+++ www/ignored-build-step.sh
@@ -5,7 +5,19 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 TARGET_BRANCH="v1"
 
 if [ "$VERCEL_GIT_COMMIT_REF" == "$TARGET_BRANCH" ]; then
-  exit 0
+  git_diff_result=$(git diff --name-only HEAD^ HEAD)
 else
+  git fetch https://github.com/yamada-ui/yamada-ui.git $TARGET_BRANCH:$TARGET_BRANCH
+
+  git_diff_result=$(git diff --name-only $(git merge-base HEAD $TARGET_BRANCH) HEAD)
+fi
+
+echo "$git_diff_result"
+
+if echo "$git_diff_result" | grep -q "^www/"; then
+  echo "Run because './www' is included in the difference."
   exit 1
+else
+  echo "Run was canceled because './www' was not included in the difference."
+  exit 0
 fi

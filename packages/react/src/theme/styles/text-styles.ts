bfcba96d7066a4814aeb07646c725f25e6c44912
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-17 04:35:33 +0900

    fix(kbd): update labels to proper casing and style

--- packages/react/src/theme/styles/text-styles.ts
+++ packages/react/src/theme/styles/text-styles.ts
@@ -1,6 +1,12 @@
 import { defineStyles } from "../../core"
 
 export const textStyles = defineStyles.textStyle({
+  mono: {
+    fontFamily: "mono",
+    fontWeight: "medium",
+    letterSpacing: "widest",
+    whiteSpace: "nowrap",
+  },
   outline: {
     color: "colorScheme.outline",
   },

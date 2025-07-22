ee5fd6fd2376720a52bc8a19ae93bcc8fc0c7dcf
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-22 19:29:07 +0900

    chore: add "use client" directive to relevant files

--- packages/react/src/components/slider/slider.tsx
+++ packages/react/src/components/slider/slider.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import type { FC, ReactElement, ReactNode } from "react"
 import type {
   CSSProps,
--- packages/react/src/components/slider/use-slider.ts
+++ packages/react/src/components/slider/use-slider.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import type { KeyboardEvent } from "react"
 import type {
   HTMLProps,

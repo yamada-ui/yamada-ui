595ef5b6bc2fa92453a08f7778a10fc46c5734a1
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-07-31 07:13:32 +0900

    refactor(layout): remove unused cookies import and props

--- www/app/[locale]/color-mode-button.tsx
+++ www/app/[locale]/color-mode-button.tsx
@@ -1,20 +1,34 @@
 "use client"
 
 import type { ButtonProps } from "@yamada-ui/react"
-import { IconButton, MoonIcon, SunIcon, useColorMode } from "@yamada-ui/react"
+import {
+  IconButton,
+  MoonIcon,
+  SunIcon,
+  useColorMode,
+  useMounted,
+} from "@yamada-ui/react"
 import { useTranslations } from "next-intl"
 
 export interface ColorModeButtonProps extends ButtonProps {}
 
 export function ColorModeButton({ ...rest }: ColorModeButtonProps) {
   const { colorMode, toggleColorMode } = useColorMode()
+  const mounted = useMounted({ state: true })
   const t = useTranslations("component.header")
 
   return (
     <IconButton
       aria-label={t("colorMode", { colorMode })}
       color="fg.emphasized"
-      icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+      icon={
+        !mounted || colorMode === "light" ? (
+          <SunIcon key="icon" />
+        ) : (
+          <MoonIcon key="icon" />
+        )
+      }
+      suppressHydrationWarning
       onClick={toggleColorMode}
       {...rest}
     />

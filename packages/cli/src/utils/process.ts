a5a76c4392481c46a34b2b9a9fe54b0f17afc3e6
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-08-25 22:59:58 +0900

    refactor(process): removed unused execAsync function

--- packages/cli/src/utils/process.ts
+++ packages/cli/src/utils/process.ts
@@ -1,15 +1,5 @@
-import type { ExecOptions } from "child_process"
-import { exec } from "child_process"
 import { HttpsProxyAgent } from "https-proxy-agent"
 
 export const agent = process.env.https_proxy
   ? new HttpsProxyAgent(process.env.https_proxy)
   : undefined
-
-export async function execAsync(command: string, options: ExecOptions = {}) {
-  return new Promise<string>((resolve, reject) => {
-    exec(command, options, function (e, stdout) {
-      return e ? reject(e) : resolve(stdout)
-    })
-  })
-}

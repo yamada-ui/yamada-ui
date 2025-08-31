97b7fd69037121cc042117f3f943e49e5c13eb9f
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-08-31 23:54:01 +0900

    build(packages/react): update dependencies and cleanup build scripts

---
+++ packages/react/src/components/icon/icons/chromium-icon.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Chromium } from "lucide-react"
+import { component, Icon } from "../icon"
+
+/**
+ * `ChromiumIcon` is [Lucide](https://lucide.dev) SVG icon component.
+ *
+ * @see https://yamada-ui.com/components/icon
+ */
+export const ChromiumIcon = component(Icon)({ as: Chromium })

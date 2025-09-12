038d9218791b1ad2b2d8b97d50a046ced22c7dc4
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-09-12 13:51:41 +0900

    build: updated icons

---
+++ packages/react/src/components/icon/icons/text-align-end-icon.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { TextAlignEnd } from "lucide-react"
+import { component, Icon } from "../icon"
+
+/**
+ * `TextAlignEndIcon` is [Lucide](https://lucide.dev) SVG icon component.
+ *
+ * @see https://yamada-ui.com/docs/components/icon
+ */
+export const TextAlignEndIcon = component(Icon)({ as: TextAlignEnd })

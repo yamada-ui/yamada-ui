f1c9391f8bfa54f21ab547857fbc05fd29268399
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-10-11 02:58:47 +0900

    chore: add branch ignore logic to build script

--- www/ignored-build-step.sh
+++ www/ignored-build-step.sh
@@ -2,7 +2,15 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
+IGNORE_BRANCHES=("v1")
 TARGET_BRANCH="main"
+
+for branch in "${IGNORE_BRANCHES[@]}"; do
+  if [ "$VERCEL_GIT_COMMIT_REF" == "$branch" ]; then
+    echo "Run was canceled because '$VERCEL_GIT_COMMIT_REF' is in the ignore list."
+    exit 0
+  fi
+done
 
 if [ "$VERCEL_GIT_COMMIT_REF" == "$TARGET_BRANCH" ]; then
   git_diff_result=$(git diff --name-only HEAD^ HEAD)

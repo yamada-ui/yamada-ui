6730d6ee70a7ffcb334aa5a039e34d46ee9d2090
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2025-10-11 03:15:20 +0900

    chore: add debug info for Vercel environment variables

--- www/ignored-build-step.sh
+++ www/ignored-build-step.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_TARGET_ENV: $VERCEL_TARGET_ENV"
+echo "VERCEL_GIT_REPO_SLUG: $VERCEL_GIT_REPO_SLUG"
+echo "VERCEL_URL: $VERCEL_URL"
+echo "VERCEL_BRANCH_URL: $VERCEL_BRANCH_URL"
+echo "VERCEL_PROJECT_PRODUCTION_URL: $VERCEL_PROJECT_PRODUCTION_URL"
 
-IGNORE_BRANCHES=("v1")
 TARGET_BRANCH="main"
 
-for branch in "${IGNORE_BRANCHES[@]}"; do
-  if [ "$VERCEL_GIT_COMMIT_REF" == "$branch" ]; then
-    echo "Run was canceled because '$VERCEL_GIT_COMMIT_REF' is in the ignore list."
-    exit 0
-  fi
-done
 
 if [ "$VERCEL_GIT_COMMIT_REF" == "$TARGET_BRANCH" ]; then
   git_diff_result=$(git diff --name-only HEAD^ HEAD)

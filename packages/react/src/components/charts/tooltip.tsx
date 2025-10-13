bf9d7b085cf54b4cce1acde99d7b95fab4433691
108yen <kazuking.1911@gmail.com>
2025-10-13 23:41:57 +0900

    feat(charts): adde chart tooltip

--- packages/react/src/components/charts/tooltip.tsx
+++ packages/react/src/components/charts/tooltip.tsx
@@ -1,17 +1,199 @@
-import type { TooltipProps as RechartsTooltipProps } from "recharts"
 import type { ThemeProps } from "../../core"
+import type { Dict } from "../../utils"
 import type { TooltipStyle } from "./tooltip.style"
+import type { UseTooltipProps } from "./use-tooltip"
 import { Tooltip as RechartsTooltip } from "recharts"
-import { createComponent } from "../../core"
+import { createComponent, getClassName, styled } from "../../core"
+import { bem, isArray } from "../../utils"
 import { tooltipStyle } from "./tooltip.style"
+import { useTooltip } from "./use-tooltip"
 
 export interface TooltipProps
-  extends RechartsTooltipProps<any, any>,
-    ThemeProps<TooltipStyle> {}
+  extends UseTooltipProps,
+    ThemeProps<TooltipStyle> {
+  /**
+   * A function to format labels on inside the tooltip.
+   */
+  labelFormatter?: (label: string) => string
+  /**
+   * Unit displayed next to each tick.
+   */
+  unit?: string
+  /**
+   * A function to format values on inside the tooltip.
+   */
+  valueFormatter?: (value: any) => string
+}
 
-const { withContext } = createComponent<TooltipProps, TooltipStyle>(
-  "chart-tooltip",
-  tooltipStyle,
-)
+const {
+  component,
+  ComponentContext: TooltipComponentContext,
+  PropsContext: TooltipPropsContext,
+  useComponentContext: useTooltipComponentContext,
+  usePropsContext: useTooltipPropsContext,
+  withContext,
+} = createComponent<TooltipProps, TooltipStyle>("chart-tooltip", tooltipStyle)
 
-export const Tooltip = withContext<"div", TooltipProps>(RechartsTooltip)()
+export {
+  TooltipComponentContext,
+  TooltipPropsContext,
+  useTooltipComponentContext,
+  useTooltipPropsContext,
+}
+
+export const Tooltip = withContext<"div", TooltipProps>(RechartsTooltip)(({
+  labelFormatter,
+  unit,
+  valueFormatter,
+  ...props
+}) => {
+  const customProps = useTooltipComponentContext()
+  const { getTooltipProps, tooltipStyledProps } = useTooltip({
+    ...customProps,
+    ...props,
+  })
+
+  return {
+    ...getTooltipProps(),
+    content: ({
+      label,
+      payload,
+    }: {
+      payload: Dict[] | undefined
+      label?: string
+    }) => (
+      <DefaultTooltip
+        label={label}
+        labelFormatter={labelFormatter}
+        payload={payload}
+        unit={unit}
+        valueFormatter={valueFormatter}
+        {...tooltipStyledProps}
+      />
+    ),
+  }
+})
+
+interface DefaultTooltipProps {
+  payload: Dict[] | undefined
+  dataKey?: string
+  label?: string
+  labelFormatter?: (label: string) => string
+  radialChart?: boolean
+  unit?: string
+  valueFormatter?: (value: any) => string
+}
+
+const DefaultTooltip = component<"div", DefaultTooltipProps>(
+  ({
+    dataKey = "value",
+    label,
+    labelFormatter,
+    payload = [],
+    radialChart,
+    unit,
+    valueFormatter,
+    ...rest
+  }) => {
+    const items = payload.map(
+      (
+        {
+          name: nameProp,
+          color: colorProp,
+          [dataKey]: valueProp,
+          payload,
+        } = {},
+        index,
+      ) => {
+        const name = radialChart ? payload?.name : nameProp
+        const color = colorProp ?? payload?.color
+        let value: string
+
+        if (isArray(valueProp)) {
+          value = valueProp
+            .map((value) => {
+              return `${valueFormatter?.(value) ?? value}`
+            })
+            .join(" - ")
+        } else {
+          value = valueFormatter?.(valueProp) ?? valueProp
+        }
+
+        return (
+          <styled.div
+            key={`tooltip-payload-${index}`}
+            className="ui-chart__tooltip-item"
+            alignItems="center"
+            display="flex"
+            fontSize="sm"
+            gap="sm"
+          >
+            <styled.div
+              className="ui-chart__tooltip-swatch"
+              background={color}
+              boxSize="3"
+              rounded="full"
+            />
+
+            <styled.span
+              className="ui-chart__tooltip-label"
+              color={["blackAlpha.800", "whiteAlpha.700"]}
+            >
+              {name}
+            </styled.span>
+
+            <styled.span
+              className="ui-chart__tooltip-value"
+              flex="1"
+              textAlign="end"
+            >
+              {value}
+              {unit ? unit : ""}
+            </styled.span>
+          </styled.div>
+        )
+      },
+    )
+
+    return (
+      <styled.div
+        className="ui-chart__tooltip"
+        bg={["white", "black"]}
+        border="1px solid"
+        borderColor={["blackAlpha.200", "whiteAlpha.100"]}
+        boxShadow={["md", "dark-md"]}
+        color="inherit"
+        display="flex"
+        flexDirection="column"
+        fontSize="sm"
+        fontWeight="medium"
+        gap="sm"
+        maxW="xs"
+        minW="48"
+        p="3"
+        rounded="md"
+        zIndex="dodoria"
+        {...rest}
+      >
+        {label ? (
+          <styled.p className="ui-chart__tooltip-title" fontSize="md">
+            {labelFormatter?.(label) ?? label}
+          </styled.p>
+        ) : null}
+
+        <styled.div
+          className="ui-chart__tooltip-list"
+          display="flex"
+          flexDirection="column"
+          gap="xs"
+        >
+          {items}
+        </styled.div>
+      </styled.div>
+    )
+  },
+  {
+    name: "default-tooltip",
+    className: getClassName(bem("chart-tooltip", "default")),
+  },
+)()
---
+++ packages/react/src/components/charts/use-tooltip.ts
@@ -0,0 +1,47 @@
+import type { TooltipProps } from "recharts"
+import type { HTMLProps, PropGetter } from "../../core"
+import type { Dict, Merge } from "../../utils"
+import { useCallback } from "react"
+import { splitObject } from "../../utils"
+import { tooltipProperties } from "./recharts-properties"
+
+export interface UseTooltipProps
+  extends Merge<HTMLProps, TooltipProps<any, any>> {
+  /**
+   * Specifies the duration of animation, the unit of this option is ms.
+   *
+   * @default 0
+   */
+  animationDuration?: number
+}
+
+export const useTooltip = ({
+  animationDuration = 0,
+  ...rest
+}: UseTooltipProps) => {
+  const [tooltipProps, tooltipStyledProps] = splitObject<Dict, string>(
+    rest,
+    tooltipProperties,
+  )
+
+  const getTooltipProps: PropGetter<
+    undefined,
+    Partial<TooltipProps<any, any>>,
+    TooltipProps<any, any>
+  > = useCallback(
+    (props) => ({
+      animationDuration,
+      isAnimationActive: (animationDuration || 0) > 0,
+      ...tooltipProps,
+      ...props,
+    }),
+    [animationDuration, tooltipProps],
+  )
+
+  return {
+    getTooltipProps,
+    tooltipStyledProps,
+  }
+}
+
+export type TooltipReturn = ReturnType<typeof useTooltip>

fe62da005ecaa19ad1b3b0d3ab4d77c6e180a114
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-11-19 18:01:23 +1300

    feat(qr-code): add qr code component

---
+++ packages/react/src/components/qr-code/qr-code.style.ts
@@ -0,0 +1,75 @@
+import { defineComponentSlotStyle } from "../../core"
+
+export const qrCodeStyle = defineComponentSlotStyle({
+  base: {
+    frame: {},
+    overlay: {
+      alignItems: "center",
+      display: "flex",
+      justifyContent: "center",
+      left: "50%",
+      position: "absolute",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+    },
+    pattern: {
+      "svg path": {
+        fill: "currentColor",
+      },
+    },
+    root: {
+      position: "relative",
+    },
+  },
+
+  sizes: {
+    "2xs": {
+      frame: {
+        h: "16",
+        w: "16",
+      },
+    },
+    xs: {
+      frame: {
+        h: "20",
+        w: "20",
+      },
+    },
+    sm: {
+      frame: {
+        h: "24",
+        w: "24",
+      },
+    },
+    md: {
+      frame: {
+        h: "32",
+        w: "32",
+      },
+    },
+    lg: {
+      frame: {
+        h: "40",
+        w: "40",
+      },
+    },
+    xl: {
+      frame: {
+        h: "48",
+        w: "48",
+      },
+    },
+    "2xl": {
+      frame: {
+        h: "56",
+        w: "56",
+      },
+    },
+  },
+
+  defaultProps: {
+    size: "md",
+  },
+})
+
+export type QrCodeStyle = typeof qrCodeStyle
---
+++ packages/react/src/components/qr-code/qr-code.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import type { HTMLStyledProps, ThemeProps } from "../../core"
+import type { QrCodeStyle } from "./qr-code.style"
+
+import { useMemo } from "react"
+import { createSlotComponent, styled } from "../../core"
+import { qrCodeStyle } from "./qr-code.style"
+import { useQrCode } from "./use-qr-code"
+
+export interface QrCodeRootProps
+  extends HTMLStyledProps,
+    ThemeProps<QrCodeStyle> {
+  /**
+   * The value to encode as a QR code.
+   */
+  value: string
+  /**
+   * The border width.
+   *
+   * @default 2
+   */
+  border?: number
+  /**
+   * The error correction level.
+   *
+   * @default 'M'
+   */
+  ecc?: "H" | "L" | "M" | "Q"
+  /**
+   * The pixel size.
+   *
+   * @default 10
+   */
+  pixelSize?: number
+}
+
+const {
+  ComponentContext,
+  PropsContext: QrCodePropsContext,
+  useComponentContext,
+  usePropsContext: useQrCodePropsContext,
+  withContext,
+  withProvider,
+} = createSlotComponent<QrCodeRootProps, QrCodeStyle>("qr-code", qrCodeStyle)
+
+export { QrCodePropsContext, useQrCodePropsContext }
+
+/**
+ * `QrCode` is a component that displays a QR code.
+ *
+ * @see https://yamada-ui.com/docs/components/qr-code
+ */
+export const QrCodeRoot = withProvider<"div", QrCodeRootProps>(
+  ({ border, ecc, pixelSize, value, ...rest }) => {
+    const { getFrameProps, getOverlayProps, getPatternProps, getRootProps } =
+      useQrCode({ border, ecc, pixelSize, value })
+
+    const context = useMemo(
+      () => ({ getFrameProps, getOverlayProps, getPatternProps }),
+      [getFrameProps, getPatternProps, getOverlayProps],
+    )
+
+    return (
+      <ComponentContext value={context}>
+        <styled.div {...getRootProps(rest)} />
+      </ComponentContext>
+    )
+  },
+  "root",
+)({ border: 2, ecc: "M" })
+
+export interface QrCodeFrameProps
+  extends HTMLStyledProps<"svg">,
+    ThemeProps<QrCodeStyle> {}
+
+export const QrCodeFrame = withContext<"div", QrCodeFrameProps>((rest) => {
+  const { getFrameProps } = useComponentContext()
+
+  return <styled.svg {...getFrameProps(rest)} />
+}, "frame")()
+
+export interface QrCodePatternProps extends HTMLStyledProps<"path"> {}
+
+export const QrCodePattern = withContext<"path", QrCodePatternProps>((rest) => {
+  const { getPatternProps } = useComponentContext()
+  const { d, ...pathProps } = getPatternProps(rest)
+
+  return (
+    <styled.path asChild {...pathProps}>
+      <path d={d} />
+    </styled.path>
+  )
+}, "pattern")()
+
+export interface QrCodeOverlayProps extends HTMLStyledProps {}
+
+export const QrCodeOverlay = withContext<"div", QrCodeOverlayProps>((rest) => {
+  const { getOverlayProps } = useComponentContext()
+
+  return <styled.div {...getOverlayProps(rest)} />
+}, "overlay")()
+
+export const QrCode = {
+  Frame: QrCodeFrame,
+  Overlay: QrCodeOverlay,
+  Pattern: QrCodePattern,
+  Root: QrCodeRoot,
+}

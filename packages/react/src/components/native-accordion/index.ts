6648d2b38208a99eff3d613efa7035894dc7431d
Shintaro Jokagi <taroj1205@gmail.com>
2025-12-19 15:03:06 +0900

    feat(native-accordion): add `NativeAccordion` component

---
+++ packages/react/src/components/native-accordion/index.ts
@@ -0,0 +1,17 @@
+export * from "./native-accordion.style"
+export * as NativeAccordion from "./namespace"
+export type {
+  NativeAccordionButtonProps,
+  NativeAccordionIconProps,
+  NativeAccordionItemProps,
+  NativeAccordionRootProps,
+} from "./native-accordion"
+export {
+  NativeAccordionButton,
+  NativeAccordionIcon,
+  NativeAccordionItem,
+  NativeAccordionPropsContext,
+  NativeAccordionRoot,
+  useNativeAccordionPropsContext,
+} from "./native-accordion"
+export * from "./use-native-accordion"

742618bcb4a4aa2dab609262d02fe3bd484dd1e6
Shintaro Jokagi <taroj1205@gmail.com>
2025-12-19 15:03:06 +0900

    feat(native-accordion): add `NativeAccordion` component

--- packages/react/src/components/accordion/accordion.style.ts
+++ packages/react/src/components/accordion/accordion.style.ts
@@ -1,71 +1,25 @@
 import { defineComponentSlotStyle } from "../../core"
+import { nativeAccordionStyle } from "../native-accordion"
 
 export const accordionStyle = defineComponentSlotStyle({
   base: {
+    ...nativeAccordionStyle.base,
     button: {
+      ...nativeAccordionStyle.base?.button,
       "&[aria-disabled=true]": {
         cursor: "default",
       },
-      alignItems: "center",
       display: "inline-flex",
       focusVisibleRing: "inside",
-      px: "4",
-      py: "3",
-      transitionDuration: "moderate",
-      transitionProperty: "common",
       w: "100%",
-      _nativeDisabled: {
-        layerStyle: "disabled",
-      },
-    },
-    icon: {
-      color: "fg.subtle",
-      fontSize: "xl",
-      ml: "auto",
-      transformOrigin: "center",
-      transitionDuration: "moderate",
-      transitionProperty: "transform",
-      _expanded: {
-        transform: "rotate(180deg)",
-      },
-      _disabled: {
-        layerStyle: "disabled",
-      },
-    },
-    item: {
-      overflowAnchor: "none",
     },
     panel: {
+      ...nativeAccordionStyle.base?.panel,
       pb: "3",
-      px: "4",
-    },
-    root: {
-      w: "full",
     },
   },
 
-  variants: {
-    panel: {
-      button: {
-        rounded: "l2",
-      },
-      item: {
-        layerStyle: "panel",
-        rounded: "l2",
-        _notFirst: {
-          mt: "md",
-        },
-      },
-    },
-    plain: {
-      item: {
-        borderTopWidth: "1px",
-        _last: {
-          borderBottomWidth: "1px",
-        },
-      },
-    },
-  },
+  variants: nativeAccordionStyle.variants,
 
   defaultProps: {
     variant: "plain",

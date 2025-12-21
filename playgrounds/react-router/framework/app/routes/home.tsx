cba1d43e9a36d6c5ba4c392ed4018a32a708a775
Shintaro Jokagi <taroj1205@gmail.com>
2025-12-22 07:22:53 +0900

    chore(home): remove meta from home.tsx

--- playgrounds/react-router/framework/app/routes/home.tsx
+++ playgrounds/react-router/framework/app/routes/home.tsx
@@ -1,16 +1,5 @@
-import type { Route } from "./+types/home"
 import { VStack } from "@yamada-ui/react"
 import { ColorModeButton } from "../components/color-mode-button"
-
-export function meta(_: Route.MetaArgs) {
-  return [
-    { title: "React Router - Yamada UI" },
-    {
-      name: "description",
-      content: "React Router playground configured with Yamada UI.",
-    },
-  ]
-}
 
 export default function Home() {
   return (

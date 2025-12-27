7e5be87da25cad7d167345faf0c9f6c64bea32b7
VahantSharma <vahantofficials12315@gmail.com>
2025-12-27 13:48:06 +0000

    refactor(playground): remove indexRoute per maintainer feedback

--- playgrounds/tanstack/router/src/routes.ts
+++ playgrounds/tanstack/router/src/routes.ts
@@ -1,14 +1,8 @@
-import { createRootRoute, createRoute } from "@tanstack/react-router"
+import { createRootRoute } from "@tanstack/react-router"
 import { Root } from "./pages/root"
 
 const rootRoute = createRootRoute({
   component: Root,
 })
 
-const indexRoute = createRoute({
-  component: Root,
-  getParentRoute: () => rootRoute,
-  path: "/",
-})
-
-export const routeTree = rootRoute.addChildren([indexRoute])
+export const routeTree = rootRoute

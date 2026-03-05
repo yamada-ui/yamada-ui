48889dd34a9fe0256aad57f00b0ff19d051a5f47
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-03-06 07:53:21 +0900

    test(motion): enhance test coverage for Motion factory

--- packages/react/src/components/motion/motion.test.tsx
+++ packages/react/src/components/motion/motion.test.tsx
@@ -1,4 +1,5 @@
 import { a11y, render, screen } from "#test"
+import { motion } from "./factory"
 import { Motion } from "./motion"
 
 describe("<Motion />", () => {
@@ -29,5 +30,13 @@ describe("<Motion />", () => {
   test("renders HTML tag correctly", () => {
     render(<Motion data-testid="motion" />)
     expect(screen.getByTestId("motion").tagName).toBe("DIV")
+  })
+
+  test("renders motion component created via factory function call", () => {
+    const MotionSpan = motion("span")
+
+    render(<MotionSpan data-testid="factory-motion">test</MotionSpan>)
+
+    expect(screen.getByTestId("factory-motion").tagName).toBe("SPAN")
   })
 })

12e0b58d9da4fc33764cfb34572aa12354bb93f2
taco-tortilla <tortillataco0@gmail.com>
2026-03-08 10:38:56 +0900

    test(hue-slider): enhance aria-valuetext and controlled value updates

--- packages/react/src/components/hue-slider/hue-slider.test.tsx
+++ packages/react/src/components/hue-slider/hue-slider.test.tsx
@@ -48,6 +48,46 @@ describe("<HueSlider />", () => {
     )
   })
 
+  test.each([
+    [29, "Red"],
+    [30, "Yellow"],
+    [89, "Yellow"],
+    [90, "Green"],
+    [149, "Green"],
+    [150, "Cyan"],
+    [209, "Cyan"],
+    [210, "Blue"],
+    [269, "Blue"],
+    [270, "Magenta"],
+    [329, "Magenta"],
+    [330, "Red"],
+  ])("sets aria-valuetext for each hue correctly", (hue, color) => {
+    render(<HueSlider.Root defaultValue={hue} />)
+
+    expect(screen.getByRole("slider")).toHaveAttribute(
+      "aria-valuetext",
+      `${hue}°, ${color}`,
+    )
+  })
+
+  test("controlled slider value updates correctly", () => {
+    const { rerender } = render(<HueSlider.Root value={60} />)
+
+    expect(screen.getByRole("slider")).toHaveAttribute("aria-valuenow", "60")
+    expect(screen.getByRole("slider")).toHaveAttribute(
+      "aria-valuetext",
+      "60°, Yellow",
+    )
+
+    rerender(<HueSlider.Root value={180} />)
+
+    expect(screen.getByRole("slider")).toHaveAttribute("aria-valuenow", "180")
+    expect(screen.getByRole("slider")).toHaveAttribute(
+      "aria-valuetext",
+      "180°, Cyan",
+    )
+  })
+
   test("disabled HueSlider renders correctly", () => {
     render(<HueSlider.Root disabled />)
 

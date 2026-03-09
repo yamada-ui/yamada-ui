8efc19620f8efb547e92924fa38ed2379f5eea25
nkoji21 <133028205+nkoji21@users.noreply.github.com>
2026-03-10 06:13:50 +0900

    chore: setup tanstack router via quick start
    https://tanstack.com/router/latest/docs/quick-start#impatient

---
+++ playgrounds/tanstack/router/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    viteReact(),
+  ],
+})
+
+export default config

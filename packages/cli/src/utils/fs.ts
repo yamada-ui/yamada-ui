deaeb3f34e9488317bef3e0a6e9a5ea92b59e7f3
fujitani sora <fujitanisora0414@gmail.com>
2026-03-13 18:06:17 +0900

    chore(cli): replace `glob` package with Node.js built-in `fs.globSync`

--- packages/cli/src/utils/fs.ts
+++ packages/cli/src/utils/fs.ts
@@ -2,14 +2,13 @@ import type { ObjectEncodingOptions } from "fs"
 import type { LintFilesOptions } from "./lint"
 import type { FormatOptions } from "./prettier"
 import { execFile } from "child_process"
-import fs, { existsSync, statSync } from "fs"
+import fs, { existsSync, globSync, statSync } from "fs"
 import {
   mkdir,
   writeFile as originalWriteFile,
   readdir,
   readFile,
 } from "fs/promises"
-import { glob } from "glob"
 import path from "path"
 import c from "picocolors"
 import { promisify } from "util"
@@ -99,7 +98,7 @@ export function timer() {
 
 export async function getFiles(pattern: string) {
   const files: { [key: string]: string } = {}
-  const [dirPath] = await glob(pattern)
+  const [dirPath] = globSync(pattern)
 
   const dirents = await readdir(dirPath!, { withFileTypes: true })
 

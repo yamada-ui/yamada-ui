67932fdf9e2ede4d0395461ef89edf813a440d6e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-03-18 01:54:04 +0900

    Revert "test(use-window-event): migrate test to browser mode (#6140)"
    This reverts commit 25a36cd7c3e2ae96123ae025fdd2c2b373dfcf4d.

--- packages/react/src/hooks/use-window-event/index.test.tsx
+++ packages/react/src/hooks/use-window-event/index.test.tsx
@@ -1,4 +1,4 @@
-import { a11y, renderHook } from "#test/browser"
+import { a11y, renderHook } from "#test"
 import { useWindowEvent } from "./"
 import { Basic } from "./index.stories"
 
@@ -7,13 +7,11 @@ describe("useWindowEvent", () => {
     await a11y(<Basic />)
   })
 
-  test("adds and removes an event listener to the window", async () => {
+  test("adds and removes an event listener to the window", () => {
     const eventType = "resize"
     const handler = vi.fn()
 
-    const { unmount } = await renderHook(() =>
-      useWindowEvent(eventType, handler),
-    )
+    const { unmount } = renderHook(() => useWindowEvent(eventType, handler))
     window.dispatchEvent(new Event(eventType))
     expect(handler).toHaveBeenCalledTimes(1)
 

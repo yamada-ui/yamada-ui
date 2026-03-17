2a7ba336e8247fadbf163789c48856db035d10e5
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-03-18 03:35:04 +0900

    Revert "test(use-breakpoint): migrate tests to browser mode (#6121)"
    This reverts commit 0f6a63ce61ad8946ace076bdb00c8e746f70c584.

--- packages/react/src/hooks/use-breakpoint/use-breakpoint-effect.test.tsx
+++ packages/react/src/hooks/use-breakpoint/use-breakpoint-effect.test.tsx
@@ -1,16 +1,26 @@
-import { page, renderHook } from "#test/browser"
+import { renderHook } from "#test"
+import MatchMediaMock from "vitest-matchmedia-mock"
 import { useBreakpointEffect } from "./use-breakpoint-effect"
 
 describe("useBreakpointEffect", () => {
-  afterEach(async () => {
-    await page.viewport(1280, 720)
+  let matchMediaMock: MatchMediaMock
+
+  beforeAll(() => {
+    matchMediaMock = new MatchMediaMock()
   })
 
-  test("Executes callback when breakpoint changes", async () => {
-    await page.viewport(600, 800)
+  afterEach(() => {
+    matchMediaMock.clear()
+  })
+
+  afterAll(() => {
+    matchMediaMock.destroy()
+  })
+
+  test("Executes callback when breakpoint changes", () => {
+    matchMediaMock.useMediaQuery("(min-width: 481px) and (max-width: 768px)")
     const callback = vi.fn()
-    await renderHook(() => useBreakpointEffect(callback, []))
-    await expect.poll(() => callback).toHaveBeenCalledWith("md")
-    await expect.poll(() => callback).toHaveBeenCalledTimes(1)
+    renderHook(() => useBreakpointEffect(callback, []))
+    expect(callback).toHaveBeenCalledExactlyOnceWith("md")
   })
 })

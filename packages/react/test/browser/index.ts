7204f2a9924e3ab5e0eab990f3af2910cd4186d1
koralle <koralle+github@koralle-mgmg.com>
2026-03-25 20:29:52 +0900

    test(loading): refactor tests

--- packages/react/test/browser/index.ts
+++ packages/react/test/browser/index.ts
@@ -1,4 +1,22 @@
+import type { Locator } from "vitest/browser"
+import { locators } from "vitest/browser"
+
+export type { Locator }
+export { locators }
+
 // eslint-disable-next-line import-alias/import-alias
 export * from "../system"
 export * from "./render"
 export { page } from "vitest/browser"
+
+declare module "vitest/browser" {
+  interface LocatorSelectors {
+    getByQuerySelector(selector: string): Locator
+  }
+}
+
+locators.extend({
+  getByQuerySelector(selector: string) {
+    return selector
+  },
+})

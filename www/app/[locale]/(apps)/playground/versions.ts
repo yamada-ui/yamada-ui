a6f9cbc203d048f1659edacaa110e7d9168f1188
bmthd <jougennotuki67@gmail.com>
2026-04-01 21:16:30 +0900

    fix(playground): add version management for playground dependencies and improve file generation

--- www/app/[locale]/(apps)/playground/versions.ts
+++ www/app/[locale]/(apps)/playground/versions.ts
@@ -1,16 +1,13 @@
+import yamadaUIPackageJson from "@yamada-ui/react/package.json"
 import htmlToImagePackageJson from "html-to-image/package.json"
-import yamadaUIPackageJson from "../../../../../packages/react/package.json"
 
-const { dependencies } = yamadaUIPackageJson
-
+export {
+  DAYJS_VERSION,
+  FAKER_VERSION,
+  LUCIDE_LAB_VERSION,
+  MOTION_VERSION,
+  REACT_HOOK_FORM_VERSION,
+  REACT_VERSION,
+} from "./versions.gen"
 export const YAMADA_UI_VERSION = yamadaUIPackageJson.version
-export const REACT_VERSION = dependencies.react
 export const HTML_TO_IMAGE_VERSION = htmlToImagePackageJson.version
-export const FAKER_VERSION =
-  yamadaUIPackageJson.devDependencies["@faker-js/faker"]
-export const LUCIDE_LAB_VERSION =
-  yamadaUIPackageJson.devDependencies["@lucide/lab"]
-export const DAYJS_VERSION = yamadaUIPackageJson.dependencies.dayjs
-export const MOTION_VERSION = yamadaUIPackageJson.dependencies.motion
-export const REACT_HOOK_FORM_VERSION =
-  yamadaUIPackageJson.devDependencies["react-hook-form"]

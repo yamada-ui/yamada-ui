cef56152ff07c26303b0aca2623080d9dd219956
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-04-11 09:47:15 +0900

    fix: remove onClick handlers and update scroll behavior

--- www/app/[locale]/docs/sidebar.tsx
+++ www/app/[locale]/docs/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type { NextLinkButtonProps } from "@/components"
-import { Box, handlerAll, Text, useSafeLayoutEffect } from "@yamada-ui/react"
+import { Box, Text, useSafeLayoutEffect } from "@yamada-ui/react"
 import { useTranslations } from "next-intl"
 import { useMemo, useRef } from "react"
 import scrollIntoView from "scroll-into-view-if-needed"
@@ -116,7 +116,7 @@ interface SidebarItemProps extends NextLinkButtonProps {
   segment: string
 }
 
-function SidebarItem({ href, segment, onClick, ...rest }: SidebarItemProps) {
+function SidebarItem({ href, segment, ...rest }: SidebarItemProps) {
   const pathname = usePathname()
   const overview = segment === "overview"
   const current = overview
@@ -134,9 +134,6 @@ function SidebarItem({ href, segment, onClick, ...rest }: SidebarItemProps) {
       fontWeight="normal"
       justifyContent="flex-start"
       mb="xs"
-      onClick={handlerAll(onClick, () =>
-        window.scrollTo({ behavior: "instant", top: 0 }),
-      )}
       {...rest}
     />
   )

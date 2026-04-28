65c355b1b2790556b35dee282c092a2e7bb8793f
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 15:15:10 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@18010f209e38ab23a2f875af40b4e12b380622ea 🚀

---
+++ assets/index.stories-BCm2zaRF.js
@@ -0,0 +1,13 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{t as n}from"./text-DSMMc5B6.js";import{t as r}from"./text-BM4F8FHS.js";import{l as i,t as a}from"./button-C66K-mOB.js";import{n as o,t as s}from"./use-boolean-DYW0OEzi.js";import{n as c,r as l}from"./iframe-Z0FuFE11.js";var u,d,f,p;e((()=>{c(),a(),r(),s(),u=t(),d={title:`Hooks / useResizeObserver`},f=()=>{let[e,{toggle:t}]=o(),[r,a]=l();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(n,{children:JSON.stringify(a)}),(0,u.jsx)(i,{ref:r,h:e?`xl`:`sm`,w:e?`xl`:`sm`,onClick:t,children:`Click me to toggle the boolean value`})]})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`() => {
+  const [flg, {
+    toggle
+  }] = useBoolean();
+  const [ref, rect] = useResizeObserver<HTMLButtonElement>();
+  return <>
+      <Text>{JSON.stringify(rect)}</Text>
+
+      <Button ref={ref} h={flg ? "xl" : "sm"} w={flg ? "xl" : "sm"} onClick={toggle}>
+        Click me to toggle the boolean value
+      </Button>
+    </>;
+}`,...f.parameters?.docs?.source}}},p=[`Basic`]}))();export{f as Basic,p as __namedExportsOrder,d as default};
---
+++ assets/status.stories-c5OUut5a.js
@@ -0,0 +1,23 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{Z as t,yn as n}from"./core-C7NnSGLR.js";import{t as r}from"./jsx-runtime-D_578c1K.js";import{a as i,r as a}from"./z-stack-D_lbkXhr.js";import{Dt as o,vt as s,yt as c}from"./iframe-Z0FuFE11.js";import{n as l,t as u}from"./storybook-DgrVb_b8.js";var d,f,p,m,h,g;e((()=>{u(),t(),o(),s(),d=r(),f={component:c,title:`Components / Status`},p=()=>(0,d.jsxs)(i,{children:[(0,d.jsx)(c,{value:`info`}),(0,d.jsx)(c,{value:`success`}),(0,d.jsx)(c,{value:`warning`}),(0,d.jsx)(c,{value:`error`})]}),m=()=>(0,d.jsxs)(i,{children:[(0,d.jsx)(c,{value:`info`,children:`Info`}),(0,d.jsx)(c,{value:`success`,children:`Success`}),(0,d.jsx)(c,{value:`warning`,children:`Warning`}),(0,d.jsx)(c,{value:`error`,children:`Error`})]}),h=()=>(0,d.jsx)(a,{children:(0,d.jsx)(l,{columns:[`info`,`success`,`warning`,`error`],rows:[`sm`,`md`,`lg`],children:(e,t,r)=>(0,d.jsx)(c,{size:t,value:e,children:n(e)},r)})}),p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`() => {
+  return <HStack>
+      <Status value="info" />
+      <Status value="success" />
+      <Status value="warning" />
+      <Status value="error" />
+    </HStack>;
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`() => {
+  return <HStack>
+      <Status value="info">Info</Status>
+      <Status value="success">Success</Status>
+      <Status value="warning">Warning</Status>
+      <Status value="error">Error</Status>
+    </HStack>;
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`() => {
+  return <VStack>
+      <PropsTable columns={["info", "success", "warning", "error"]} rows={["sm", "md", "lg"]}>
+        {(column, row, key) => <Status key={key} size={row} value={column}>
+            {toTitleCase(column)}
+          </Status>}
+      </PropsTable>
+    </VStack>;
+}`,...h.parameters?.docs?.source}}},g=[`Basic`,`Label`,`Size`]}))();export{p as Basic,m as Label,h as Size,g as __namedExportsOrder,f as default};

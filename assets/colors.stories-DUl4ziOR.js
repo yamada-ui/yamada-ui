96c3256e575aee7f61e5e646dd7e144d5b61e4ab
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 09:14:30 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@73593491d364cac61e55e54b961de676b4f22a09 🚀

---
+++ assets/colors.stories-DUl4ziOR.js
@@ -0,0 +1,127 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{Dn as t,En as n,On as r,Z as i,kn as a,yn as o}from"./core-C7NnSGLR.js";import{t as s}from"./jsx-runtime-D_578c1K.js";import{t as c}from"./text-DSMMc5B6.js";import{t as l}from"./text-BM4F8FHS.js";import{t as u}from"./center-B9iB-Iph.js";import{t as d}from"./center-DwGXhAh8.js";import{r as f}from"./grid-item-DDwW3cLH.js";import{t as p}from"./grid--uOXgUdY.js";import{t as m}from"./box-D_8v7EGt.js";import{t as h}from"./heading-DnIVTUkI.js";import{Fo as g,Gi as _,Hr as v,Ki as y,Ur as b,Wi as x,Wr as S,qi as C}from"./iframe-CqkJGIDp.js";var w,T,E,D;e((()=>{g(),d(),x(),b(),p(),v(),l(),i(),w=s(),T={title:`Theme / Tokens`},E=()=>(0,w.jsxs)(w.Fragment,{children:[(0,w.jsx)(h,{children:`Colors`}),(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`2xl`,children:`Tokens`})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(S,{each:t,children:(e,t)=>(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(${a.length}, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:a,children:(t,n)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(m,{bg:`${e}.${t}`,h:`12`,rounded:`l2`,w:`full`}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:`${e}.${t}`})]},n)})},t)})})]}),(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`2xl`,children:`Semantic Tokens`})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(S,{each:r,children:(e,t)=>(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(${a.length}, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:a,children:(t,n)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(m,{bg:`${e}.${t}`,h:`12`,rounded:`l2`,w:`full`}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:`${e}.${t}`})]},n)})},t)})})]}),(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`xl`,children:`Background`})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(6, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:[``,`subtle`,`overlay`,`ghost`,`muted`,`emphasized`,`contrast`,`panel`,`float`,`info`,`success`,`warning`,`error`],children:(e,t)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(m,{bg:e?`bg.${e}`:`bg`,borderColor:`border.muted`,borderWidth:`1px`,h:`24`,rounded:`l2`,w:`full`}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:e?`bg.${e}`:`bg`})]},t)})})})]}),(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`xl`,children:`Foreground`})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(6, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:[``,`subtle`,`muted`,`emphasized`,`contrast`,`info`,`success`,`warning`,`error`],children:(e,t)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(u,{bg:e===`contrast`?`bg.contrast`:void 0,borderColor:`border.muted`,borderWidth:`1px`,h:`24`,rounded:`l2`,w:`full`,children:(0,w.jsx)(c,{color:e?`fg.${e}`:`fg`,children:`Ag`})}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:e?`fg.${e}`:`fg`})]},t)})})})]}),(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`xl`,children:`Border`})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(6, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:[``,`subtle`,`muted`,`emphasized`,`contrast`,`info`,`success`,`warning`,`error`],children:(e,t)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(m,{borderColor:e?`border.${e}`:`border`,borderWidth:`3px`,h:`24`,rounded:`l2`,w:`full`}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:e?`border.${e}`:`border`})]},t)})})})]}),(0,w.jsx)(S,{each:n,children:(e,t)=>(0,w.jsxs)(C,{children:[(0,w.jsx)(y,{children:(0,w.jsx)(h,{size:`xl`,children:o(e)})}),(0,w.jsx)(_,{gap:`sm`,children:(0,w.jsx)(f,{gap:`sm`,templateColumns:`repeat(6, 1fr)`,w:`full`,children:(0,w.jsx)(S,{each:[``,`contrast`,`fg`,`bg`,`ghost`,`subtle`,`muted`,`emphasized`,`solid`,`outline`],children:(t,n)=>(0,w.jsxs)(f,{gap:`xs`,children:[(0,w.jsx)(m,{bg:t?`${e}.${t}`:e,borderColor:`border.muted`,borderWidth:`1px`,h:`24`,rounded:`l2`,w:`full`}),(0,w.jsx)(c,{color:`fg.muted`,fontSize:`xs`,lineClamp:1,textAlign:`center`,children:t?`${e}.${t}`:e})]},n)})})})]},t)})]}),E.parameters={...E.parameters,docs:{...E.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Heading>Colors</Heading>
+
+      <Container.Root>
+        <Container.Header>
+          <Heading size="2xl">Tokens</Heading>
+        </Container.Header>
+
+        <Container.Body gap="sm">
+          <For each={DEFAULT_COLOR_SCHEMES}>
+            {(colorScheme, index) => <Grid key={index} gap="sm" templateColumns={\`repeat(\${TONES.length}, 1fr)\`} w="full">
+                <For each={TONES}>
+                  {(tone, index) => <Grid key={index} gap="xs">
+                      <Box bg={\`\${colorScheme}.\${tone}\`} h="12" rounded="l2" w="full" />
+                      <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">{\`\${colorScheme}.\${tone}\`}</Text>
+                    </Grid>}
+                </For>
+              </Grid>}
+          </For>
+        </Container.Body>
+      </Container.Root>
+
+      <Container.Root>
+        <Container.Header>
+          <Heading size="2xl">Semantic Tokens</Heading>
+        </Container.Header>
+
+        <Container.Body gap="sm">
+          <For each={SEMANTIC_COLOR_SCHEMES}>
+            {(colorScheme, index) => <Grid key={index} gap="sm" templateColumns={\`repeat(\${TONES.length}, 1fr)\`} w="full">
+                <For each={TONES}>
+                  {(tone, index) => <Grid key={index} gap="xs">
+                      <Box bg={\`\${colorScheme}.\${tone}\`} h="12" rounded="l2" w="full" />
+                      <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">{\`\${colorScheme}.\${tone}\`}</Text>
+                    </Grid>}
+                </For>
+              </Grid>}
+          </For>
+        </Container.Body>
+      </Container.Root>
+
+      <Container.Root>
+        <Container.Header>
+          <Heading size="xl">Background</Heading>
+        </Container.Header>
+
+        <Container.Body gap="sm">
+          <Grid gap="sm" templateColumns="repeat(6, 1fr)" w="full">
+            <For each={["", "subtle", "overlay", "ghost", "muted", "emphasized", "contrast", "panel", "float", "info", "success", "warning", "error"]}>
+              {(value, index) => <Grid key={index} gap="xs">
+                  <Box bg={value ? \`bg.\${value}\` : "bg"} borderColor="border.muted" borderWidth="1px" h="24" rounded="l2" w="full" />
+
+                  <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">
+                    {value ? \`bg.\${value}\` : "bg"}
+                  </Text>
+                </Grid>}
+            </For>
+          </Grid>
+        </Container.Body>
+      </Container.Root>
+
+      <Container.Root>
+        <Container.Header>
+          <Heading size="xl">Foreground</Heading>
+        </Container.Header>
+
+        <Container.Body gap="sm">
+          <Grid gap="sm" templateColumns="repeat(6, 1fr)" w="full">
+            <For each={["", "subtle", "muted", "emphasized", "contrast", "info", "success", "warning", "error"]}>
+              {(value, index) => <Grid key={index} gap="xs">
+                  <Center bg={value === "contrast" ? "bg.contrast" : undefined} borderColor="border.muted" borderWidth="1px" h="24" rounded="l2" w="full">
+                    <Text color={value ? \`fg.\${value}\` : "fg"}>Ag</Text>
+                  </Center>
+
+                  <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">
+                    {value ? \`fg.\${value}\` : "fg"}
+                  </Text>
+                </Grid>}
+            </For>
+          </Grid>
+        </Container.Body>
+      </Container.Root>
+
+      <Container.Root>
+        <Container.Header>
+          <Heading size="xl">Border</Heading>
+        </Container.Header>
+
+        <Container.Body gap="sm">
+          <Grid gap="sm" templateColumns="repeat(6, 1fr)" w="full">
+            <For each={["", "subtle", "muted", "emphasized", "contrast", "info", "success", "warning", "error"]}>
+              {(value, index) => <Grid key={index} gap="xs">
+                  <Box borderColor={value ? \`border.\${value}\` : "border"} borderWidth="3px" h="24" rounded="l2" w="full" />
+
+                  <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">
+                    {value ? \`border.\${value}\` : "border"}
+                  </Text>
+                </Grid>}
+            </For>
+          </Grid>
+        </Container.Body>
+      </Container.Root>
+
+      <For each={COLOR_SCHEMES}>
+        {(colorScheme, index) => <Container.Root key={index}>
+            <Container.Header>
+              <Heading size="xl">{toTitleCase(colorScheme)}</Heading>
+            </Container.Header>
+
+            <Container.Body gap="sm">
+              <Grid gap="sm" templateColumns="repeat(6, 1fr)" w="full">
+                <For each={["", "contrast", "fg", "bg", "ghost", "subtle", "muted", "emphasized", "solid", "outline"]}>
+                  {(value, index) => <Grid key={index} gap="xs">
+                      <Box bg={value ? \`\${colorScheme}.\${value}\` : colorScheme} borderColor="border.muted" borderWidth="1px" h="24" rounded="l2" w="full" />
+
+                      <Text color="fg.muted" fontSize="xs" lineClamp={1} textAlign="center">
+                        {value ? \`\${colorScheme}.\${value}\` : colorScheme}
+                      </Text>
+                    </Grid>}
+                </For>
+              </Grid>
+            </Container.Body>
+          </Container.Root>}
+      </For>
+    </>;
+}`,...E.parameters?.docs?.source}}},D=[`Colors`]}))();export{E as Colors,D as __namedExportsOrder,T as default};
---
+++ assets/slide-fide.stories-B20gKsVQ.js
@@ -0,0 +1,76 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{n,t as r}from"./slide-fade-CnwLDv3D.js";import{l as i,t as a}from"./button-C66K-mOB.js";import{t as o}from"./box-D_8v7EGt.js";import{n as s,t as c}from"./use-boolean-DYW0OEzi.js";import{Fo as l}from"./iframe-CqkJGIDp.js";var u,d,f,p,m,h,g,_;e((()=>{c(),l(),a(),n(),u=t(),d={component:r,title:`Components / SlideFade`},f=()=>{let[e,{toggle:t}]=s();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(i,{onClick:t,children:`Please Click`}),(0,u.jsx)(r,{bg:`orange.500`,color:`white`,open:e,p:`md`,rounded:`l2`,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),(0,u.jsx)(o,{bg:`purple.500`,color:`white`,p:`md`,rounded:`l2`,w:`full`,children:`私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……`})]})},p=()=>{let[e,{toggle:t}]=s();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(i,{onClick:t,children:`Please Click`}),(0,u.jsx)(r,{bg:`orange.500`,color:`white`,duration:.4,open:e,p:`md`,rounded:`l2`,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),(0,u.jsx)(o,{bg:`purple.500`,color:`white`,p:`md`,rounded:`l2`,w:`full`,children:`私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……`})]})},m=()=>{let[e,{toggle:t}]=s();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(i,{onClick:t,children:`Please Click`}),(0,u.jsx)(r,{bg:`orange.500`,color:`white`,offsetX:8,offsetY:0,open:e,p:`md`,rounded:`l2`,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),(0,u.jsx)(o,{bg:`purple.500`,color:`white`,p:`md`,rounded:`l2`,w:`full`,children:`私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……`})]})},h=()=>{let[e,{toggle:t}]=s();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(i,{onClick:t,children:`Please Click`}),(0,u.jsx)(r,{bg:`orange.500`,color:`white`,offsetY:-8,open:e,p:`md`,rounded:`l2`,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),(0,u.jsx)(o,{bg:`purple.500`,color:`white`,p:`md`,rounded:`l2`,w:`full`,children:`私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……`})]})},g=()=>{let[e,{toggle:t}]=s();return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(i,{onClick:t,children:`Please Click`}),(0,u.jsx)(r,{bg:`orange.500`,color:`white`,offsetY:-8,open:e,p:`md`,rounded:`l2`,unmountOnExit:!0,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),(0,u.jsx)(o,{bg:`purple.500`,color:`white`,p:`md`,rounded:`l2`,w:`full`,children:`私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……`})]})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`() => {
+  const [open, {
+    toggle
+  }] = useBoolean();
+  return <>
+      <Button onClick={toggle}>Please Click</Button>
+
+      <SlideFade bg="orange.500" color="white" open={open} p="md" rounded="l2">
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </SlideFade>
+
+      <Box bg="purple.500" color="white" p="md" rounded="l2" w="full">
+        私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……
+      </Box>
+    </>;
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`() => {
+  const [open, {
+    toggle
+  }] = useBoolean();
+  return <>
+      <Button onClick={toggle}>Please Click</Button>
+
+      <SlideFade bg="orange.500" color="white" duration={0.4} open={open} p="md" rounded="l2">
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </SlideFade>
+
+      <Box bg="purple.500" color="white" p="md" rounded="l2" w="full">
+        私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……
+      </Box>
+    </>;
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`() => {
+  const [open, {
+    toggle
+  }] = useBoolean();
+  return <>
+      <Button onClick={toggle}>Please Click</Button>
+
+      <SlideFade bg="orange.500" color="white" offsetX={8} offsetY={0} open={open} p="md" rounded="l2">
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </SlideFade>
+
+      <Box bg="purple.500" color="white" p="md" rounded="l2" w="full">
+        私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……
+      </Box>
+    </>;
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`() => {
+  const [open, {
+    toggle
+  }] = useBoolean();
+  return <>
+      <Button onClick={toggle}>Please Click</Button>
+
+      <SlideFade bg="orange.500" color="white" offsetY={-8} open={open} p="md" rounded="l2">
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </SlideFade>
+
+      <Box bg="purple.500" color="white" p="md" rounded="l2" w="full">
+        私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……
+      </Box>
+    </>;
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`() => {
+  const [open, {
+    toggle
+  }] = useBoolean();
+  return <>
+      <Button onClick={toggle}>Please Click</Button>
+
+      <SlideFade bg="orange.500" color="white" offsetY={-8} open={open} p="md" rounded="l2" unmountOnExit>
+        クリリンのことか……クリリンのことかーーーっ！！！！！
+      </SlideFade>
+
+      <Box bg="purple.500" color="white" p="md" rounded="l2" w="full">
+        私の戦闘力は530000です。ですがもちろんフルパワーであなたと戦う気はありませんからご心配なく……
+      </Box>
+    </>;
+}`,...g.parameters?.docs?.source}}},_=[`Basic`,`Duration`,`OffsetX`,`OffsetY`,`UnmountOnExit`]}))();export{f as Basic,p as Duration,m as OffsetX,h as OffsetY,g as UnmountOnExit,_ as __namedExportsOrder,d as default};

cd39470b226891962dc844a34a16b40103b91b2a
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 15:41:12 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@079db55d54c2283e1304c5a0de9589d83918d90b 🚀

--- assets/index.stories-BGCwvmTN.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{t as n}from"./box-D_8v7EGt.js";import{Fo as r,h as i,m as a}from"./iframe-Dy6xR5AA.js";var o,s,c,l;e((()=>{a(),r(),o=t(),s={title:`Hooks / useIdle`},c=()=>{let e=i(2e3);return(0,o.jsxs)(n,{bg:e?`blue.500`:`red.500`,color:`white`,p:`md`,rounded:`l2`,transitionDuration:`moderate`,transitionProperty:`all`,children:[`Current state: `,e?`idle`:`not idle`]})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => {
-  const idle = useIdle(2000);
-  return <Box bg={idle ? "blue.500" : "red.500"} color="white" p="md" rounded="l2" transitionDuration="moderate" transitionProperty="all">
-      Current state: {idle ? "idle" : "not idle"}
-    </Box>;
-}`,...c.parameters?.docs?.source}}},l=[`Basic`]}))();export{c as Basic,l as __namedExportsOrder,s as default};

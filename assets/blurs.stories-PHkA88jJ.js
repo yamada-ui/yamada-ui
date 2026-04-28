cd9e8dc9f08bfec920573ea89c375970dca54766
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 08:08:37 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@c9f38c548910e62194173965db131ee4407b794f 🚀

---
+++ assets/blurs.stories-PHkA88jJ.js
@@ -0,0 +1,18 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{n,t as r}from"./blurs-_zrSdNan.js";import{t as i}from"./text-DSMMc5B6.js";import{t as a}from"./text-BM4F8FHS.js";import{t as o}from"./center-B9iB-Iph.js";import{t as s}from"./center-DwGXhAh8.js";import{t as c}from"./heading-DnIVTUkI.js";import{Gi as l,Hr as u,Ur as d,Wi as f,Wr as p,qi as m}from"./iframe-vPRTF_vg.js";var h,g,_,v;e((()=>{s(),f(),d(),u(),a(),n(),h=t(),g={title:`Theme / Tokens`},_=()=>(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)(c,{children:`Blurs`}),(0,h.jsx)(m,{children:(0,h.jsx)(l,{alignItems:`center`,display:`grid`,gap:`lg`,gridTemplateColumns:`repeat(auto-fill, minmax({sizes.xs}, 1fr))`,children:(0,h.jsx)(p,{each:Object.entries(r),children:([e,t],n)=>(0,h.jsx)(o,{aspectRatio:1,bg:`bg`,bgImage:`https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=1000&q=90`,bgSize:`cover`,borderColor:`border.subtle`,borderWidth:`1px`,overflow:`hidden`,rounded:`l2`,children:(0,h.jsxs)(o,{backdropBlur:e,boxSize:`full`,flexDirection:`column`,children:[(0,h.jsx)(i,{children:e}),(0,h.jsx)(i,{children:t})]})},n)})})})]}),_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Heading>Blurs</Heading>
+
+      <Container.Root>
+        <Container.Body alignItems="center" display="grid" gap="lg" gridTemplateColumns="repeat(auto-fill, minmax({sizes.xs}, 1fr))">
+          <For each={Object.entries(blurs)}>
+            {([token, value], index) => <Center key={index} aspectRatio={1} bg="bg" bgImage="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=1000&q=90" bgSize="cover" borderColor="border.subtle" borderWidth="1px" overflow="hidden" rounded="l2">
+                <Center backdropBlur={token} boxSize="full" flexDirection="column">
+                  <Text>{token}</Text>
+                  <Text>{value}</Text>
+                </Center>
+              </Center>}
+          </For>
+        </Container.Body>
+      </Container.Root>
+    </>;
+}`,..._.parameters?.docs?.source}}},v=[`Blurs`]}))();export{_ as Blurs,v as __namedExportsOrder,g as default};

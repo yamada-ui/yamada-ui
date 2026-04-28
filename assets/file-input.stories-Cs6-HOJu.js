348c4cd0db49e650f100a8def8c0045406a530e0
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 16:08:32 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@61538c4fd98173f9581a02dee8d2d42cc8284a44 🚀

---
+++ assets/file-input.stories-Cs6-HOJu.js
@@ -0,0 +1,201 @@
+import{n as e,o as t}from"./chunk-jRWAZmH_.js";import{t as n}from"./react-mE7dFmCG.js";import{En as r,Z as i,yn as a}from"./core-C7NnSGLR.js";import{t as o}from"./jsx-runtime-D_578c1K.js";import{lt as s,r as c,t as l}from"./icon-BMXCkoCb.js";import{t as u}from"./text-DSMMc5B6.js";import{t as d}from"./text-BM4F8FHS.js";import{t as f}from"./center-B9iB-Iph.js";import{t as p}from"./center-DwGXhAh8.js";import{f as m,i as h,n as g,r as _,t as v,v as y}from"./input-9Ni-qBwb.js";import{l as b,t as x}from"./button-C66K-mOB.js";import{r as S}from"./z-stack-D_lbkXhr.js";import{Dt as C,K as w,Qr as T,Ur as E,Wr as D,Zr as O,q as k}from"./iframe-C3ESMJik.js";import{n as A,t as ee}from"./storybook-MAI8zWJa.js";import{n as te,r as ne,t as j}from"./index.esm-COyjMJRW.js";var M,N,P,F,I,L,R,z,B,V,H,U,W,G,K,q,J,Y,X,Z,Q,$;e((()=>{M=t(n(),1),te(),ee(),i(),x(),p(),m(),E(),l(),v(),C(),w(),d(),T(),N=o(),P={component:O,title:`Components / FileInput`},F=()=>(0,N.jsx)(O,{placeholder:`placeholder`}),I=()=>(0,N.jsx)(A,{variant:`stack`,columns:[`outline`,`filled`,`flushed`],rows:r,children:(e,t,n)=>(0,N.jsx)(O,{colorScheme:t,variant:e,placeholder:a(e)},n)}),L=()=>(0,N.jsx)(A,{variant:`stack`,columns:[`xs`,`sm`,`md`,`lg`,`xl`],rows:[`outline`,`filled`,`flushed`],children:(e,t,n)=>(0,N.jsx)(O,{size:e,variant:t,placeholder:`Size (${e})`},n)}),R=()=>(0,N.jsx)(O,{multiple:!0,placeholder:`multiple`}),z=()=>(0,N.jsx)(O,{accept:`image/png,image/jpeg`,placeholder:`only png, jpeg`}),B=()=>(0,N.jsx)(O,{multiple:!0,placeholder:`multiple`,separator:`;`}),V=()=>(0,N.jsx)(O,{component:({value:{name:e}})=>(0,N.jsx)(k,{children:e}),gapY:`xs`,multiple:!0,placeholder:`multiple`}),H=()=>(0,N.jsx)(O,{format:({name:e})=>e.substring(0,e.indexOf(`.`)),multiple:!0,placeholder:`multiple`}),U=()=>(0,N.jsx)(O,{"aria-label":`Files`,multiple:!0,children:e=>(0,N.jsxs)(u,{children:[`Selected: `,e?.length??0]})}),W=()=>(0,N.jsxs)(N.Fragment,{children:[(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsx)(O,{variant:e,disabled:!0,placeholder:a(e)},t)}),(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsxs)(g,{variant:e,disabled:!0,children:[(0,N.jsx)(h,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:a(e)})]},t)}),(0,N.jsx)(y,{disabled:!0,label:`Upload file`,children:(0,N.jsx)(O,{placeholder:`your file`})})]}),G=()=>(0,N.jsxs)(N.Fragment,{children:[(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsx)(O,{variant:e,placeholder:a(e),readOnly:!0},t)}),(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsxs)(g,{variant:e,readOnly:!0,children:[(0,N.jsx)(h,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:a(e)})]},t)}),(0,N.jsx)(y,{label:`Upload file`,readOnly:!0,children:(0,N.jsx)(O,{placeholder:`your file`})})]}),K=()=>(0,N.jsxs)(N.Fragment,{children:[(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsx)(O,{variant:e,invalid:!0,placeholder:a(e)},t)}),(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsxs)(g,{variant:e,invalid:!0,children:[(0,N.jsx)(h,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:a(e)})]},t)}),(0,N.jsx)(y,{errorMessage:`File is required.`,invalid:!0,label:`Upload file`,children:(0,N.jsx)(O,{placeholder:`your file`})})]}),q=()=>(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsxs)(g,{variant:e,children:[(0,N.jsx)(h,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:`Please upload file`})]},t)}),J=()=>(0,N.jsx)(D,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,N.jsxs)(g,{variant:e,children:[(0,N.jsx)(_,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:`Please upload file`})]},t)}),Y=()=>(0,N.jsxs)(N.Fragment,{children:[(0,N.jsx)(O,{placeholder:`Default border color`}),(0,N.jsx)(O,{focusBorderColor:`green.500`,placeholder:`Custom border color`}),(0,N.jsxs)(g,{variant:`flushed`,focusBorderColor:`green.500`,children:[(0,N.jsx)(_,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:`Custom border color`})]}),(0,N.jsx)(O,{errorBorderColor:`orange.500`,invalid:!0,placeholder:`Custom border color`}),(0,N.jsxs)(g,{errorBorderColor:`orange.500`,invalid:!0,children:[(0,N.jsx)(h,{children:(0,N.jsx)(s,{})}),(0,N.jsx)(O,{placeholder:`Custom border color`})]})]}),X=()=>{let[e,t]=(0,M.useState)(void 0),n=(0,M.useRef)(null);return(0,N.jsxs)(S,{children:[(0,N.jsxs)(u,{children:[`files: `,e?.length??0]}),(0,N.jsxs)(g,{children:[(0,N.jsx)(O,{multiple:!0,placeholder:`placeholder`,resetRef:n,value:e,onChange:t}),e?.length?(0,N.jsx)(_,{clickable:!0,onClick:()=>{t(void 0),n.current?.()},children:(0,N.jsx)(f,{as:`button`,focusVisibleRing:`outside`,p:`0.5`,rounded:`xs`,children:(0,N.jsx)(c,{fontSize:`xl`})})}):null]})]})},Z=()=>{let[e,t]=(0,M.useState)([]);return(0,N.jsxs)(S,{children:[(0,N.jsxs)(u,{children:[`files: `,e?.length]}),(0,N.jsx)(O,{placeholder:`placeholder`,value:e,onChange:t})]})},Q=()=>{let e=(0,M.useRef)(null),{control:t,formState:{errors:n},handleSubmit:r,setValue:i}=ne(),a=()=>{i(`fileInput`,void 0),e.current?.()};return(0,N.jsxs)(S,{as:`form`,onSubmit:r(e=>console.log(`submit:`,e)),children:[(0,N.jsx)(y,{errorMessage:n.fileInput?.message,invalid:!!n.fileInput,label:`Files`,children:(0,N.jsx)(j,{name:`fileInput`,control:t,render:({field:t})=>(0,N.jsxs)(g,{children:[(0,N.jsx)(O,{multiple:!0,...t,resetRef:e}),t.value?.length?(0,N.jsx)(_,{clickable:!0,onClick:a,children:(0,N.jsx)(f,{as:`button`,focusVisibleRing:`outside`,p:`0.5`,rounded:`xs`,children:(0,N.jsx)(c,{fontSize:`xl`})})}):null]}),rules:{required:{message:`This is required.`,value:!0}}})}),(0,N.jsx)(b,{type:`submit`,alignSelf:`flex-end`,children:`Submit`})]})},F.parameters={...F.parameters,docs:{...F.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput placeholder="placeholder" />;
+}`,...F.parameters?.docs?.source}}},I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["outline", "filled", "flushed"]} rows={COLOR_SCHEMES}>
+      {(column, row, key) => {
+      return <FileInput key={key} colorScheme={row} variant={column} placeholder={toTitleCase(column)} />;
+    }}
+    </PropsTable>;
+}`,...I.parameters?.docs?.source}}},L.parameters={...L.parameters,docs:{...L.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["xs", "sm", "md", "lg", "xl"]} rows={["outline", "filled", "flushed"]}>
+      {(column, row, key) => {
+      return <FileInput key={key} size={column} variant={row} placeholder={\`Size (\${column})\`} />;
+    }}
+    </PropsTable>;
+}`,...L.parameters?.docs?.source}}},R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput multiple placeholder="multiple" />;
+}`,...R.parameters?.docs?.source}}},z.parameters={...z.parameters,docs:{...z.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput accept="image/png,image/jpeg" placeholder="only png, jpeg" />;
+}`,...z.parameters?.docs?.source}}},B.parameters={...B.parameters,docs:{...B.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput multiple placeholder="multiple" separator=";" />;
+}`,...B.parameters?.docs?.source}}},V.parameters={...V.parameters,docs:{...V.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput component={({
+    value: {
+      name
+    }
+  }) => <Tag>{name}</Tag>} gapY="xs" multiple placeholder="multiple" />;
+}`,...V.parameters?.docs?.source}}},H.parameters={...H.parameters,docs:{...H.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput format={({
+    name
+  }) => name.substring(0, name.indexOf("."))} multiple placeholder="multiple" />;
+}`,...H.parameters?.docs?.source}}},U.parameters={...U.parameters,docs:{...U.parameters?.docs,source:{originalSource:`() => {
+  return <FileInput aria-label="Files" multiple>
+      {files => <Text>Selected: {files?.length ?? 0}</Text>}
+    </FileInput>;
+}`,...U.parameters?.docs?.source}}},W.parameters={...W.parameters,docs:{...W.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <FileInput key={index} variant={variant} disabled placeholder={toTitleCase(variant)} />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} disabled>
+            <InputGroup.Addon>
+              <FileIcon />
+            </InputGroup.Addon>
+            <FileInput placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root disabled label="Upload file">
+        <FileInput placeholder="your file" />
+      </Field.Root>
+    </>;
+}`,...W.parameters?.docs?.source}}},G.parameters={...G.parameters,docs:{...G.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <FileInput key={index} variant={variant} placeholder={toTitleCase(variant)} readOnly />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} readOnly>
+            <InputGroup.Addon>
+              <FileIcon />
+            </InputGroup.Addon>
+            <FileInput placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root label="Upload file" readOnly>
+        <FileInput placeholder="your file" />
+      </Field.Root>
+    </>;
+}`,...G.parameters?.docs?.source}}},K.parameters={...K.parameters,docs:{...K.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <FileInput key={index} variant={variant} invalid placeholder={toTitleCase(variant)} />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} invalid>
+            <InputGroup.Addon>
+              <FileIcon />
+            </InputGroup.Addon>
+            <FileInput placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root errorMessage="File is required." invalid label="Upload file">
+        <FileInput placeholder="your file" />
+      </Field.Root>
+    </>;
+}`,...K.parameters?.docs?.source}}},q.parameters={...q.parameters,docs:{...q.parameters?.docs,source:{originalSource:`() => {
+  return <For each={["outline", "filled", "flushed"] as const}>
+      {(variant, index) => <InputGroup.Root key={index} variant={variant}>
+          <InputGroup.Addon>
+            <FileIcon />
+          </InputGroup.Addon>
+          <FileInput placeholder="Please upload file" />
+        </InputGroup.Root>}
+    </For>;
+}`,...q.parameters?.docs?.source}}},J.parameters={...J.parameters,docs:{...J.parameters?.docs,source:{originalSource:`() => {
+  return <For each={["outline", "filled", "flushed"] as const}>
+      {(variant, index) => <InputGroup.Root key={index} variant={variant}>
+          <InputGroup.Element>
+            <FileIcon />
+          </InputGroup.Element>
+          <FileInput placeholder="Please upload file" />
+        </InputGroup.Root>}
+    </For>;
+}`,...J.parameters?.docs?.source}}},Y.parameters={...Y.parameters,docs:{...Y.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <FileInput placeholder="Default border color" />
+
+      <FileInput focusBorderColor="green.500" placeholder="Custom border color" />
+
+      <InputGroup.Root variant="flushed" focusBorderColor="green.500">
+        <InputGroup.Element>
+          <FileIcon />
+        </InputGroup.Element>
+        <FileInput placeholder="Custom border color" />
+      </InputGroup.Root>
+
+      <FileInput errorBorderColor="orange.500" invalid placeholder="Custom border color" />
+
+      <InputGroup.Root errorBorderColor="orange.500" invalid>
+        <InputGroup.Addon>
+          <FileIcon />
+        </InputGroup.Addon>
+        <FileInput placeholder="Custom border color" />
+      </InputGroup.Root>
+    </>;
+}`,...Y.parameters?.docs?.source}}},X.parameters={...X.parameters,docs:{...X.parameters?.docs,source:{originalSource:`() => {
+  const [value, onChange] = useState<File[] | undefined>(undefined);
+  const resetRef = useRef<() => void>(null);
+  const onReset = () => {
+    onChange(undefined);
+    resetRef.current?.();
+  };
+  return <VStack>
+      <Text>files: {value?.length ?? 0}</Text>
+
+      <InputGroup.Root>
+        <FileInput multiple placeholder="placeholder" resetRef={resetRef} value={value} onChange={onChange} />
+
+        {value?.length ? <InputGroup.Element clickable onClick={onReset}>
+            <Center as="button" focusVisibleRing="outside" p="0.5" rounded="xs">
+              <XIcon fontSize="xl" />
+            </Center>
+          </InputGroup.Element> : null}
+      </InputGroup.Root>
+    </VStack>;
+}`,...X.parameters?.docs?.source}}},Z.parameters={...Z.parameters,docs:{...Z.parameters?.docs,source:{originalSource:`() => {
+  const [value, onChange] = useState<File[] | undefined>([]);
+  return <VStack>
+      <Text>files: {value?.length}</Text>
+
+      <FileInput placeholder="placeholder" value={value} onChange={onChange} />
+    </VStack>;
+}`,...Z.parameters?.docs?.source}}},Q.parameters={...Q.parameters,docs:{...Q.parameters?.docs,source:{originalSource:`() => {
+  interface Data {
+    fileInput: File[] | undefined;
+  }
+  const resetRef = useRef<() => void>(null);
+  const {
+    control,
+    formState: {
+      errors
+    },
+    handleSubmit,
+    setValue
+  } = useForm<Data>();
+  const onReset = () => {
+    setValue("fileInput", undefined);
+    resetRef.current?.();
+  };
+  const onSubmit: SubmitHandler<Data> = data => console.log("submit:", data);
+  return <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Field.Root errorMessage={errors.fileInput?.message} invalid={!!errors.fileInput} label="Files">
+        <Controller name="fileInput" control={control} render={({
+        field
+      }) => <InputGroup.Root>
+              <FileInput multiple {...field} resetRef={resetRef} />
+
+              {field.value?.length ? <InputGroup.Element clickable onClick={onReset}>
+                  <Center as="button" focusVisibleRing="outside" p="0.5" rounded="xs">
+                    <XIcon fontSize="xl" />
+                  </Center>
+                </InputGroup.Element> : null}
+            </InputGroup.Root>} rules={{
+        required: {
+          message: "This is required.",
+          value: true
+        }
+      }} />
+      </Field.Root>
+
+      <Button type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </VStack>;
+}`,...Q.parameters?.docs?.source}}},$=[`Basic`,`Variant`,`Size`,`Multiple`,`Accept`,`Separator`,`Component`,`Format`,`Children`,`Disabled`,`ReadOnly`,`Invalid`,`Addon`,`Element`,`BorderColor`,`Reset`,`CustomControl`,`ReactHookForm`]}))();export{z as Accept,q as Addon,F as Basic,Y as BorderColor,U as Children,V as Component,Z as CustomControl,W as Disabled,J as Element,H as Format,K as Invalid,R as Multiple,Q as ReactHookForm,G as ReadOnly,X as Reset,B as Separator,L as Size,I as Variant,$ as __namedExportsOrder,P as default};
---
+++ assets/index.stories-BtUlqZOa.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{t as n}from"./box-D_8v7EGt.js";import{Fo as r,_ as i,g as a}from"./iframe-C3ESMJik.js";var o,s,c,l;e((()=>{a(),r(),o=t(),s={title:`Hooks / useHover`},c=()=>{let{ref:e,hovered:t}=i();return(0,o.jsx)(n,{ref:e,bg:`mono`,color:`mono.contrast`,p:`md`,rounded:`l2`,w:`full`,children:t?`I am hovered`:`Put mouse over me please`})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => {
+  const {
+    ref,
+    hovered
+  } = useHover();
+  return <Box ref={ref} bg="mono" color="mono.contrast" p="md" rounded="l2" w="full">
+      {hovered ? "I am hovered" : "Put mouse over me please"}
+    </Box>;
+}`,...c.parameters?.docs?.source}}},l=[`Basic`]}))();export{c as Basic,l as __namedExportsOrder,s as default};

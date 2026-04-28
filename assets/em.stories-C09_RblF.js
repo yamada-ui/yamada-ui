aa6166dc9813c59a3ce203a68bf89ad7b575947b
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-28 15:27:39 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@86caf9adacdb1dff07a6bbd0ccb0746e84b483a6 🚀

--- assets/em.stories-C09_RblF.js
+++
@@ -1,6 +0,0 @@
-import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{t as n}from"./text-DSMMc5B6.js";import{t as r}from"./text-BM4F8FHS.js";import{ai as i,ii as a}from"./iframe-Z0FuFE11.js";var o,s,c,l;e((()=>{r(),i(),o=t(),s={component:a,title:`Components / Em`},c=()=>(0,o.jsxs)(n,{children:[(0,o.jsx)(a,{children:`Unleash the Power`}),` of Styling, Animation, and Flexibility in React Apps.`]}),c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => {
-  return <Text>
-      <Em>Unleash the Power</Em> of Styling, Animation, and Flexibility in React
-      Apps.
-    </Text>;
-}`,...c.parameters?.docs?.source}}},l=[`Basic`]}))();export{c as Basic,l as __namedExportsOrder,s as default};

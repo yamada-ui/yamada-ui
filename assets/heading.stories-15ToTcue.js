22b4aed5148439713efa971841d0f9bbf7f55d28
claude-for-yamada-ui <hirotomo.yamada+claude@avap.co.jp>
2026-04-29 07:39:57 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@864fd4fabc9b7da17177af3ee0d5e53cff70db71 🚀

---
+++ assets/heading.stories-15ToTcue.js
@@ -0,0 +1,39 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{n,t as r}from"./heading-DnIVTUkI.js";import{r as i}from"./z-stack-D_lbkXhr.js";import{Dt as a}from"./iframe-CL-k_-ao.js";var o,s,c,l,u;e((()=>{a(),n(),o=t(),s={component:r,title:`Components / Heading`},c=()=>(0,o.jsxs)(i,{children:[(0,o.jsx)(r,{as:`h1`,size:`4xl`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h2`,size:`3xl`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h2`,size:`2xl`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h3`,size:`xl`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h3`,size:`lg`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h4`,size:`md`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h5`,size:`sm`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`}),(0,o.jsx)(r,{as:`h6`,size:`xs`,truncated:!0,children:`ギャルのパンティーおくれーーーっ！！！！！`})]}),l=()=>(0,o.jsx)(r,{size:`2xl`,bgClip:`text`,bgGradient:`linear(to-l, #7928CA, #FF0080)`,truncated:!0,w:`full`,children:`クリリンのことか……クリリンのことかーーーっ！！！！！`}),c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => {
+  return <VStack>
+      <Heading as="h1" size="4xl" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h2" size="3xl" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h2" size="2xl" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h3" size="xl" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h3" size="lg" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h4" size="md" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h5" size="sm" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+
+      <Heading as="h6" size="xs" truncated>
+        ギャルのパンティーおくれーーーっ！！！！！
+      </Heading>
+    </VStack>;
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`() => {
+  return <Heading size="2xl" bgClip="text" bgGradient="linear(to-l, #7928CA, #FF0080)" truncated w="full">
+      クリリンのことか……クリリンのことかーーーっ！！！！！
+    </Heading>;
+}`,...l.parameters?.docs?.source}}},u=[`Basic`,`Gradient`]}))();export{c as Basic,l as Gradient,u as __namedExportsOrder,s as default};

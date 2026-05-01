cc99ecd3f59b82cbdd7d55ce64a2d5b256f9fcdc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-01 15:57:31 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@ad56ae7e9bf0b816e7821947229ffa2705615033 🚀

---
+++ assets/index.stories-BEyaK3Li.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{t}from"./jsx-runtime-D_578c1K.js";import{Go as n,Wo as r,c as i,l as a}from"./iframe--m2aenPZ.js";var o,s,c,l;e((()=>{r(),i(),o=t(),s={title:`Hooks / useOnline`},c=()=>{let e=a();return(0,o.jsx)(n,{colorScheme:e?`green`:`red`,children:e?`Online`:`Offline`})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => {
+  const online = useOnline();
+  return <Badge colorScheme={online ? "green" : "red"}>
+      {online ? "Online" : "Offline"}
+    </Badge>;
+}`,...c.parameters?.docs?.source}}},l=[`Basic`]}))();export{c as Basic,l as __namedExportsOrder,s as default};

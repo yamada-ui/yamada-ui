a2d0e2b02cca93257bf5bcef82a3281d7ccd66e8
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-01 15:31:13 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@ad325126fd1c58ed042106887db9c62cb7d98687 🚀

---
+++ assets/wrap.stories-CgUiGiDb.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{Dn as t,Z as n}from"./core-C7NnSGLR.js";import{t as r}from"./jsx-runtime-D_578c1K.js";import{t as i}from"./box-D_8v7EGt.js";import{n as a,t as o}from"./wrap-DVXQ7Jc8.js";import{Fo as s,Ur as c,Wr as l}from"./iframe-D6A47nst.js";var u,d,f,p;e((()=>{n(),s(),c(),a(),u=r(),d={component:o,title:`Components / Wrap`},f=()=>(0,u.jsx)(o,{gap:`md`,children:(0,u.jsx)(l,{each:t,children:(e,t)=>(0,u.jsx)(i,{bg:`${e}.500`,color:`white`,p:`md`,rounded:`l2`,children:`Box`},t)})}),f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`() => {
+  return <Wrap gap="md">
+      <For each={DEFAULT_COLOR_SCHEMES}>
+        {(colorScheme, index) => <Box key={index} bg={\`\${colorScheme}.500\`} color="white" p="md" rounded="l2">
+            Box
+          </Box>}
+      </For>
+    </Wrap>;
+}`,...f.parameters?.docs?.source}}},p=[`Basic`]}))();export{f as Basic,p as __namedExportsOrder,d as default};

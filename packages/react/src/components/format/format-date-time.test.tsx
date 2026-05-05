d0e22cdb38b05ba18c36b2dd3e4aba793518a82b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-05 20:14:21 +0900

    test(format): recategorize and clean up browser tests

---
+++ packages/react/src/components/format/format-date-time.test.tsx
@@ -0,0 +1,38 @@
+import { a11y, render, renderHook, screen } from "#test"
+import { FormatDateTime } from "./format-date-time"
+import { useFormatDateTime } from "./use-format-date-time"
+
+describe("<FormatDateTime />", () => {
+  test("renders component correctly", async () => {
+    await a11y(<FormatDateTime value={new Date()} />)
+  })
+
+  test("renders correctly", () => {
+    render(<FormatDateTime value={new Date(2025, 9, 6)} />)
+    expect(screen.getByText("10/6/2025")).toBeInTheDocument()
+  })
+
+  test("renders with locale correctly", () => {
+    render(<FormatDateTime locale="de-DE" value={new Date(2025, 9, 6)} />)
+    expect(screen.getByText("6.10.2025")).toBeInTheDocument()
+  })
+})
+
+describe("useFormatDateTime", () => {
+  test("formats date time correctly", () => {
+    const { result } = renderHook(() => useFormatDateTime(new Date(2025, 9, 6)))
+    expect(result.current).toBe("10/6/2025")
+  })
+
+  test("formats date time with different locales correctly", () => {
+    const { result: resultDE } = renderHook(() =>
+      useFormatDateTime(new Date(2025, 9, 6), { locale: "de-DE" }),
+    )
+    const { result: resultJP } = renderHook(() =>
+      useFormatDateTime(new Date(2025, 9, 6), { locale: "ja-JP" }),
+    )
+
+    expect(resultDE.current).toBe("6.10.2025")
+    expect(resultJP.current).toBe("2025/10/6")
+  })
+})

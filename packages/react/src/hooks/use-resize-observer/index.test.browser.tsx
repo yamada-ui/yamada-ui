798cd2caac901fd5840ef31976f343a8f598e3a9
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-06 16:41:38 +0900

    test(use-resize-observer): recategorize and clean up browser tests

--- packages/react/src/hooks/use-resize-observer/index.test.browser.tsx
+++ packages/react/src/hooks/use-resize-observer/index.test.browser.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react"
 import { page, render } from "#test/browser"
-import { useElementSize, useResizeObserver } from "./"
+import { useResizeObserver } from "./"
 
 describe("useResizeObserver", () => {
   const ButtonWithSize: FC = () => {
@@ -18,24 +18,5 @@ describe("useResizeObserver", () => {
 
     const button = page.getByRole("button")
     await expect.element(button).toHaveTextContent("400 x 320")
-  })
-})
-
-describe("useElementSize", () => {
-  const ButtonWithSize: FC = () => {
-    const { ref, height, width } = useElementSize()
-
-    return (
-      <button ref={ref} style={{ blockSize: 200, inlineSize: 150 }}>
-        {width} x {height}
-      </button>
-    )
-  }
-
-  test("returns width and height correctly", async () => {
-    await render(<ButtonWithSize />)
-
-    const button = page.getByRole("button")
-    await expect.element(button).toHaveTextContent("150 x 200")
   })
 })

a5757cdfda3b5e8c67e83dfd7dc6c3a2d416748b
Hirotomo Yamada <hirotomo.yamada@avap.co.jp>
2026-05-06 16:53:30 +0900

    test(box): recategorize and clean up browser tests (#7353)

---
+++ packages/react/src/components/box/box.test.tsx
@@ -0,0 +1,14 @@
+import { a11y, render, screen } from "#test"
+import { Box } from "./box"
+
+describe("<Box />", () => {
+  test("renders component correctly", async () => {
+    await a11y(<Box>Box</Box>)
+  })
+
+  test("renders HTML tag correctly", () => {
+    render(<Box>Box</Box>)
+
+    expect(screen.getByText("Box").tagName).toBe("DIV")
+  })
+})

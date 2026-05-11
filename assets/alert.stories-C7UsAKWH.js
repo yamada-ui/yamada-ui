a95a14fe249bc7095d9f7547410239becce6fd73
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-11 10:39:11 +0000

    Deploying to gh-pages from @ yamada-ui/yamada-ui@c04e68910739ea69db9fbc275cd98d03730e2525 🚀

---
+++ assets/alert.stories-C7UsAKWH.js
@@ -0,0 +1,126 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{Dn as t,Z as n,wn as r}from"./core-BHmHG40b.js";import{t as i}from"./jsx-runtime-D_578c1K.js";import{a,i as o,n as s,o as c,r as l,t as u}from"./alert-Bn2Y2ZnY.js";import{n as d,t as f}from"./close-button-1Kw8R_u-.js";import{r as p}from"./z-stack-lWqh8Oxz.js";import{Dt as m}from"./iframe-DO0z5V9x.js";import{n as h,t as g}from"./storybook-Durjg2B7.js";var _,v,y,b,x,S,C,w,T;e((()=>{g(),n(),f(),m(),u(),_=i(),v={component:a,title:`Components / Alert`},y=()=>(0,_.jsxs)(a,{children:[(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),b=()=>(0,_.jsx)(h,{variant:`stack`,columns:[`plain`,`solid`,`subtle`,`surface`,`island`],rows:[`info`,`success`,`warning`,`error`],children:(e,t,n)=>(0,_.jsxs)(a,{variant:e,status:t,children:[e===`island`?null:(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]},n)}),x=()=>(0,_.jsx)(h,{variant:`stack`,columns:[`plain`,`solid`,`subtle`,`surface`,`island`],rows:t,children:(e,t,n)=>(0,_.jsxs)(a,{colorScheme:t,variant:e,children:[e===`island`?null:(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]},n)}),S=()=>(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)(a,{status:`info`,children:[(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{status:`success`,children:[(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{status:`warning`,children:[(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{status:`error`,children:[(0,_.jsx)(l,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]})]}),C=()=>(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)(a,{variant:`plain`,status:`info`,children:[(0,_.jsx)(o,{}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{variant:`subtle`,status:`success`,children:[(0,_.jsx)(o,{loadingScheme:`grid`}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{variant:`solid`,status:`warning`,children:[(0,_.jsx)(o,{loadingScheme:`puff`}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{variant:`surface`,status:`error`,children:[(0,_.jsx)(o,{loadingScheme:`dots`}),(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]})]}),w=()=>(0,_.jsxs)(_.Fragment,{children:[(0,_.jsxs)(a,{alignItems:`center`,flexDirection:`column`,h:`2xs`,justifyContent:`center`,status:`success`,textAlign:`center`,children:[(0,_.jsx)(l,{boxSize:`40px`,mr:0}),(0,_.jsx)(c,{fontSize:`lg`,mb:1,mr:0,mt:4,children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsxs)(a,{status:`success`,children:[(0,_.jsx)(l,{}),(0,_.jsxs)(p,{gap:`xs`,children:[(0,_.jsx)(c,{children:`セル`}),(0,_.jsx)(s,{children:`か…完全体に………完全体になれさえすれば………！！！`})]}),(0,_.jsx)(d,{colorScheme:`mono`,position:`absolute`,right:2,top:2,onClick:r})]})]}),y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`() => {
+  return <Alert.Root>
+      <Alert.Icon />
+      <Alert.Title>セル</Alert.Title>
+      <Alert.Description>
+        か…完全体に………完全体になれさえすれば………！！！
+      </Alert.Description>
+    </Alert.Root>;
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["plain", "solid", "subtle", "surface", "island"]} rows={["info", "success", "warning", "error"]}>
+      {(column, row, key) => {
+      return <Alert.Root key={key} variant={column} status={row}>
+            {column !== "island" ? <Alert.Icon /> : null}
+            <Alert.Title>セル</Alert.Title>
+            <Alert.Description>
+              か…完全体に………完全体になれさえすれば………！！！
+            </Alert.Description>
+          </Alert.Root>;
+    }}
+    </PropsTable>;
+}`,...b.parameters?.docs?.source}}},x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["plain", "solid", "subtle", "surface", "island"]} rows={COLOR_SCHEMES}>
+      {(column, row, key) => {
+      return <Alert.Root key={key} colorScheme={row} variant={column}>
+            {column !== "island" ? <Alert.Icon /> : null}
+            <Alert.Title>セル</Alert.Title>
+            <Alert.Description>
+              か…完全体に………完全体になれさえすれば………！！！
+            </Alert.Description>
+          </Alert.Root>;
+    }}
+    </PropsTable>;
+}`,...x.parameters?.docs?.source}}},S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Alert.Root status="info">
+        <Alert.Icon />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root status="success">
+        <Alert.Icon />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root status="warning">
+        <Alert.Icon />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root status="error">
+        <Alert.Icon />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+    </>;
+}`,...S.parameters?.docs?.source}}},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Alert.Root variant="plain" status="info">
+        <Alert.Loading />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root variant="subtle" status="success">
+        <Alert.Loading loadingScheme="grid" />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root variant="solid" status="warning">
+        <Alert.Loading loadingScheme="puff" />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root variant="surface" status="error">
+        <Alert.Loading loadingScheme="dots" />
+        <Alert.Title>セル</Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+    </>;
+}`,...C.parameters?.docs?.source}}},w.parameters={...w.parameters,docs:{...w.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Alert.Root alignItems="center" flexDirection="column" h="2xs" justifyContent="center" status="success" textAlign="center">
+        <Alert.Icon boxSize="40px" mr={0} />
+        <Alert.Title fontSize="lg" mb={1} mr={0} mt={4}>
+          セル
+        </Alert.Title>
+        <Alert.Description>
+          か…完全体に………完全体になれさえすれば………！！！
+        </Alert.Description>
+      </Alert.Root>
+
+      <Alert.Root status="success">
+        <Alert.Icon />
+
+        <VStack gap="xs">
+          <Alert.Title>セル</Alert.Title>
+          <Alert.Description>
+            か…完全体に………完全体になれさえすれば………！！！
+          </Alert.Description>
+        </VStack>
+
+        <CloseButton colorScheme="mono" position="absolute" right={2} top={2} onClick={noop} />
+      </Alert.Root>
+    </>;
+}`,...w.parameters?.docs?.source}}},T=[`Basic`,`Variant`,`ColorScheme`,`Status`,`Loading`,`CustomLayout`]}))();export{y as Basic,x as ColorScheme,w as CustomLayout,C as Loading,S as Status,b as Variant,T as __namedExportsOrder,v as default};
---
+++ assets/textarea.stories-DZTtsHo6.js
@@ -0,0 +1,203 @@
+import{n as e,o as t}from"./chunk-jRWAZmH_.js";import{t as n}from"./react-CMXXrKHu.js";import{Dn as r,Z as i,bn as a}from"./core-BHmHG40b.js";import{t as o}from"./jsx-runtime-D_578c1K.js";import{K as s,t as c}from"./icon-CclJcEq2.js";import{f as l,i as u,n as d,r as f,t as p,v as m}from"./input-5yNzPiHc.js";import{l as h,t as g}from"./button-B2ZpDhpb.js";import{r as _}from"./z-stack-lWqh8Oxz.js";import{Dt as v,G as y,Ur as b,W as x,Wr as S}from"./iframe-DO0z5V9x.js";import{n as C,t as w}from"./storybook-Durjg2B7.js";import{n as T,r as E}from"./index.esm-DC_DqwHY.js";var D,O,k,A,j,M,N,P,F,I,L,R,z,B,V,H,U,W,G;e((()=>{D=t(n(),1),T(),w(),x(),i(),g(),l(),b(),c(),p(),v(),O=o(),k={component:y,title:`Components / Textarea`},A=()=>(0,O.jsx)(y,{placeholder:`basic`}),j=()=>(0,O.jsx)(C,{variant:`stack`,columns:[`outline`,`filled`,`flushed`],rows:r,children:(e,t,n)=>(0,O.jsx)(y,{colorScheme:t,variant:e,placeholder:a(e)},n)}),M=()=>(0,O.jsx)(C,{variant:`stack`,columns:[`xs`,`sm`,`md`,`lg`,`xl`],rows:[`outline`,`filled`,`flushed`],children:(e,t,n)=>(0,O.jsx)(y,{size:e,variant:t,placeholder:`Size (${e})`},n)}),N=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsx)(y,{variant:e,disabled:!0,placeholder:e},t)}),(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsxs)(d,{variant:e,disabled:!0,children:[(0,O.jsx)(u,{children:`Email`}),(0,O.jsx)(y,{placeholder:a(e)})]},t)}),(0,O.jsx)(m,{disabled:!0,helperMessage:`We would like to get your feedback.`,label:`Feedback`,children:(0,O.jsx)(y,{variant:`outline`,placeholder:`your feedback`})})]}),P=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsx)(y,{variant:e,placeholder:e,readOnly:!0},t)}),(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsxs)(d,{variant:e,readOnly:!0,children:[(0,O.jsx)(u,{children:`Email`}),(0,O.jsx)(y,{placeholder:a(e)})]},t)}),(0,O.jsx)(m,{helperMessage:`We would like to get your feedback.`,label:`Feedback`,readOnly:!0,children:(0,O.jsx)(y,{variant:`outline`,placeholder:`your feedback`})})]}),F=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsx)(y,{variant:e,invalid:!0,placeholder:e},t)}),(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsxs)(d,{variant:e,invalid:!0,children:[(0,O.jsx)(u,{children:`Email`}),(0,O.jsx)(y,{placeholder:a(e)})]},t)}),(0,O.jsx)(m,{errorMessage:`Feedback is required.`,invalid:!0,label:`Feedback`,children:(0,O.jsx)(y,{variant:`outline`,placeholder:`your feedback`})})]}),I=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsxs)(d,{variant:e,children:[(0,O.jsx)(u,{children:`Email`}),(0,O.jsx)(y,{placeholder:`Your email address`})]},t)}),(0,O.jsxs)(d,{children:[(0,O.jsx)(u,{children:`https://`}),(0,O.jsx)(y,{placeholder:`Your site address`}),(0,O.jsx)(u,{children:`.com`})]})]}),L=()=>(0,O.jsx)(S,{each:[`outline`,`filled`,`flushed`],children:(e,t)=>(0,O.jsxs)(d,{variant:e,children:[(0,O.jsx)(f,{children:(0,O.jsx)(s,{fontSize:`xl`})}),(0,O.jsx)(y,{placeholder:`Your email address`})]},t)}),R=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(y,{placeholder:`Default border color`}),(0,O.jsx)(y,{focusBorderColor:`green.500`,placeholder:`Custom border color`}),(0,O.jsxs)(d,{variant:`flushed`,focusBorderColor:`green.500`,children:[(0,O.jsx)(f,{children:(0,O.jsx)(s,{fontSize:`xl`})}),(0,O.jsx)(y,{placeholder:`Custom border color`})]}),(0,O.jsx)(y,{errorBorderColor:`orange.500`,invalid:!0,placeholder:`Custom border color`}),(0,O.jsxs)(d,{errorBorderColor:`orange.500`,invalid:!0,children:[(0,O.jsx)(u,{children:`Email`}),(0,O.jsx)(y,{placeholder:`Custom border color`})]})]}),z=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(y,{placeholder:`default placeholder`}),(0,O.jsx)(y,{placeholder:`custom placeholder`,_placeholder:{color:`gray.500`,opacity:1}}),(0,O.jsx)(y,{color:`green.500`,placeholder:`custom placeholder`,_placeholder:{color:`inherit`}})]}),B=()=>(0,O.jsx)(S,{each:[`block`,`horizontal`,`vertical`,`none`],children:(e,t)=>(0,O.jsx)(y,{placeholder:e,resize:e},t)}),V=()=>(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(y,{autosize:!0,placeholder:`autosize`}),(0,O.jsx)(y,{autosize:!0,minRows:4,placeholder:`autosize, min rows 4`}),(0,O.jsx)(y,{autosize:!0,maxRows:4,placeholder:`autosize, max rows 4`}),(0,O.jsx)(y,{placeholder:`rows 4`,rows:4})]}),H=()=>{let e=(0,D.useRef)(null);return(0,O.jsxs)(_,{children:[(0,O.jsx)(y,{placeholder:`use resize`,resizeRef:e}),(0,O.jsx)(h,{alignSelf:`flex-end`,onClick:()=>{e.current?.()},children:`Resize`})]})},U=()=>{let{formState:{errors:e},handleSubmit:t,register:n}=E();return(0,O.jsxs)(_,{as:`form`,onSubmit:t(e=>console.log(`submit:`,e)),children:[(0,O.jsx)(m,{errorMessage:e.textarea?.message,invalid:!!e.textarea,label:`Feedback`,children:(0,O.jsx)(y,{placeholder:`your feedback`,...n(`textarea`,{required:{message:`This is required.`,value:!0}})})}),(0,O.jsx)(h,{type:`submit`,alignSelf:`flex-end`,children:`Submit`})]})},W=()=>{let{formState:{errors:e},handleSubmit:t,register:n}=E({defaultValues:{textarea:`孫悟空`}});return(0,O.jsxs)(_,{as:`form`,onSubmit:t(e=>console.log(`submit:`,e)),children:[(0,O.jsx)(m,{errorMessage:e.textarea?.message,invalid:!!e.textarea,label:`Feedback`,children:(0,O.jsx)(y,{placeholder:`your feedback`,...n(`textarea`,{required:{message:`This is required.`,value:!0}})})}),(0,O.jsx)(h,{type:`submit`,alignSelf:`flex-end`,children:`Submit`})]})},A.parameters={...A.parameters,docs:{...A.parameters?.docs,source:{originalSource:`() => {
+  return <Textarea placeholder="basic" />;
+}`,...A.parameters?.docs?.source}}},j.parameters={...j.parameters,docs:{...j.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["outline", "filled", "flushed"]} rows={COLOR_SCHEMES}>
+      {(column, row, key) => {
+      return <Textarea key={key} colorScheme={row} variant={column} placeholder={toTitleCase(column)} />;
+    }}
+    </PropsTable>;
+}`,...j.parameters?.docs?.source}}},M.parameters={...M.parameters,docs:{...M.parameters?.docs,source:{originalSource:`() => {
+  return <PropsTable variant="stack" columns={["xs", "sm", "md", "lg", "xl"]} rows={["outline", "filled", "flushed"]}>
+      {(column, row, key) => {
+      return <Textarea key={key} size={column} variant={row} placeholder={\`Size (\${column})\`} />;
+    }}
+    </PropsTable>;
+}`,...M.parameters?.docs?.source}}},N.parameters={...N.parameters,docs:{...N.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <Textarea key={index} variant={variant} disabled placeholder={variant} />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} disabled>
+            <InputGroup.Addon>Email</InputGroup.Addon>
+            <Textarea placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root disabled helperMessage="We would like to get your feedback." label="Feedback">
+        <Textarea variant="outline" placeholder="your feedback" />
+      </Field.Root>
+    </>;
+}`,...N.parameters?.docs?.source}}},P.parameters={...P.parameters,docs:{...P.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <Textarea key={index} variant={variant} placeholder={variant} readOnly />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} readOnly>
+            <InputGroup.Addon>Email</InputGroup.Addon>
+            <Textarea placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root helperMessage="We would like to get your feedback." label="Feedback" readOnly>
+        <Textarea variant="outline" placeholder="your feedback" />
+      </Field.Root>
+    </>;
+}`,...P.parameters?.docs?.source}}},F.parameters={...F.parameters,docs:{...F.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <Textarea key={index} variant={variant} invalid placeholder={variant} />}
+      </For>
+
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant} invalid>
+            <InputGroup.Addon>Email</InputGroup.Addon>
+            <Textarea placeholder={toTitleCase(variant)} />
+          </InputGroup.Root>}
+      </For>
+
+      <Field.Root errorMessage="Feedback is required." invalid label="Feedback">
+        <Textarea variant="outline" placeholder="your feedback" />
+      </Field.Root>
+    </>;
+}`,...F.parameters?.docs?.source}}},I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <For each={["outline", "filled", "flushed"] as const}>
+        {(variant, index) => <InputGroup.Root key={index} variant={variant}>
+            <InputGroup.Addon>Email</InputGroup.Addon>
+            <Textarea placeholder="Your email address" />
+          </InputGroup.Root>}
+      </For>
+
+      <InputGroup.Root>
+        <InputGroup.Addon>https://</InputGroup.Addon>
+        <Textarea placeholder="Your site address" />
+        <InputGroup.Addon>.com</InputGroup.Addon>
+      </InputGroup.Root>
+    </>;
+}`,...I.parameters?.docs?.source}}},L.parameters={...L.parameters,docs:{...L.parameters?.docs,source:{originalSource:`() => {
+  return <For each={["outline", "filled", "flushed"] as const}>
+      {(variant, index) => <InputGroup.Root key={index} variant={variant}>
+          <InputGroup.Element>
+            <MailIcon fontSize="xl" />
+          </InputGroup.Element>
+          <Textarea placeholder="Your email address" />
+        </InputGroup.Root>}
+    </For>;
+}`,...L.parameters?.docs?.source}}},R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Textarea placeholder="Default border color" />
+
+      <Textarea focusBorderColor="green.500" placeholder="Custom border color" />
+
+      <InputGroup.Root variant="flushed" focusBorderColor="green.500">
+        <InputGroup.Element>
+          <MailIcon fontSize="xl" />
+        </InputGroup.Element>
+        <Textarea placeholder="Custom border color" />
+      </InputGroup.Root>
+
+      <Textarea errorBorderColor="orange.500" invalid placeholder="Custom border color" />
+
+      <InputGroup.Root errorBorderColor="orange.500" invalid>
+        <InputGroup.Addon>Email</InputGroup.Addon>
+        <Textarea placeholder="Custom border color" />
+      </InputGroup.Root>
+    </>;
+}`,...R.parameters?.docs?.source}}},z.parameters={...z.parameters,docs:{...z.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Textarea placeholder="default placeholder" />
+
+      <Textarea placeholder="custom placeholder" _placeholder={{
+      color: "gray.500",
+      opacity: 1
+    }} />
+
+      <Textarea color="green.500" placeholder="custom placeholder" _placeholder={{
+      color: "inherit"
+    }} />
+    </>;
+}`,...z.parameters?.docs?.source}}},B.parameters={...B.parameters,docs:{...B.parameters?.docs,source:{originalSource:`() => {
+  return <For each={["block", "horizontal", "vertical", "none"] as const}>
+      {(resize, index) => <Textarea key={index} placeholder={resize} resize={resize} />}
+    </For>;
+}`,...B.parameters?.docs?.source}}},V.parameters={...V.parameters,docs:{...V.parameters?.docs,source:{originalSource:`() => {
+  return <>
+      <Textarea autosize placeholder="autosize" />
+      <Textarea autosize minRows={4} placeholder="autosize, min rows 4" />
+      <Textarea autosize maxRows={4} placeholder="autosize, max rows 4" />
+      <Textarea placeholder="rows 4" rows={4} />
+    </>;
+}`,...V.parameters?.docs?.source}}},H.parameters={...H.parameters,docs:{...H.parameters?.docs,source:{originalSource:`() => {
+  const resizeRef = useRef<() => void>(null);
+  const onResize = () => {
+    resizeRef.current?.();
+  };
+  return <VStack>
+      <Textarea placeholder="use resize" resizeRef={resizeRef} />
+
+      <Button alignSelf="flex-end" onClick={onResize}>
+        Resize
+      </Button>
+    </VStack>;
+}`,...H.parameters?.docs?.source}}},U.parameters={...U.parameters,docs:{...U.parameters?.docs,source:{originalSource:`() => {
+  interface Data {
+    textarea: string;
+  }
+  const {
+    formState: {
+      errors
+    },
+    handleSubmit,
+    register
+  } = useForm<Data>();
+  const onSubmit: SubmitHandler<Data> = data => console.log("submit:", data);
+  return <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Field.Root errorMessage={errors.textarea?.message} invalid={!!errors.textarea} label="Feedback">
+        <Textarea placeholder="your feedback" {...register("textarea", {
+        required: {
+          message: "This is required.",
+          value: true
+        }
+      })} />
+      </Field.Root>
+
+      <Button type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </VStack>;
+}`,...U.parameters?.docs?.source}}},W.parameters={...W.parameters,docs:{...W.parameters?.docs,source:{originalSource:`() => {
+  interface Data {
+    textarea: string;
+  }
+  const defaultValues: Data = {
+    textarea: "孫悟空"
+  };
+  const {
+    formState: {
+      errors
+    },
+    handleSubmit,
+    register
+  } = useForm<Data>({
+    defaultValues
+  });
+  const onSubmit: SubmitHandler<Data> = data => console.log("submit:", data);
+  return <VStack as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Field.Root errorMessage={errors.textarea?.message} invalid={!!errors.textarea} label="Feedback">
+        <Textarea placeholder="your feedback" {...register("textarea", {
+        required: {
+          message: "This is required.",
+          value: true
+        }
+      })} />
+      </Field.Root>
+
+      <Button type="submit" alignSelf="flex-end">
+        Submit
+      </Button>
+    </VStack>;
+}`,...W.parameters?.docs?.source}}},G=[`Basic`,`Variant`,`Size`,`Disabled`,`ReadOnly`,`Invalid`,`Addon`,`Element`,`BorderColor`,`Placeholder`,`Resize`,`Autosize`,`ControlResize`,`ReactHookForm`,`ReactHookFormDefaultValue`]}))();export{I as Addon,V as Autosize,A as Basic,R as BorderColor,H as ControlResize,N as Disabled,L as Element,F as Invalid,z as Placeholder,U as ReactHookForm,W as ReactHookFormDefaultValue,P as ReadOnly,B as Resize,M as Size,j as Variant,G as __namedExportsOrder,k as default};

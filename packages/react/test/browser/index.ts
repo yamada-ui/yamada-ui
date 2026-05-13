1a2d8ef38874f60bf906f28a660d07e2bd00fcff
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-05-13 09:37:50 +0900

    test(browser): add export from system module

--- packages/react/test/browser/index.ts
+++ packages/react/test/browser/index.ts
@@ -1,3 +1,5 @@
+// oxlint-disable-next-line import-alias/import-alias
+export * from "../system"
 export * from "./render"
 export { act, waitFor } from "@testing-library/react"
 export { page } from "vitest/browser"

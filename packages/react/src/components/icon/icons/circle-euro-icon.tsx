b4ee4a11981a59d617099bedf50ca65547d5d98d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-07-27 15:44:32 +0900

    ci(dependabot): update dependencies

--- packages/react/src/components/icon/icons/circle-euro-icon.tsx
+++ packages/react/src/components/icon/icons/circle-euro-icon.tsx
@@ -2,14 +2,14 @@
 
 import type { Component } from "../../../core"
 import type { IconProps } from "../icon"
-import { CircleEuroSign as OriginalCircleEuroSignIcon } from "lucide-react"
+import { CircleEuro as OriginalCircleEuroIcon } from "lucide-react"
 import { component, Icon } from "../icon"
 
 /**
- * `CircleEuroSignIcon` is [Lucide](https://lucide.dev) SVG icon component.
+ * `CircleEuroIcon` is [Lucide](https://lucide.dev) SVG icon component.
  *
  * @see https://yamada-ui.com/docs/components/icon
  */
-export const CircleEuroSignIcon = component(Icon)({
-  as: OriginalCircleEuroSignIcon,
+export const CircleEuroIcon = component(Icon)({
+  as: OriginalCircleEuroIcon,
 }) as Component<"svg", IconProps>

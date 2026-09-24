8b66469cd5cd976a71adb995dd1a661cf01131ec
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2026-09-25 04:29:16 +0900

    fix(list): forward item and icon props

---
+++ .changeset/friendly-lists-shine.md
@@ -0,0 +1,5 @@
+---
+"@yamada-ui/react": patch
+---
+
+Fix `List` item and icon customization by forwarding `itemProps` and `iconProps` from the root and supporting per-item `iconProps` overrides.
--- packages/react/src/components/list/list.tsx
+++ packages/react/src/components/list/list.tsx
@@ -12,6 +12,11 @@ import { cloneElement, isValidElement, useMemo } from "react"
 import { createSlotComponent, styled } from "../../core"
 import { listStyle } from "./list.style"
 
+interface ComponentContext extends Pick<
+  ListRootProps,
+  "iconProps" | "itemProps"
+> {}
+
 export interface ListItem extends ListItemProps {}
 
 export interface ListRootProps
@@ -28,14 +33,27 @@ export interface ListRootProps
    * The CSS `list-style-position` property.
    */
   styleType?: CSSProps["listStyleType"]
+  /**
+   * Props for the icon component.
+   */
+  iconProps?: ListIconProps
+  /**
+   * Props for the item component.
+   */
+  itemProps?: ListItemProps
 }
 
 const {
+  ComponentContext,
   PropsContext: ListPropsContext,
+  useComponentContext,
   usePropsContext: useListPropsContext,
   withContext,
   withProvider,
-} = createSlotComponent<ListRootProps, ListStyle>("list", listStyle)
+} = createSlotComponent<ListRootProps, ListStyle, ComponentContext>(
+  "list",
+  listStyle,
+)
 
 export { ListPropsContext, useListPropsContext }
 
@@ -51,6 +69,8 @@ export const ListRoot = withProvider<"ul", ListRootProps, "styleType">(
     items,
     stylePosition: listStylePosition,
     styleType: listStyleType = "none",
+    iconProps,
+    itemProps,
     ...rest
   }) => {
     const as = asProp ?? (listStyleType == "decimal" ? "ol" : undefined)
@@ -59,17 +79,23 @@ export const ListRoot = withProvider<"ul", ListRootProps, "styleType">(
 
       return items?.map((props, index) => <ListItem key={index} {...props} />)
     }, [children, items])
+    const context = useMemo(
+      () => ({ iconProps, itemProps }),
+      [iconProps, itemProps],
+    )
 
     return (
-      <styled.ul
-        as={as}
-        listStylePosition={listStylePosition}
-        listStyleType={listStyleType}
-        role="list"
-        {...rest}
-      >
-        {computedChildren}
-      </styled.ul>
+      <ComponentContext value={context}>
+        <styled.ul
+          as={as}
+          listStylePosition={listStylePosition}
+          listStyleType={listStyleType}
+          role="list"
+          {...rest}
+        >
+          {computedChildren}
+        </styled.ul>
+      </ComponentContext>
     )
   },
   "root",
@@ -81,18 +107,34 @@ export interface ListItemProps extends HTMLStyledProps<"li"> {
    * The icon to use.
    */
   icon?: ReactElement<any>
+  /**
+   * Props for the icon component.
+   */
+  iconProps?: ListIconProps
 }
 
 export const ListItem = withContext<"li", ListItemProps>(
-  ({ children, icon, ...rest }) => (
+  ({ children, icon, iconProps, ...rest }) => (
     <styled.li {...rest}>
-      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemIcon {...iconProps}>{icon}</ListItemIcon>
 
       {children}
     </styled.li>
   ),
   "item",
-)()
+)((props) => {
+  const { iconProps, itemProps } = useComponentContext()
+
+  return {
+    ...itemProps,
+    ...props,
+    iconProps: {
+      ...iconProps,
+      ...itemProps?.iconProps,
+      ...props.iconProps,
+    },
+  }
+})
 
 export interface ListIconProps extends HTMLStyledProps<"svg"> {}
 
